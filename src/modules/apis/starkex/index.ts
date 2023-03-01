b670dded686bbddedb79417bdb9e1784ad9116a5
Deepti Luthra <deepti.luthra@immutable.com>
2023-03-01 13:42:05 +1000

    [WT-1135] Retaining StarkEx for now

--- src/modules/apis/starkex/index.ts
+++ src/modules/apis/starkex/index.ts
@@ -1,1 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ImmutableX, Config } from '@imtbl/core-sdk';
+
+const imtblClient = new ImmutableX(Config.SANDBOX);
+
+// Remove provider specific methods
+const {
+  deposit,
+  registerOffchain,
+  isRegisteredOnchain,
+  prepareWithdrawal,
+  completeWithdrawal,
+  createOrder,
+  cancelOrder,
+  createTrade,
+  transfer,
+  batchNftTransfer,
+  ...StarkEx
+} = imtblClient;
+
+export { StarkEx };
 export * from "./immutable";
