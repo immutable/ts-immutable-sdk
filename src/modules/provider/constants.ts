abc6e416a48bebf5767563f094ee30389e9627cc
Felipe Tavares <felipe.tavares@pragma.team>
2023-03-01 15:08:03 +1100

    add newline

--- src/modules/provider/constants.ts
+++ src/modules/provider/constants.ts
@@ -1,5 +1,5 @@
 export enum ENVIRONMENTS {
-    DEVELOPMENT = 'development',
-    STAGING = 'staging',
-    PRODUCTION = 'production',
+  DEVELOPMENT = 'development',
+  STAGING = 'staging',
+  PRODUCTION = 'production',
 }
