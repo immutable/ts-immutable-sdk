6b34d89baa4c90adda9705c7d388b4245ffa06e4
Carmen(Jia) Liu <carmen.liujia@gmail.com>
2023-03-02 14:32:43 +1100

    [ID-329]Add Passport IMX provider (#19)
    * PassportImxProvider
    
    * Pure approach
    
    * Add the changed based on discussion
    
    * changed the test file to use JWT instead of user
    
    * change the user import

---
+++ src/modules/passport/imxProvider/passportImxProvider.ts
@@ -0,0 +1,97 @@
+import { TransactionResponse } from '@ethersproject/abstract-provider';
+import {
+  AnyToken,
+  CancelOrderResponse,
+  CreateOrderResponse,
+  CreateTradeResponse,
+  CreateTransferResponse,
+  CreateTransferResponseV1,
+  CreateWithdrawalResponse,
+  GetSignableCancelOrderRequest,
+  GetSignableTradeRequest,
+  NftTransferDetails,
+  RegisterUserResponse,
+  StarkSigner,
+  TokenAmount,
+  UnsignedExchangeTransferRequest,
+  UnsignedOrderRequest,
+  UnsignedTransferRequest,
+} from '@imtbl/core-sdk';
+import { User } from '../types';
+import { IMXProvider } from '../../provider/imxProvider';
+
+export type JWT = Pick<User, 'access_token' | 'refresh_token'>;
+
+export default class PassportImxProvider implements IMXProvider {
+  private jwt: JWT;
+  private starkSigner: StarkSigner;
+
+  constructor(jwt: JWT, starkSigner: StarkSigner) {
+    this.jwt = jwt;
+    this.starkSigner = starkSigner;
+  }
+
+  transfer(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    request: UnsignedTransferRequest
+  ): Promise<CreateTransferResponseV1> {
+    throw new Error('Method not implemented.');
+  }
+
+  registerOffchain(): Promise<RegisterUserResponse> {
+    throw new Error('Method not implemented.');
+  }
+
+  isRegisteredOnchain(): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  createOrder(request: UnsignedOrderRequest): Promise<CreateOrderResponse> {
+    throw new Error('Method not implemented.');
+  }
+
+  cancelOrder(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    request: GetSignableCancelOrderRequest
+  ): Promise<CancelOrderResponse> {
+    throw new Error('Method not implemented.');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  createTrade(request: GetSignableTradeRequest): Promise<CreateTradeResponse> {
+    throw new Error('Method not implemented.');
+  }
+
+  batchNftTransfer(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    request: NftTransferDetails[]
+  ): Promise<CreateTransferResponse> {
+    throw new Error('Method not implemented.');
+  }
+
+  exchangeTransfer(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    request: UnsignedExchangeTransferRequest
+  ): Promise<CreateTransferResponseV1> {
+    throw new Error('Method not implemented.');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  deposit(deposit: TokenAmount): Promise<TransactionResponse> {
+    throw new Error('Method not implemented.');
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  prepareWithdrawal(request: TokenAmount): Promise<CreateWithdrawalResponse> {
+    throw new Error('Method not implemented.');
+  }
+  completeWithdrawal(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    starkPublicKey: string,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    token: AnyToken
+  ): Promise<TransactionResponse> {
+    throw new Error('Method not implemented.');
+  }
+}
