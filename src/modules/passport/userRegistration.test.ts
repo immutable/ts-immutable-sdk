7226586358680f8b8d42a3a4c5c46ce2ed4fb328
yundi <2666544+yundifu@users.noreply.github.com>
2023-03-06 09:56:30 +1100

    Update src/modules/passport/userRegistration.test.ts
    Co-authored-by: Carmen(Jia) Liu <carmen.liujia@gmail.com>

--- src/modules/passport/userRegistration.test.ts
+++ src/modules/passport/userRegistration.test.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {PassportUserRegistrationRequest, registerPassportUser} from './userRegistration';
 
-jest.mock("axios");
+jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('registerPassportUser', () => {
