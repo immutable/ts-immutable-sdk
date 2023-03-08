ff486b5754e164b2fa88b769c1038de01e8723f2
MitchSaad <mitch.saad@immutable.com>
2023-03-08 15:28:17 +1000

    return transaction response from sendTransaction stub

--- src/modules/provider/test/helpers.ts
+++ src/modules/provider/test/helpers.ts
@@ -1,6 +1,7 @@
 import { ImmutableXConfiguration, StarkSigner } from "@imtbl/core-sdk";
 import { Signers } from "../signable-actions/types";
 import { Configuration, Environment } from "../../../config";
+import { TransactionResponse } from '@ethersproject/providers';
 
 export const privateKey1 = "d90915fa5bce418a23184c9asdfasfasdf5c8e900e3035cf34e2dd36"
 export const privateKey2 = "013fe4a5265bc6deb3f3b524b987sdf987f8c7a8ec2a998ae0512f493d763c8f"
@@ -70,7 +71,7 @@ export const generateSigners = async (
     },
     getAddress: async () => ethKey,
     getChainId: async () => testChainId,
-    sendTransaction: async () => (true)
+    sendTransaction: async () => ({} as TransactionResponse)
   }
 
   // L2 credentials
