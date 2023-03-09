cb650386106a07ded467cec7c2016f94dd5634ec
Deepti Luthra <deepti.luthra@immutable.com>
2023-03-09 11:09:00 +1000

    [WT-1158] add unit test for completeERC20Withdrawal when user is not registered

--- src/modules/provider/signable-actions/withdrawal-actions/completeERC20Withdrawal.test.ts
+++ src/modules/provider/signable-actions/withdrawal-actions/completeERC20Withdrawal.test.ts
@@ -1,6 +1,6 @@
 
 import { getEncodeAssetInfo } from "./getEncodeAssetInfo";
-import { isRegisteredOnChain } from "../registration";
+import { getSignableRegistrationOnchain, isRegisteredOnChain } from "../registration";
 import { Contracts } from "@imtbl/core-sdk";
 import { completeERC20WithdrawalAction } from "./completeERC20Withdrawal";
 import { generateSigners, privateKey1, testConfig, transactionResponse } from "../../test/helpers";
@@ -48,7 +48,30 @@ describe('completeERC20Withdrawal action', () => {
     beforeEach(() => {
       jest.restoreAllMocks();
       (getEncodeAssetInfo as jest.Mock).mockResolvedValue(encodeAssetResponse);
-      (isRegisteredOnChain as jest.Mock).mockResolvedValue(false);
+      (getSignableRegistrationOnchain as jest.Mock).mockResolvedValue({
+        operator_signature: 'operator-signature',
+        payload_hash: 'payload hash',
+      });
+      (Contracts.Registration.connect as jest.Mock).mockReturnValue({
+        populateTransaction: {
+          registerAndWithdraw: jest.fn().mockResolvedValue(transactionResponse)
+        }
+      });
+    });
+
+    it('should execute withdrawal process for ERC20', async () => {
+      const signers = await generateSigners(privateKey1);
+      const response = await completeERC20WithdrawalAction({
+        ethSigner: signers.ethSigner,
+        config: testConfig,
+        starkPublicKey: "789912305",
+        token: {
+          type: 'ERC20',
+          tokenAddress: "0x12as3"
+        }
+      });
+      await expect(response).toEqual(transactionResponse);
+
     });
   });
 
