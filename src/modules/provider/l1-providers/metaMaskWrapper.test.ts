16cabfa3bf17a5c6952b3a19315924641b330e56
Mikhala <mikhala.kurtjak@immutable.com>
2023-03-09 11:55:27 +0800

    Update test order

--- src/modules/provider/l1-providers/metaMaskWrapper.test.ts
+++ src/modules/provider/l1-providers/metaMaskWrapper.test.ts
@@ -8,6 +8,30 @@ jest.mock('./metaMask');
 jest.mock('../imx-wallet/imxWallet');
 
 describe('metaMetaWrapper', () => {
+    describe('signMessage', () => {
+        it('should throw error when provider not connected when calling sign message', async () => {
+            await expect(MetaMaskIMXProvider.signMessage("Message to sign")).rejects.toThrow(new ProviderConnectionError('Attempted to sign a message with the MetaMask IMX provider without an established connection.'));
+        });
+        
+        it('should call sign message on imx signer and return a string', async () => {
+            const getSignerMock = jest.fn().mockReturnValue({});
+            (connect as jest.Mock).mockResolvedValue({
+                getSigner: getSignerMock
+            });
+            const signMessageMock = jest.fn().mockReturnValue("Signed message");
+            (buildImxSigner as jest.Mock).mockResolvedValue({
+                signMessage: signMessageMock
+            });
+
+            await MetaMaskIMXProvider.connect(new Configuration(PRODUCTION));
+            const signedMessage = await MetaMaskIMXProvider.signMessage("Message to sign");
+
+            expect(signMessageMock).toBeCalledTimes(1);
+            expect(signMessageMock).toBeCalledWith("Message to sign");
+            expect(signedMessage).toEqual("Signed message");
+        });
+    });
+
     describe('connect', () => {
         it('should create a metamask imx provider with a eth signer and imx signer', async () => {
             const config = new Configuration(PRODUCTION);
@@ -34,33 +58,13 @@ describe('metaMetaWrapper', () => {
             expect(getSignerMock).toBeCalledTimes(1);
             expect(metamaskIMXProvider).toBeInstanceOf(MetaMaskIMXProvider)
         });
+    });
 
+    describe('disconnect', () => {
         it('should call disconnect with the imx signer', async () => {
             disconnectImxSigner as jest.Mock
             await MetaMaskIMXProvider.disconnect();
             expect(disconnectImxSigner).toBeCalledTimes(1);
-        });
-
-        it('should call sign message on imx signer and return a string', async () => {
-            const getSignerMock = jest.fn().mockReturnValue({});
-            (connect as jest.Mock).mockResolvedValue({
-                getSigner: getSignerMock
-            });
-            const signMessageMock = jest.fn().mockReturnValue("Signed message");
-            (buildImxSigner as jest.Mock).mockResolvedValue({
-                signMessage: signMessageMock
-            });
-
-            await MetaMaskIMXProvider.connect(new Configuration(PRODUCTION));
-            const signedMessage = await MetaMaskIMXProvider.signMessage("Message to sign");
-
-            expect(signMessageMock).toBeCalledTimes(1);
-            expect(signMessageMock).toBeCalledWith("Message to sign");
-            expect(signedMessage).toEqual("Signed message");
-        });
-
-        it('should throw error when provider not connected when calling sign message', async () => {
-            await expect(MetaMaskIMXProvider.signMessage("Message to sign")).rejects.toThrow(new ProviderConnectionError('Attempted to sign a message with the MetaMask IMX provider without an established connection.'));
         });
     });
 });
