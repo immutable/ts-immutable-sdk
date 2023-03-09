843bde4e7973a7b7f70d4d1b2e6e795853b23a77
MitchSaad <mitch.saad@immutable.com>
2023-03-09 14:58:23 +1000

    add a test for the deposit entry file to ensure it calls the right functions

---
+++ src/modules/provider/signable-actions/deposit.test.ts
@@ -0,0 +1,64 @@
+import { Configuration } from "config";
+import { deposit } from "./deposit";
+import * as depositActions from './deposit-actions';
+import { Signers } from "./types";
+
+
+jest.mock('@imtbl/core-sdk')
+jest.mock('./deposit-actions')
+
+describe('deposit', () => {
+  describe('deposit()', () => {
+    let depositERC721Mock: jest.Mock;
+    let depositERC20Mock: jest.Mock;
+    let depositEthMock: jest.Mock;
+
+    beforeEach(() => {
+      jest.restoreAllMocks();
+
+      depositERC721Mock = jest.fn();
+      depositERC20Mock = jest.fn();
+      depositEthMock = jest.fn();
+
+      (depositActions.depositERC20 as jest.Mock).mockImplementation(depositERC20Mock);
+      (depositActions.depositERC721 as jest.Mock).mockImplementation(depositERC721Mock);
+      (depositActions.depositEth as jest.Mock).mockImplementation(depositEthMock);
+    })
+
+    test('should call depositERC20() when the type in the paylod is ERC20', async () => {
+      deposit({
+        signers: {} as Signers,
+        deposit: { type: 'ERC20' },
+        config: {} as Configuration
+      })
+
+      expect(depositERC20Mock).toBeCalledTimes(1)
+      expect(depositERC721Mock).toBeCalledTimes(0)
+      expect(depositEthMock).toBeCalledTimes(0)
+    })
+
+    test('should call depositERC721() when the type in the paylod is ERC721', async () => {
+      deposit({
+        signers: {} as Signers,
+        deposit: { type: 'ERC721' },
+        config: {} as Configuration
+      })
+
+      expect(depositERC20Mock).toBeCalledTimes(0)
+      expect(depositERC721Mock).toBeCalledTimes(1)
+      expect(depositEthMock).toBeCalledTimes(0)
+    })
+
+    test('should call depositEth() when the type in the paylod is ETH', async () => {
+      deposit({
+        signers: {} as Signers,
+        deposit: { type: 'ETH' },
+        config: {} as Configuration
+      })
+
+      expect(depositERC20Mock).toBeCalledTimes(0)
+      expect(depositERC721Mock).toBeCalledTimes(0)
+      expect(depositEthMock).toBeCalledTimes(1)
+    })
+  })
+})
