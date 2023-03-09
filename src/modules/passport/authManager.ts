c8a9831ff500023859f8431487b13e06651ebd82
aerim-yi <107908828+aerim-yi@users.noreply.github.com>
2023-03-09 18:01:06 +1100

    change scope from array to string

--- src/modules/passport/authManager.ts
+++ src/modules/passport/authManager.ts
@@ -23,7 +23,7 @@ const getAuthConfiguration = ({ clientId, redirectUri }: AuthInput) => ({
     userinfo_endpoint: `${passportAuthDomain}/userinfo`
   },
   loadUserInfo: true,
-  scope: ['openid', 'offline_access']
+  scope: 'openid offline_access'
 });
 
 export default class AuthManager {
