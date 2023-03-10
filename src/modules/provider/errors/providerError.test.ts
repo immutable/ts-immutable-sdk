84bfa17268b8a2b9b5ead16e5c8838218970f38e
Mikhala <mikhala.kurtjak@immutable.com>
2023-03-10 10:45:08 +0800

    Indent consistency

--- src/modules/provider/errors/providerError.test.ts
+++ src/modules/provider/errors/providerError.test.ts
@@ -5,39 +5,40 @@ import {
   } from './providerError';
   
 describe('providerError', () => {
-    afterEach(jest.resetAllMocks);
-    it('should execute the function without throwing the error', async () => {
-        const returnValue = 'success';
-        const anyFn = jest.fn();
-        anyFn.mockReturnValue(returnValue);
+  afterEach(jest.resetAllMocks);
+  
+  it('should execute the function without throwing the error', async () => {
+    const returnValue = 'success';
+    const anyFn = jest.fn();
+    anyFn.mockReturnValue(returnValue);
 
-        expect(await withProviderError(anyFn, {
-            type: ProviderErrorType.PROVIDER_CONNECTION_ERROR,
-        })).toEqual(returnValue);
-    });
+    expect(await withProviderError(anyFn, {
+        type: ProviderErrorType.PROVIDER_CONNECTION_ERROR,
+    })).toEqual(returnValue);
+  });
 
-    it('should throw ProviderError with the provider error type', async () => {
-        const errorFunction = jest.fn();
-        errorFunction.mockRejectedValue(new Error('Error message'));
+  it('should throw ProviderError with the provider error type', async () => {
+    const errorFunction = jest.fn();
+    errorFunction.mockRejectedValue(new Error('Error message'));
 
-        await expect(withProviderError(errorFunction, {
-            type: ProviderErrorType.PROVIDER_CONNECTION_ERROR,
-        })).rejects.toThrow(new ProviderError(
-            'PROVIDER_CONNECTION_ERROR: Error message',
-            ProviderErrorType.PROVIDER_CONNECTION_ERROR
-        ));
-    });
+    await expect(withProviderError(errorFunction, {
+        type: ProviderErrorType.PROVIDER_CONNECTION_ERROR,
+    })).rejects.toThrow(new ProviderError(
+        'PROVIDER_CONNECTION_ERROR: Error message',
+        ProviderErrorType.PROVIDER_CONNECTION_ERROR
+    ));
+  });
 
-    it('should throw ProviderError with the provider error type and custom error message', async () => {
-        const errorFunction = jest.fn();
-        errorFunction.mockRejectedValue(new Error('Error message'));
+  it('should throw ProviderError with the provider error type and custom error message', async () => {
+    const errorFunction = jest.fn();
+    errorFunction.mockRejectedValue(new Error('Error message'));
 
-        await expect(withProviderError(errorFunction, {
-            type: ProviderErrorType.PROVIDER_CONNECTION_ERROR,
-            message: "Custom message"
-        })).rejects.toThrow(new ProviderError(
-            'Custom message',
-            ProviderErrorType.PROVIDER_CONNECTION_ERROR
-        ));
-    });
+    await expect(withProviderError(errorFunction, {
+        type: ProviderErrorType.PROVIDER_CONNECTION_ERROR,
+        message: "Custom message"
+    })).rejects.toThrow(new ProviderError(
+        'Custom message',
+        ProviderErrorType.PROVIDER_CONNECTION_ERROR
+    ));
+  });
 });
--- src/modules/provider/l1-providers/metaMaskWrapper.ts
+++ src/modules/provider/l1-providers/metaMaskWrapper.ts
@@ -7,38 +7,38 @@ import { ImxSigner } from '../imx-wallet/ImxSigner';
 import { ProviderError, ProviderErrorType, withProviderError } from '../errors/providerError';
 
 export class MetaMaskIMXProvider extends GenericIMXProvider {
-    private static imxSigner: ImxSigner;
+  private static imxSigner: ImxSigner;
 
-    constructor(config: Configuration, ethSigner: EthSigner, starkExSigner: StarkSigner) {
-        super(config, ethSigner, starkExSigner);
+  constructor(config: Configuration, ethSigner: EthSigner, starkExSigner: StarkSigner) {
+    super(config, ethSigner, starkExSigner);
+  }
+
+  public static async connect(config: Configuration): Promise<MetaMaskIMXProvider> {
+    const starkExConfig = config.getStarkExConfig();
+    return await withProviderError<MetaMaskIMXProvider>(async () => {
+      const metaMaskProvider = await connect({chainID: starkExConfig.ethConfiguration.chainID});
+      this.imxSigner = await buildImxSigner(metaMaskProvider, starkExConfig.env);
+      return new MetaMaskIMXProvider(config, metaMaskProvider.getSigner(), this.imxSigner);
+    }, { type: ProviderErrorType.WALLET_CONNECTION_ERROR });
+  }
+
+  public static async disconnect(): Promise<void> {
+    if (!this.imxSigner) {
+      throw new ProviderError('PROVIDER_CONNECTION_ERROR: Attempted to disconnect from the MetaMask IMX provider without an established connection.', ProviderErrorType.PROVIDER_CONNECTION_ERROR)
     }
 
-    public static async connect(config: Configuration): Promise<MetaMaskIMXProvider> {
-        const starkExConfig = config.getStarkExConfig();
-        return await withProviderError<MetaMaskIMXProvider>(async () => {
-            const metaMaskProvider = await connect({chainID: starkExConfig.ethConfiguration.chainID});
-            this.imxSigner = await buildImxSigner(metaMaskProvider, starkExConfig.env);
-            return new MetaMaskIMXProvider(config, metaMaskProvider.getSigner(), this.imxSigner);
-        }, { type: ProviderErrorType.WALLET_CONNECTION_ERROR });
+    return withProviderError<void>(async () => {
+      await disconnectImxSigner(this.imxSigner);
+    }, { type: ProviderErrorType.PROVIDER_CONNECTION_ERROR });
+  }
+
+  public static async signMessage(message: string): Promise<string> {
+    if (!this.imxSigner) {
+      throw new ProviderError('PROVIDER_CONNECTION_ERROR: Attempted to sign a message with the MetaMask IMX provider without an established connection.', ProviderErrorType.PROVIDER_CONNECTION_ERROR)
     }
 
-    public static async disconnect(): Promise<void> {
-        if (!this.imxSigner) {
-            throw new ProviderError('PROVIDER_CONNECTION_ERROR: Attempted to disconnect from the MetaMask IMX provider without an established connection.', ProviderErrorType.PROVIDER_CONNECTION_ERROR)
-        }
-
-        return withProviderError<void>(async () => {
-            await disconnectImxSigner(this.imxSigner);
-        }, { type: ProviderErrorType.PROVIDER_CONNECTION_ERROR });
-    }
-
-    public static async signMessage(message: string): Promise<string> {
-        if (!this.imxSigner) {
-            throw new ProviderError('PROVIDER_CONNECTION_ERROR: Attempted to sign a message with the MetaMask IMX provider without an established connection.', ProviderErrorType.PROVIDER_CONNECTION_ERROR)
-        }
-
-        return withProviderError<string>(async () => {
-            return await this.imxSigner.signMessage(message);
-        }, { type: ProviderErrorType.PROVIDER_CONNECTION_ERROR });
-    }
+    return withProviderError<string>(async () => {
+      return await this.imxSigner.signMessage(message);
+    }, { type: ProviderErrorType.PROVIDER_CONNECTION_ERROR });
+  }
 }
