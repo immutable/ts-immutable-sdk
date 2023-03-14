d1b9369e4bba87a7bf9f36a1329a3c1e3ee2b2db
yundifu <yundi.fu77@gmail.com>
2023-03-14 11:56:46 +1100

    code review

--- src/modules/passport/workflows/registration.test.ts
+++ src/modules/passport/workflows/registration.test.ts
@@ -37,6 +37,45 @@ describe('registerPassportWorkflow', () => {
     const res = await registerPassportWorkflow(request, mockToken);
 
     expect(res).toEqual("No Content");
+    expect(mockStarkSigner.signMessage).toHaveBeenCalled();
+    expect(mockEthSigner.signMessage).toHaveBeenCalled();
+    expect(mockUserApi.registerPassportUser).toHaveBeenCalledWith({
+      authorization: mockToken,
+      registerPassportUserRequest: {
+        ...requestBody,
+        eth_signature: "0x0000000000000000000000000000000000000000000000000000000000000123000000000000000000000000000000000000000000000000000000000000000000"
+      }
+    });
+  });
+  it('registerPassportWorkflow failed to call api client to register passport user', async () => {
+    const mockUserApi = {
+      registerPassportUser: jest.fn().mockRejectedValue("error"),
+      getSignableRegistrationOffchain: jest.fn().mockReturnValue({
+          data: {
+            payload_hash: "0x34",
+            signable_message: "message to sign"
+          }
+        }
+      )
+    };
+    const mockEthSigner = {
+      getAddress: jest.fn().mockReturnValue(requestBody.ether_key),
+      signMessage: jest.fn().mockReturnValue(requestBody.eth_signature)
+    };
+    const mockStarkSigner = {
+      getAddress: jest.fn().mockReturnValue(requestBody.stark_key),
+      signMessage: jest.fn().mockReturnValue(requestBody.stark_signature)
+    };
+    const request: registerPassportWorkflowParams = {
+      ethSigner: mockEthSigner as never,
+      starkSigner: mockStarkSigner as never,
+      usersApi: mockUserApi as never,
+    };
+
+    await expect(registerPassportWorkflow(request, mockToken)).rejects.toEqual("error");
+
+    expect(mockStarkSigner.signMessage).toHaveBeenCalled();
+    expect(mockEthSigner.signMessage).toHaveBeenCalled();
     expect(mockUserApi.registerPassportUser).toHaveBeenCalledWith({
       authorization: mockToken,
       registerPassportUserRequest: {
