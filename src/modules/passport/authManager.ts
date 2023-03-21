a09fbbf1300d6b7034029b9d7c98501b024ba0b0
aerim-yi <107908828+aerim-yi@users.noreply.github.com>
2023-03-21 15:37:58 +1100

    rebuild

--- src/modules/passport/authManager.ts
+++ src/modules/passport/authManager.ts
@@ -18,7 +18,7 @@ const getAuthConfiguration = ({
     userinfo_endpoint: `${oidcConfiguration.authenticationDomain}/userinfo`,
   },
   loadUserInfo: true,
-  scope: 'openid offline_access profile email create:users',
+  scope: 'openid offline_access profile email create:users passport:user_create',
   extraQueryParams: {
     audience: 'platform_api',
   }
