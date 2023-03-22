1e9f8db1d108cb525cf28e0c6f24e918dfcc7160
aerim-yi <107908828+aerim-yi@users.noreply.github.com>
2023-03-22 14:06:33 +1100

    feat: implement createTrade functionality

--- src/modules/passport/errors/passportError.ts
+++ src/modules/passport/errors/passportError.ts
@@ -1,33 +1,35 @@
 export enum PassportErrorType {
-  AUTHENTICATION_ERROR = 'AUTHENTICATION_ERROR',
-  INVALID_CONFIGURATION = 'INVALID_CONFIGURATION',
-  WALLET_CONNECTION_ERROR = 'WALLET_CONNECTION_ERROR',
-  NOT_LOGGED_IN_ERROR = 'NOT_LOGGED_IN_ERROR',
-  REFRESH_TOKEN_ERROR = 'REFRESH_TOKEN_ERROR',
-  USER_REGISTRATION_ERROR = 'USER_REGISTRATION_ERROR',
-  TRANSFER_ERROR = 'TRANSFER_ERROR',
-  CREATE_ORDER_ERROR = 'CREATE_ORDER_ERROR',
-  CANCEL_ORDER_ERROR = 'CANCEL_ORDER_ERROR'
+    AUTHENTICATION_ERROR = 'AUTHENTICATION_ERROR',
+    INVALID_CONFIGURATION = 'INVALID_CONFIGURATION',
+    WALLET_CONNECTION_ERROR = 'WALLET_CONNECTION_ERROR',
+    NOT_LOGGED_IN_ERROR = 'NOT_LOGGED_IN_ERROR',
+    REFRESH_TOKEN_ERROR = 'REFRESH_TOKEN_ERROR',
+    USER_REGISTRATION_ERROR = 'USER_REGISTRATION_ERROR',
+    TRANSFER_ERROR = 'TRANSFER_ERROR',
+    CREATE_ORDER_ERROR = 'CREATE_ORDER_ERROR',
+    CANCEL_ORDER_ERROR = 'CANCEL_ORDER_ERROR',
+    CREATE_TRADE_ERROR = 'CREATE_TRADE_ERROR'
 }
 
 export class PassportError extends Error {
-  public type: PassportErrorType;
-  constructor(message: string, type: PassportErrorType) {
-    super(message);
-    this.type = type;
-  }
+    public type: PassportErrorType;
+
+    constructor(message: string, type: PassportErrorType) {
+        super(message);
+        this.type = type;
+    }
 }
 
 export const withPassportError = async <T>(
-  fn: () => Promise<T>,
-  customErrorType: PassportErrorType
+    fn: () => Promise<T>,
+    customErrorType: PassportErrorType
 ): Promise<T> => {
-  try {
-    return await fn();
-  } catch (error) {
-    const errorMessage =
-      `${customErrorType}: ${(error as Error).message}` ||
-      'UnknownError';
-    throw new PassportError(errorMessage, customErrorType);
-  }
+    try {
+        return await fn();
+    } catch (error) {
+        const errorMessage =
+            `${customErrorType}: ${(error as Error).message}` ||
+            'UnknownError';
+        throw new PassportError(errorMessage, customErrorType);
+    }
 }
---
+++ src/modules/passport/workflows/trades.ts
@@ -0,0 +1,74 @@
+import {
+    CreateTradeResponse,
+    GetSignableTradeRequest,
+    StarkSigner,
+    TradesApi,
+    TradesApiCreateTradeRequest
+} from "@imtbl/core-sdk";
+import { PassportErrorType, withPassportError } from '../errors/passportError';
+import { UserWithEtherKey } from "../types";
+
+type CreateTradeParams = {
+    request: GetSignableTradeRequest;
+    tradesApi: TradesApi;
+    user: UserWithEtherKey;
+    starkSigner: StarkSigner;
+}
+
+export async function createTrade({
+                                      request,
+                                      tradesApi,
+                                      user,
+                                      starkSigner
+                                  }: CreateTradeParams): Promise<CreateTradeResponse> {
+    return withPassportError<CreateTradeResponse>(async () => {
+        const ethAddress = user.etherKey;
+        const getSignableTradeRequest: GetSignableTradeRequest = {
+            'expiration_timestamp': request.expiration_timestamp,
+            'fees': request.fees,
+            'order_id': request.order_id,
+            'user': ethAddress
+        }
+
+        const getSignableTradeResponse = await tradesApi.getSignableTrade({
+            getSignableTradeRequest: getSignableTradeRequest,
+        })
+
+        const {payload_hash: payloadHash} = getSignableTradeResponse.data;
+        const starkSignature = await starkSigner.signMessage(payloadHash);
+        const signableResultData = getSignableTradeResponse.data;
+
+        const tradeParams: TradesApiCreateTradeRequest = {
+            createTradeRequest: {
+                include_fees: true,
+                fees: request.fees,
+                stark_signature: starkSignature,
+                order_id: request.order_id,
+
+                fee_info: signableResultData.fee_info,
+                amount_buy: signableResultData.amount_buy,
+                amount_sell: signableResultData.amount_sell,
+                asset_id_buy: signableResultData.asset_id_buy,
+                asset_id_sell: signableResultData.asset_id_sell,
+                expiration_timestamp: signableResultData.expiration_timestamp,
+                nonce: signableResultData.nonce,
+                stark_key: signableResultData.stark_key,
+                vault_id_buy: signableResultData.vault_id_buy,
+                vault_id_sell: signableResultData.vault_id_sell,
+            },
+            // Notes[ID-451]: this is 2 params to bypass the Client non-empty check,
+            // Should be able to remove it once the Backend have update the API
+            // and generated the New Client
+            xImxEthAddress: '',
+            xImxEthSignature: '',
+        }
+
+        const headers = {Authorization: 'Bearer ' + user.accessToken}
+        const createTradeResponse = await tradesApi.createTrade(tradeParams, {
+            headers,
+        })
+        return {
+            ...createTradeResponse.data,
+        }
+    }, PassportErrorType.CREATE_TRADE_ERROR)
+}
