a5c27e7e42163dc8150b0f5bb4d03cff9725a517
aerim-yi <107908828+aerim-yi@users.noreply.github.com>
2023-03-27 17:15:14 +1100

    rebuild

--- dist/modules/passport/authManager.d.ts
+++ dist/modules/passport/authManager.d.ts
@@ -4,9 +4,9 @@ export default class AuthManager {
     private userManager;
     private config;
     constructor(config: PassportConfiguration);
-    private mapOidcUserToDomainModel;
     login(): Promise<User>;
     loginCallback(): Promise<void>;
     getUser(): Promise<User>;
     requestRefreshTokenAfterRegistration(jwt: string): Promise<UserWithEtherKey | null>;
+    private mapOidcUserToDomainModel;
 }
