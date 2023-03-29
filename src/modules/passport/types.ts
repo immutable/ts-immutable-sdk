56a2ce050b8617da2ae79300df7f040b9caa2943
Hayden Fowler <hayden.fowler@immutable.com>
2023-03-30 10:10:49 +1100

    Removed unused type

--- src/modules/passport/types.ts
+++ src/modules/passport/types.ts
@@ -1,9 +1,3 @@
-export interface PassportConfigurationArguments {
-  clientId: string;
-  redirectUri: string;
-  logoutRedirectUri: string;
-}
-
 export type UserProfile = {
   email?: string;
   nickname?: string;
