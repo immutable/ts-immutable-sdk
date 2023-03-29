d94ddd51c56b619627d9f5a6a3c6a633fcf951be
Matthew Muscat <officialmatthewmuscat@gmail.com>
2023-03-29 12:13:49 +1000

    feat: wip

--- jest.config.ts
+++ jest.config.ts
@@ -71,10 +71,7 @@ export default {
   // maxWorkers: "50%",
 
   // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: [
-    "node_modules",
-    "<rootDir>/src"
-  ],
+  moduleDirectories: ['node_modules', '<rootDir>/src/modules'],
 
   // An array of file extensions your modules use
   // moduleFileExtensions: [
@@ -185,7 +182,7 @@ export default {
   // unmockedModulePathPatterns: undefined,
 
   // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
