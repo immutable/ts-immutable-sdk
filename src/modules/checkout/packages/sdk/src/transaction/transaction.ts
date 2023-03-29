bd5fae12ed29a5f2fcef0a56faf7cbe97fec22dc
Mikhala <mikhala.kurtjak@immutable.com>
2023-03-29 13:08:40 +0800

    Clean up

--- src/modules/checkout/packages/sdk/src/transaction/transaction.ts
+++ src/modules/checkout/packages/sdk/src/transaction/transaction.ts
@@ -3,11 +3,8 @@ import { SendTransactionParams, SendTransactionResult, TransactionStatus } from 
 
 export const sendTransaction = async (params: SendTransactionParams): Promise<SendTransactionResult> => {
     const { provider, transaction } = params;
-
     return await withCheckoutError<SendTransactionResult>(async () => {
-        // todo: how do we know from response whether its success or fail?
-        const response = await provider.getSigner().sendTransaction(transaction);
-        console.log('sendTransaction response: ', response);
+        await provider.getSigner().sendTransaction(transaction);
         return {
             status: TransactionStatus.SUCCESS,
             transaction
