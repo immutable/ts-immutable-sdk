c6ec0c7e22a62bd7eea5dd70d313b4e4c32ef5d5
aerim-yi <107908828+aerim-yi@users.noreply.github.com>
2023-04-04 17:23:29 +1000

    refactor: import location

--- packages/passport/src/Passport.ts
+++ packages/passport/src/Passport.ts
@@ -3,7 +3,7 @@ import MagicAdapter from './magicAdapter';
 import PassportImxProvider from './imxProvider/passportImxProvider';
 import { getPassportConfiguration, PassportConfiguration } from './config';
 import { PassportError, PassportErrorType } from './errors/passportError';
-import { IMXProvider } from '@imtbl/provider';
+import { IMXProvider } from '../../provider/src';
 import { getStarkSigner } from './stark';
 import {
   EnvironmentConfiguration,
