a81deaf63b57e4291cbf36f85e4ad70880967804
Jhonatan Gonzalez <jhesgodi@gmail.com>
2023-04-04 11:22:59 +1000

    [GEF-3] Setup economy building blocks module (#93)
    setup economy building blocks module

---
+++ packages/economy/packages/sdk/src/crafting/index.spec.ts
@@ -0,0 +1,9 @@
+import { craft } from '.';
+
+describe('craft', () => {
+  it('should work', async () => {
+    const input = { requiresWeb3: true, web3Assets: {} };
+    const result = await craft(input);
+    expect(result).toEqual('COMPLETE');
+  });
+});
