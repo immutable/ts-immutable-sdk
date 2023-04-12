59332203bb5930d20de8ce78d6fdbaa37c78e32a
Hayden Fowler <hayden.fowler@gmail.com>
2023-04-12 11:13:17 +1000

    ID-535 fixed failing tests

--- packages/passport/src/workflows/exchange.ts
+++ packages/passport/src/workflows/exchange.ts
@@ -4,7 +4,7 @@ import {
   StarkSigner,
   UnsignedExchangeTransferRequest,
 } from '@imtbl/core-sdk';
-import { convertToSignableToken } from '@imtbl/toolkit/src';
+import { convertToSignableToken } from '@imtbl/toolkit';
 import { PassportErrorType, withPassportError } from '../errors/passportError';
 import { UserWithEtherKey } from '../types';
 
