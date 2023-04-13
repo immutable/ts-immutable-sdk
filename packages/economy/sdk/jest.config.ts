c4a5a7f94932bb197d7bd42fbb0986be833405fa
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-04-13 12:22:59 +1000

    fix jest.config

--- packages/economy/sdk/jest.config.ts
+++ packages/economy/sdk/jest.config.ts
@@ -2,13 +2,38 @@
 
 import type { Config } from 'jest';
 
+const swcJestConfig = {
+  swcrc: false,
+  jsc: {
+    target: 'es2017',
+    parser: {
+      syntax: 'typescript',
+      decorators: true,
+      dynamicImport: true,
+    },
+    transform: {
+      decoratorMetadata: true,
+      legacyDecorator: true,
+    },
+    keepClassNames: true,
+    externalHelpers: true,
+    loose: true,
+  },
+  module: {
+    type: 'es6',
+    strict: true,
+    noInterop: true,
+  },
+  sourceMaps: true,
+};
+
 const config: Config = {
   clearMocks: true,
   coverageProvider: 'v8',
   moduleDirectories: ['node_modules', 'src'],
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': ['@swc/jest', swcJestConfig],
   },
   transformIgnorePatterns: [],
 };
