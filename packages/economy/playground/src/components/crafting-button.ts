c39680fb7edf97080323c625ca27e91082363a39
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-04-13 14:09:03 +1000

    migrate economy playground to yarn workspace

---
+++ packages/economy/playground/src/components/crafting-button.ts
@@ -0,0 +1,47 @@
+import { LitElement, css, html } from 'lit';
+import {
+  customElement,
+  eventOptions,
+  property,
+  state,
+} from 'lit/decorators.js';
+
+import { Economy } from '@imtbl/economy';
+import { CraftInput } from '@imtbl/economy';
+
+@customElement('imtbl-craft-button')
+export class CraftButton extends LitElement {
+  static styles = css``;
+
+  private economy: Economy;
+
+  constructor() {
+    super();
+    this.economy = new Economy();
+  }
+
+  @property()
+  label?: string = '%craft-button%';
+
+  @state()
+  private craftInput: CraftInput = {
+    requiresWeb3: true,
+    web3Assets: {},
+  };
+
+  @eventOptions({ capture: true })
+  handleClick(event: MouseEvent) {
+    event.preventDefault();
+    this.economy.craft(this.craftInput);
+  }
+
+  firstUpdated() {
+    console.log('Component mounted!');
+  }
+
+  render() {
+    return html`<div @click=${this.handleClick}>
+      <slot></slot>
+    </div>`;
+  }
+}
