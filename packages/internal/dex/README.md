29517533f80367b09f20b444af59df56b15c0bae
Pano Skylakis <pano.skylakis@immutable.com>
2023-04-18 10:48:55 +1200

    Feat: add DEX sdk to internal packages, Fix: add missing node-gyp dependency

---
+++ packages/internal/dex/README.md
@@ -0,0 +1,1 @@
+# Immutable DEX SDK
---
+++ packages/internal/dex/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  moduleDirectories: ['node_modules', 'src'],
+  verbose: true,
+  testEnvironment: 'jsdom',
+  transform: {
+    '\\.ts$': ['babel-jest', { configFile: './babel.test.config.js' }],
+  },
+  transformIgnorePatterns: [],
+};
+
+export default config;
