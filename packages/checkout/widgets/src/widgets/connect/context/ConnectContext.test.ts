7ad0f7cb4f93625ae147389632e48e5f2587953b
Mikhala <mikhala.kurtjak@immutable.com>
2023-04-17 12:37:31 +0800

    Indenting

--- packages/checkout/widgets/src/widgets/connect/context/ConnectContext.test.ts
+++ packages/checkout/widgets/src/widgets/connect/context/ConnectContext.test.ts
@@ -4,23 +4,23 @@ import {describe, expect, test} from '@jest/globals';
 import { Actions, connectReducer, initialState, SetCheckoutPayload, SetProviderPayload } from './ConnectContext';
 
 describe('connect-context', () => {
-    test('when reducer called with SET_CHECKOUT payload > state updated with new checkout', () => {
-        const setCheckoutPayload: SetCheckoutPayload = {
-            type: Actions.SET_CHECKOUT,
-            checkout: new Checkout()
-        }
-        expect(initialState.checkout).toBeNull();
-        const { checkout } = connectReducer(initialState, { payload: setCheckoutPayload });
-        expect(checkout).toBeInstanceOf(Checkout);
-    });
+  test('when reducer called with SET_CHECKOUT payload > state updated with new checkout', () => {
+    const setCheckoutPayload: SetCheckoutPayload = {
+      type: Actions.SET_CHECKOUT,
+      checkout: new Checkout()
+    }
+    expect(initialState.checkout).toBeNull();
+    const { checkout } = connectReducer(initialState, { payload: setCheckoutPayload });
+    expect(checkout).toBeInstanceOf(Checkout);
+  });
 
-    test('when reducer called with SET_PROVIDER payload > state updated with new provider', () => {
-        const SetProviderPayload: SetProviderPayload = {
-            type: Actions.SET_PROVIDER,
-            provider: {} as Web3Provider
-        }
-        expect(initialState.provider).toBeNull();
-        const { provider } = connectReducer(initialState, { payload: SetProviderPayload });
-        expect(provider).not.toBeNull();
-    });
+  test('when reducer called with SET_PROVIDER payload > state updated with new provider', () => {
+    const SetProviderPayload: SetProviderPayload = {
+      type: Actions.SET_PROVIDER,
+      provider: {} as Web3Provider
+    }
+    expect(initialState.provider).toBeNull();
+    const { provider } = connectReducer(initialState, { payload: SetProviderPayload });
+    expect(provider).not.toBeNull();
+  });
 });
--- packages/checkout/widgets/src/widgets/connect/context/ConnectContext.ts
+++ packages/checkout/widgets/src/widgets/connect/context/ConnectContext.ts
@@ -3,61 +3,61 @@ import { createContext } from "react";
 import { Checkout } from '@imtbl/checkout-sdk-web';
 
 export interface ConnectState {
-    checkout: Checkout | null
-    provider: Web3Provider | null
+  checkout: Checkout | null
+  provider: Web3Provider | null
 }
 
 export const initialState: ConnectState = {
-    checkout: null,
-    provider: null
+  checkout: null,
+  provider: null
 }
 
 export interface ConnectContextState {
-    state: ConnectState,
-    dispatch: React.Dispatch<Action>,
+  state: ConnectState,
+  dispatch: React.Dispatch<Action>,
 }
 
 export interface Action {
-    payload: ActionPayload
+  payload: ActionPayload
 }
 
 type ActionPayload = SetCheckoutPayload | SetProviderPayload
 
 export enum Actions {
-    SET_CHECKOUT = "set-checkout",
-    SET_PROVIDER = "set-provider",
+  SET_CHECKOUT = "set-checkout",
+  SET_PROVIDER = "set-provider",
 }
 
 export interface SetCheckoutPayload {
-    type: Actions.SET_CHECKOUT,
-    checkout: Checkout
+  type: Actions.SET_CHECKOUT,
+  checkout: Checkout
 }
 
 export interface SetProviderPayload {
-    type: Actions.SET_PROVIDER,
-    provider: Web3Provider
+  type: Actions.SET_PROVIDER,
+  provider: Web3Provider
 }
 
 export const ConnectContext = createContext<ConnectContextState>({
-    state: initialState,
-    dispatch: () => {},
+  state: initialState,
+  dispatch: () => {},
 });
 
 export type Reducer<S, A> = (prevState: S, action: A) => S;
 
 export const connectReducer: Reducer<ConnectState, Action> = (state: ConnectState, action: Action) => {
-    switch (action.payload.type) {
-        case Actions.SET_CHECKOUT:
-            return {
-                ...state,
-                checkout: action.payload.checkout
-            }
-        case Actions.SET_PROVIDER:
-            return {
-                ...state,
-                provider: action.payload.provider
-            }
-        default:
-            return state;
-    }
+  switch (action.payload.type) {
+    case Actions.SET_CHECKOUT:
+      return {
+        ...state,
+        checkout: action.payload.checkout
+      }
+    case Actions.SET_PROVIDER:
+      return {
+        ...state,
+        provider: action.payload.provider
+      }
+    default:
+      return state;
+  }
 }
