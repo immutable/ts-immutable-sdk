524d1dd53be1f3ae7ab9444cc32d5c3b3807faad
Mikhala <122326421+imx-mikhala@users.noreply.github.com>
2023-04-18 07:19:15 +0800

    WT-1212 Connect Context NO-CHANGELOG (#128)

--- packages/checkout/widgets/src/widgets/connect/components/other-wallets/OtherWallets.tsx
+++ packages/checkout/widgets/src/widgets/connect/components/other-wallets/OtherWallets.tsx
@@ -1,42 +1,45 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from '@biom3/react'
-import {ConnectionProviders, Checkout, ConnectResult} from '@imtbl/checkout-sdk-web'
-import { ConnectWidgetViews } from '../../ConnectWidget'
-import { Web3Provider } from '@ethersproject/providers'
+import {ConnectionProviders} from '@imtbl/checkout-sdk-web'
 import { ButtonWrapperStyle } from '../../ConnectStyles'
+import { Actions, ConnectContext } from '../../context/ConnectContext'
+import { useContext } from 'react'
+import { ConnectWidgetViews } from '../../ConnectWidget'
 
 export interface OtherWalletProps {
   updateView: (newView: ConnectWidgetViews, err?: any) => void;
-  updateProvider: (provider: Web3Provider) => void;
 }
 
 export function OtherWallets (props:OtherWalletProps) {
-  const checkout:Checkout = new Checkout()
-
-  const { updateView, updateProvider } = props
+  const { state, dispatch } = useContext(ConnectContext);
+  const { checkout } = state;
+  const { updateView } = props
 
   async function metamaskClick() {
-
-    let connectResult:ConnectResult
-
     try {
-      connectResult = await checkout.connect({ providerPreference: ConnectionProviders.METAMASK });
+      if (!checkout) return;
+      const connectResult = await checkout.connect({ providerPreference: ConnectionProviders.METAMASK });
+      dispatch({
+        payload: {
+          type: Actions.SET_PROVIDER,
+          provider: connectResult.provider,
+        },
+      });
     } catch (err:any) {
       updateView(ConnectWidgetViews.FAIL, err)
       return
     }
-    updateProvider(connectResult.provider)
     updateView(ConnectWidgetViews.CHOOSE_NETWORKS)
   }
 
-    return (
-      <div className="imtbl-other-wallets">
+  return (
+    <div className="imtbl-other-wallets">
       <Button
         testId='other-metamask'
         sx={ButtonWrapperStyle} onClick={metamaskClick}>MetaMask</Button>
       <Button
         testId='other-walletconnect'
         onClick={() => updateView(ConnectWidgetViews.CHOOSE_NETWORKS)}>Wallet Connect</Button>
-      </div>
-    )
+    </div>
+  )
 }
