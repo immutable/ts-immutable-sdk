3574893d9c8f0fff56de238c1b571a84a1d5c1e9
Mikhala <mikhala.kurtjak@immutable.com>
2023-04-19 15:08:50 +0800

    Rebase view context

--- packages/checkout/widgets/src/widgets/connect/ConnectWidget.tsx
+++ packages/checkout/widgets/src/widgets/connect/ConnectWidget.tsx
@@ -14,23 +14,10 @@ import {
   WidgetHeaderStyle 
 } from './ConnectStyles'
 import { BaseTokens, onDarkBase, onLightBase } from '@biom3/design-tokens'
-<<<<<<< main
 import { ConnectActions, ConnectContext, connectReducer, initialConnectState } from './context/ConnectContext'
 import { initialViewState, ViewActions, ViewContext, viewReducer } from '../../context/ViewContext';
 import { ConnectWidgetViews } from '../../context/ConnectViewContextTypes';
-=======
-import { Actions, ConnectContext, connectReducer, initialState } from './context/ConnectContext'
 import { Layout } from '../../components/Layout';
-
-export enum ConnectWidgetViews {
-  CONNECT_WALLET = "CONNECT_WALLET",
-  PASSPORT = "PASSPORT",
-  OTHER_WALLETS = "OTHER_WALLETS",
-  CHOOSE_NETWORKS = "CHOOSE_NETWORKS",
-  SUCCESS = "SUCCESS",
-  FAIL = "FAIL",
-}
->>>>>>> WT-1252 Reusable Layout Component
 
 export interface ConnectWidgetProps {
   params: ConnectWidgetParams;
@@ -87,108 +74,67 @@ export function ConnectWidget(props:ConnectWidgetProps) {
 
   return (
     <BiomeThemeProvider theme={{base: biomeTheme}}>
-<<<<<<< main
       <ViewContext.Provider value={{ viewState, viewDispatch }}>
         <ConnectContext.Provider value={{ connectState, connectDispatch }}>
-=======
-      <ConnectContext.Provider value={{ state: state, dispatch: dispatch }}>
-        <Layout header={<>Header</>} footer={<>Footer</>}>
->>>>>>> WT-1252 Reusable Layout Component
-          <Box sx={ConnectWidgetStyle}>
-            <Box sx={WidgetHeaderStyle}>
-              <Box sx={BackButtonStyle}>
-              <Button 
-                testId='back-button'
-<<<<<<< main
-                sx={(viewState.view.type === ConnectWidgetViews.CONNECT_WALLET) ? InactiveStyle : ActiveStyle}
-=======
-                sx={(currentView === ConnectWidgetViews.CONNECT_WALLET) ? InactiveStyle : ActiveStyle}
->>>>>>> WT-1252 Reusable Layout Component
-                onClick={() => goBack()}>Back</Button>
-              </Box>  
-              <Box>
+          <Layout header={<>Header</>} footer={<>Footer</>} >
+            <Box sx={ConnectWidgetStyle}>
+              <Box sx={WidgetHeaderStyle}>
+                <Box sx={BackButtonStyle}>
                 <Button 
-                testId='close-button'
-<<<<<<< main
-                onClick={() => 
-                  viewDispatch({
-                    payload: {
-                      type: ViewActions.UPDATE_VIEW,
-                      view: {
-                        type: ConnectWidgetViews.FAIL,
-                        error: new Error('User closed the connect widget')
+                  testId='back-button'
+                  sx={(viewState.view.type === ConnectWidgetViews.CONNECT_WALLET) ? InactiveStyle : ActiveStyle}
+                  onClick={() => goBack()}>Back</Button>
+                </Box>  
+                <Box>
+                  <Button 
+                  testId='close-button'
+                  onClick={() => 
+                    viewDispatch({
+                      payload: {
+                        type: ViewActions.UPDATE_VIEW,
+                        view: {
+                          type: ConnectWidgetViews.FAIL,
+                          error: new Error('User closed the connect widget')
+                        }
                       }
-                    }
-                  })                
-                }>x</Button>
+                    })                
+                  }>x</Button>
+                </Box>
+              </Box>       
+              <Box 
+                testId="connect-wallet" 
+                sx={(viewState.view.type === ConnectWidgetViews.CONNECT_WALLET) ? ActiveStyle : InactiveStyle}
+              >
+                <ConnectWallet />
               </Box>
-            </Box>       
-            <Box 
-              testId="connect-wallet" 
-              sx={(viewState.view.type === ConnectWidgetViews.CONNECT_WALLET) ? ActiveStyle : InactiveStyle}
-            >
-              <ConnectWallet />
-            </Box>
-            <Box 
-              testId="other-wallets" 
-              sx={(viewState.view.type === ConnectWidgetViews.OTHER_WALLETS) ? ActiveStyle : InactiveStyle}
-            >
-              <OtherWallets />
-            </Box>
-            <Box 
-              testId="choose-networks" 
-              sx={(viewState.view.type === ConnectWidgetViews.CHOOSE_NETWORKS) ? ActiveStyle : InactiveStyle}
-            >
-              <ChooseNetwork />
-            </Box>
-            <Box 
-              testId="fail"
-              sx={(viewState.view.type === ConnectWidgetViews.FAIL) ? ActiveStyle : InactiveStyle}
-            >
-=======
-                onClick={() => updateView(ConnectWidgetViews.FAIL, new Error("User closed the connect widget"))}>x</Button>
+              <Box 
+                testId="other-wallets" 
+                sx={(viewState.view.type === ConnectWidgetViews.OTHER_WALLETS) ? ActiveStyle : InactiveStyle}
+              >
+                <OtherWallets />
               </Box>
-            </Box>       
-
-            <Box 
-              testId="connect-wallet" 
-              sx={(currentView === ConnectWidgetViews.CONNECT_WALLET) ? ActiveStyle : InactiveStyle}>
-              <ConnectWallet updateView={updateView} />
+              <Box 
+                testId="choose-networks" 
+                sx={(viewState.view.type === ConnectWidgetViews.CHOOSE_NETWORKS) ? ActiveStyle : InactiveStyle}
+              >
+                <ChooseNetwork />
+              </Box>
+              <Box 
+                testId="fail"
+                sx={(viewState.view.type === ConnectWidgetViews.FAIL) ? ActiveStyle : InactiveStyle}
+              >
+                <Body style={{color:'#FFF'}}>User did not connect</Body>
+              </Box>
+              <Box 
+                testId="success"
+                sx={(viewState.view.type === ConnectWidgetViews.SUCCESS) ? ActiveStyle : InactiveStyle}
+              >
+                <Body style={{color:'#FFF'}}>User connected</Body>
+              </Box>
             </Box>
-            <Box 
-              testId="other-wallets" 
-              sx={(currentView === ConnectWidgetViews.OTHER_WALLETS) ? ActiveStyle : InactiveStyle}>
-              <OtherWallets updateView={updateView} />
-            </Box>
-            <Box 
-              testId="choose-networks" 
-              sx={(currentView === ConnectWidgetViews.CHOOSE_NETWORKS) ? ActiveStyle : InactiveStyle}>
-              <ChooseNetwork updateView={updateView} />
-            </Box>
-            <Box 
-              testId="fail"
-              sx={(currentView === ConnectWidgetViews.FAIL) ? ActiveStyle : InactiveStyle}>
->>>>>>> WT-1252 Reusable Layout Component
-              <Body style={{color:'#FFF'}}>User did not connect</Body>
-            </Box>
-            <Box 
-              testId="success"
-<<<<<<< main
-              sx={(viewState.view.type === ConnectWidgetViews.SUCCESS) ? ActiveStyle : InactiveStyle}
-            >
-              <Body style={{color:'#FFF'}}>User connected</Body>
-            </Box>
-          </Box>
+          </Layout>
         </ConnectContext.Provider>
       </ViewContext.Provider>
-=======
-              sx={(currentView === ConnectWidgetViews.SUCCESS) ? ActiveStyle : InactiveStyle}>
-              <Body style={{color:'#FFF'}}>User connected</Body>
-            </Box>
-          </Box>
-        </Layout>
-      </ConnectContext.Provider>
->>>>>>> WT-1252 Reusable Layout Component
     </BiomeThemeProvider>
   )
 }
