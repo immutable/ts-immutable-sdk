6c2a6f71a33a180c6ae297380c5202f514452079
Zach Couchman <zachary.couchman@immutable.com>
2023-04-20 11:47:22 +1000

    Refactoring

--- packages/checkout/widgets/src/widgets/connect/ConnectWidget.tsx
+++ packages/checkout/widgets/src/widgets/connect/ConnectWidget.tsx
@@ -1,25 +1,19 @@
-import { BiomeThemeProvider, Button, Body, Box } from '@biom3/react'
+import { BiomeThemeProvider, Body, Box } from '@biom3/react'
 import { Checkout, ConnectionProviders } from '@imtbl/checkout-sdk-web'
 import { WidgetTheme } from '@imtbl/checkout-ui-types'
 import { sendConnectFailedEvent, sendConnectSuccessEvent} from './ConnectWidgetEvents'
-import { ConnectWallet } from './components/connect-wallet/ConnectWallet';
 import { OtherWallets } from './components/other-wallets/OtherWallets';
 import { ChooseNetwork } from './components/choose-network/ChooseNetwork';
 import { useEffect, useReducer } from 'react'
 import { 
   ActiveStyle, 
-  BackButtonStyle,
   InactiveStyle, 
-  WidgetHeaderStyle 
 } from './ConnectStyles'
 import { BaseTokens, onDarkBase, onLightBase } from '@biom3/design-tokens'
 import { ConnectActions, ConnectContext, connectReducer, initialConnectState } from './context/ConnectContext'
 import { initialViewState, ViewActions, ViewContext, viewReducer } from '../../context/ViewContext';
 import { ConnectWidgetViews } from '../../context/ConnectViewContextTypes';
-import { SimpleLayout } from '../../components/SimpleLayout/SimpleLayout';
-import { HeaderNavigation } from '../../components/HeaderNavigation';
-import { FooterNavigation } from '../../components/Footer/FooterNavigation';
-import immutableNetwork from '../../components/SimpleLayout/PurpleDownGradient.svg';
+import { ConnectAWallet } from './components/connect-a-wallet/ConnectAWallet';
 export interface ConnectWidgetProps {
   params: ConnectWidgetParams;
   theme: WidgetTheme;
@@ -65,26 +59,6 @@ export function ConnectWidget(props:ConnectWidgetProps) {
     }
   }, [viewState])
 
-  const goBack = async () => {
-    viewDispatch({
-      payload: {
-        type: ViewActions.GO_BACK
-      }
-    });
-  }
-
-  const close = () => {
-    viewDispatch({
-      payload: {
-        type: ViewActions.UPDATE_VIEW,
-        view: {
-          type: ConnectWidgetViews.FAIL,
-          error: new Error('User closed the connect widget')
-        }
-      }
-    })        
-  }
-
   useEffect(() => {
     console.log("Connect Widget current view ", viewState.view.type);
   }, [viewState.view.type])
@@ -98,65 +72,33 @@ export function ConnectWidget(props:ConnectWidgetProps) {
            * These views are components which are made up of a layout and content components
            * and handle the behaviour of the view 
            */}
-          {/* {viewState.view.type === ConnectWidgetViews.CONNECT_WALLET && 
+          {viewState.view.type === ConnectWidgetViews.CONNECT_WALLET && 
             <ConnectAWallet />
-          } */}
-          <SimpleLayout 
-            header={
-              <HeaderNavigation
-                title='Connect a wallet' 
-                showClose
-                />
-            }
-            heroImage={immutableNetwork}
-            footer={<FooterNavigation />}
-            >
-            <Box>
-              <Box sx={WidgetHeaderStyle}>
-                <Box sx={BackButtonStyle}>
-                <Button 
-                  testId='back-button'
-                  sx={(viewState.view.type === ConnectWidgetViews.CONNECT_WALLET) ? InactiveStyle : ActiveStyle}
-                  onClick={() => goBack()}>Back</Button>
-                </Box>  
-                <Box>
-                  <Button 
-                  testId='close-button'
-                  onClick={close}>x</Button>
-                </Box>
-              </Box>       
-              <Box 
-                testId="connect-wallet" 
-                sx={(viewState.view.type === ConnectWidgetViews.CONNECT_WALLET) ? ActiveStyle : InactiveStyle}
-              >
-                <ConnectWallet />
-              </Box>
-              <Box 
-                testId="other-wallets" 
-                sx={(viewState.view.type === ConnectWidgetViews.OTHER_WALLETS) ? ActiveStyle : InactiveStyle}
-              >
-                <OtherWallets />
-              </Box>
-              <Box 
-                testId="choose-networks" 
-                sx={(viewState.view.type === ConnectWidgetViews.CHOOSE_NETWORKS) ? ActiveStyle : InactiveStyle}
-              >
-                <ChooseNetwork />
-              </Box>
-              <Box 
-                testId="fail"
-                sx={(viewState.view.type === ConnectWidgetViews.FAIL) ? ActiveStyle : InactiveStyle}
-              >
-                <Body style={{color:'#FFF'}}>User did not connect</Body>
-              </Box>
-              <Box 
-                testId="success"
-                sx={(viewState.view.type === ConnectWidgetViews.SUCCESS) ? ActiveStyle : InactiveStyle}
-              >
-                <Body style={{color:'#FFF'}}>User connected</Body>
-              </Box>
-            </Box>
-          </SimpleLayout>
+          }
+          <Box 
+            testId="other-wallets" 
+            sx={(viewState.view.type === ConnectWidgetViews.OTHER_WALLETS) ? ActiveStyle : InactiveStyle}
+          >
+            <OtherWallets />
+          </Box>
+          <Box 
+            testId="choose-networks" 
+            sx={(viewState.view.type === ConnectWidgetViews.CHOOSE_NETWORKS) ? ActiveStyle : InactiveStyle}
+          >
+            <ChooseNetwork />
+          </Box>
+          <Box 
+            testId="fail"
+            sx={(viewState.view.type === ConnectWidgetViews.FAIL) ? ActiveStyle : InactiveStyle}
+          >
+            <Body style={{color:'#FFF'}}>User did not connect</Body>
+          </Box>
+          <Box 
+            testId="success"
+            sx={(viewState.view.type === ConnectWidgetViews.SUCCESS) ? ActiveStyle : InactiveStyle}
+          >
+            <Body style={{color:'#FFF'}}>User connected</Body>
+          </Box>
         </ConnectContext.Provider>
       </ViewContext.Provider>
     </BiomeThemeProvider>
