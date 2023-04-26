729d99cb49158a6fcc123e989c2cc77d5b632730
Pano Skylakis <pano.skylakis@immutable.com>
2023-04-26 13:28:11 +1200

    Fix: linting

--- packages/internal/dex/src/constants/rpc.ts
+++ packages/internal/dex/src/constants/rpc.ts
@@ -1,5 +1,5 @@
 export const POLYGON_ZKEVM_TESTNET_RPC_URL =
   'https://rpc.public.zkevm-test.net';
 
-  export const IMX_PRIVATE_TESTNET_RPC_URL = 'https://rpc.private.immutable.net'
-  export const IMX_ZKEVM_RPC_URL = 'https://rpc.public.immutable.net'
+export const IMX_PRIVATE_TESTNET_RPC_URL = 'https://rpc.private.immutable.net';
+export const IMX_ZKEVM_RPC_URL = 'https://rpc.public.immutable.net';
