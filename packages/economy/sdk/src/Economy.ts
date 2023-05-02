e41401ca8c19459b65dfdda6f12132b1e727fe56
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-05-02 15:33:31 +1000

    rewrite crafting into its own class

--- packages/economy/sdk/src/Economy.ts
+++ packages/economy/sdk/src/Economy.ts
@@ -1,36 +1,24 @@
 import { SDK } from './SDK';
-import { craft } from './crafting';
-import { withSDKError } from './Errors';
+import { Crafting } from './crafting/Crafting';
 
-import type { CraftInput, CraftStatus, CraftEvent } from './crafting';
+import type { CraftEvent } from './crafting/Crafting';
 
 /** @internal Economy SDK actions */
 export type EconomyEvents = CraftEvent;
 
 export class Economy extends SDK<EconomyEvents> {
+  public crafting!: Crafting;
+
+  constructor(crafting?: Crafting) {
+    super();
+    this.crafting =
+      crafting instanceof Crafting
+        ? crafting
+        : new Crafting(this.getEmitEventHandler());
+  }
+
   /** Lifecycle method: Self invoked after class instanciation */
   connect(): void {
     this.log('mounted');
-
-    this.subscribe((event) => {
-      if (event.action === 'CRAFT' && event.status === 'COMPLETED') {
-        event.data;
-      }
-    });
-  }
-
-  /**
-   * Given inputs for a recipe crafting
-   * process the recipe by sending it to the backend service
-   * @param input crafting recipe inputs
-   * @returns crafting status
-   */
-  @withSDKError({ type: 'CRAFTING_ERROR' })
-  async craft(input: CraftInput): Promise<CraftStatus> {
-    this.emitEvent({ action: 'CRAFT', status: 'STARTED' });
-
-    const status = await craft(input, this.getEmitEventHandler());
-
-    return status;
   }
 }
