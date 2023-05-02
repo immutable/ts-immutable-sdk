826e9c78778bce2615fdee767e60a934305b5b54
Mikhala <mikhala.kurtjak@immutable.com>
2023-05-02 08:01:47 +0800

    embed the SVG

--- packages/checkout/widgets/src/components/Hero/SatelliteHero.tsx
+++ packages/checkout/widgets/src/components/Hero/SatelliteHero.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@biom3/react';
-import satellite from '../../assets/Satellite.svg';
+import { ReactComponent as Satellite } from '../../assets/Satellite.svg';
 import { HeroBackGroundStyles, HeroImageStyles } from './HeroImageStyles';
 
 export const SatelliteHero = () => {
   return (
     <Box sx={HeroBackGroundStyles}>
       <Box sx={HeroImageStyles}>
-        <img alt="Satellite" src={satellite} />
+        <Satellite />
       </Box>
     </Box>
   );
