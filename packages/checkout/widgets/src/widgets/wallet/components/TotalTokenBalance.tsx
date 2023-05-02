bce21c362d6a5f33871445aa3124ed2803c642c6
proletesseract <craig.b.macgregor@gmail.com>
2023-05-02 15:18:34 +1200

    approximately equals and prettier formatting

--- packages/checkout/widgets/src/widgets/wallet/components/TotalTokenBalance.tsx
+++ packages/checkout/widgets/src/widgets/wallet/components/TotalTokenBalance.tsx
@@ -7,18 +7,26 @@ interface TotalTokenBalanceProps {
 export const TotalTokenBalance = (props: TotalTokenBalanceProps) => {
   const { totalBalance } = props;
   return (
-    <Box sx={{
-      display: 'flex', 
-      flexDirection: 'row', 
-      justifyContent: 'space-between',
-      paddingX: 'base.spacing.x3',
-      paddingY: 'base.spacing.x2'
-      }}>
-      <Body testId="heading" size={'medium'}>Coins</Body>
-      <Box sx={{ display: 'flex', direction: 'row', columnGap: 'base.spacing.x1' }}>
-        <Body size={'medium'} weight="bold">Value:</Body>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingX: 'base.spacing.x3',
+        paddingY: 'base.spacing.x2',
+      }}
+    >
+      <Body testId="heading" size={'medium'}>
+        Coins
+      </Body>
+      <Box
+        sx={{ display: 'flex', direction: 'row', columnGap: 'base.spacing.x1' }}
+      >
+        <Body size={'medium'} weight="bold">
+          Value
+        </Body>
         <Body testId="total-token-balance" size={'medium'} weight="bold">
-          ${totalBalance.toFixed(2)}
+          &asymp; USD ${totalBalance.toFixed(2)}
         </Body>
       </Box>
     </Box>
