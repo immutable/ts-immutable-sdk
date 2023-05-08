54ef49c1e13da6cdb8bbecb857323b77264603b9
MitchSaad <mitch.saad@immutable.com>
2023-05-08 13:15:21 +1000

    Split out filtering functionality of getTokenAllowList

--- packages/checkout/sdk/src/tokens/tokens.ts
+++ packages/checkout/sdk/src/tokens/tokens.ts
@@ -3,20 +3,16 @@ import {
   GetTokenAllowListResult,
   TokenFilterTypes,
   TokenInfo,
+  TokenMasterInfo,
 } from '../types';
 import masterTokenList from './token_master_list.json';
-import { utils } from 'ethers';
-import axios from 'axios';
-import { CheckoutError, CheckoutErrorType, withCheckoutError } from '../errors';
 
-export const getTokenAllowList = async function ({
-  type = TokenFilterTypes.ALL,
+const filterTokenList = function ({
+  type,
   chainId,
   exclude,
-}: GetTokenAllowListParams): Promise<GetTokenAllowListResult> {
-  // todo:For API call, use the CheckoutError with errorType:API_CALL_ERROR?? or any other
-
-  const filteredTokenList = masterTokenList
+}: GetTokenAllowListParams): TokenMasterInfo[] {
+  return masterTokenList
     .filter((token) => {
       const chainIdMatches = !chainId || token.chainId == chainId;
       const tokenNotExcluded = !exclude
@@ -30,8 +26,18 @@ export const getTokenAllowList = async function ({
         tokenNotExcluded &&
         (allowAllTokens || tokenAllowedForType)
       );
-    })
-    .map((token) => {
+    }) as TokenMasterInfo[]
+}
+
+export const getTokenAllowList = async function ({
+  type = TokenFilterTypes.ALL,
+  chainId,
+  exclude,
+}: GetTokenAllowListParams): Promise<GetTokenAllowListResult> {
+  // todo:For API call, use the CheckoutError with errorType:API_CALL_ERROR?? or any other
+
+  const filteredTokenList = filterTokenList({ type, chainId, exclude }).map(
+    (token) => {
       const { chainId, tokenFeatures, ...tokenInfo } = token;
       return tokenInfo as TokenInfo;
     });
