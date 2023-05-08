9651d1543b2500854d664c05eccde0abfa657af1
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-05-08 12:21:00 +1000

    add http client using axios

---
+++ packages/economy/sdk/src/services/HttpClient.ts
@@ -0,0 +1,76 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export class HttpClient {
+  private readonly axiosInstance: AxiosInstance;
+
+  constructor(baseURL: string, defaultHeaders: Record<string, string> = {}) {
+    this.axiosInstance = axios.create({
+      baseURL,
+      headers: defaultHeaders,
+    });
+
+    // Setup request
+    this.axiosInstance.interceptors.request.use((config) => {
+      console.info({
+        method: config.method?.toUpperCase(),
+        url: config.url,
+        params: config.params,
+        headers: config.headers,
+      });
+      return config;
+    });
+
+    // Add response interceptor to log response details
+    this.axiosInstance.interceptors.response.use(
+      (response) => {
+        console.info({
+          status: response.status,
+          statusText: response.statusText,
+          data: response.data,
+        });
+        return response;
+      },
+      (error) => {
+        console.error({
+          status: error.response?.status,
+          statusText: error.response?.statusText,
+          data: error.response?.data,
+          message: error.message,
+        });
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  public async get<T>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.get<T>(url, config);
+  }
+
+  public async post<T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.post<T>(url, data, config);
+  }
+
+  public async put<T>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.put<T>(url, data, config);
+  }
+
+  public async delete<T>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): Promise<AxiosResponse<T>> {
+    return this.axiosInstance.delete<T>(url, config);
+  }
+}
+
+export default HttpClient;
