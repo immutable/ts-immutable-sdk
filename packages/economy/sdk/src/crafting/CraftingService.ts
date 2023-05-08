9c7770f87d57f84a960e30a469b454bc17bf0948
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-05-08 13:08:05 +1000

    add http-client and craft service

---
+++ packages/economy/sdk/src/crafting/CraftingService.ts
@@ -0,0 +1,54 @@
+import { AxiosResponse, AxiosRequestConfig } from 'axios';
+import HttpClient from '../HttpClient';
+
+// TODO: Use generated types
+type CraftOutput = {};
+
+type CraftInput = {
+  ingredients: CraftIngredient[];
+  recipeId: string;
+  userId: string;
+};
+
+type CraftIngredient = {
+  conditionId: string;
+  itemId: string;
+};
+
+// TODO: Read from .env
+// FIXME: target https://api.dev.games.immutable.com/crafting/swagger/index.html#/root/post_craft
+const defaultBaseURL = 'http://127.0.0.1:3031/crafting';
+
+export class CraftingService {
+  private httpClient: HttpClient;
+
+  constructor(
+    httpClientOrBaseUrl: HttpClient | string = defaultBaseURL,
+    defaultHeaders: Record<string, string> = {}
+  ) {
+    if (httpClientOrBaseUrl instanceof HttpClient) {
+      this.httpClient = httpClientOrBaseUrl;
+    } else {
+      this.httpClient = new HttpClient(httpClientOrBaseUrl, defaultHeaders);
+    }
+  }
+
+  public async craft(input: CraftInput): Promise<AxiosResponse<CraftOutput>> {
+    const url = `/craft`;
+    // FIXME: transform
+    const data = {
+      recipe_id: input.recipeId,
+      user_id: input.userId,
+      ingredients: input.ingredients.map((i) => ({
+        condition_id: i.conditionId,
+        item_id: i.itemId,
+      })),
+    };
+    const config: AxiosRequestConfig = {
+      headers: {},
+    };
+    return this.httpClient.post<CraftOutput>(url, data, config);
+  }
+}
+
+export default CraftingService;
