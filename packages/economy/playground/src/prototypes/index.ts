e7fd5da7c901adfbdc11a1910da35b70cce9a7af
Jhonatan Gonzalez <jhesgodi@gmail.com>
2023-05-12 14:01:33 +1000

    [NO-CHANGELOG][ECONOMY] Add Crafting prototype components (#274)

---
+++ packages/economy/playground/src/prototypes/index.ts
@@ -0,0 +1,6 @@
+import './craft-button';
+import './craft-connect';
+import './craft-passport';
+//
+// import './craft-card-upgrade';
+import './card-upgrade-widget';
--- packages/economy/sdk/src/index.ts
+++ packages/economy/sdk/src/index.ts
@@ -1,5 +1,12 @@
 export { EconomyCustomEventTypes, EventActions, EventStatuses } from './types';
-export type { EconomyCustomEventType, EventAction, EventStatus } from './types';
+export type {
+  EconomyCustomEventType,
+  EventAction,
+  EventStatus,
+  ItemDefinition,
+  InventoryItem,
+  Recipe,
+} from './types';
 
 export * from './Economy';
 export type { CraftInput, CraftStatus, CraftEvent } from './crafting/Crafting';
