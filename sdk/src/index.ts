d3762bd81c7df44d3e9493cddf55a156e887714d
Sam Jeston <sam.jeston@gmail.com>
2023-05-15 15:09:43 +1000

    fix: merge

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,3 +1,6 @@
 export * from './config';
 export * from './immutablex_client';
 export * from './passport';
+export * from './provider';
+export * from './checkout_sdk';
+export * from './checkout_widgets';
