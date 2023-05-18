3dfaa00b2e819460178018f1a3ea90888f5e70c0
James Hardwick <james.hardwick@immutable.com>
2023-05-18 12:01:28 +1000

    Updates Inventory event naming.

--- packages/economy/sdk/src/inventory/Inventory.ts
+++ packages/economy/sdk/src/inventory/Inventory.ts
@@ -1,24 +1,33 @@
 import { Service } from 'typedi';
-import type { EventType } from '../types';
+import type { EventData, EventType } from '../types';
 import { withSDKError } from '../Errors';
 
 import { GetItemsInput, InventoryService } from './InventoryService';
+import { EventClient } from '../EventClient';
 
 /**
  * @internal Assets events
  */
-export type InventoryEvent = EventType<'INVENTORY'>;
+export type InventoryEvent = EventType<
+'INVENTORY_GET_ITEMS',
+| EventData<'STARTED' | 'IN_PROGRESS'>
+| EventData<'COMPLETED', { data: {} }>
+| EventData<'FAILED', { error: { code: string; reason: string } }>
+>
+| EventType<'INVENTORY_FILTER_ITEMS_BY'>;
 
 /** List of specific Assets statuses */
 export type InventoryStatus = InventoryEvent['status'];
 
 @Service()
 export class Inventory {
-  constructor(private inventoryService: InventoryService) {
+  constructor(private inventoryService: InventoryService, private events: EventClient<InventoryEvent>) {
   }
 
-  @withSDKError({ type: 'INVENTORY_ERROR' })
+  @withSDKError({ type: 'INVENTORY_GET_ITEMS_ERROR' })
   public async getItems(input: GetItemsInput) {
+    this.events.emitEvent({ status: 'STARTED', action: 'INVENTORY_GET_ITEMS' });
+
     const { data, status } = await this.inventoryService.getItems(input);
 
     if (status !== 200) {
