ebeb1529adc3c232acd14eef0a344d53a1ef1340
James Hardwick <james.hardwick@immutable.com>
2023-05-19 11:43:13 +1000

    Update to auto generated types.

--- packages/economy/sdk/src/inventory/InventoryService.ts
+++ packages/economy/sdk/src/inventory/InventoryService.ts
@@ -2,19 +2,8 @@
 import { AxiosResponse, AxiosRequestConfig } from 'axios';
 import { Service } from 'typedi';
 import { HttpClient } from '../HttpClient';
-import { InventoryItem } from '../types';
 import { Config } from '../Config';
-
-// TODO: Use generated types
-type GetItemsOutput = {
-  limit: number;
-  page: number;
-  sort: string;
-  direction: string;
-  total_rows: number;
-  total_pages: number;
-  rows: Array<InventoryItem>;
-};
+import { InventoryPaginatedItems } from '../__codegen__/inventory';
 
 export type GetItemsInput = {
   gameId: string;
@@ -29,12 +18,12 @@ export class InventoryService {
 
   public async getItems(
     input: GetItemsInput,
-  ): Promise<AxiosResponse<GetItemsOutput>> {
+  ): Promise<AxiosResponse<InventoryPaginatedItems>> {
     const url = `/${input.gameId}/items`;
     const config: AxiosRequestConfig = {
       headers: {},
     };
-    return this.httpClient.get<GetItemsOutput>(
+    return this.httpClient.get<InventoryPaginatedItems>(
       `${url}?owner=${input.owner}`,
       config,
     );
