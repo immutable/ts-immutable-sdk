6dbd054270009638680061c801077fe43541a427
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-05-19 12:49:07 +1000

    remove unused import

--- packages/economy/playground/.storybook/main.ts
+++ packages/economy/playground/.storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
-import { optimizeDeps } from 'vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
