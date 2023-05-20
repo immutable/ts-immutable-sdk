d47d257560ab88a34f8b9e41ac4cf58d329a5546
Andrea Rampin <andrea.rampin@gmail.com>
2023-05-21 09:19:19 +1000

    wt-1359 patch global

--- packages/checkout/widgets/src/definitions/global.ts
+++ packages/checkout/widgets/src/definitions/global.ts
@@ -1,4 +1,30 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import React from 'react';
+
+declare global {
+  interface Window {
+    ImtblCheckoutWidgetConfig: any;
+  }
+
+  namespace JSX {
+    interface IntrinsicElements {
+      'imtbl-connect': ImtblConnectProps;
+      'imtbl-wallet': ImtblWalletProps;
+      'imtbl-swap': ImtblSwapProps;
+      'imtbl-bridge': ImtblBridgeProps;
+      'imtbl-buy': ImtblBuyProps;
+      'imtbl-example': ImtblExampleProps;
+      'imtbl-transition-example': ImtblTransitionExampleProps;
+      'imtbl-inner-widget-example': ImtblInnerWidgetExampleProps;
+      'imtbl-outer-widget-example': ImtblOuterWidgetExampleProps;
+    }
+  }
+
+  interface ImmutableWebComponent {
+    setProvider: Function;
+    setAttribute: Function;
+  }
+}
 
 /**
  * Props for the Connect Widget component
