343d37ea431721042829aaff6db5275565320c82
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-05-22 08:56:35 +1000

    remove archived stuff

--- packages/economy/playground/src/widgets/crafting-widget/crafting-widget.stories.ts
+++ packages/economy/playground/src/widgets/crafting-widget/crafting-widget.stories.ts
@@ -1,13 +1,20 @@
 import { html } from 'lit-html';
 
 const meta = {
-  title: 'Crafting/crafting-widget',
+  title: 'Crafting/widget',
+  args: {
+    gameId: '',
+    userId: '',
+    walletAddress: '',
+  },
 };
 export default meta;
 
-const Template = () => html`
-  <crafting-widget game-id="" user-id="" wallet-address=""></crafting-widget>
-`;
+const Template = (args: any) => {
+  return html`
+    <crafting-widget game-id="${args.gameId}" user-id="${args.userId}" wallet-address="${args.walletAddress}"></crafting-widget>
+  `;
+};
 
 export const Widget: any = Template.bind({});
 Widget.args = {};
--- packages/economy/playground/src/widgets/index.ts
+++ packages/economy/playground/src/widgets/index.ts
@@ -1,7 +1,1 @@
-import './craft-button';
-import './craft-checkout-connect';
-// import './craft-passport-connect';
-//
-import './imtbl-crafting-widget';
-import './imtbl-crafting-widget-2';
 import './crafting-widget';
