af44f7a7dde3a2b8b6a360ebdda2a71a9afc0074
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-05-22 18:22:46 +1000

    connect inventory, recipes, crafts, add item selection

--- packages/economy/playground/src/widgets/crafting-widget/crafting-summary.ts
+++ packages/economy/playground/src/widgets/crafting-widget/crafting-summary.ts
@@ -4,7 +4,7 @@ import { customElement } from 'lit/decorators.js';
 @customElement('crafting-summary')
 export class CraftingSummary extends LitElement {
   render() {
-    return html` <div>Summary</div> `;
+    return html`<div>Summary</div> `;
   }
 
   protected createRenderRoot(): Element | ShadowRoot {
