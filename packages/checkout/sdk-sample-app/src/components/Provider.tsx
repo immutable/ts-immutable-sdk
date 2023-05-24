7f2e11356159b3f6275278697ebe815153faaf40
Craig M <craig.b.macgregor@gmail.com>
2023-05-24 14:21:35 +1200

    trying with eip1193 provider

--- packages/checkout/sdk-sample-app/src/components/Provider.tsx
+++ packages/checkout/sdk-sample-app/src/components/Provider.tsx
@@ -10,6 +10,8 @@ import { useEffect, useMemo, useState } from 'react';
 import { SuccessMessage, ErrorMessage } from './messages';
 import { Environment } from '@imtbl/config';
 import { Box } from '@biom3/react';
+import Web3 from 'web3';
+import HttpProvider from 'web3';
 
 interface ProviderProps {
   checkout: Checkout;
@@ -22,8 +24,12 @@ export default function Provider(props: ProviderProps) {
 
   const [result1, setResult1] = useState<Web3Provider>();
   const [result2, setResult2] = useState<Providers>();
+  const [result3, setResult3] = useState<Providers>();
+
   const [error1, setError1] = useState<any>(null);
   const [error2, setError2] = useState<any>(null);
+  const [error3, setError3] = useState<any>(null);
+
   const [loading, setLoading] = useState<boolean>(false);
 
   async function createMetamaskClick() {
@@ -46,13 +52,15 @@ export default function Provider(props: ProviderProps) {
     }
   }
 
-  async function setProviderClick() {
+  async function setEIPProviderClick() {
     setError2(null);
     setLoading(true);
     try {
+      const eipProvider = new Web3.providers.HttpProvider('https://eth.llamarpc.com')
+
       const resp = await checkout.setProvider({
         name: 'metamask',
-        web3Provider: provider,
+        provider: eipProvider,
       });
       // setProvider(resp.web3Provider);
       console.log(resp);
@@ -68,12 +76,36 @@ export default function Provider(props: ProviderProps) {
     }
   }
 
+  async function setWeb3ProviderClick() {
+    setError3(null);
+    setLoading(true);
+    try {
+      const resp = await checkout.setProvider({
+        name: 'metamask',
+        web3Provider: provider,
+      });
+      // setProvider(resp.web3Provider);
+      console.log(resp);
+      setResult3(resp.providers);
+      setLoading(false);
+    } catch (err: any) {
+      setError3(err);
+      setLoading(false);
+      console.log(err.message);
+      console.log(err.type);
+      console.log(err.data);
+      console.log(err.stack);
+    }
+  }
+
   useEffect(() => {
     // reset state wehn checkout changes from environment switch
     setResult1(undefined);
     setResult2(undefined);
+    setResult3(undefined);
     setError1(null);
     setError2(null);
+    setError3(null);
     setLoading(false);
   }, [checkout]);
 
@@ -85,10 +117,10 @@ export default function Provider(props: ProviderProps) {
         }}
       >
         <LoadingButton onClick={createMetamaskClick} loading={loading}>
-          Create Metamask Provider
+          Create Metamask Web3Provider
         </LoadingButton>
         {result1 && !error1 && (
-          <SuccessMessage>Provider Created.</SuccessMessage>
+          <SuccessMessage>Web3Provider Created.</SuccessMessage>
         )}
         {error1 && (
           <ErrorMessage>
@@ -101,8 +133,8 @@ export default function Provider(props: ProviderProps) {
           marginTop: 'base.spacing.x4',
         }}
       >
-        <LoadingButton onClick={setProviderClick} loading={loading}>
-          Set Provider
+        <LoadingButton onClick={setEIPProviderClick} loading={loading}>
+          Set provider using EIP-1193 Provider
         </LoadingButton>
         {result2 && !error2 && (
           <SuccessMessage>
@@ -110,6 +142,25 @@ export default function Provider(props: ProviderProps) {
           </SuccessMessage>
         )}
         {error2 && (
+          <ErrorMessage>
+            {error2.message}. Check console logs for more details.
+          </ErrorMessage>
+        )}
+      </Box>
+      <Box
+        sx={{
+          marginTop: 'base.spacing.x4',
+        }}
+      >
+        <LoadingButton onClick={setWeb3ProviderClick} loading={loading}>
+          Set Provider using Web3Provider
+        </LoadingButton>
+        {result3 && !error3 && (
+          <SuccessMessage>
+            <Box>Providers Set for Available Networks.</Box>
+          </SuccessMessage>
+        )}
+        {error3 && (
           <ErrorMessage>
             {error2.message}. Check console logs for more details.
           </ErrorMessage>
