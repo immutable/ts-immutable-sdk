44e4b8a1277c54c3a3b9e36fd93531ab81a1d070
Carmen Liu <carmen.liujia@gmail.com>
2023-05-25 10:12:16 +1000

    resolve the Transaciton clash by exporting the guardian

--- packages/passport/src/index.ts
+++ packages/passport/src/index.ts
@@ -1,7 +1,7 @@
+import * as guardian from '@imtbl/guardian';
 import { PassportError } from './errors/passportError';
 import { Passport } from './Passport';
 import { PassportModuleConfiguration } from './types';
 
 export { Passport, PassportError, PassportModuleConfiguration };
-
-export * from '@imtbl/guardian';
+export { guardian };
