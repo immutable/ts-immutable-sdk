4dc4ccc5af8698a4ea0b9409a79170af898817f2
yundi <2666544+yundifu@users.noreply.github.com>
2023-05-25 10:30:44 +1000

    Create guardian client, manual for now, automate later (no-changelog) (#342)
    Co-authored-by: Carmen Liu <carmen.liujia@gmail.com>

--- packages/passport/src/index.ts
+++ packages/passport/src/index.ts
@@ -1,5 +1,7 @@
+import * as guardian from '@imtbl/guardian';
 import { PassportError } from './errors/passportError';
 import { Passport } from './Passport';
 import { PassportModuleConfiguration } from './types';
 
 export { Passport, PassportError, PassportModuleConfiguration };
+export { guardian };
