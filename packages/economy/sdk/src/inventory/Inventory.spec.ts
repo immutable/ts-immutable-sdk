ec802b96a7f1d7631eafb84dff7bf4e03db98e01
Sharon Sheah <sharonsheah@gmail.com>
2023-05-29 13:59:46 +1000

    fix tests

--- packages/economy/sdk/src/inventory/Inventory.spec.ts
+++ packages/economy/sdk/src/inventory/Inventory.spec.ts
@@ -2,12 +2,14 @@
 import Container from 'typedi';
 import 'reflect-metadata';
 
+import { Config, defaultConfig } from '../Config';
 import { Inventory } from './Inventory';
 import { InventoryItem } from '../__codegen__/inventory';
 
 describe(Inventory.name, () => {
   beforeAll(() => {
     Container.reset();
+    Container.set(Config, new Config(defaultConfig));
   });
 
   describe('filterItemsBy', () => {
