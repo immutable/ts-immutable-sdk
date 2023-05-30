a45f0d61230a60f97c5855f8d4dfd02f1a63ad23
MitchSaad <mitch.saad@immutable.com>
2023-05-30 14:49:14 +1000

    fix select option attributes in bridge form

--- packages/checkout/widgets-lib/src/widgets/bridge/components/BridgeForm.tsx
+++ packages/checkout/widgets-lib/src/widgets/bridge/components/BridgeForm.tsx
@@ -52,7 +52,8 @@ export function BridgeForm(props: BridgeFormProps) {
       .map(
         (t) => ({
           id: `${t.token.symbol}-${t.token.name}`,
-          label: t.token.symbol,
+          name: t.token.name,
+          symbol: t.token.symbol,
           icon: t.token.icon,
         } as SelectOption),
       ),
