7190a3033e76df04812aedecdf1982d3f7558656
Allan Almeida <allan.almeida@immutable.com>
2023-05-30 17:06:03 +1000

    fill out activities api

--- packages/blockchain-data/src/blockchain-data.ts
+++ packages/blockchain-data/src/blockchain-data.ts
@@ -1,7 +1,13 @@
 import {
   ActivitiesApi,
   ActivitiesApiListActivitiesRequest,
+  ActivitiesApiGetActivityRequest,
   ListActivitiesResult,
+  GetActivityResult,
+  ChainsApi,
+  CollectionsApi,
+  NftsApi,
+  NftOwnersApi,
 } from '@imtbl/multi-rollup-api-client';
 import {
   BlockchainDataConfiguration,
@@ -11,17 +17,49 @@ import {
 export class BlockchainData {
   private readonly activities: ActivitiesApi;
 
+  private readonly chains: ChainsApi;
+
+  private readonly collections: CollectionsApi;
+
+  private readonly nfts: NftsApi;
+
+  private readonly nftOwners: NftOwnersApi;
+
   constructor(moduleConfig: BlockchainDataModuleConfiguration) {
     const config = new BlockchainDataConfiguration(moduleConfig);
 
     this.activities = new ActivitiesApi(config.apiConfig);
   }
 
+  /**
+   * List all activities
+   * @param request - the request object containing the parameters to be provided in the API request
+   * @returns a promise that resolves with a list of activities
+   * @throws {@link index.IMXError} // TODO FIXME
+   */
   public async listActivities(
-    params: ActivitiesApiListActivitiesRequest
+    request: ActivitiesApiListActivitiesRequest
   ): Promise<ListActivitiesResult> {
     return await this.activities
-      .listActivities(params)
+      .listActivities(request)
+      .then((res) => res.data)
+      .catch((err) => {
+        // throw formatError(err); TODO format error correctly according to our error shape
+        throw new Error(err);
+      });
+  }
+
+  /**
+   * Get a single activity by ID
+   * @param request - the request object containing the parameters to be provided in the API request
+   * @returns a promise that resolves with a single activity
+   * @throws {@link index.IMXError} // TODO FIXME
+   */
+  public async getActivity(
+    request: ActivitiesApiGetActivityRequest
+  ): Promise<GetActivityResult> {
+    return await this.activities
+      .getActivity(request)
       .then((res) => res.data)
       .catch((err) => {
         // throw formatError(err); TODO format error correctly according to our error shape
