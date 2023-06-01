5289e93a9c86a352a792d8badb9b1e6fb47d4159
Tim Paul <tim@glo.id.au>
2023-06-01 11:12:41 +1000

    [NO-CHANGELOG] update biome deps across packages (#380)

--- packages/checkout/widgets-lib/src/widgets/bridge/components/BridgeFormStyles.ts
+++ packages/checkout/widgets-lib/src/widgets/bridge/components/BridgeFormStyles.ts
@@ -17,5 +17,5 @@ export const bridgeFormButtonContainerStyles = {
   flexDirection: 'column',
   paddingY: 'base.spacing.x6',
   paddingX: 'base.spacing.x4',
-  backgroundColor: 'base.color.translucent.container.200',
+  backgroundColor: 'base.color.translucent.emphasis.200',
 };
