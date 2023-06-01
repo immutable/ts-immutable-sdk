6e0c85ad9ebe73ce15bb3ba2ebfec1ca8aa146ed
Mikhala <mikhala.kurtjak@immutable.com>
2023-06-01 09:36:02 +0800

    dispatch view change or fire event in top up view

--- packages/checkout/widgets-lib/src/views/top-up/TopUpView.tsx
+++ packages/checkout/widgets-lib/src/views/top-up/TopUpView.tsx
@@ -1,16 +1,35 @@
 import {
   Box, Heading, MenuItem,
 } from '@biom3/react';
+import {
+  IMTBLWidgetEvents, RequestBridgeEvent, RequestOnrampEvent, RequestSwapEvent,
+} from '@imtbl/checkout-widgets';
+import { useContext } from 'react';
 import { FooterLogo } from '../../components/Footer/FooterLogo';
 import { HeaderNavigation } from '../../components/Header/HeaderNavigation';
 import { SimpleLayout } from '../../components/SimpleLayout/SimpleLayout';
-import { SharedViews } from '../../context/view-context/ViewContext';
+import { SharedViews, ViewActions, ViewContext } from '../../context/view-context/ViewContext';
 import { text } from '../../resources/text/textConfig';
+import {
+  orchestrationEvents,
+} from '../../lib/orchestrationEvents';
+import { SwapWidgetViews } from '../../context/view-context/SwapViewContextTypes';
+import { BridgeWidgetViews } from '../../context/view-context/BridgeViewContextTypes';
+
+enum CurrentWidget {
+  ONRAMP = 'ONRAMP',
+  SWAP = 'SWAP',
+  BRIDGE = 'BRIDGE',
+}
 
 interface TopUpViewProps {
   showOnrampOption: boolean,
   showSwapOption: boolean,
   showBridgeOption: boolean,
+  currentWidget?: CurrentWidget,
+  onrampEventData?: RequestOnrampEvent,
+  swapEventData?: RequestSwapEvent,
+  bridgeEventData?: RequestBridgeEvent,
   onBackButtonClick?: () => void,
   onCloseButtonClick: () => void,
 }
@@ -19,11 +38,16 @@ export function TopUpView({
   showOnrampOption,
   showSwapOption,
   showBridgeOption,
+  currentWidget,
+  onrampEventData,
+  swapEventData,
+  bridgeEventData,
   onBackButtonClick,
   onCloseButtonClick,
 }: TopUpViewProps) {
   const { header, topUpOptions } = text.views[SharedViews.TOP_UP_VIEW];
   const { card, swap, bridge } = topUpOptions;
+  const { viewDispatch } = useContext(ViewContext);
 
   const renderMenuItem = (
     testId: string,
@@ -79,8 +103,13 @@ export function TopUpView({
             card.caption,
             card.subcaption,
             () => {
-              console.log('fire show onramp widget event');
-              // sendRequestOnrampEvent();
+              if (currentWidget === CurrentWidget.ONRAMP) {
+                // dispatch onramp view
+              }
+              orchestrationEvents.sendRequestOnrampEvent(IMTBLWidgetEvents.IMTBL_WALLET_WIDGET_EVENT, {
+                tokenAddress: onrampEventData?.tokenAddress ?? '',
+                amount: onrampEventData?.amount ?? '',
+              });
             },
           )}
           {showSwapOption && renderMenuItem(
@@ -90,8 +119,20 @@ export function TopUpView({
             swap.caption,
             swap.subcaption,
             () => {
-              console.log('fire show swap widget event');
-              // sendRequestSwapEvent();
+              if (currentWidget === CurrentWidget.SWAP) {
+                viewDispatch({
+                  payload: {
+                    type: ViewActions.UPDATE_VIEW,
+                    view: { type: SwapWidgetViews.SWAP },
+                  },
+                });
+                return;
+              }
+              orchestrationEvents.sendRequestSwapEvent(IMTBLWidgetEvents.IMTBL_WALLET_WIDGET_EVENT, {
+                fromTokenAddress: swapEventData?.fromTokenAddress ?? '',
+                toTokenAddress: swapEventData?.toTokenAddress ?? '',
+                amount: swapEventData?.amount ?? '',
+              });
             },
           )}
           {showBridgeOption && renderMenuItem(
@@ -101,8 +142,19 @@ export function TopUpView({
             bridge.caption,
             bridge.subcaption,
             () => {
-              console.log('fire show bridge widget event');
-              // sendRequestBridgeEvent();
+              if (currentWidget === CurrentWidget.BRIDGE) {
+                viewDispatch({
+                  payload: {
+                    type: ViewActions.UPDATE_VIEW,
+                    view: { type: BridgeWidgetViews.BRIDGE },
+                  },
+                });
+                return;
+              }
+              orchestrationEvents.sendRequestBridgeEvent(IMTBLWidgetEvents.IMTBL_WALLET_WIDGET_EVENT, {
+                tokenAddress: bridgeEventData?.tokenAddress ?? '',
+                amount: bridgeEventData?.amount ?? '',
+              });
             },
           )}
         </Box>
