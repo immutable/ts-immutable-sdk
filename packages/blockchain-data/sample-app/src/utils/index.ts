b584a3d3659a58083843b632d0424dbc9649fce0
Allan Almeida <allan.almeida@immutable.com>
2023-06-05 15:38:04 +1000

    fix lint

--- packages/blockchain-data/sample-app/src/utils/index.ts
+++ packages/blockchain-data/sample-app/src/utils/index.ts
@@ -1,7 +1,3 @@
-export const separator = (url: string) => {
-  return url.includes('?') ? '&' : '?';
-};
+export const separator = (url: string) => (url.includes('?') ? '&' : '?');
 
-export const capitalizeFirstLetter = (string: string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+export const capitalizeFirstLetter = (string: string) => string.charAt(0).toUpperCase() + string.slice(1);
