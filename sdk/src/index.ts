623baf612a113279759a223cf96c9de98532dfa1
Matthew Muscat <officialmatthewmuscat@gmail.com>
2023-06-09 12:36:57 +1000

    rename immutablex_client to immutablex-client for consistency

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,6 +1,6 @@
 export * from './config';
 export * from './blockchain-data';
-export * from './immutablex_client';
+export * from './immutablex-client';
 export * from './passport';
 export * from './orderbook';
 export * from './provider';
