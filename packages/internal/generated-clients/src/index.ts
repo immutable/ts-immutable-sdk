d03d829a54028b6533c039f740b6e67add83947c
Nik Ho <studying.mole@gmail.com>
2023-06-12 17:29:35 +1200

    Export config objects and update README.

--- packages/internal/generated-clients/src/index.ts
+++ packages/internal/generated-clients/src/index.ts
@@ -2,4 +2,10 @@ export * as imx from './imx';
 export * as mr from './multi-rollup';
 export { ImxApiClients } from './imx-api-clients';
 export { MultiRollupApiClients } from './mr-api-clients';
-export { ImmutableAPIConfiguration } from './config';
+export {
+  ImmutableAPIConfiguration,
+  imxApiConfig,
+  multiRollupConfig,
+  MultiRollupAPIConfiguration,
+  createConfig,
+} from './config';
