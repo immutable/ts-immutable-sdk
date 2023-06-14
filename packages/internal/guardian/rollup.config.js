7511454071ab206b2ac3dff08a91481b49500ecc
Hayden Fowler <hayden.fowler@immutable.com>
2023-06-14 11:42:41 +1000

    ID-710 Fixed tests not running in Webstorm

--- packages/internal/guardian/rollup.config.js
+++ packages/internal/guardian/rollup.config.js
@@ -1,5 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
   input: './src/index.ts',
@@ -8,6 +10,8 @@ export default {
   },
   plugins: [
     json(),
+    commonjs(),
+    nodeResolve( { browser: true }),
     typescript({
       exclude: ['**/ABIs/*', '**/*.test.*', '**/utils/testUtils.ts'],
     }),
