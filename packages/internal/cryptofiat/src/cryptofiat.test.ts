611d2c1434d5f28cbe8f1352f175b623257e4041
Andrea Rampin <andrea.rampin@gmail.com>
2023-06-17 17:19:52 +1000

    WT-1421 - moved to checkout proxy for fiat oracle

--- packages/internal/cryptofiat/src/cryptofiat.test.ts
+++ packages/internal/cryptofiat/src/cryptofiat.test.ts
@@ -1,17 +1,34 @@
 import { describe, expect } from '@jest/globals';
 import { CryptoFiatConfiguration } from 'config';
 import axios from 'axios';
+import { Environment } from '@imtbl/config';
 import { CryptoFiat } from './cryptofiat';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const config = new CryptoFiatConfiguration({
+  baseConfig: {
+    environment: Environment.PRODUCTION,
+  },
+});
 
 describe('CryptoFiat', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should fetch symbols and cache them', async () => {
+  // Utility function to test the success case for a given environment
+  const success = async (conf: CryptoFiatConfiguration, base: string) => {
+    const mockedOverridesResponse = {
+      status: 200,
+      data: {
+        eth: 'ethereum',
+        imx: 'immutable-x',
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedOverridesResponse);
+
     const mockedSymbolsResponse = {
       status: 200,
       data: [
@@ -30,27 +47,56 @@ describe('CryptoFiat', () => {
     };
     mockedAxios.get.mockResolvedValue(mockedConversionResponse);
 
-    const config = new CryptoFiatConfiguration({});
-    const cryptoFiat = new CryptoFiat(config);
+    const cryptoFiat = new CryptoFiat(conf);
     await cryptoFiat.convert({ tokenSymbols: ['btc'] });
     await cryptoFiat.convert({ tokenSymbols: ['btc'] });
 
-    expect(mockedAxios.get).toHaveBeenCalledTimes(3);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(4);
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       1,
-      'https://api.coingecko.com/api/v3/coins/list',
+      `${base}/v1/fiat/coins/overrides`,
     );
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       2,
-      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd',
+      `${base}/v1/fiat/coins/all`,
     );
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       3,
-      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd',
+      `${base}/v1/fiat/conversion?ids=bitcoin&currencies=usd`,
     );
-  });
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      4,
+      `${base}/v1/fiat/conversion?ids=bitcoin&currencies=usd`,
+    );
+  };
+
+  it(
+    `should fetch symbols and overrides and cache them [${Environment.SANDBOX}]]`,
+    async () => {
+      const conf = new CryptoFiatConfiguration({
+        baseConfig: {
+          environment: Environment.SANDBOX,
+        },
+      });
+      success(conf, 'https://checkout-api.dev.immutable.com');
+    },
+  );
+
+  it(
+    `should fetch symbols and overrides and cache them [${Environment.PRODUCTION}]]`,
+    async () => success(config, 'https://checkout-api.immutable.com'),
+  );
 
   it('should fetch and convert token symbols to fiat', async () => {
+    const mockedOverridesResponse = {
+      status: 200,
+      data: {
+        eth: 'ethereum',
+        imx: 'immutable-x',
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedOverridesResponse);
+
     const mockedSymbolsResponse = {
       status: 200,
       data: [
@@ -69,29 +115,40 @@ describe('CryptoFiat', () => {
     };
     mockedAxios.get.mockResolvedValueOnce(mockedConversionResponse);
 
-    const config = new CryptoFiatConfiguration({});
     const cryptoFiat = new CryptoFiat(config);
     const result = await cryptoFiat.convert({ tokenSymbols: ['btc', 'eth'] });
 
-    expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(3);
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       1,
-      'https://api.coingecko.com/api/v3/coins/list',
+      'https://checkout-api.immutable.com/v1/fiat/coins/overrides',
     );
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       2,
-      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd',
+      'https://checkout-api.immutable.com/v1/fiat/coins/all',
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      3,
+      'https://checkout-api.immutable.com/v1/fiat/conversion?ids=bitcoin,ethereum&currencies=usd',
     );
     expect(result.btc).toEqual({ usd: 50000 });
     expect(result.eth).toEqual({ usd: 4000 });
   });
 
-  it('should fetch and convert token symbols to fiat using the symbolsOverrides map', async () => {
+  it('should fetch and convert token symbols to fiat using the overrides', async () => {
+    const mockedOverridesResponse = {
+      status: 200,
+      data: {
+        eth: 'ethereum',
+        imx: 'immutable-x',
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedOverridesResponse);
+
     const mockedSymbolsResponse = {
       status: 200,
       data: [
         { id: 'bitcoin', symbol: 'btc', name: 'Bitcoin' },
-        { id: 'ethereum', symbol: 'eth', name: 'Ethereum' },
         { id: 'ethereum-wormhole', symbol: 'eth', name: 'Ethereum' },
         { id: 'usd-coin', symbol: 'usdc', name: 'USDC' },
       ],
@@ -108,24 +165,36 @@ describe('CryptoFiat', () => {
     };
     mockedAxios.get.mockResolvedValueOnce(mockedConversionResponse);
 
-    const config = new CryptoFiatConfiguration({});
     const cryptoFiat = new CryptoFiat(config);
     const result = await cryptoFiat.convert({ tokenSymbols: ['eth', 'usdc'] });
 
-    expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(3);
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       1,
-      'https://api.coingecko.com/api/v3/coins/list',
+      'https://checkout-api.immutable.com/v1/fiat/coins/overrides',
     );
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       2,
-      'https://api.coingecko.com/api/v3/simple/price?ids=ethereum,usd-coin&vs_currencies=usd',
+      'https://checkout-api.immutable.com/v1/fiat/coins/all',
+    );
+    expect(mockedAxios.get).toHaveBeenNthCalledWith(
+      3,
+      'https://checkout-api.immutable.com/v1/fiat/conversion?ids=ethereum,usd-coin&currencies=usd',
     );
     expect(result.usdc).toEqual({ usd: 10000 });
     expect(result.eth).toEqual({ usd: 4000 });
   });
 
   it('should fetch and convert token symbols to fiat symbols', async () => {
+    const mockedOverridesResponse = {
+      status: 200,
+      data: {
+        eth: 'ethereum',
+        imx: 'immutable-x',
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedOverridesResponse);
+
     const mockedSymbolsResponse = {
       status: 200,
       data: [
@@ -144,66 +213,30 @@ describe('CryptoFiat', () => {
     };
     mockedAxios.get.mockResolvedValueOnce(mockedConversionResponse);
 
-    const config = new CryptoFiatConfiguration({});
     const cryptoFiat = new CryptoFiat(config);
     const result = await cryptoFiat.convert({
       tokenSymbols: ['btc', 'eth'],
       fiatSymbols: ['AUD', 'usd'],
     });
 
-    expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(3);
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       1,
-      'https://api.coingecko.com/api/v3/coins/list',
+      'https://checkout-api.immutable.com/v1/fiat/coins/overrides',
     );
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
       2,
-      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=aud,usd',
-    );
-    expect(result.btc).toEqual({ usd: 50000 });
-    expect(result.eth).toEqual({ usd: 4000 });
-  });
-
-  it('should fetch and convert token symbols to fiat with api key', async () => {
-    const mockedSymbolsResponse = {
-      status: 200,
-      data: [
-        { id: 'bitcoin', symbol: 'btc', name: 'Bitcoin' },
-        { id: 'ethereum', symbol: 'eth', name: 'Ethereum' },
-      ],
-    };
-    mockedAxios.get.mockResolvedValueOnce(mockedSymbolsResponse);
-
-    const mockedConversionResponse = {
-      status: 200,
-      data: {
-        bitcoin: { usd: 50000 },
-        ethereum: { usd: 4000 },
-      },
-    };
-    mockedAxios.get.mockResolvedValueOnce(mockedConversionResponse);
-
-    const apiKey = 'fake-api';
-    const config = new CryptoFiatConfiguration({ apiKey });
-    const cryptoFiat = new CryptoFiat(config);
-    const result = await cryptoFiat.convert({ tokenSymbols: ['btc', 'eth'] });
-
-    expect(mockedAxios.get).toHaveBeenCalledTimes(2);
-    expect(mockedAxios.get).toHaveBeenNthCalledWith(
-      1,
-      'https://pro-api.coingecko.com/api/v3/coins/list?x_cg_pro_api_key=fake-api',
+      'https://checkout-api.immutable.com/v1/fiat/coins/all',
     );
     expect(mockedAxios.get).toHaveBeenNthCalledWith(
-      2,
-      // eslint-disable-next-line max-len
-      'https://pro-api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd&x_cg_pro_api_key=fake-api',
+      3,
+      'https://checkout-api.immutable.com/v1/fiat/conversion?ids=bitcoin,ethereum&currencies=aud,usd',
     );
     expect(result.btc).toEqual({ usd: 50000 });
     expect(result.eth).toEqual({ usd: 4000 });
   });
 
   it('should throw an error when tokenSymbols is empty or not provided', async () => {
-    const config = new CryptoFiatConfiguration({});
     const cryptoFiat = new CryptoFiat(config);
 
     await expect(cryptoFiat.convert({ tokenSymbols: [] })).rejects.toThrow(
@@ -211,21 +244,51 @@ describe('CryptoFiat', () => {
     );
   });
 
-  it('should throw an error when fetching symbols fails', async () => {
+  it('should throw an error when fetching overrides fails', async () => {
     const mockedSymbolsResponse = {
       status: 400,
       statusText: 'Bad Request',
     };
     mockedAxios.get.mockResolvedValueOnce(mockedSymbolsResponse);
 
-    const config = new CryptoFiatConfiguration({});
     const cryptoFiat = new CryptoFiat(config);
     await expect(
       cryptoFiat.convert({ tokenSymbols: ['btc', 'eth'] }),
-    ).rejects.toThrow('Error fetching coin list: 400 Bad Request');
+    ).rejects.toThrow('Error fetching coins overrides: 400 Bad Request');
+  });
+
+  it('should throw an error when fetching symbols fails', async () => {
+    const mockedOverridesResponse = {
+      status: 200,
+      data: {
+        eth: 'ethereum',
+        imx: 'immutable-x',
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedOverridesResponse);
+
+    const mockedSymbolsResponse = {
+      status: 400,
+      statusText: 'Bad Request',
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedSymbolsResponse);
+
+    const cryptoFiat = new CryptoFiat(config);
+    await expect(
+      cryptoFiat.convert({ tokenSymbols: ['btc', 'eth'] }),
+    ).rejects.toThrow('Error fetching coins list: 400 Bad Request');
   });
 
   it('should throw an error when fetching prices fails', async () => {
+    const mockedOverridesResponse = {
+      status: 200,
+      data: {
+        eth: 'ethereum',
+        imx: 'immutable-x',
+      },
+    };
+    mockedAxios.get.mockResolvedValueOnce(mockedOverridesResponse);
+
     const mockedSymbolsResponse = {
       status: 200,
       data: [
@@ -241,7 +304,6 @@ describe('CryptoFiat', () => {
     };
     mockedAxios.get.mockResolvedValueOnce(mockedConversionResponse);
 
-    const config = new CryptoFiatConfiguration({});
     const cryptoFiat = new CryptoFiat(config);
     await expect(
       cryptoFiat.convert({ tokenSymbols: ['btc', 'eth'] }),
