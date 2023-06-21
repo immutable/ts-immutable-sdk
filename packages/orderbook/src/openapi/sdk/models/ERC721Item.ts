e4dfed032953d180e7ba3099b5fa274dcbf6efd1
Kenley Bastari <revrost@gmail.com>
2023-06-21 12:22:18 +1000

    [NO-CHANGELOG] TD-637: SDK update for unifying buy and sell item in orderbook MR (#454)
    Signed-off-by: Frank Li <b439988l@gmail.com>
    Co-authored-by: Frank Li <b439988l@gmail.com>

---
+++ packages/orderbook/src/openapi/sdk/models/ERC721Item.ts
@@ -0,0 +1,19 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type ERC721Item = {
+  /**
+   * Token type user is offering, which in this case is ERC721
+   */
+  item_type: 'ERC721';
+  /**
+   * Address of ERC721 token
+   */
+  contract_address: string;
+  /**
+   * ID of ERC721 token
+   */
+  token_id: string;
+};
+
