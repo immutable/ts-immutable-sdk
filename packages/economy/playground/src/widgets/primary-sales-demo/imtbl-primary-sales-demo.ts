9d3d0188b25a5dc549ba4d21bcd8aff1963764c6
Mimi Immutable <mimi.tran@immutable.com>
2023-06-26 16:10:50 +1000

    wip - add signTypedData generation

--- packages/economy/playground/src/widgets/primary-sales-demo/imtbl-primary-sales-demo.ts
+++ packages/economy/playground/src/widgets/primary-sales-demo/imtbl-primary-sales-demo.ts
@@ -1,8 +1,74 @@
-import { LitElement, css, html } from 'lit';
+import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { ERC20__factory } from '@imtbl/contracts/';
 import { TransactionRequest } from '@ethersproject/providers';
 import { Checkout, WalletProviderName } from '@imtbl/checkout-sdk';
+
+// mocked eth_signTypedData_v4 parameters. All of these parameters affect the resulting signature.
+const mockMsgDataParams = {
+  domain: {
+    // This defines the network, in this case, Mainnet.
+    chainId: 1,
+    // Give a user-friendly name to the specific contract you're signing for.
+    name: 'Ether Mail',
+    // Add a verifying contract to make sure you're establishing contracts with the proper entity.
+    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
+    // This identifies the latest version.
+    version: '1',
+  },
+
+  // This defines the message you're proposing the user to sign, is dapp-specific, and contains
+  // anything you want. There are no required fields. Be as explicit as possible when building out
+  // the message schema.
+  message: {
+    contents: 'Hello, Bob!',
+    attachedMoneyInEth: 4.2,
+    from: {
+      name: 'Cow',
+      wallets: [
+        '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+        '0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF',
+      ],
+    },
+    to: [
+      {
+        name: 'Bob',
+        wallets: [
+          '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+          '0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57',
+          '0xB0B0b0b0b0b0B000000000000000000000000000',
+        ],
+      },
+    ],
+  },
+  // This refers to the keys of the following types object.
+  primaryType: 'Mail',
+  types: {
+    // This refers to the domain the contract is hosted on.
+    EIP712Domain: [
+      { name: 'name', type: 'string' },
+      { name: 'version', type: 'string' },
+      { name: 'chainId', type: 'uint256' },
+      { name: 'verifyingContract', type: 'address' },
+    ],
+    // Not an EIP712Domain definition.
+    Group: [
+      { name: 'name', type: 'string' },
+      { name: 'members', type: 'Person[]' },
+    ],
+    // Refer to primaryType.
+    Mail: [
+      { name: 'from', type: 'Person' },
+      { name: 'to', type: 'Person[]' },
+      { name: 'contents', type: 'string' },
+    ],
+    // Not an EIP712Domain definition.
+    Person: [
+      { name: 'name', type: 'string' },
+      { name: 'wallets', type: 'address[]' },
+    ],
+  },
+};
 
 @customElement('imtbl-primary-sales-demo')
 export class PrimarySalesDemo extends LitElement {
@@ -10,11 +76,10 @@ export class PrimarySalesDemo extends LitElement {
 
   private provider: any = undefined;
 
+  private signTypedData: any = undefined;
+
   @property({ type: String, attribute: 'wallet-address' })
   connectedAddress: string | undefined = undefined;
-
-  @property({ type: String, attribute: 'sign-typed-data' })
-  signTypedData: string | undefined = undefined;
 
   constructor() {
     super();
@@ -76,12 +141,6 @@ export class PrimarySalesDemo extends LitElement {
   }
 
   async success() {
-    // const checkout = new Checkout();
-
-    // const resp = await checkout.createProvider({
-    //   walletProvider: WalletProviderName.METAMASK,
-    // });
-
     if (this.checkoutSDK) {
       const res = await this.checkoutSDK.checkIsWalletConnected({
         provider: this.provider,
@@ -198,8 +257,52 @@ export class PrimarySalesDemo extends LitElement {
     // });
   }
 
+  async generateSignTypedData() {
+    console.log('generating sign typed data');
+    const msgParams = JSON.stringify(mockMsgDataParams);
+
+    const params = [this.connectedAddress, msgParams];
+    const method = 'eth_signTypedData_v4';
+
+    // web3.currentProvider.sendAsync(
+    //   {
+    //     method,
+    //     params,
+    //     from: from[0],
+    //   },
+    //   function (err, result) {
+    //     if (err) return console.dir(err);
+    //     if (result.error) {
+    //       alert(result.error.message);
+    //     }
+    //     if (result.error) return console.error('ERROR', result);
+    //     console.log('TYPED SIGNED:' + JSON.stringify(result.result));
+
+    //     const recovered = sigUtil.recoverTypedSignature_v4({
+    //       data: JSON.parse(msgParams),
+    //       sig: result.result,
+    //     });
+
+    //     if (
+    //       ethUtil.toChecksumAddress(recovered) === ethUtil.toChecksumAddress(from)
+    //     ) {
+    //       alert('Successfully recovered signer as ' + from);
+    //     } else {
+    //       alert(
+    //         'Failed to verify signer when comparing ' + result + ' to ' + from
+    //       );
+    //     }
+    //   }
+    // );
+  }
+
+  async onInitiateClick() {
+    await this.generateSignTypedData();
+  }
+
   render() {
-    return html` <div class="prose mb-4">
+    return html`
+      <div class="prose mb-4">
         <h1>Primary Sales Demo</h1>
       </div>
 
@@ -218,7 +321,17 @@ export class PrimarySalesDemo extends LitElement {
           theme="dark"
           environment="sandbox"
         ></imtbl-connect>
-      </div>`;
+
+        <div class="ml-4">
+          <button
+            class="btn btn-wide btn-primary"
+            @click="${this.onInitiateClick}"
+          >
+            Initiate
+          </button>
+        </div>
+      </div>
+    `;
   }
 
   protected createRenderRoot(): Element | ShadowRoot {
