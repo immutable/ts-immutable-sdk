318e49ea2445986d6e9d44c1354cf5ee2891aea9
Deepti Luthra <deepti.luthra@immutable.com>
2023-06-26 14:08:19 +1000

    fix validate-provider tests

--- packages/checkout/sdk/src/provider/validateProvider.test.ts
+++ packages/checkout/sdk/src/provider/validateProvider.test.ts
@@ -48,10 +48,16 @@ describe('provider validation', () => {
       (RemoteConfigFetcher as jest.Mock).mockReturnValue({
         get: jest.fn().mockResolvedValue([
           {
+            chainId: 1,
+          },
+          {
             chainId: 11155111,
           },
           {
-            chainId: 13383,
+            chainId: 13372,
+          },
+          {
+            chainId: 13373,
           },
         ]),
       });
@@ -61,16 +67,16 @@ describe('provider validation', () => {
       });
     });
 
-    // it('should not throw an error when valid web3provider', async () => {
-    //   requestMock.mockResolvedValue('0x1');
-    //   const testWeb3Provider = new Web3Provider(
-    //     underlyingProviderMock,
-    //     ChainId.ETHEREUM,
-    //   );
-    //   expect(await validateProvider(testCheckoutConfig, testWeb3Provider)).toBe(
-    //     testWeb3Provider,
-    //   );
-    // });
+    it('should not throw an error when valid web3provider', async () => {
+      requestMock.mockResolvedValue('0x1');
+      const testWeb3Provider = new Web3Provider(
+        underlyingProviderMock,
+        ChainId.ETHEREUM,
+      );
+      expect(await validateProvider(testCheckoutConfig, testWeb3Provider)).toBe(
+        testWeb3Provider,
+      );
+    });
 
     it('should throw an error if the underlying provider is not on the same network as the Web3Provider', async () => {
       requestMock.mockResolvedValue('0x1');
@@ -85,62 +91,62 @@ describe('provider validation', () => {
       );
     });
 
-    // it('should not throw an error if allowMistmatchedChainId is true and underlying network different', async () => {
-    //   requestMock.mockResolvedValue('0x1');
-    //   const testWeb3Provider = new Web3Provider(
-    //     underlyingProviderMock,
-    //     ChainId.IMTBL_ZKEVM_TESTNET,
-    //   );
-    //   const validationOverrides = {
-    //     allowMistmatchedChainId: true,
-    //     allowUnsupportedProvider: false,
-    //   };
-    //   expect(
-    //     await validateProvider(
-    //       testCheckoutConfig,
-    //       testWeb3Provider,
-    //       validationOverrides,
-    //     ),
-    //   ).toBe(testWeb3Provider);
-    // });
+    it('should not throw an error if allowMistmatchedChainId is true and underlying network different', async () => {
+      requestMock.mockResolvedValue('0x1');
+      const testWeb3Provider = new Web3Provider(
+        underlyingProviderMock,
+        ChainId.IMTBL_ZKEVM_TESTNET,
+      );
+      const validationOverrides = {
+        allowMistmatchedChainId: true,
+        allowUnsupportedProvider: false,
+      };
+      expect(
+        await validateProvider(
+          testCheckoutConfig,
+          testWeb3Provider,
+          validationOverrides,
+        ),
+      ).toBe(testWeb3Provider);
+    });
 
-    // it('should throw an error if the underlying provider is on an unsupported network', async () => {
-    //   requestMock.mockResolvedValue('0xfa');
-    //   const testWeb3Provider = new Web3Provider(
-    //     underlyingProviderMock,
-    //     ChainId.ETHEREUM,
-    //   );
-    //   const validationOverrides = {
-    //     allowMistmatchedChainId: true,
-    //     allowUnsupportedProvider: false,
-    //   };
-    //   await expect(
-    //     validateProvider(
-    //       testCheckoutConfig,
-    //       testWeb3Provider,
-    //       validationOverrides,
-    //     ),
-    //   ).rejects.toThrowError(
-    //     // eslint-disable-next-line max-len
-    //     '[WEB3_PROVIDER_ERROR] Cause:Your wallet is connected to an unsupported network, please switch to a supported network',
-    //   );
-    // });
+    it('should throw an error if the underlying provider is on an unsupported network', async () => {
+      requestMock.mockResolvedValue('0xfa');
+      const testWeb3Provider = new Web3Provider(
+        underlyingProviderMock,
+        ChainId.ETHEREUM,
+      );
+      const validationOverrides = {
+        allowMistmatchedChainId: true,
+        allowUnsupportedProvider: false,
+      };
+      await expect(
+        validateProvider(
+          testCheckoutConfig,
+          testWeb3Provider,
+          validationOverrides,
+        ),
+      ).rejects.toThrowError(
+        // eslint-disable-next-line max-len
+        '[WEB3_PROVIDER_ERROR] Cause:Your wallet is connected to an unsupported network, please switch to a supported network',
+      );
+    });
 
-    // it('should not throw an error if allowUnsupportedProvider true and underlying network is unsupported', async () => {
-    //   // HEX 0xfa -> 250 , underlying provider matches Web3Provider, but unsupported chain
-    //   requestMock.mockResolvedValue('0xfa');
-    //   const testWeb3Provider = new Web3Provider(underlyingProviderMock, 250);
-    //   const validationOverrides = {
-    //     allowMistmatchedChainId: false,
-    //     allowUnsupportedProvider: true,
-    //   };
-    //   expect(
-    //     await validateProvider(
-    //       testCheckoutConfig,
-    //       testWeb3Provider,
-    //       validationOverrides,
-    //     ),
-    //   ).toBe(testWeb3Provider);
-    // });
+    it('should not throw an error if allowUnsupportedProvider true and underlying network is unsupported', async () => {
+      // HEX 0xfa -> 250 , underlying provider matches Web3Provider, but unsupported chain
+      requestMock.mockResolvedValue('0xfa');
+      const testWeb3Provider = new Web3Provider(underlyingProviderMock, 250);
+      const validationOverrides = {
+        allowMistmatchedChainId: false,
+        allowUnsupportedProvider: true,
+      };
+      expect(
+        await validateProvider(
+          testCheckoutConfig,
+          testWeb3Provider,
+          validationOverrides,
+        ),
+      ).toBe(testWeb3Provider);
+    });
   });
 });
