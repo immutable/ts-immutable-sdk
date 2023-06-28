d4d10582817a96da7a6545bd1d139a141a61a199
Mikhala <122326421+imx-mikhala@users.noreply.github.com>
2023-06-28 14:05:24 +0800

    WT-1434 Show loading spinner on balances until fetched NO-CHANGELOG (#490)

--- packages/checkout/widgets-lib/src/lib/testUtils.ts
+++ packages/checkout/widgets-lib/src/lib/testUtils.ts
@@ -28,3 +28,35 @@ export const cyGetByTestId = (
 export const cySmartGet = (selector: string) => (selector.includes('@') || selector.includes(' ')
   ? cy.get(selector)
   : cyGetByTestId(selector));
+
+export const cyIntercept = (overrides?: {
+  configOverrides?: {},
+  cryptoFiatOverrides?: {
+    coins?: any[],
+    conversion?: any[],
+  },
+}) => {
+  const checkoutApi = 'https://checkout-api.dev.immutable.com/v1';
+  cy.intercept(
+    `${checkoutApi}/config`,
+    overrides?.configOverrides
+    || {
+      allowedNetworks: [
+        {
+          chainId: 11155111,
+        },
+        {
+          chainId: 13383,
+        },
+      ],
+    },
+  );
+  cy.intercept(
+    `${checkoutApi}/fiat/coins/*`,
+    overrides?.cryptoFiatOverrides?.coins || [],
+  );
+  cy.intercept(
+    `${checkoutApi}/fiat/conversion*`,
+    overrides?.cryptoFiatOverrides?.conversion || [],
+  );
+};
