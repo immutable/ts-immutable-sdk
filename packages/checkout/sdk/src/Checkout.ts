07788bc57a1df438190aa26ebb317b4ac05b7fa4
proletesseract <craig.b.macgregor@gmail.com>
2023-06-29 14:38:54 +1200

    checkout.ts done

--- packages/checkout/sdk/src/Checkout.ts
+++ packages/checkout/sdk/src/Checkout.ts
@@ -49,16 +49,8 @@ export class Checkout {
   private readOnlyProviders: Map<ChainId, ethers.providers.JsonRpcProvider>;
 
   /**
- * This is a constructor function that initializes a CheckoutModuleConfiguration object and a Map of
- * JsonRpcProviders.
- * @param {CheckoutModuleConfiguration} config - The `config` parameter is an optional argument of type
- * `CheckoutModuleConfiguration` that is used to configure the checkout module. If no configuration is
- * provided, the module will use the default `SANDBOX_CONFIGURATION`.
- */
-  /**
-   * Constructs a new instance of the CheckoutModule class with the given configuration.
+   * Constructs a new instance of the CheckoutModule class.
    * @param {CheckoutModuleConfiguration} [config=SANDBOX_CONFIGURATION] - The configuration object for the CheckoutModule.
-   * @returns None
    */
   constructor(config: CheckoutModuleConfiguration = SANDBOX_CONFIGURATION) {
     this.config = new CheckoutConfiguration(config);
@@ -69,13 +61,10 @@ export class Checkout {
   }
 
   /**
- * This function creates a web3 provider using a wallet provider and returns it as a promise.
- * @param {CreateProviderParams} params - The `params` parameter is of type `CreateProviderParams` and
- * is an object that contains the following properties:
- * @returns The `createProvider` function is returning a Promise that resolves to an object with a
- * `provider` property, which is a `Web3Provider` object created using the `walletProvider` parameter
- * passed to the function.
- */
+   * Creates a provider using the given parameters.
+   * @param {CreateProviderParams} params - The parameters for creating the provider.
+   * @returns {Promise<CreateProviderResult>} A promise that resolves to the created provider.
+   */
   public async createProvider(
     params: CreateProviderParams,
   ): Promise<CreateProviderResult> {
@@ -88,13 +77,9 @@ export class Checkout {
   }
 
   /**
-   * This function checks if a wallet is connected to a web3 provider.
-   * @param {CheckConnectionParams} params - CheckConnectionParams is a type of object that contains
-   * information about the provider being used to connect to a wallet. It may include properties such
-   * as the provider URL, network ID, and other relevant details.
-   * @returns The function `checkIsWalletConnected` is returning a Promise that resolves to a
-   * `CheckConnectionResult` object. The `CheckConnectionResult` type is not defined in the code
-   * snippet provided, so it is unclear what properties or values it contains.
+   * Checks if a wallet is connected to the specified provider.
+   * @param {CheckConnectionParams} params - The parameters for checking the wallet connection.
+   * @returns {Promise<CheckConnectionResult>} - A promise that resolves to the result of the check.
    */
   public async checkIsWalletConnected(
     params: CheckConnectionParams,
@@ -108,14 +93,11 @@ export class Checkout {
   }
 
   /**
-  * This function connects to a web3 provider and returns the provider and network information.
-  * @param {ConnectParams} params - The `params` parameter is an object that contains information
-  * needed to connect to a blockchain network. It may include the provider URL, account information,
-  * and other relevant details.
-  * @returns An object with two properties: "provider" and "network". The "provider" property contains
-  * the validated web3 provider, and the "network" property contains information about the connected
-  * network.
-  */
+   * Connects to a blockchain network using the specified provider.
+   * @param {ConnectParams} params - The parameters for connecting to the network.
+   * @returns {Promise<ConnectResult>} A promise that resolves to an object containing the provider and network information.
+   * @throws {Error} If the provider is not valid or if there is an error connecting to the network.
+   */
   public async connect(params: ConnectParams): Promise<ConnectResult> {
     const web3Provider = await provider.validateProvider(
       this.config,
@@ -132,12 +114,10 @@ export class Checkout {
   }
 
   /**
- * This function switches the network of a wallet using a given provider and chain ID.
- * @param {SwitchNetworkParams} params - The `params` parameter is an object that contains the
- * following properties:
- * @returns The function `switchNetwork` returns a Promise that resolves to a `SwitchNetworkResult`
- * object.
- */
+   * Switches the network for the current wallet provider.
+   * @param {SwitchNetworkParams} params - The parameters for switching the network.
+   * @returns {Promise<SwitchNetworkResult>} - A promise that resolves to the result of switching the network.
+   */
   public async switchNetwork(
     params: SwitchNetworkParams,
   ): Promise<SwitchNetworkResult> {
@@ -160,12 +140,9 @@ export class Checkout {
   }
 
   /**
-   * This function retrieves the balance of a wallet address for either Ether or an ERC20 token.
-   * @param {GetBalanceParams} params - The `params` parameter is an object that contains the following
-   * properties:
-   * @returns The function `getBalance` returns a Promise that resolves to a `GetBalanceResult` object.
-   * The contents of this object depend on the execution of the function, which can either return the
-   * balance of a wallet address or the balance of an ERC20 token held by a wallet address.
+   * Retrieves the balance of a wallet address.
+   * @param {GetBalanceParams} params - The parameters for retrieving the balance.
+   * @returns {Promise<GetBalanceResult>} - A promise that resolves to the balance result.
    */
   public async getBalance(params: GetBalanceParams): Promise<GetBalanceResult> {
     const web3Provider = await provider.validateProvider(
@@ -188,13 +165,10 @@ export class Checkout {
   }
 
   /**
-  * This function retrieves all balances for a given wallet address on a specified chain using a
-  * validated web3 provider.
-  * @param {GetAllBalancesParams} params - GetAllBalancesParams object containing the following
-  * properties:
-  * @returns The `getAllBalances` function is being called with the provided parameters, and the result
-  * of that function call is being returned as a Promise with the type `GetAllBalancesResult`.
-  */
+   * Retrieves the balances of all tokens for a given wallet address on a specific chain.
+   * @param {GetAllBalancesParams} params - The parameters for retrieving the balances.
+   * @returns {Promise<GetAllBalancesResult>} - A promise that resolves to the result of retrieving the balances.
+   */
   public async getAllBalances(
     params: GetAllBalancesParams,
   ): Promise<GetAllBalancesResult> {
@@ -212,14 +186,9 @@ export class Checkout {
   }
 
   /**
-   * This function retrieves the network allow list using the provided parameters.
-   * @param {GetNetworkAllowListParams} params - GetNetworkAllowListParams is a type of object that
-   * contains the parameters required for the getNetworkAllowList function. The specific properties of
-   * this object depend on the implementation of the function, but typically include information such
-   * as network configuration settings and filters for the returned data.
-   * @returns A promise that resolves to a `GetNetworkAllowListResult` object. The result is obtained
-   * by calling the `network.getNetworkAllowList` function with the provided `params` and `config`
-   * properties of the current object.
+   * Retrieves the supported networks based on the provided parameters.
+   * @param {GetNetworkAllowListParams} params - The parameters for retrieving the network allow list.
+   * @returns {Promise<GetNetworkAllowListResult>} - A promise that resolves to the network allow list result.
    */
   public async getNetworkAllowList(
     params: GetNetworkAllowListParams,
@@ -228,14 +197,9 @@ export class Checkout {
   }
 
   /**
-   * This function returns a promise that resolves to the result of calling the `getTokenAllowList`
-   * function with the provided parameters.
-   * @param {GetTokenAllowListParams} params - The `params` parameter is an object that contains the
-   * necessary information to retrieve the token allow list. The specific properties of this object
-   * will depend on the implementation of the `getTokenAllowList` method.
-   * @returns The `getTokenAllowList` method is returning a Promise that resolves to a
-   * `GetTokenAllowListResult` object. The `tokens.getTokenAllowList(params)` method is being called
-   * with the `params` argument and its result is being returned.
+   * Retrieves the supported tokens based on the provided parameters.
+   * @param {GetTokenAllowListParams} params - The parameters for retrieving the token allow list.
+   * @returns {Promise<GetTokenAllowListResult>} - A promise that resolves to the token allow list result.
    */
   public async getTokenAllowList(
     params: GetTokenAllowListParams,
@@ -244,15 +208,9 @@ export class Checkout {
   }
 
   /**
-   * This function retrieves the wallet allow list using the parameters provided.
-   * @param {GetWalletAllowListParams} params - GetWalletAllowListParams is a type of object that
-   * contains the parameters required for the getWalletAllowList function. The specific properties of
-   * this object depend on the implementation of the function, but typically it would include things
-   * like filters or pagination options for retrieving a list of wallet addresses.
-   * @returns The `getWalletAllowList` function is returning a Promise that resolves to a
-   * `GetWalletAllowListResult` object. The `GetWalletAllowListResult` type is not shown in the code
-   * snippet, but it is likely an interface or type that defines the structure of the data that will be
-   * returned.
+   * Retrieves the default supported wallets based on the provided parameters.
+   * @param {GetWalletAllowListParams} params - The parameters for retrieving the wallet allow list.
+   * @returns {Promise<GetWalletAllowListResult>} - A promise that resolves to the wallet allow list result.
    */
   public async getWalletAllowList(
     params: GetWalletAllowListParams,
@@ -261,12 +219,10 @@ export class Checkout {
   }
 
   /**
-  * This function sends a transaction using a validated web3 provider and returns the result.
-  * @param {SendTransactionParams} params - The `params` parameter is an object of type
-  * `SendTransactionParams` which contains the following properties:
-  * @returns The `sendTransaction` method is returning a `Promise` that resolves to a
-  * `SendTransactionResult` object.
-  */
+   * Sends a transaction using the specified provider and transaction parameters.
+   * @param {SendTransactionParams} params - The parameters for sending the transaction.
+   * @returns {Promise<SendTransactionResult>} A promise that resolves to the result of the transaction.
+   */
   public async sendTransaction(
     params: SendTransactionParams,
   ): Promise<SendTransactionResult> {
@@ -278,12 +234,9 @@ export class Checkout {
   }
 
   /**
-   * This function retrieves network information using a validated web3 provider.
-   * @param {GetNetworkParams} params - The `params` parameter is an object that contains the following
-   * properties:
-   * @returns The `getNetworkInfo` function is returning a Promise that resolves to a `NetworkInfo`
-   * object. The `NetworkInfo` object likely contains information about the current network, such as
-   * the network ID, name, and other relevant details.
+   * Retrieves network information using the specified provider.
+   * @param {GetNetworkParams} params - The parameters for retrieving network information.
+   * @returns {Promise<NetworkInfo>} A promise that resolves to the network information.
    */
   public async getNetworkInfo(params: GetNetworkParams): Promise<NetworkInfo> {
     const web3Provider = await provider.validateProvider(
@@ -298,28 +251,18 @@ export class Checkout {
   }
 
   /**
-   * This function checks if a given object is a Web3 provider.
-   * @param {Web3Provider} web3Provider - The web3Provider parameter is an object that represents a
-   * provider for interacting with a blockchain network using the Web3.js library. It can be used to
-   * send transactions, read data from the blockchain, and interact with smart contracts.
-   * @returns The function `isWeb3Provider` is being returned, which takes a `web3Provider` parameter
-   * of type `Web3Provider` and checks if it is a valid web3 provider by calling the `isWeb3Provider`
-   * method of the `provider` object. The return value of this function is a boolean value indicating
-   * whether the `web3Provider` is a valid web3 provider or
+   * Checks if the given object is a Web3 provider.
+   * @param {Web3Provider} web3Provider - The object to check.
+   * @returns {boolean} - True if the object is a Web3 provider, false otherwise.
    */
   static isWeb3Provider(web3Provider: Web3Provider) {
     return provider.isWeb3Provider(web3Provider);
   }
 
   /**
-   * This function estimates gas for a swap or bridge transaction using read-only providers and a gas
-   * estimator service.
-   * @param {GasEstimateParams} params - The `params` parameter is an object that contains the
-   * necessary information to estimate the gas cost of a transaction. The specific properties of this
-   * object depend on whether the function is estimating gas for a swap or a bridge to Layer 2
-   * transaction. The `GasEstimateSwapResult` and `GasEst
-   * @returns a Promise that resolves to either a `GasEstimateSwapResult` or a
-   * `GasEstimateBridgeToL2Result`.
+   * Estimates the gas required for a swap or bridge transaction.
+   * @param {GasEstimateParams} params - The parameters for the gas estimation.
+   * @returns {Promise<GasEstimateSwapResult | GasEstimateBridgeToL2Result>} - A promise that resolves to the gas estimation result.
    */
   public async gasEstimate(
     params: GasEstimateParams,
