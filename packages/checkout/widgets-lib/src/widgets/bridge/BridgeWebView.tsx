907b4e56ef4dd7fb43b39c26689f5d8100f58bdc
proletesseract <craig.b.macgregor@gmail.com>
2023-06-29 12:32:55 +1200

    widgets-lib switched back to using the web-components

--- packages/checkout/widgets-lib/src/widgets/bridge/BridgeWebView.tsx
+++ packages/checkout/widgets-lib/src/widgets/bridge/BridgeWebView.tsx
@@ -1,10 +1,4 @@
 import { Environment } from '@imtbl/config';
-import { Checkout, WalletProviderName } from '@imtbl/checkout-sdk';
-import {
-  CheckoutWidgets, BridgeReact,
-} from '@imtbl/checkout-widgets';
-import { useEffect, useState } from 'react';
-import { Web3Provider } from '@ethersproject/providers';
 import { WidgetTheme } from '../../lib';
 
 function BridgeWebView() {
@@ -13,23 +7,8 @@ function BridgeWebView() {
     environment: Environment.SANDBOX,
   };
 
-  const [provider, setProvider] = useState<Web3Provider>();
-
-  CheckoutWidgets(config);
-
-  const checkout = new Checkout({ baseConfig: { environment: Environment.SANDBOX } });
-
-  useEffect(() => {
-    (async () => {
-      if (!provider) {
-        const createProviderRes = await checkout.createProvider({ walletProvider: WalletProviderName.METAMASK });
-        setProvider(createProviderRes.provider);
-      }
-    })();
-  });
-
   return (
-    <BridgeReact provider={provider} />
+    <imtbl-bridge widgetConfig={JSON.stringify(config)} walletProvider="metamask" />
   );
 }
 
