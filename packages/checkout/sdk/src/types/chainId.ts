c21150da35679054ff27170b9b395209041bdbd4
Deepti Luthra <deepti.luthra@immutable.com>
2023-06-30 16:44:18 +1000

    Remove token-master json and update network details

--- packages/checkout/sdk/src/types/chainId.ts
+++ packages/checkout/sdk/src/types/chainId.ts
@@ -10,5 +10,5 @@ export enum ChainId {
   ETHEREUM = 1,
   IMTBL_ZKEVM_TESTNET = 13372, // to be used in prod config for testnet launch
   SEPOLIA = 11155111,
-  IMTBL_ZKEVM_DEVNET = 13383,
+  IMTBL_ZKEVM_DEVNET = 13393,
 }
