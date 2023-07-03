770f142fd9230c17bbf44233f5ccc1bb69d5cf25
Chris James <chris@webglowit.net>
2023-07-03 10:38:48 +1000

    support global exports

--- sdk/scripts/generateIndex.js
+++ sdk/scripts/generateIndex.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // Read the JSON file
 const fileData = fs.readFileSync(
-  path.join(__dirname, '..', 'module-release.json')
+  path.join(dirname, '..', 'module-release.json'),
 );
 const moduleData = JSON.parse(fileData);
 
@@ -14,23 +14,31 @@ const releaseType = process.env.RELEASE_TYPE || 'alpha';
 
 // Generate the index.ts file contents based on the release type
 let indexFileContent = '';
-for (const moduleName in moduleData.modules) {
+moduleData.modules.forEach((moduleName) => {
   const moduleReleaseType = moduleData.modules[moduleName];
 
   if (
-    releaseType === 'alpha' ||
-    (releaseType !== 'alpha' && moduleReleaseType === 'prod')
+    releaseType === 'alpha'
+    || (releaseType !== 'alpha' && moduleReleaseType === 'prod')
   ) {
+    // Split underscores and capitalize each word from the second
+    const moduleNameCapitalized = moduleName.split('_')
+      .map((part, index) => {
+        if (index === 0) {
+          return part;
+        }
+        return part.charAt(0).toUpperCase() + part.slice(1);
+      })
+      .join('');
+
     const modulePath = `./${moduleName}`;
-    const exportStatement = `export * from '${modulePath}';`;
-    indexFileContent += exportStatement + '\n';
+    const exportStatement = `export * as ${moduleNameCapitalized} from '${modulePath}';\n`;
+    indexFileContent += exportStatement;
   }
-}
+});
 
 // Write the index.ts file
 fs.writeFileSync(
   path.join(__dirname, '..', 'src', 'index.ts'),
-  indexFileContent
+  indexFileContent,
 );
-
-console.log('index.ts file generated successfully!');
