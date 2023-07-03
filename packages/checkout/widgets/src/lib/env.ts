70fa80146e41e1fd327252c5440d6d37bd6182aa
Andrea Rampin <andrea.rampin@gmail.com>
2023-07-03 15:14:10 +1000

    wt-1432 fixed CHECKOUT_DEV_MODE value

--- packages/checkout/widgets/src/lib/env.ts
+++ packages/checkout/widgets/src/lib/env.ts
@@ -1,6 +1,6 @@
 export const SDK_VERSION_MARKER = '__SDK_VERSION__';
 
-export const isDevMode = () => process.env.CHECKOUT_ENVIRONMENT === 'local';
+export const isDevMode = () => process.env.CHECKOUT_DEV_MODE !== undefined;
 
 // This SDK version is replaced by the `yarn build` command ran on the root level
 export const globalPackageVersion = () => SDK_VERSION_MARKER;
