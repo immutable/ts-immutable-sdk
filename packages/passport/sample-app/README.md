02f2a83a4b6c67fc0c3ee0130583896f9577eec6
Hayden Fowler <hayden.fowler@immutable.com>
2023-07-04 11:24:59 +1000

    ID-818 Migrated Passport sample app

---
+++ packages/passport/sample-app/README.md
@@ -0,0 +1,10 @@
+# Passport Sample Application
+
+## Running Locally
+
+```bash
+yarn install
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ packages/passport/sample-app/src/components/Environment.tsx
@@ -0,0 +1,48 @@
+import { Heading } from "@biom3/react";
+import React from "react";
+import { Card, Stack, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import { EnvironmentNames, EnvironmentPropsType } from "@/types";
+import { useImmutableProvider } from "@/context/ImmutableProvider";
+
+function Environment({ disabled }: EnvironmentPropsType) {
+  const { environment, setEnvironment } = useImmutableProvider();
+  const onClick = (name: EnvironmentNames) => {
+    if (setEnvironment) {
+      setEnvironment(name);
+    }
+  };
+
+  return (
+    <Card>
+      <Stack direction="horizontal" gap={3}>
+        <Card.Title>
+          <Heading size="small">Environment</Heading>
+        </Card.Title>
+        <Card.Body>
+          <ToggleButtonGroup
+            name="environment"
+            value={environment}
+            onChange={onClick}
+            type="radio"
+          >
+            {Object.entries(EnvironmentNames).map(([key, value]) => (
+              <ToggleButton
+                key={key}
+                id={`radio-${key}`}
+                type="radio"
+                variant="outline-dark"
+                name="environment"
+                value={value}
+                disabled={disabled}
+              >
+                {value}
+              </ToggleButton>
+            ))}
+          </ToggleButtonGroup>
+        </Card.Body>
+      </Stack>
+    </Card>
+  );
+}
+
+export default Environment;
