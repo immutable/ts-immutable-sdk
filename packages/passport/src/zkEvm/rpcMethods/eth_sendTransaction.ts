13d7ce2a6d865a069ebfd9c3e08d2c711537af48
Felipe Tavares <felipe.oltavares@gmail.com>
2023-07-05 11:40:19 +1000

    ID-871: Poll the transaction hash ♻ (#534)

--- packages/passport/src/zkEvm/rpcMethods/eth_sendTransaction.ts
+++ packages/passport/src/zkEvm/rpcMethods/eth_sendTransaction.ts
@@ -7,6 +7,10 @@ import { getNonce, getSignedSequenceTransactions } from '../sequence';
 import { Transaction } from '../types';
 import { EthMethodWithAuthParams } from './types';
 import { JsonRpcError, RpcErrorCode } from '../JsonRpcError';
+import { retryWithDelay } from '../../network/retry';
+
+const MAX_TRANSACTION_HASH_RETRIEVAL_RETRIES = 30;
+const TRANSACTION_HASH_RETRIEVAL_WAIT = 1000;
 
 export const ethSendTransaction = async ({
   params,
@@ -69,8 +73,20 @@ export const ethSendTransaction = async ({
 
   // TODO: ID-697 Evaluate transactions through Guardian
 
-  const transactionHash = await relayerAdapter.ethSendTransaction(transactionRequest.to, signedTransactions);
+  const relayerId = await relayerAdapter.ethSendTransaction(transactionRequest.to, signedTransactions);
 
-  const relayerTransaction = await relayerAdapter.imGetTransactionByHash(transactionHash);
+  const relayerTransaction = await retryWithDelay(async () => {
+    const tx = await relayerAdapter.imGetTransactionByHash(relayerId);
+    if (tx.status === 'PENDING') {
+      throw new Error();
+    }
+
+    return tx;
+  }, {
+    retries: MAX_TRANSACTION_HASH_RETRIEVAL_RETRIES,
+    interval: TRANSACTION_HASH_RETRIEVAL_WAIT,
+    finalErr: new JsonRpcError(RpcErrorCode.RPC_SERVER_ERROR, 'transaction hash not generated in time'),
+  });
+
   return relayerTransaction.hash;
 };
