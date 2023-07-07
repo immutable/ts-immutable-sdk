698708a2023847ecfc1fa5825a906a52d4399455
Mikhala <mikhala.kurtjak@immutable.com>
2023-07-07 08:39:47 +0800

    remove

--- packages/internal/cryptofiat/src/cryptofiat.ts
+++ packages/internal/cryptofiat/src/cryptofiat.ts
@@ -4,7 +4,6 @@ import { CryptoFiatConfiguration } from 'config';
 import { CryptoFiatConvertParams, CryptoFiatConvertReturn } from 'types';
 
 const CHECKOUT_API_BASE_URL = {
-  ['development' as Environment]: 'https://checkout-api.dev.immutable.com',
   [Environment.SANDBOX]: 'https://checkout-api.sandbox.immutable.com',
   // TODO: https://immutable.atlassian.net/browse/WT-1425
   [Environment.PRODUCTION]: 'https://checkout-api.sandbox.immutable.com',
