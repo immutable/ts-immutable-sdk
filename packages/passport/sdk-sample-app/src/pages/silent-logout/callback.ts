9f3ff7c2c3624afc1eb6632fd98fea7699904d9e
Jon <jon.alonso@immutable.com>
2023-07-18 09:45:36 +1000

    ID-930 Use env var for silent logout parent URI

--- packages/passport/sdk-sample-app/src/pages/silent-logout/callback.ts
+++ packages/passport/sdk-sample-app/src/pages/silent-logout/callback.ts
@@ -1,11 +1,12 @@
 import { useImmutableProvider } from '@/context/ImmutableProvider';
 import { useEffect } from 'react';
+import { silentLogoutParentUri } from '@/config';
 
 export default function HandleCallback() {
   const { passportClient } = useImmutableProvider();
 
   useEffect(() => {
     console.log('emitting event to silently log user out');
-    passportClient?.logoutSilentCallback('http://localhost:3000');
+    passportClient?.logoutSilentCallback(silentLogoutParentUri);
   }, [passportClient]);
 }
