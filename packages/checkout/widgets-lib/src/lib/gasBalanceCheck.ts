7056526dd99d07189feef8359571c8cbd00610d9
Mikhala <mikhala.kurtjak@immutable.com>
2023-07-19 11:30:33 +0800

    remove unnecessary filter

--- packages/checkout/widgets-lib/src/lib/gasBalanceCheck.ts
+++ packages/checkout/widgets-lib/src/lib/gasBalanceCheck.ts
@@ -3,9 +3,9 @@ import { GetBalanceResult } from '@imtbl/checkout-sdk';
 export const hasZeroBalance = (tokenBalances: GetBalanceResult[], symbol: string) => {
   if (tokenBalances.length === 0) return true;
   let zeroBalance = false;
-  tokenBalances.filter((b) => b.balance.eq(0))
+  tokenBalances
     .forEach((t) => {
-      if (t.token.symbol === symbol) {
+      if (t.token.symbol === symbol && t.balance.eq(0)) {
         zeroBalance = true;
       }
     });
