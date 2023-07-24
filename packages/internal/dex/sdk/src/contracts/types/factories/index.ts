0b74ea95aeaf1497a6682c94509d3d68ca04f29b
Pano Skylakis <pano.skylakis@immutable.com>
2023-07-24 15:09:44 +1200

    Feat: add SecondaryFee contract ABI and typechain bindings

--- packages/internal/dex/sdk/src/contracts/types/factories/index.ts
+++ packages/internal/dex/sdk/src/contracts/types/factories/index.ts
@@ -4,5 +4,6 @@
 export { ERC20__factory } from "./ERC20__factory";
 export { Multicall__factory } from "./Multicall__factory";
 export { QuoterV2__factory } from "./QuoterV2__factory";
+export { SecondaryFee__factory } from "./SecondaryFee__factory";
 export { UniswapV3Pool__factory } from "./UniswapV3Pool__factory";
 export { WETH__factory } from "./WETH__factory";
--- packages/internal/dex/sdk/src/contracts/types/index.ts
+++ packages/internal/dex/sdk/src/contracts/types/index.ts
@@ -4,11 +4,13 @@
 export type { ERC20 } from "./ERC20";
 export type { Multicall } from "./Multicall";
 export type { QuoterV2 } from "./QuoterV2";
+export type { SecondaryFee } from "./SecondaryFee";
 export type { UniswapV3Pool } from "./UniswapV3Pool";
 export type { WETH } from "./WETH";
 export * as factories from "./factories";
 export { ERC20__factory } from "./factories/ERC20__factory";
 export { Multicall__factory } from "./factories/Multicall__factory";
 export { QuoterV2__factory } from "./factories/QuoterV2__factory";
+export { SecondaryFee__factory } from "./factories/SecondaryFee__factory";
 export { UniswapV3Pool__factory } from "./factories/UniswapV3Pool__factory";
 export { WETH__factory } from "./factories/WETH__factory";
