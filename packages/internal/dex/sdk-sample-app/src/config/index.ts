609f1a69cd3a8ca229a086aaaa23f358c32c62b9
Pano Skylakis <pano.skylakis@immutable.com>
2023-07-26 22:31:30 +1200

    Fix: update chain id after reset

--- packages/internal/dex/sdk-sample-app/src/config/index.ts
+++ packages/internal/dex/sdk-sample-app/src/config/index.ts
@@ -1,6 +1,6 @@
 import { Environment, ImmutableConfiguration } from '@imtbl/sdk';
 
-const chainId = 13392; // You can optionally retrieve the chain ID from the users wallet, or prompt the user to change networks
+const chainId = 13372; // You can optionally retrieve the chain ID from the users wallet, or prompt the user to change networks
 
 const immutableConfig = new ImmutableConfiguration({
   environment: Environment.SANDBOX,
