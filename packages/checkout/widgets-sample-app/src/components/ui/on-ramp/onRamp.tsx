d9d4c4e1eed6958c5ae2c2c6a3c1ce487b3014a1
Deepti Luthra <deepti.luthra@immutable.com>
2023-07-27 11:38:55 +1000

    [poc] pre-populate user address and amounts

--- packages/checkout/widgets-sample-app/src/components/ui/on-ramp/onRamp.tsx
+++ packages/checkout/widgets-sample-app/src/components/ui/on-ramp/onRamp.tsx
@@ -1,24 +1,34 @@
 import { BiomeCombinedProviders, Body, Box } from "@biom3/react";
 import { onDarkBase } from "@biom3/design-tokens";
 import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk';
-import { useEffect } from 'react';
+import { useEffect, useRef } from "react";
 
 export function OnRampUI() {
   let domElement: HTMLElement | undefined;
+  const firstRender = useRef(true);
 
   useEffect(()=>{
     domElement = document.getElementById('ramp-container') || undefined;
   },[]);
   useEffect(() => {
-    new RampInstantSDK({
-      hostAppName: 'Immutable',
-      hostLogoUrl: 'https://assets.ramp.network/misc/test-logo.png',
-      hostApiKey: 'onr7tgoqgx8jxzggsc52ud9xx3hm9gkzf7czw93x',
-      url: 'https://app.demo.ramp.network',
-      variant: 'embedded-mobile',
-      containerNode:  domElement ,
-      enabledFlows: ['ONRAMP', 'OFFRAMP'],
-    }).show();
+    if(firstRender.current) {
+      new RampInstantSDK({
+        hostAppName: 'Immutable',
+        hostLogoUrl: 'https://assets.ramp.network/misc/test-logo.png',
+        hostApiKey: 'onr7tgoqgx8jxzggsc52ud9xx3hm9gkzf7czw93x',
+        url: 'https://app.demo.ramp.network',
+        variant: 'embedded-mobile',
+        containerNode: domElement,
+        enabledFlows: ['ONRAMP'],
+        userAddress: '0xDaA1842cF7E43B45385F956b89Ae814C0fE4BD20',
+        // fiatCurrency: 'USD',
+        // fiatValue: '20'
+        swapAmount: '200000000000000000',
+        swapAsset: 'SEPOLIA_ETH',
+      }).on('*', (event) => console.log(event))
+        .show();
+    }
+    firstRender.current = false;
     console.log('is it rendering twice!')
   }, [domElement]);
 
@@ -27,7 +37,7 @@ export function OnRampUI() {
       <Box style={{ height: '630px', width: '430px' }}>
         <Body>OnRampWidget with iframe</Body>
         <iframe style={{ height: '600px', width: '430px' }}
-                src="https://app.demo.ramp.network/?enabledFlows=OFFRAMP&hostAppName=Immutable&hostLogoUrl=https%3A%2F%2Fassets.ramp.network%2Fmisc%2Ftest-logo.png&hostApiKey=onr7tgoqgx8jxzggsc52ud9xx3hm9gkzf7czw93x&sdkType=WEB&sdkVersion=4.0.4&widgetInstanceId=1012796856&hostUrl=http%3A%2F%2Flocalhost%3A3000"></iframe>
+                src="https://app.demo.ramp.network/?hostAppName=Immutable&hostLogoUrl=https%3A%2F%2Fassets.ramp.network%2Fmisc%2Ftest-logo.png&hostApiKey=onr7tgoqgx8jxzggsc52ud9xx3hm9gkzf7czw93x&enabledFlows=ONRAMP&userAddress=0xDaA1842cF7E43B45385F956b89Ae814C0fE4BD20&fiatCurrency=USD&fiatValue=20&sdkType=WEB&sdkVersion=4.0.4&hostUrl=http%3A%2F%2Flocalhost%3A3000"></iframe>
       </Box>
 
       <Box id="ramp-container" style={{ height: '667px', width: '430px' }} >
