20f61b7b0f0fedc55499eeeb0a5d14c851bc61d4
Hayden Fowler <hayden.fowler@immutable.com>
2023-08-02 15:45:56 +1000

    Simplify data property sent to guardian

--- packages/passport/sdk/src/guardian/guardian.ts
+++ packages/passport/sdk/src/guardian/guardian.ts
@@ -35,7 +35,7 @@ const transformGuardianTransactions = (txs: MetaTransaction[]): guardian.MetaTra
       gasLimit: t.gasLimit ? convertBigNumberishToString(t.gasLimit) : '0',
       target: t.to ?? ethers.constants.AddressZero,
       value: t.value ? convertBigNumberishToString(t.value) : '0',
-      data: t.data ? t.data.toString() : '0x00',
+      data: t.data ? t.data.toString() : '0x',
     }));
   } catch (error) {
     const errorMessage = (error instanceof Error) ? error.message : String(error);
