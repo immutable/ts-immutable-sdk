216d6f44631feb15a9cfd6d9242471e41d02c9eb
Nik Ho <studying.mole@gmail.com>
2023-08-09 13:44:16 +1200

    Try oauth URL

--- .github/scripts/push-docs-pr.sh
+++ .github/scripts/push-docs-pr.sh
@@ -20,7 +20,8 @@ CLONE_DIR=$(mktemp -d)
 echo "CLONE_DIR: $CLONE_DIR"
 
 echo "Cloning destination git repository"
-git clone "https://github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+# git clone "https://github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+git clone "https://oauth2:$GITHUB_TOKEN@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
