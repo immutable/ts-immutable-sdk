1e8ace05b9905e5247c698a8cb47ebc6c1298fc7
Sharon Sheah <sharonsheah@gmail.com>
2023-08-10 11:52:49 +1000

    Create PassportConnect Hero

---
+++ packages/checkout/widgets-lib/src/components/Hero/PassportConnectHero.tsx
@@ -0,0 +1,18 @@
+import { Box, Logo } from '@biom3/react';
+import { heroBackGroundStyles, heroImageStyles } from './HeroImageStyles';
+import { ReactComponent as PurpleDownGradient } from '../../assets/PurpleDownGradient.svg';
+
+export function PassportConnectHero() {
+  return (
+    <Box testId="passport-connect-hero" sx={heroBackGroundStyles}>
+      <PurpleDownGradient />
+      <Box sx={heroImageStyles}>
+        <Logo
+          testId="passport-connect-hero-logo"
+          logo="PassportSymbolOutlined"
+          sx={{ width: '206px' }}
+        />
+      </Box>
+    </Box>
+  );
+}
