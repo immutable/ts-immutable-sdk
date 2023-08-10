fcc92db842515e1a4e8f21d571320e8dcdac90e8
Pano Skylakis <49353965+pano-skylakis@users.noreply.github.com>
2023-08-10 19:35:56 +1200

    [TP-1418][NO-CHANGELOG] DEX SDK Fee breakdown (#663)
    Co-authored-by: Pano Skylakis <pano.skylakis@immutable.com>

--- packages/internal/dex/sdk/src/lib/fees.ts
+++ packages/internal/dex/sdk/src/lib/fees.ts
@@ -1,14 +1,56 @@
 import { BASIS_POINT_PRECISION } from 'constants/router';
 import { ethers } from 'ethers';
-import { SecondaryFee } from 'lib';
+import {
+  Fee, SecondaryFee, TokenInfo, newAmount,
+} from 'lib';
 
-export function calculateFees(amount: ethers.BigNumber, secondaryFees: SecondaryFee[]) {
-  let totalFees = ethers.BigNumber.from(0);
+export class Fees {
+  private secondaryFees: SecondaryFee[];
 
-  for (let i = 0; i < secondaryFees.length; i++) {
-    const feeAmount = amount.mul(secondaryFees[i].feeBasisPoints).div(BASIS_POINT_PRECISION);
-    totalFees = totalFees.add(feeAmount);
+  private token: TokenInfo;
+
+  private amount: ethers.BigNumber = ethers.BigNumber.from(0);
+
+  constructor(secondaryFees: SecondaryFee[], token: TokenInfo) {
+    this.secondaryFees = secondaryFees;
+    this.token = token;
   }
 
-  return totalFees;
+  addAmount(amount: ethers.BigNumber): void {
+    this.amount = this.amount.add(amount);
+  }
+
+  amountWithFeesApplied(): ethers.BigNumber {
+    return this.amount.add(this.total());
+  }
+
+  amountLessFees(): ethers.BigNumber {
+    return this.amount.sub(this.total());
+  }
+
+  withAmounts(): Fee[] {
+    return this.secondaryFees.map((fee) => {
+      const feeAmount = this.amount
+        .mul(fee.feeBasisPoints)
+        .div(BASIS_POINT_PRECISION);
+
+      return {
+        ...fee,
+        amount: newAmount(feeAmount, this.token),
+      };
+    });
+  }
+
+  private total(): ethers.BigNumber {
+    let totalFees = ethers.BigNumber.from(0);
+
+    for (let i = 0; i < this.secondaryFees.length; i++) {
+      const feeAmount = this.amount
+        .mul(this.secondaryFees[i].feeBasisPoints)
+        .div(BASIS_POINT_PRECISION);
+      totalFees = totalFees.add(feeAmount);
+    }
+
+    return totalFees;
+  }
 }
