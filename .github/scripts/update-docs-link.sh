9d2852f58b90977d7247bb85d741a980da6c00b6
Carl Menezes <carl.menezes@immutable.com>
2023-08-11 11:21:04 +1200

    Update SDK docs link

--- .github/scripts/update-docs-link.sh
+++ .github/scripts/update-docs-link.sh
@@ -15,13 +15,14 @@ then
   exit 1
 fi
 
-FILE=_typescript.mdx
 
-pushd $CLONE_DIR
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # On Mac OS, sed requires an empty string as an argument to -i to avoid creating a backup file
-    sed -i '' -E "s/[0-9]\\.[0-9]\\.[0-9](.* class=\"ts-immutable-sdk-ref-link\")/$VERSION\1/g;" $FILE
-else
-    sed -i -E "s/[0-9]\\.[0-9]\\.[0-9](.* class=\"ts-immutable-sdk-ref-link\")/$VERSION\1/g;" $FILE
-fi
-popd
+(
+  cd $CLONE_DIR;
+  FILE=docs/main/sdks/_typescript.mdx
+  if [ "$(uname)" == "Darwin" ]; then
+      # On Mac OS, sed requires an empty string as an argument to -i to avoid creating a backup file
+      sed -i '' -E "s/[0-9]\\.[0-9]\\.[0-9](.* class=\"ts-immutable-sdk-ref-link\")/$VERSION\1/g;" $FILE
+  else
+      sed -i -E "s/[0-9]\\.[0-9]\\.[0-9](.* class=\"ts-immutable-sdk-ref-link\")/$VERSION\1/g;" $FILE
+  fi
+)
