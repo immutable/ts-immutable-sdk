5ac8542a274a7a1e45837e9af00ebd4860dff0b2
Nik Ho <studying.mole@gmail.com>
2023-08-22 18:32:37 +1200

    git status

--- .github/scripts/push-pr.sh
+++ .github/scripts/push-pr.sh
@@ -7,35 +7,39 @@ echo "Version: $VERSION"
 set -e
 set -x
 
-# if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
-# then
-#   PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
-# else
-#   PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
-# fi
+if [ -z "VERSION" ]
+then
+  echo "VERSION is not set"
+  exit 1
+fi
 
-echo "Adding git commit"
+INPUT_DESTINATION_HEAD_BRANCH="release-changelog-$VERSION"
+
+if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
+then
+  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
+else
+  PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
+fi
 
 echo "Foo" >> CHANGELOG.md
 
+echo "Staging changes"
 git add $*
-git status
 
-# if git status | grep -q "Changes to be committed"
-# then
-#   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-#   echo "Pushing git commit"
-#   git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+echo "Adding git commit"
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
 
-#   # Without this sleep, the checks on the imx-docs repo fail
-#   # but pass on a re-run from within Netlify
-#   echo "Waiting for 1 minute to allow Netlify to catch up"
-#   sleep 60
+  sleep 10
 
-#   echo "Creating a pull request"
-#   gh pr create --title "Release SDK reference docs v$VERSION" \
-#                --body "Released from ts-immutable-sdk" \
-#                --reviewer "$GITHUB_ACTOR"
-# else
-#   echo "No changes detected"
-# fi
+  echo "Creating a pull request"
+  gh pr create --title "Release SDK CHANGELOG $VERSION" \
+               --body "Updated CHANGELOG.md from ts-immutable-sdk release workflow" \
+               --reviewer "$GITHUB_ACTOR"
+else
+  echo "No changes detected"
+fi
