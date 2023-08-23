e79b8df4a9f46fefe4ebbed8a6898f5007bea7c9
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-08-24 09:35:38 +1000

    add top-up view and redirects to pay views

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/views/PaymentMethods.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/views/PaymentMethods.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 
+import { useCallback, useContext } from 'react';
 import { Body, Box } from '@biom3/react';
 
 import { SimpleLayout } from '../../../components/SimpleLayout/SimpleLayout';
@@ -9,15 +10,72 @@ import { text } from '../../../resources/text/textConfig';
 import { PaymentOptions } from '../components/PaymentOptions';
 import { PrimaryRevenueWidgetViews } from '../../../context/view-context/PrimaryRevenueViewContextTypes';
 
-export function PaymentMethods() {
+import {
+  ViewContext,
+  ViewActions,
+  SharedViews,
+} from '../../../context/view-context/ViewContext';
+
+export interface PaymentMethodsProps {
+  checkBalances: () => Promise<boolean>;
+}
+
+export function PaymentMethods(props: PaymentMethodsProps) {
+  const { checkBalances } = props;
   const { header } = text.views[PrimaryRevenueWidgetViews.PAYMENT_METHODS];
+  const { viewDispatch } = useContext(ViewContext);
+
+  const handleOptionClick = useCallback(
+    async (type: PrimaryRevenueWidgetViews) => {
+      try {
+        viewDispatch({
+          payload: {
+            type: ViewActions.UPDATE_VIEW,
+            view: { type: SharedViews.LOADING_VIEW },
+          },
+        });
+
+        const hasEnoughBalance = await checkBalances();
+
+        // FIXME: best way to handle conditional routing?
+        if (!hasEnoughBalance) {
+          viewDispatch({
+            payload: {
+              type: ViewActions.UPDATE_VIEW,
+              view: { type: SharedViews.TOP_UP_VIEW },
+            },
+          });
+
+          return;
+        }
+
+        viewDispatch({
+          payload: {
+            type: ViewActions.UPDATE_VIEW,
+            view: { type: type as any },
+          },
+        });
+      } catch (err: any) {
+        viewDispatch({
+          payload: {
+            type: ViewActions.UPDATE_VIEW,
+            view: { type: SharedViews.ERROR_VIEW, error: err },
+          },
+        });
+      }
+    },
+    [],
+  );
 
   return (
     <SimpleLayout
       testId="payment-methods"
-      header={
-        <HeaderNavigation title={header.heading} onCloseButtonClick={() => {}} />
-      }
+      header={(
+        <HeaderNavigation
+          title={header.heading}
+          onCloseButtonClick={() => {}}
+        />
+      )}
       footer={<FooterLogo />}
     >
       <Box
@@ -37,7 +95,7 @@ export function PaymentMethods() {
         >
           {header.caption}
         </Body>
-        <PaymentOptions />
+        <PaymentOptions onClick={handleOptionClick} />
       </Box>
     </SimpleLayout>
   );
