58bfab3bfe900ed00f5823b239a5d127d7e3f7e5
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-08-23 12:24:04 +1000

    scaffold a primary revenue widget with connect wrapper

--- packages/checkout/widgets-lib/src/build.ts
+++ packages/checkout/widgets-lib/src/build.ts
@@ -3,6 +3,7 @@ import { ImmutableConnect } from './widgets/connect/ConnectWebComponent';
 import { ImmutableSwap } from './widgets/swap/SwapWebComponent';
 import { ImmutableWallet } from './widgets/wallet/WalletWebComponent';
 import { ImmutableBridge } from './widgets/bridge/BridgeWebComponent';
+import { ImmutablePrimaryRevenue } from './widgets/primary-revenue/PrimaryRevenueWebComponent';
 
 customElements.get('imtbl-connect')
   || customElements.define('imtbl-connect', ImmutableConnect);
@@ -12,3 +13,7 @@ customElements.get('imtbl-swap')
   || customElements.define('imtbl-swap', ImmutableSwap);
 customElements.get('imtbl-bridge')
   || customElements.define('imtbl-bridge', ImmutableBridge);
+
+// FIXME: bundle separately
+customElements.get('imtbl-primary-revenue')
+  || customElements.define('imtbl-primary-revenue', ImmutablePrimaryRevenue);
