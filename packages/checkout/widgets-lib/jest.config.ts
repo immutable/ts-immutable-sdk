16ea40e2c0e58fd1beb6f439dc85dabd4f807466
Keith Broughton <101608096+keithbro-imx@users.noreply.github.com>
2023-08-23 10:29:28 +1000

    Use moduleNameMapper [NO-CHANGELOG] (#696)

--- packages/checkout/widgets-lib/jest.config.ts
+++ packages/checkout/widgets-lib/jest.config.ts
@@ -4,6 +4,14 @@ const config: Config = {
   clearMocks: true,
   coverageProvider: 'v8',
   moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '@imtbl/bridge-sdk': '<rootDir>../../internal/bridge/sdk/src',
+    '@imtbl/checkout-sdk': '<rootDir>../sdk/src',
+    '@imtbl/config': '<rootDir>../../config/src',
+    '@imtbl/cryptofiat': '<rootDir>../../internal/cryptofiat/src',
+    '@imtbl/dex-sdk': '<rootDir>../../internal/dex/sdk/src',
+    '@imtbl/orderbook': '<rootDir>../../orderbook/src',
+  },
   testEnvironment: 'node',
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
