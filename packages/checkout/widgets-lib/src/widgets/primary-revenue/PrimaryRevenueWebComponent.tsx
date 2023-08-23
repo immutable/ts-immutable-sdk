f9703ed6f775c7d473c9d0e994081ffa0900beba
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-08-23 12:24:04 +1000

    scaffold a primary revenue widget with connect wrapper

---
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/PrimaryRevenueWebComponent.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { PrimaryRevenueWidget } from './PrimaryRevenueWidget';
+import {
+  ConnectLoader,
+  ConnectLoaderParams,
+} from '../../components/ConnectLoader/ConnectLoader';
+import { ImmutableWebComponent } from '../ImmutableWebComponent';
+import { ConnectTargetLayer, getL1ChainId, getL2ChainId } from '../../lib';
+
+export class ImmutablePrimaryRevenue extends ImmutableWebComponent {
+  constructor() {
+    console.log('ImmutablePrimaryRevenue constructor'); // eslint-disable-line no-console
+    super();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    // TODO: initialise widget inputs
+    this.renderWidget();
+  }
+
+  validateInputs(): void {
+    // TODO: validate widget inputs
+  }
+
+  renderWidget() {
+    this.validateInputs();
+    const connectLoaderParams: ConnectLoaderParams = {
+      targetLayer: ConnectTargetLayer.LAYER2,
+      web3Provider: this.provider,
+      allowedChains: [
+        getL1ChainId(this.checkout!.config),
+        getL2ChainId(this.checkout!.config),
+      ],
+    };
+
+    if (!this.reactRoot) {
+      this.reactRoot = ReactDOM.createRoot(this);
+    }
+    this.reactRoot.render(
+      <React.StrictMode>
+        <ConnectLoader
+          widgetConfig={this.widgetConfig!}
+          params={connectLoaderParams}
+          closeEvent={() => {
+          }}
+        >
+          <PrimaryRevenueWidget
+            config={this.widgetConfig!}
+          />
+        </ConnectLoader>
+      </React.StrictMode>,
+    );
+  }
+}
