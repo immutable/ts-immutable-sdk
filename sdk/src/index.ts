733fcc39c9596ee6a13faac07149e64b4146bc6b
Carl Menezes <carl.menezes@immutable.com>
2023-08-28 13:47:44 +1200

    everything except erc721_mint_by_id and erc721
    build still hangs

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -7,4 +7,3 @@ export * as provider from './provider';
 export * as checkoutSdk from './checkout_sdk';
 export * as checkoutWidgets from './checkout_widgets';
 export * as erc20 from './erc20';
-export * as erc721 from './erc721';
