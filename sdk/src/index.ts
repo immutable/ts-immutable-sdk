f2da12c291411c3e06a5d9e80198181a11703df2
Carl Menezes <carl.menezes@immutable.com>
2023-08-28 15:12:53 +1200

    remove orderbook

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -2,4 +2,3 @@ export * as config from './config';
 export * as blockchainData from './blockchain_data';
 export * as immutablexClient from './immutablex_client';
 export * as passport from './passport';
-export * as orderbook from './orderbook';
