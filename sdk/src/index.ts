e2cd0d75fb58211d9206e8ec7d42159d247c9400
Carl Menezes <carl.menezes@immutable.com>
2023-08-28 15:02:07 +1200

    remove provider
    build hangs on orderbook

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -3,4 +3,3 @@ export * as blockchainData from './blockchain_data';
 export * as immutablexClient from './immutablex_client';
 export * as passport from './passport';
 export * as orderbook from './orderbook';
-export * as provider from './provider';
