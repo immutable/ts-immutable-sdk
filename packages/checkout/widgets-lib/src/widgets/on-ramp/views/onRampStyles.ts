2af413289c39ff54b5bb71a3ad27371c2f521b70
Deepti Luthra <deepti.luthra@immutable.com>
2023-08-29 15:55:04 +0530

    show back button when view-state history has top-up view

---
+++ packages/checkout/widgets-lib/src/widgets/on-ramp/views/onRampStyles.ts
@@ -0,0 +1,9 @@
+export const containerStyle = {
+  position: 'relative',
+  width: '420px',
+  height: '565px',
+  boxShadow: '0 0 15px #d0d0d0',
+  borderRadius: '15px',
+  overflow: 'hidden',
+  marginLeft: '8px',
+};
