74251594505b3c5aa61f5d820c1a1204b7670f88
Deepti Luthra <deepti.luthra@immutable.com>
2023-08-29 08:38:01 +0530

    Add segment analytics for on-ramp

---
+++ packages/checkout/widgets-lib/src/context/SegmentAnalyticsProvider.ts
@@ -0,0 +1,30 @@
+import { createAnalytics, StandardAnalyticsActions, StandardAnalyticsControlTypes } from '@imtbl/react-analytics';
+import { Environment } from '@imtbl/config';
+
+export type AnalyticsActions = StandardAnalyticsActions
+| 'Opened'
+| 'Started'
+| 'Processing'
+| 'Succeeded'
+| 'Failed';
+
+export type AnalyticsControlTypes = StandardAnalyticsControlTypes
+| 'Button'
+| 'Trigger'
+| 'OnRampWidget';
+
+export const getSegmentWriteKey = (env: Environment) => (env === Environment.PRODUCTION
+  ? 'hecEjBUtJP8IvC9rBx9IkBFR0UuDiIos'
+  : 'b69BcXnFXdaiFC6MqRQiHvjcPrTxftZl');
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const { AnalyticsProvider, useAnalytics } = createAnalytics<
+'OnRamp' | 'Swap' | 'Bridge',
+string,
+'Click' | 'Confirm' | 'WebhookEvent',
+AnalyticsControlTypes,
+AnalyticsActions
+>({
+  writeKey: '',
+  appName: 'Checkout-widgets',
+});
--- packages/checkout/widgets-lib/src/widgets/on-ramp/views/OnRampMain.tsx
+++ packages/checkout/widgets-lib/src/widgets/on-ramp/views/OnRampMain.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { HeaderNavigation } from '../../../components/Header/HeaderNavigation';
 import { SimpleLayout } from '../../../components/SimpleLayout/SimpleLayout';
 import { sendOnRampWidgetCloseEvent } from '../OnRampWidgetEvents';
+import { getSegmentWriteKey, useAnalytics } from '../../../context/SegmentAnalyticsProvider';
 
 interface OnRampProps {
   environment: Environment
@@ -17,16 +18,67 @@ export function OnRampMain({ environment }: OnRampProps) {
 
   const finalUrl = `${url}&${configurations}`;
 
+  const { track, updateWriteKey } = useAnalytics();
+
+  const trackSegmentEvents = (event: any) => {
+    switch (event.event_id) {
+      case 'TRANSAK_WIDGET_OPEN':
+        track({
+          userJourney: 'OnRamp',
+          screen: 'Initial-onramp-screen',
+          control: 'WebhookEvent',
+          controlType: 'Trigger',
+          action: 'Opened',
+          userId: '0x00address00', // todo: insert wallet-address
+        });
+        break;
+      case 'TRANSAK_ORDER_CREATED': // AWAITING_PAYMENT_FROM_USER
+        track({
+          userJourney: 'OnRamp',
+          screen: 'order-creation',
+          control: 'WebhookEvent',
+          controlType: 'Trigger',
+          action: 'Started',
+          userId: '0x00address00', // todo: insert wallet-address
+        });
+        break;
+      case 'TRANSAK_ORDER_SUCCESSFUL': // user paid
+        track({
+          userJourney: 'OnRamp',
+          screen: 'payment-confirmation',
+          control: 'Confirm',
+          controlType: 'Button',
+          action: 'Processing',
+          userId: '0x00address00', // todo: insert wallet-address
+        });
+        break;
+      case 'TRANSAK_ORDER_FAILED': // payment failed
+        track({
+          userJourney: 'OnRamp',
+          screen: 'failure-screen',
+          control: 'WebhookEvent',
+          controlType: 'Trigger',
+          action: 'Failed',
+          userId: '0x00address00', // todo: insert wallet-address
+        });
+        break;
+      default:
+    }
+  };
+
   useEffect(() => {
     const domIframe:HTMLIFrameElement = document.getElementById('transak-iframe') as HTMLIFrameElement;
 
     if (domIframe === undefined) return;
+
+    updateWriteKey(getSegmentWriteKey(environment));
 
     const handler = (event: any) => {
       if (event.source === domIframe.contentWindow) {
         if (event.origin === 'https://global-stg.transak.com') {
           // eslint-disable-next-line no-console
           console.log('TRANSAK event data: ', event.data);
+          trackSegmentEvents(event);
         }
       }
     };
