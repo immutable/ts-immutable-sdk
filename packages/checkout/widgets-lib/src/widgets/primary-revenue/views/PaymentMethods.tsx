8c4b7c4b418292332860792ebcea457c1ea1d3df
Mimi Tran <80493680+mimi-imtbl@users.noreply.github.com>
2023-08-29 13:43:09 +1000

    [NO-CHANGELOG][GPR-120] Primary revenue widget - approve with passport  (#765)

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/views/PaymentMethods.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/views/PaymentMethods.tsx
@@ -35,8 +35,7 @@ export function PaymentMethods(props: PaymentMethodsProps) {
           },
         });
 
-        let hasEnoughBalance = await checkBalances();
-        hasEnoughBalance = true;
+        const hasEnoughBalance = await checkBalances();
 
         // FIXME: best way to handle conditional routing?
         if (!hasEnoughBalance) {
