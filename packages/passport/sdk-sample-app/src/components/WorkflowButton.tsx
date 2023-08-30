a0210ca816355c550336375a019c389c19e51bfa
tim <tim@glo.id.au>
2023-08-30 15:27:29 +1000

    fix eslint errors

--- packages/passport/sdk-sample-app/src/components/WorkflowButton.tsx
+++ packages/passport/sdk-sample-app/src/components/WorkflowButton.tsx
@@ -1,14 +1,14 @@
-import { Button, ButtonProps } from "@biom3/react";
-import { ReactElement } from "react";
+import { Button, ButtonProps } from '@biom3/react';
+import { ReactElement } from 'react';
 
 function WorkflowButton<RC extends ReactElement | undefined>({
   children,
   ...remainingProps
 }: ButtonProps<RC>) {
   const { disabled } =
-    "disabled" in remainingProps ? remainingProps : { disabled: undefined };
+    'disabled' in remainingProps ? remainingProps : { disabled: undefined };
   return (
-    <Button variant={disabled ? "tertiary" : "primary"} {...remainingProps}>
+    <Button variant={disabled ? 'tertiary' : 'primary'} {...remainingProps}>
       {children}
     </Button>
   );
