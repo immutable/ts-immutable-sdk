20798de64c4657bbb09d6a8e8ceea4e1bf870a7b
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-08-31 17:40:55 +1000

    remove unused code

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/components/OrderItem.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/components/OrderItem.tsx
@@ -21,7 +21,6 @@ export function OrderItem(props: OrderItemProps) {
   return (
     <MenuItem emphasized size="small">
       <MenuItem.FramedImage imageUrl={item.image} />
-
       <MenuItem.Label>{item.name}</MenuItem.Label>
       <MenuItem.Caption>{item.description}</MenuItem.Caption>
       {item.price && (
@@ -33,55 +32,4 @@ export function OrderItem(props: OrderItemProps) {
       )}
     </MenuItem>
   );
-
-  // return (
-  //   <MenuItem size="medium" emphasized>
-  //     <MenuItem.FramedImage
-  //       src={item.image}
-  //     />
-  //     <MenuItem.Label size="medium">{optionText.heading}</MenuItem.Label>
-  //     <MenuItem.IntentIcon />
-  //     <MenuItem.Caption>{optionText.caption}</MenuItem.Caption>
-  //   </MenuItem>
-  // );
-
-  // return (
-  //   <Box
-  //     testId="order-list-item-container"
-  //     sx={{
-  //       width: '100%',
-  //       display: 'flex',
-  //       flexDirection: 'row',
-  //       justifyContent: 'space-between',
-  //       padding: '24px 16px 24px 16px',
-  //       backgroundColor: 'base.color.translucent.emphasis.100',
-  //       borderRadius: 'base.borderRadius.x6',
-  //     }}
-  //   >
-  //     <Box
-  //       sx={{
-  //         width: '100%',
-  //         display: 'flex',
-  //         flexDirection: 'row',
-  //         justifyContent: 'space-between',
-  //         paddingX: 'base.spacing.x3',
-  //         paddingY: 'base.spacing.x2',
-  //       }}
-  //     >
-  //       <Body size="medium">{item.name}</Body>
-  //       <Box
-  //         sx={{
-  //           display: 'flex',
-  //           direction: 'row',
-  //           columnGap: 'base.spacing.x1',
-  //           alignItems: 'center',
-  //         }}
-  //       >
-  //         <Body size="medium" weight="bold">
-  //           {`${item.price} ${currency}`}
-  //         </Body>
-  //       </Box>
-  //     </Box>
-  //   </Box>
-  // );
 }
