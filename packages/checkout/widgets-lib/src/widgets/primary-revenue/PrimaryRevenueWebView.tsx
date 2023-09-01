5352fab3996d03de82ad235191c8758136fb80b3
Jhonatan Gonzalez <jhesgodi@gmail.com>
2023-09-01 14:04:34 +1000

    [NO-CHANGELOG] Primary revenue widget | Init from attributes (#806)
    Co-authored-by: Jhon Gonzalez <jhon.gonzalez@immutable.com>
    Co-authored-by: Mimi Immutable <mimi.tran@immutable.com>

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/PrimaryRevenueWebView.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/PrimaryRevenueWebView.tsx
@@ -4,6 +4,7 @@ import { config, passport } from '@imtbl/sdk';
 
 import { WidgetTheme } from '../../lib';
 import { ImmutableWebComponent } from '../ImmutableWebComponent';
+import { Item } from './PrimaryRevenueWidget';
 
 const defaultPassportConfig = {
   environment: 'sandbox',
@@ -14,31 +15,48 @@ const defaultPassportConfig = {
   scope: 'openid offline_access email transact',
 };
 
+const defaultItems: Item[] = [
+  {
+    id: '60',
+    qty: 1,
+    price: '0.5',
+    name: 'Poliwag',
+    image: 'https://pokemon-nfts.s3.ap-southeast-2.amazonaws.com/images/60.png',
+    description: 'Poliwag',
+  },
+  {
+    id: '61',
+    qty: 1,
+    price: '1',
+    name: 'Poliwhirl',
+    image: 'https://pokemon-nfts.s3.ap-southeast-2.amazonaws.com/images/61.png',
+    description: 'Poliwhirl',
+  },
+  {
+    id: '62',
+    qty: 1,
+    price: '2',
+    name: 'Poliwrath',
+    image: 'https://pokemon-nfts.s3.ap-southeast-2.amazonaws.com/images/62.png',
+    description: 'Poliwrath',
+  },
+];
+
 const useParams = () => {
   const urlParams = new URLSearchParams(window.location.search);
 
   const login = urlParams.get('login') as string;
-  const clientId = urlParams.get('clientId') as string;
-  const redirectUri = urlParams.get('redirectUri') as string;
-  const logoutRedirectUri = urlParams.get('logoutRedirectUri') as string;
-  const audience = urlParams.get('audience') as string;
-  const scope = urlParams.get('scope') as string;
-  const environment = {
-    sandbox: config.Environment.SANDBOX,
-    production: config.Environment.PRODUCTION,
-  }[urlParams.get('environment') || 'sandbox'];
   const amount = urlParams.get('amount') as string;
+  const envId = urlParams.get('envId') as string;
+  const fromCurrency = urlParams.get('fromCurrency') as string;
+  // @deprecated
   const fromContractAddress = urlParams.get('fromContractAddress') as string;
 
   return {
     login,
-    clientId,
-    redirectUri,
-    logoutRedirectUri,
-    audience,
-    scope,
-    environment,
     amount,
+    envId,
+    fromCurrency,
     fromContractAddress,
   };
 };
@@ -73,11 +91,14 @@ const usePassportInstance = (passportConfig: any) => {
 
 function PrimaryRevenueWebView() {
   const params = useParams();
-  const { login, amount, fromContractAddress } = params;
+  const {
+    login, amount, envId, fromCurrency, fromContractAddress,
+  } = params;
+  const [passportOn, setPassportOn] = useState(false);
   const [passportConfig, setPassportConfig] = useState(
     JSON.stringify(defaultPassportConfig, null, 2),
   );
-  const [passportOn, setPassportOn] = useState(false);
+  const [items, setItems] = useState(JSON.stringify(defaultItems, null, 2));
 
   const handlePassportConfigChange = (e: any) => {
     setPassportConfig(e.target.value);
@@ -87,11 +108,31 @@ function PrimaryRevenueWebView() {
     let value;
     try {
       value = JSON.parse(e.target.value);
-    } catch (error) { /** */ }
+    } catch (error) {
+      /** */
+    }
 
     if (value) {
       setPassportConfig(JSON.stringify(value, null, 2));
-      localStorage.setItem('passportConfig', JSON.stringify(value));
+      localStorage.setItem('imtbl/prw_passportConfig', JSON.stringify(value));
+    }
+  };
+
+  const handleItemsChange = (e: any) => {
+    setItems(e.target.value);
+  };
+
+  const handleItemsFormat = (e: any) => {
+    let value;
+    try {
+      value = JSON.parse(e.target.value);
+    } catch (error) {
+      /** */
+    }
+
+    if (value) {
+      setItems(JSON.stringify(value, null, 2));
+      localStorage.setItem('imtbl/prw_items', JSON.stringify(value));
     }
   };
 
@@ -103,20 +144,25 @@ function PrimaryRevenueWebView() {
     );
     const passportInstance = usePassportInstance(JSON.parse(passportConfig));
     primaryRevenueElement?.addPassportOption(passportInstance as any);
-  }, [passportOn, passportConfig]);
+  }, [passportOn, passportConfig, items]);
 
   useEffect(() => {
     const passportInstance = usePassportInstance(JSON.parse(passportConfig));
 
-    if (login && passportInstance) {
+    if (passportInstance) {
       passportInstance.loginCallback();
     }
   }, [login]);
 
   useEffect(() => {
-    const lsPassportConfig = localStorage.getItem('passportConfig');
+    const lsPassportConfig = localStorage.getItem('imtbl/prw_passportConfig');
     if (lsPassportConfig) {
       setPassportConfig(JSON.stringify(JSON.parse(lsPassportConfig), null, 2));
+    }
+
+    const lsItems = localStorage.getItem('imtbl/prw_items');
+    if (lsItems) {
+      setItems(JSON.stringify(JSON.parse(lsItems), null, 2));
     }
   }, []);
 
@@ -130,22 +176,35 @@ function PrimaryRevenueWebView() {
       <imtbl-primary-revenue
         widgetConfig={JSON.stringify(widgetConfig)}
         amount={amount}
+        envId={envId}
+        fromCurrency={fromCurrency}
+        items={items}
+        // @deprecated
         fromContractAddress={fromContractAddress}
       />
       <br />
-      <h1>
-        <textarea
-          rows={10}
-          cols={50}
-          value={passportConfig}
-          onChange={handlePassportConfigChange}
-          onBlur={handlePassportConfigFormat}
-        />
-        <br />
-        <button type="button" onClick={() => { setPassportOn(true); }}>
-          Passport On
-        </button>
-      </h1>
+      <h3>Passport Config</h3>
+      <textarea
+        rows={12}
+        cols={80}
+        value={passportConfig}
+        onChange={handlePassportConfigChange}
+        onBlur={handlePassportConfigFormat}
+      />
+      <br />
+      <button type="button" onClick={() => setPassportOn(true)}>
+        Passport On
+      </button>
+      <br />
+      <br />
+      <h3>Items</h3>
+      <textarea
+        rows={20}
+        cols={80}
+        value={items}
+        onChange={handleItemsChange}
+        onBlur={handleItemsFormat}
+      />
     </>
   );
 }
