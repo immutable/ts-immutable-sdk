c792f0b297d39c6d6470fbbb6afabf9f1a00b643
Andrea Rampin <andrea.rampin@gmail.com>
2023-09-02 10:41:51 +1000

    [WT-1615] added nonRetryable flag to retry

--- packages/checkout/widgets-lib/src/lib/retry.ts
+++ packages/checkout/widgets-lib/src/lib/retry.ts
@@ -5,25 +5,29 @@ export interface RetryType {
   retryIntervalMs: number
   // if not specified the retry function will retry until a success state is returned
   retries?: number;
+  // Condition in which the retry logic must exit although there are still retires
+  nonRetryable?: (err: any) => boolean
 }
 
 export const retry = async <T>(
   fn: () => Promise<T> | T,
-  { retries, retryIntervalMs }: RetryType,
+  { retries, retryIntervalMs, nonRetryable }: RetryType,
 ): Promise<T> => {
   let currentRetries = retries;
 
   try {
     return await fn();
-  } catch (error) {
+  } catch (error: any) {
     if (currentRetries !== undefined) {
       if (currentRetries <= 0) {
         throw error;
       }
       currentRetries -= 1;
     }
+
+    if (nonRetryable && nonRetryable(error)) throw error;
   }
 
   await sleep(retryIntervalMs);
-  return retry(fn, { retries: currentRetries, retryIntervalMs });
+  return retry(fn, { retries: currentRetries, retryIntervalMs, nonRetryable });
 };
