68a5f6e8739379e0ba94423f67fa7566a5cdd356
Mimi Immutable <mimi.tran@immutable.com>
2023-09-04 15:45:50 +1000

    Clean up comments

--- packages/internal/economy-building-blocks/playground/src/pages/PrimaryRevenueWidget.tsx
+++ packages/internal/economy-building-blocks/playground/src/pages/PrimaryRevenueWidget.tsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import { IMTBLWidgetEvents } from '@imtbl/checkout-widgets';
 
-// http://localhost:3000/mint-sale?amount=0.001&fromContractAddress=0x21B51Ec6fB7654B7e59e832F9e9687f29dF94Fb8&envId=123&fromCurrency=USDC&items=%5B%7B%22id%22%3A%2260%22%2C%22qty%22%3A1%2C%22price%22%3A%220.5%22%2C%22name%22%3A%22Poliwag%22%2C%22image%22%3A%22https%3A%2F%2Fpokemon-nfts.s3.ap-southeast-2.amazonaws.com%2Fimages%2F60.png%22%2C%22description%22%3A%22Poliwag%22%7D%2C%7B%22id%22%3A%2261%22%2C%22qty%22%3A1%2C%22price%22%3A%221%22%2C%22name%22%3A%22Poliwhirl%22%2C%22image%22%3A%22https%3A%2F%2Fpokemon-nfts.s3.ap-southeast-2.amazonaws.com%2Fimages%2F61.png%22%2C%22description%22%3A%22Poliwhirl%22%7D%2C%7B%22id%22%3A%2262%22%2C%22qty%22%3A1%2C%22price%22%3A%222%22%2C%22name%22%3A%22Poliwrath%22%2C%22image%22%3A%22https%3A%2F%2Fpokemon-nfts.s3.ap-southeast-2.amazonaws.com%2Fimages%2F62.png%22%2C%22description%22%3A%22Poliwrath%22%7D%5D
-
 const useParams = () => {
   const urlParams = new URLSearchParams(window.location.search);
 
