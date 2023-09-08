3eedc7d8bbb09bd32a0047f143a3aaaf8aa6a5e8
Sharon Sheah <sharonsheah@gmail.com>
2023-09-08 14:46:22 +1000

    add getNativeTokenByWalletAddress tests

--- packages/checkout/sdk/src/client/blockscout.test.ts
+++ packages/checkout/sdk/src/client/blockscout.test.ts
@@ -63,12 +63,12 @@ describe('Blockscout', () => {
                   icon_url: 'https://assets.coingecko.com',
                   name: 'Immutable X',
                   symbol: 'IMX',
-                  total_supply: '2000000000000000000000000000',
+                  total_supply: '8000000000000000000000000000',
                   type: 'ERC-20',
                 },
                 token_id: null,
                 token_instance: null,
-                value: '3000000000000000000',
+                value: '6000000000000000000',
               },
             ],
             next_page_params: null,
@@ -229,6 +229,127 @@ describe('Blockscout', () => {
       const client = new Blockscout({ chainId: ChainId.IMTBL_ZKEVM_TESTNET });
       try {
         await client.getTokensByWalletAddress({ walletAddress: '0x1234567890', tokenType: token });
+      } catch (error: any) {
+        expect(Blockscout.isBlockscoutError(error)).toBe(true);
+        expect((error as BlockscoutError).code).toEqual(HttpStatusCode.InternalServerError);
+        expect((error as BlockscoutError).message).toEqual('InternalServerError');
+      }
+    });
+  });
+
+  describe('getNativeTokenByWalletAddress', () => {
+    it('success', async () => {
+      const mockResponse = {
+        status: 200,
+        data: {
+          block_number_balance_updated_at: 1062475,
+          coin_balance: '55290000000000000000',
+          exchange_rate: '0.569',
+          hash: '0x933d4CE1B6334d2Ede312765Dc31e3105CA28e31',
+        },
+      };
+      mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+      const client = new Blockscout({ chainId: ChainId.IMTBL_ZKEVM_TESTNET });
+      const resp = await client.getNativeTokenByWalletAddress(
+        {
+          walletAddress: '0x1234567890',
+        },
+      );
+
+      expect(resp.value).toEqual('55290000000000000000');
+      expect(resp.token.address).toEqual(IMX_ADDRESS_ZKEVM);
+
+      expect(mockedAxios.get).toHaveBeenNthCalledWith(
+        1,
+        `${client.url}/api/v2/addresses/0x1234567890`,
+      );
+    });
+
+    it('success cached', async () => {
+      const mockResponse = {
+        status: 200,
+        data:
+          {
+            coin_balance: null,
+          },
+      };
+      mockedAxios.get.mockResolvedValue(mockResponse);
+
+      const client = new Blockscout({ chainId: ChainId.IMTBL_ZKEVM_TESTNET });
+
+      await client.getNativeTokenByWalletAddress({ walletAddress: '0x1234567890' });
+      await client.getNativeTokenByWalletAddress({ walletAddress: '0x1234567890' });
+
+      expect(mockedAxios.get).toHaveBeenNthCalledWith(
+        1,
+        `${client.url}/api/v2/addresses/0x1234567890`,
+      );
+    });
+
+    it('success zero TTL', async () => {
+      const mockResponse = {
+        status: 200,
+        data:
+          {
+            coin_balance: null,
+          },
+      };
+      mockedAxios.get.mockResolvedValue(mockResponse);
+
+      const client = new Blockscout({ chainId: ChainId.IMTBL_ZKEVM_TESTNET, ttl: 0 });
+
+      await client.getNativeTokenByWalletAddress({ walletAddress: '0x1234567890' });
+      await client.getNativeTokenByWalletAddress({ walletAddress: '0x1234567890' });
+
+      expect(mockedAxios.get).toHaveBeenNthCalledWith(
+        2,
+        `${client.url}/api/v2/addresses/0x1234567890`,
+      );
+    });
+
+    it('fails 400', async () => {
+      const mockResponse = {
+        status: HttpStatusCode.BadRequest,
+        statusText: 'error',
+        data: {},
+      };
+      mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+      const client = new Blockscout({ chainId: ChainId.IMTBL_ZKEVM_TESTNET });
+      try {
+        await client.getNativeTokenByWalletAddress({ walletAddress: '0x1234567890' });
+      } catch (error: any) {
+        expect(Blockscout.isBlockscoutError(error)).toBe(true);
+        expect((error as BlockscoutError).code).toEqual(HttpStatusCode.BadRequest);
+        expect((error as BlockscoutError).message).toEqual('error');
+      }
+    });
+
+    it('fails 500', async () => {
+      const mockResponse = {
+        status: HttpStatusCode.InternalServerError,
+        statusText: 'error',
+        data: {},
+      };
+      mockedAxios.get.mockResolvedValueOnce(mockResponse);
+
+      const client = new Blockscout({ chainId: ChainId.IMTBL_ZKEVM_TESTNET });
+      try {
+        await client.getNativeTokenByWalletAddress({ walletAddress: '0x1234567890' });
+      } catch (error: any) {
+        expect(Blockscout.isBlockscoutError(error)).toBe(true);
+        expect((error as BlockscoutError).code).toEqual(HttpStatusCode.InternalServerError);
+        expect((error as BlockscoutError).message).toEqual('error');
+      }
+    });
+
+    it('throws', async () => {
+      mockedAxios.get.mockRejectedValueOnce('error');
+
+      const client = new Blockscout({ chainId: ChainId.IMTBL_ZKEVM_TESTNET });
+      try {
+        await client.getNativeTokenByWalletAddress({ walletAddress: '0x1234567890' });
       } catch (error: any) {
         expect(Blockscout.isBlockscoutError(error)).toBe(true);
         expect((error as BlockscoutError).code).toEqual(HttpStatusCode.InternalServerError);
