7ca938bbfc0332d52fa8d2af7a6c0a0dc1d672b1
Mimi Tran <80493680+mimi-imtbl@users.noreply.github.com>
2023-09-12 11:32:17 +1000

    Update Primary Rev Widget to review orders dynamically (#834)

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/hooks/useSignOrder.ts
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/hooks/useSignOrder.ts
@@ -1,46 +1,58 @@
 /* eslint-disable no-console */
-/* eslint-disable no-await-in-loop */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/naming-convention */
 import { useCallback, useEffect, useState } from 'react';
 import { Web3Provider } from '@ethersproject/providers';
 import { PrimaryRevenueSuccess } from '@imtbl/checkout-widgets';
 
-type SignDataType = {
+export enum PaymentType {
+  CRYPTO = 'crypto',
+  FIAT = 'fiat',
+}
+
+export type SignProductResponse = {
+  product_id: string;
+  detail: {
+    amount: number;
+    collector_address: string;
+    token_id: string;
+  }[];
+};
+
+export type SignOrderResponse = {
+  currency: string;
+  total_amount: number;
+  products: SignProductResponse[];
+};
+
+export type SignResponse = {
+  order: SignOrderResponse;
   transactions: {
     contract_address: string;
+    gas_estimate: number;
     method_call: string;
     params: {
-      amount?: number;
-      spender?: string;
-      data?: string[];
-      deadline?: number;
-      multicallSigner?: string;
-      reference?: string;
-      signature?: string;
-      targets?: string[];
+      amount: number;
+      spender: string;
     };
     raw_data: string;
   }[];
 };
 
-type Input = {
+type SignOrderInput = {
   items: {
     productId: string;
     qty: number;
   }[];
   fromCurrency?: string;
-  paymentType?: string;
   provider: Web3Provider | undefined;
 };
 
 export const useSignOrder = ({
   items,
   fromCurrency,
-  paymentType,
   provider,
-}: Input) => {
-  const [signData, setSignData] = useState<SignDataType | undefined>();
+}: SignOrderInput) => {
+  const [signData, setSignData] = useState<SignResponse | undefined>();
   const [recipientAddress, setRecipientAddress] = useState<string | undefined>(
     undefined,
   );
@@ -77,44 +89,49 @@ export const useSignOrder = ({
     [provider],
   );
 
-  const sign = useCallback(async (): Promise<void> => {
-    const data = {
-      recipient_address: recipientAddress,
-      currency: fromCurrency,
-      payment_type: paymentType,
-      products: items.map((item) => ({
-        product_id: item.productId,
-        quantity: item.qty,
-      })),
-    };
+  const sign = useCallback(
+    async (paymentType: PaymentType): Promise<SignResponse | undefined> => {
+      console.log('@@@ paymentType', paymentType);
+      const data = {
+        recipient_address: recipientAddress,
+        currency: fromCurrency,
+        payment_type: paymentType,
+        products: items.map((item) => ({
+          product_id: item.productId,
+          quantity: item.qty,
+        })),
+      };
 
-    console.log('@@@ data', data);
-
-    try {
-      const response = await fetch(
-        'https://game-primary-sales.sandbox.imtbl.com/v1/games/pokemon/order/sign',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'x-immutable-api-key': 'sk_imapik-Ekz6cLnnwREtqjGn$xo6_fb97b8',
+      try {
+        const response = await fetch(
+          'https://game-primary-sales.sandbox.imtbl.com/v1/games/pokemon/order/sign',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'x-immutable-api-key': 'sk_imapik-Ekz6cLnnwREtqjGn$xo6_fb97b8', // FIXME: store in api key in env
+            },
+            body: JSON.stringify(data),
           },
-          body: JSON.stringify(data),
-        },
-      );
+        );
 
-      const json = await response.json();
+        if (!response.ok) {
+          // Handle HTTP errors
+          throw new Error(`HTTP Error: ${response.statusText}`);
+        }
 
-      setSignData(json);
-    } catch (error) {
-      //
-    }
-  }, [
-    items,
-    fromCurrency,
-    paymentType,
-    recipientAddress,
-  ]);
+        const json = await response.json();
+        setSignData(json);
+
+        return json;
+      } catch (error) {
+        console.error('Signing order failed:', error);
+        // TODO: Consider setting an error state here
+      }
+      return undefined;
+    },
+    [items, fromCurrency, recipientAddress],
+  );
 
   const execute = useCallback(async (): Promise<PrimaryRevenueSuccess> => {
     if (!signData) {
@@ -129,6 +146,8 @@ export const useSignOrder = ({
         raw_data: data,
         method_call: method,
       } = transaction;
+      // sending transactions one-at-a-time
+      // eslint-disable-next-line no-await-in-loop
       const transactionHash = await sendTx(to, data, 5000000);
 
       if (!transactionHash) {
