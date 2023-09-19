54ef7924927a091d85440befc03faf98f3ee4644
Mikhala <mikhala.kurtjak@immutable.com>
2023-09-20 07:17:02 +0800

    swap route

--- packages/checkout/sdk/src/smartCheckout/routing/routingCalculator.ts
+++ packages/checkout/sdk/src/smartCheckout/routing/routingCalculator.ts
@@ -1,31 +1,83 @@
 import { BigNumber } from 'ethers';
-import { RoutingOptionsAvailable } from '../../types';
-import { BalanceCheckResult, BalanceRequirement } from '../balanceCheck/types';
 import {
+  ChainId,
   FundingRouteType,
+  RoutingOptionsAvailable,
+  TokenInfo,
+} from '../../types';
+import {
+  BalanceCheckResult,
+  BalanceRequirement,
+} from '../balanceCheck/types';
+import {
+  DexQuoteCache,
+  DexQuotes,
+  FundingRouteStep,
   RouteCalculatorType,
   RoutingCalculatorResult,
+  TokenBalanceResult,
 } from './types';
 import { getAllTokenBalances } from './tokenBalances';
 import { bridgeRoute } from './bridge/bridgeRoute';
-import { CheckoutConfiguration } from '../../config';
+import { CheckoutConfiguration, getL2ChainId } from '../../config';
 import { createReadOnlyProviders } from '../../readOnlyProviders/readOnlyProvider';
 import { CheckoutError, CheckoutErrorType } from '../../errors';
+import { swapRoute } from './swap/swapRoute';
+import { allowListCheck } from '../allowList';
 
 export const getInsufficientRequirement = (
   balanceRequirements: BalanceCheckResult,
 ): BalanceRequirement | undefined => {
   let insufficientBalanceCount = 0;
-  let insufficientBridgeRequirement;
+  let insufficientRequirement;
   for (const balanceRequirement of balanceRequirements.balanceRequirements) {
     if (!balanceRequirement.sufficient) {
       insufficientBalanceCount++;
-      insufficientBridgeRequirement = balanceRequirement;
+      insufficientRequirement = balanceRequirement;
     }
   }
-  if (insufficientBalanceCount === 1) return insufficientBridgeRequirement;
+  if (insufficientBalanceCount === 1) return insufficientRequirement;
 
   return undefined;
+};
+
+export const getSwapFundingStep = async (
+  config: CheckoutConfiguration,
+  availableRoutingOptions: RoutingOptionsAvailable,
+  insufficientRequirement: BalanceRequirement | undefined,
+  dexQuoteCache: DexQuoteCache,
+  ownerAddress: string,
+  tokenBalances: Map<ChainId, TokenBalanceResult>,
+  swapTokenAllowList: TokenInfo[] | undefined,
+): Promise<FundingRouteStep | undefined> => {
+  if (!availableRoutingOptions.swap) return undefined;
+  if (insufficientRequirement === undefined) return undefined;
+  if (swapTokenAllowList === undefined) return undefined;
+
+  const tokenBalanceResult = tokenBalances.get(getL2ChainId(config));
+  if (!tokenBalanceResult) return undefined;
+  if (tokenBalanceResult.error !== undefined || !tokenBalanceResult.success) return undefined;
+
+  if (swapTokenAllowList.length === 0) return undefined;
+  const swappableTokens: string[] = [];
+  for (const token of swapTokenAllowList) {
+    if (token.address === undefined) continue;
+    if (token.address === '') continue;
+    swappableTokens.push(token.address);
+  }
+  if (swappableTokens.length === 0) return undefined;
+
+  const swapFundingStep = await swapRoute(
+    config,
+    availableRoutingOptions,
+    dexQuoteCache,
+    ownerAddress,
+    insufficientRequirement,
+    tokenBalances,
+    swappableTokens,
+  );
+
+  return swapFundingStep;
 };
 
 export const routingCalculator = async (
@@ -52,12 +104,18 @@ export const routingCalculator = async (
     availableRoutingOptions,
   );
 
+  const allowList = await allowListCheck(config, tokenBalances, availableRoutingOptions);
+
   // Bridge and swap fee cache
   const feeEstimates = new Map<FundingRouteType, BigNumber>();
 
-  // Ensures only 1 balance requirement is insufficient otherwise one bridge route cannot be recommended
-  let bridgeFundingStep;
+  // Dex quotes cache
+  const dexQuoteCache: DexQuoteCache = new Map<string, DexQuotes>();
+
+  // Ensures only 1 balance requirement is insufficient otherwise one bridge or one swap route cannot be recommended
   const insufficientRequirement = getInsufficientRequirement(balanceRequirements);
+
+  let bridgeFundingStep;
   if (availableRoutingOptions.bridge && insufficientRequirement) {
     bridgeFundingStep = await bridgeRoute(
       config,
@@ -70,12 +128,44 @@ export const routingCalculator = async (
     );
   }
 
+  const swapFundingStep = await getSwapFundingStep(
+    config,
+    availableRoutingOptions,
+    insufficientRequirement,
+    dexQuoteCache,
+    ownerAddress,
+    tokenBalances,
+    allowList.swap,
+  );
+
   // Check on-ramp routes
 
   // Check swap routes
   // > Could bridge first
   // > Could on-ramp first
   // > Could double swap
+
+  // todo: cleanup below if/else to a better way
+
+  if (bridgeFundingStep && swapFundingStep) {
+    return {
+      availableOptions: [],
+      response: {
+        type: RouteCalculatorType.ROUTES_FOUND,
+        message: 'Routes found',
+      },
+      fundingRoutes: [
+        {
+          priority: 1,
+          steps: [bridgeFundingStep],
+        },
+        {
+          priority: 2,
+          steps: [swapFundingStep],
+        },
+      ],
+    };
+  }
 
   if (bridgeFundingStep) {
     return {
@@ -88,6 +178,22 @@ export const routingCalculator = async (
         priority: 1,
         steps: [bridgeFundingStep],
       }],
+    };
+  }
+
+  if (swapFundingStep) {
+    return {
+      availableOptions: [],
+      response: {
+        type: RouteCalculatorType.ROUTES_FOUND,
+        message: 'Routes found',
+      },
+      fundingRoutes: [
+        {
+          priority: 1,
+          steps: [swapFundingStep],
+        },
+      ],
     };
   }
 
