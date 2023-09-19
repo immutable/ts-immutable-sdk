c4dc18fc1b77f22318ebc76446bd11e29abecc2e
Mimi Tran <80493680+mimi-imtbl@users.noreply.github.com>
2023-09-19 12:53:17 +1000

    GPR-159/Primary rev widget mint multiple items playground (#871)

--- packages/internal/economy-building-blocks/playground/src/components/ItemCard.tsx
+++ packages/internal/economy-building-blocks/playground/src/components/ItemCard.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-import { Card } from "@biom3/react";
+import React, { useEffect, useState } from "react";
+import { Body, Box, Button, Card, StatefulButtCon } from "@biom3/react";
 import { NFT } from "@imtbl/generated-clients/dist/multi-rollup";
-
-const selectedStyle = {
-  border: "base.border.size.100 solid",
-  borderColor: "base.color.accent.1",
-};
 
 function ItemCard({
   nft,
@@ -13,29 +8,57 @@ function ItemCard({
   isSelected,
 }: {
   nft: any;
-  onClick?: (nft: NFT) => void;
+  onClick?: (nft: NFT, quantity: number) => void;
   isSelected?: (nft: NFT) => boolean;
 }) {
+  const [quantity, setQuantity] = useState<number>(0);
+
   const onCardClick = (nft: NFT) => {
-    onClick && onClick(nft);
+    if (quantity === 0) return;
+
+    onClick && onClick(nft, quantity);
   };
 
   return (
-    <Card
-      onClick={() => onCardClick(nft)}
-      sx={isSelected && isSelected(nft) ? selectedStyle : {}}
-    >
-      <Card.Title>
-        <div>{nft.name}</div>
-        <div>Token {nft.token_id}</div>
-      </Card.Title>
-      <Card.Caption>USDC ${nft.price}</Card.Caption>
-      <Card.AssetImage
-        imageUrl={nft.image}
-        aspectRatio="4:3"
-        relativeImageSizeInLayout="60vw"
-      />
-    </Card>
+    <Box>
+      <Card>
+        <Card.Title>
+          <div>{nft.name}</div>
+          <div>Token {nft.token_id}</div>
+        </Card.Title>
+        <Card.Caption>USDC ${nft.price}</Card.Caption>
+        <Card.AssetImage
+          imageUrl={nft.image}
+          aspectRatio="4:3"
+          relativeImageSizeInLayout="60vw"
+        />
+      </Card>
+      <Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <StatefulButtCon
+            icon="Minus"
+            onClick={() => {
+              quantity > 0 ? setQuantity(quantity - 1) : setQuantity(0);
+            }}
+          />
+          <Body>{quantity}</Body>
+          <StatefulButtCon
+            icon="Add"
+            onClick={() => setQuantity(quantity + 1)}
+          />
+          <Button size={"medium"} onClick={() => onCardClick(nft)}>
+            Add
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
