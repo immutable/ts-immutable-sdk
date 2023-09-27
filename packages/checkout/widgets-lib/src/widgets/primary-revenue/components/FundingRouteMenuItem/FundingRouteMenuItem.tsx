678a382e3e2801049e1eae06ad71161b267ca186
James Hardwick <james.hardwick@immutable.com>
2023-09-27 11:35:50 +1000

    Adds SC view types.
    Adds FundWithSmartCheckout view + initial components.
    
    Adds a cypress test for FundWithSmartCheckout template switching.
    
    removing dev code.
    
    Fixes var naming.
    
    migrates select + drawer code.
    
    Adds a wrapper for rendering SC options as MenuItems.
    
    Adds a component to display current purchase as a MenuItem.
    
    Adds FundingRouteMenuItem + PurchaseMenuItem to FundingRouteSelect.
    
    Adds UI for bottom sheet drawer.
    
    Adds cypress test for funding route selection.
    
    removes mock data.
    
    removes some eslint comments.
    
    Adds empty state for funding routes.

---
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/components/FundingRouteMenuItem/FundingRouteMenuItem.tsx
@@ -0,0 +1,35 @@
+import { ETH_TOKEN_IMAGE_URL, Heading, MenuItem } from '@biom3/react';
+
+export interface FundingRouteMenuItemProps {
+  onClick: () => void;
+  fundingRoute: any;
+  toggleVisible?: boolean;
+  selected?: boolean;
+}
+export function FundingRouteMenuItem({
+  onClick, fundingRoute, toggleVisible, selected,
+}: FundingRouteMenuItemProps) {
+  return (
+    <MenuItem
+      testId="funding-route-menu-item"
+      onClick={onClick}
+      selected={selected}
+      size="small"
+    >
+      {toggleVisible && <MenuItem.IntentIcon icon="ChevronExpand" />}
+      <MenuItem.FramedImage imageUrl={ETH_TOKEN_IMAGE_URL} />
+      <MenuItem.PriceDisplay
+        use={<Heading size="xSmall" />}
+        price="0.0252565"
+        fiatAmount="USD $2.49"
+        currencyImageUrl={ETH_TOKEN_IMAGE_URL}
+      />
+      <MenuItem.Label>
+        { fundingRoute.steps[0].asset.token.symbol}
+      </MenuItem.Label>
+      <MenuItem.Caption>
+        Fees - USD $0.10
+      </MenuItem.Caption>
+    </MenuItem>
+  );
+}
--- packages/checkout/widgets-lib/src/widgets/primary-revenue/components/FundingRouteSelectDrawer/FundingRouteDrawer.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/components/FundingRouteSelectDrawer/FundingRouteDrawer.tsx
@@ -1,11 +1,44 @@
-import { Box } from '@biom3/react';
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
-export function FundingRouteDrawer() {
+import {
+  BottomSheet,
+} from '@biom3/react';
+import { FundingRouteMenuItem } from '../FundingRouteMenuItem/FundingRouteMenuItem';
+
+type FundingRouteDrawerProps = {
+  visible: boolean;
+  onCloseBottomSheet: (selectedFundingRouteIndex: number) => void;
+  fundingRoutes: any;
+  activeFundingRouteIndex: number;
+};
+
+export function FundingRouteDrawer({
+  visible, onCloseBottomSheet, fundingRoutes, activeFundingRouteIndex,
+}:
+FundingRouteDrawerProps) {
+  const onClickMenuItem = (selectedFundingRouteIndex: number) => {
+    onCloseBottomSheet(selectedFundingRouteIndex);
+  };
+
   return (
-    <Box>
-      <p>
-        hello world from FundingRouteDrawer
-      </p>
-    </Box>
+    <BottomSheet
+      size="full"
+      onCloseBottomSheet={() => onCloseBottomSheet(activeFundingRouteIndex)}
+      visible={visible}
+      showHeaderBar
+      headerBarTitle="Available balance"
+    >
+      <BottomSheet.Content>
+        {fundingRoutes.map((fundingRoute: any, i: number) => (
+          <FundingRouteMenuItem
+            onClick={() => onClickMenuItem(i)}
+            fundingRoute={fundingRoute}
+            selected={activeFundingRouteIndex === i}
+            key={fundingRoute.priority}
+          />
+        ))}
+      </BottomSheet.Content>
+    </BottomSheet>
   );
 }
