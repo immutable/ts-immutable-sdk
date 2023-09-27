2d360db6396ab01ed463b780bb32989a4d7debe7
James Hardwick <james.hardwick@immutable.com>
2023-09-27 15:25:55 +1000

    removing dev code.

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/views/FundWithSmartCheckout.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/views/FundWithSmartCheckout.tsx
@@ -1,51 +1,16 @@
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { Box } from '@biom3/react';
-import { useContext, useEffect } from 'react';
 import {
   FundWithSmartCheckoutSubViews,
-  PrimaryRevenueWidgetViews,
 } from '../../../context/view-context/PrimaryRevenueViewContextTypes';
-import { ViewActions, ViewContext } from '../../../context/view-context/ViewContext';
+import { LoadingView } from '../../../views/loading/LoadingView';
 import { FundingRouteExecute } from '../components/FundingRouteExecute/FundingRouteExecute';
 import { FundingRouteSelect } from '../components/FundingRouteSelect/FundingRouteSelect';
-import { useSharedContext } from '../context/SharedContextProvider';
-import { LoadingView } from '../../../views/loading/LoadingView';
-
-/**
- * LOADING - get SignResponse and SmartCheckoutResponse
- */
 
 type FundWithSmartCheckoutProps = {
   subView: FundWithSmartCheckoutSubViews;
 };
 
 export function FundWithSmartCheckout({ subView }: FundWithSmartCheckoutProps) {
-  const { viewDispatch } = useContext(ViewContext);
-
-  const { signResponse } = useSharedContext();
-  const smartCheckoutResponse = undefined; // todo add to shared context
-
-  useEffect(() => {
-    if (!signResponse) {
-      return;
-    }
-    if (subView === FundWithSmartCheckoutSubViews.INIT) {
-      viewDispatch({
-        payload: {
-          type: ViewActions.UPDATE_VIEW,
-          view: {
-            type: PrimaryRevenueWidgetViews.SMART_CHECKOUT,
-            data: {
-              subView: FundWithSmartCheckoutSubViews.FUNDING_ROUTE_SELECT,
-            },
-          },
-        },
-      });
-    }
-  }, [signResponse]);
-
   return (
     <Box>
       <p>
