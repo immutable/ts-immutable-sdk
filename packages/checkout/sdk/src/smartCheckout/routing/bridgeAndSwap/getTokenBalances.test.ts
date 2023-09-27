4d01304c5303d6a2e4bcf1a88d411204967a2cec
Mikhala <mikhala.kurtjak@immutable.com>
2023-09-27 14:28:45 +0800

    add test for getTokenBalances

---
+++ packages/checkout/sdk/src/smartCheckout/routing/bridgeAndSwap/getTokenBalances.test.ts
@@ -0,0 +1,226 @@
+import { BigNumber } from 'ethers';
+import { Environment } from '@imtbl/config';
+import { ChainId } from '../../../types';
+import { getTokenBalances } from './getTokenBalances';
+import { CheckoutConfiguration } from '../../../config';
+
+describe('getTokenBalances', () => {
+  const config = new CheckoutConfiguration({
+    baseConfig: { environment: Environment.SANDBOX },
+  });
+
+  const l1balances = {
+    success: true,
+    balances: [
+      {
+        balance: BigNumber.from(1),
+        formattedBalance: '1',
+        token: {
+          decimals: 18,
+          symbol: 'ETH',
+          name: 'ETH',
+        },
+      },
+      {
+        balance: BigNumber.from(2),
+        formattedBalance: '2',
+        token: {
+          address: '0xIMX',
+          decimals: 18,
+          symbol: 'IMX',
+          name: 'IMX',
+        },
+      },
+    ],
+  };
+
+  const l2balances = {
+    success: true,
+    balances: [
+      {
+        balance: BigNumber.from(3),
+        formattedBalance: '3',
+        token: {
+          address: '0xIMX',
+          decimals: 18,
+          symbol: 'IMX',
+          name: 'IMX',
+        },
+      },
+      {
+        balance: BigNumber.from(4),
+        formattedBalance: '4',
+        token: {
+          address: '0xYEET',
+          decimals: 18,
+          symbol: 'zkYEET',
+          name: 'zkYEET',
+        },
+      },
+    ],
+  };
+
+  it('should return l1balances and l2balances', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.SEPOLIA, l1balances);
+
+    tokenBalances.set(ChainId.IMTBL_ZKEVM_TESTNET, l2balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [
+          {
+            balance: BigNumber.from(1),
+            formattedBalance: '1',
+            token: {
+              decimals: 18,
+              symbol: 'ETH',
+              name: 'ETH',
+            },
+          },
+          {
+            balance: BigNumber.from(2),
+            formattedBalance: '2',
+            token: {
+              address: '0xIMX',
+              decimals: 18,
+              symbol: 'IMX',
+              name: 'IMX',
+            },
+          },
+        ],
+        l2balances: [
+          {
+            balance: BigNumber.from(3),
+            formattedBalance: '3',
+            token: {
+              address: '0xIMX',
+              decimals: 18,
+              symbol: 'IMX',
+              name: 'IMX',
+            },
+          },
+          {
+            balance: BigNumber.from(4),
+            formattedBalance: '4',
+            token: {
+              address: '0xYEET',
+              decimals: 18,
+              symbol: 'zkYEET',
+              name: 'zkYEET',
+            },
+          },
+        ],
+      },
+    );
+  });
+
+  it('should return empty arrays if no l1balances', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.IMTBL_ZKEVM_TESTNET, l2balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [],
+        l2balances: [],
+      },
+    );
+  });
+
+  it('should return empty arrays if l1balances undefined', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.SEPOLIA, undefined);
+    tokenBalances.set(ChainId.IMTBL_ZKEVM_TESTNET, l2balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [],
+        l2balances: [],
+      },
+    );
+  });
+
+  it('should return empty arrays if l1balances has an error', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.SEPOLIA, {
+      error: new Error('error'),
+      balances: [],
+    });
+    tokenBalances.set(ChainId.IMTBL_ZKEVM_TESTNET, l2balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [],
+        l2balances: [],
+      },
+    );
+  });
+
+  it('should return empty arrays if l1balances failed', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.SEPOLIA, {
+      success: false,
+      balances: [],
+    });
+    tokenBalances.set(ChainId.IMTBL_ZKEVM_TESTNET, l2balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [],
+        l2balances: [],
+      },
+    );
+  });
+
+  it('should return empty arrays if no l2balances', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.SEPOLIA, l1balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [],
+        l2balances: [],
+      },
+    );
+  });
+
+  it('should return empty arrays if l2balances has an error', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.IMTBL_ZKEVM_TESTNET, {
+      error: new Error('error'),
+      balances: [],
+    });
+    tokenBalances.set(ChainId.SEPOLIA, l1balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [],
+        l2balances: [],
+      },
+    );
+  });
+
+  it('should return empty arrays if l2balances failed', () => {
+    const tokenBalances = new Map();
+    tokenBalances.set(ChainId.IMTBL_ZKEVM_TESTNET, {
+      success: false,
+      balances: [],
+    });
+    tokenBalances.set(ChainId.SEPOLIA, l1balances);
+
+    const result = getTokenBalances(config, tokenBalances);
+    expect(result).toEqual(
+      {
+        l1balances: [],
+        l2balances: [],
+      },
+    );
+  });
+});
