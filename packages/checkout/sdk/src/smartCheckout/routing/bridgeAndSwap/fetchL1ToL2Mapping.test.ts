f0998ab2aa017009c2749a17dae6e2ee5e8eb8c0
Mikhala <122326421+imx-mikhala@users.noreply.github.com>
2023-09-28 14:53:08 +0800

    WT-1662 Bridge and swap route (#907)
    Co-authored-by: Charlie McKenzie <charlie@dreamoftrees.com>

---
+++ packages/checkout/sdk/src/smartCheckout/routing/bridgeAndSwap/fetchL1ToL2Mapping.test.ts
@@ -0,0 +1,64 @@
+import { Environment } from '@imtbl/config';
+import { CheckoutConfiguration } from '../../../config';
+import { fetchL1Representation } from '../indexer/fetchL1Representation';
+import { fetchL1ToL2Mappings } from './fetchL1ToL2Mappings';
+
+jest.mock('../indexer/fetchL1Representation');
+
+describe('fetchL1ToL2Mappings', () => {
+  const config = new CheckoutConfiguration({
+    baseConfig: { environment: Environment.SANDBOX },
+  });
+
+  it('should fetch the l1 to l2 token mapping', async () => {
+    (fetchL1Representation as jest.Mock)
+      .mockResolvedValueOnce(
+        {
+          l1address: '0xIMXL1',
+          l2address: '0xIMX',
+        },
+      )
+      .mockResolvedValueOnce(
+        {
+          l1address: '0xYEETL1',
+          l2address: '0xYEET',
+        },
+      );
+
+    const mapping = await fetchL1ToL2Mappings(config, [
+      {
+        address: '0xIMX',
+        name: 'IMX',
+        symbol: 'IMX',
+        decimals: 18,
+      },
+      {
+        address: '0xYEET',
+        name: 'zkYEET',
+        symbol: 'zkYEET',
+        decimals: 18,
+      },
+    ]);
+
+    expect(fetchL1Representation).toHaveBeenCalledTimes(2);
+    expect(mapping).toEqual([
+      {
+        l1address: '0xIMXL1',
+        l2address: '0xIMX',
+      },
+      {
+        l1address: '0xYEETL1',
+        l2address: '0xYEET',
+      },
+    ]);
+  });
+
+  it('should return an empty array if no swappable l2 addresses', async () => {
+    (fetchL1Representation as jest.Mock).mockResolvedValue({});
+
+    const mapping = await fetchL1ToL2Mappings(config, []);
+
+    expect(fetchL1Representation).toHaveBeenCalledTimes(0);
+    expect(mapping).toEqual([]);
+  });
+});
