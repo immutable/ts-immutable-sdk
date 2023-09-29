b24f6c5b4b0897bbe0c9c80ae8d5d6f32e2d4a45
James Hardwick <james.hardwick@immutable.com>
2023-09-29 10:35:18 +1000

    removes mock data.

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/views/FundWithSmartCheckout.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/views/FundWithSmartCheckout.tsx
@@ -1,6 +1,4 @@
 import { Box } from '@biom3/react';
-import { ChainId } from '@imtbl/checkout-sdk';
-import { BigNumber } from 'ethers';
 import {
   FundWithSmartCheckoutSubViews,
 } from '../../../context/view-context/PrimaryRevenueViewContextTypes';
@@ -13,45 +11,7 @@ type FundWithSmartCheckoutProps = {
 };
 
 export function FundWithSmartCheckout({ subView }: FundWithSmartCheckoutProps) {
-  // const { signResponse } = useSharedContext();
-  // const signResponse = { todo: 'get a real signresponse' } as any; // todo add to shared context
-  const smartCheckoutResponse = {
-    fundingRoutes: [
-      {
-        priority: 1,
-        steps: [{
-          type: 'BRIDGE',
-          chainId: ChainId.SEPOLIA,
-          asset: {
-            balance: BigNumber.from(1),
-            formattedBalance: '1',
-            token: {
-              name: 'ETH',
-              symbol: 'ETH',
-              decimals: 18,
-            },
-          },
-        }],
-      },
-      {
-        priority: 2,
-        steps: [{
-          type: 'SWAP',
-          chainId: ChainId.IMTBL_ZKEVM_TESTNET,
-          asset: {
-            balance: BigNumber.from(10),
-            formattedBalance: '10',
-            token: {
-              name: 'ERC20',
-              symbol: 'ERC20',
-              decimals: 18,
-              address: '0xERC20_2',
-            },
-          },
-        }],
-      },
-    ],
-  }; // todo add to shared context
+  const smartCheckoutResponse: any = {};
 
   return (
     <Box>
