fd21570837f4fe79753c1a595018a114167e5fdd
Jhonatan Gonzalez <jhesgodi@gmail.com>
2023-10-03 10:24:58 +1100

    [NO-CHANGELOG] feat: primary revenue widget setup - part 1 (#896)

---
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/PrimaryRevenueWidget.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable no-console */
+import {
+  useCallback, useContext, useEffect, useMemo, useReducer,
+} from 'react';
+
+import { BiomeCombinedProviders } from '@biom3/react';
+
+import { LoadingView } from '../../views/loading/LoadingView';
+import { StrongCheckoutWidgetsConfig } from '../../lib/withDefaultWidgetConfig';
+import { text } from '../../resources/text/textConfig';
+import {
+  viewReducer,
+  initialViewState,
+  ViewContext,
+  ViewActions,
+  SharedViews,
+} from '../../context/view-context/ViewContext';
+import { ConnectLoaderContext } from '../../context/connect-loader-context/ConnectLoaderContext';
+
+import { PrimaryRevenueWidgetViews } from '../../context/view-context/PrimaryRevenueViewContextTypes';
+import { Item } from './types';
+import { widgetTheme } from '../../lib/theme';
+
+export interface PrimaryRevenueWidgetProps {
+  config: StrongCheckoutWidgetsConfig;
+  amount: string;
+  items: Item[];
+  fromContractAddress: string;
+  env: string;
+  environmentId: string;
+}
+
+export function PrimaryRevenueWidget(props: PrimaryRevenueWidgetProps) {
+  const {
+    config, amount, items, fromContractAddress, env, environmentId,
+  } = props;
+
+  console.log('@@@ PrimaryRevenueWidget', config, amount, items, fromContractAddress, env, environmentId);
+
+  const { connectLoaderState } = useContext(ConnectLoaderContext);
+  const { checkout, provider } = connectLoaderState;
+
+  const loadingText = text.views[SharedViews.LOADING_VIEW].text;
+
+  const { theme } = config;
+  const biomeTheme = useMemo(() => widgetTheme(theme), [theme]);
+
+  const [viewState, viewDispatch] = useReducer(viewReducer, initialViewState);
+  const viewReducerValues = useMemo(
+    () => ({ viewState, viewDispatch }),
+    [viewState, viewDispatch],
+  );
+
+  const mount = useCallback(() => {
+    if (!checkout || !provider) return;
+
+    viewDispatch({
+      payload: {
+        type: ViewActions.UPDATE_VIEW,
+        view: {
+          type: PrimaryRevenueWidgetViews.PAYMENT_METHODS,
+        },
+      },
+    });
+  }, [checkout, provider]);
+
+  useEffect(() => {
+    if (!checkout || !provider) return;
+    mount();
+  }, [checkout, provider]);
+
+  return (
+    <BiomeCombinedProviders theme={{ base: biomeTheme }}>
+      <ViewContext.Provider value={viewReducerValues}>
+        {viewState.view.type === SharedViews.LOADING_VIEW && (
+          <LoadingView loadingText={loadingText} />
+        )}
+        Primary Revenue Widget
+      </ViewContext.Provider>
+    </BiomeCombinedProviders>
+  );
+}
--- packages/checkout/widgets/src/definitions/global.ts
+++ packages/checkout/widgets/src/definitions/global.ts
@@ -28,6 +28,7 @@ declare global {
       'imtbl-swap': ImtblSwapProps;
       'imtbl-bridge': ImtblBridgeProps;
       'imtbl-onramp': ImtblOnRampProps;
+      'imtbl-primary-revenue': ImtblPrimaryRevenueProps;
     }
   }
 
@@ -133,4 +134,29 @@ export interface ImtblOnRampProps
   widgetConfig?: string;
   amount?: string;
   contractAddress?: string;
+}
+
+/**
+ * Interface for the properties of the primary revenue web component.
+ * Extends the React.DetailedHTMLProps interface to inherit HTML attributes for an HTMLElement.
+ * @interface ImtblPrimaryRevenueProps
+ * @extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+ * @property {string | undefined} widgetConfig - Optional string representing the widget configuration.
+ * @property {string | undefined} amount - The amount to be paid.
+ * @property {string | undefined} products - A base64 encoded string of the array of items to be purchased.
+ * @property {string | undefined} fromContractAddress - The contract address of the token to pay with.
+ * @property {string | undefined} env - The environment to use: SANDBOX, DEV, PRODUCTION, ...
+ * @property {string | undefined} environmentId - The environment id from Immutable Hub.
+ */
+export interface ImtblPrimaryRevenueProps
+  extends React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLElement>,
+  HTMLElement
+  > {
+  widgetConfig?: string;
+  amount: string;
+  products: string;
+  fromContractAddress: string;
+  env: string;
+  environmentId: string;
 }
