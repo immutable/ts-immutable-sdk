94de1535a15624fde6618d31c09d1ab042c6dede
Mimi Immutable <mimi.tran@immutable.com>
2023-10-05 11:52:30 +1100

    Add different failure screens

--- packages/checkout/widgets-lib/src/widgets/primary-revenue/PrimaryRevenueWidget.tsx
+++ packages/checkout/widgets-lib/src/widgets/primary-revenue/PrimaryRevenueWidget.tsx
@@ -18,12 +18,33 @@ import {
 import { ConnectLoaderContext } from '../../context/connect-loader-context/ConnectLoaderContext';
 
 import { PrimaryRevenueWidgetViews } from '../../context/view-context/PrimaryRevenueViewContextTypes';
-import { Item } from './types';
+import { Item, MintErrorTypes } from './types';
 import { widgetTheme } from '../../lib/theme';
 import { SharedContextProvider } from './context/SharedContextProvider';
 import { PaymentMethods } from './views/PaymentMethods';
 import { PayWithCard } from './views/PayWithCard';
 import { PayWithCoins } from './views/PayWithCoins';
+import {
+  StatusView,
+  StatusViewProps,
+} from '../../components/Status/StatusView';
+import { StatusType } from '../../components/Status/StatusType';
+import { EventTargetContext } from '../../context/event-target-context/EventTargetContext';
+import { sendPrimaryRevenueWidgetCloseEvent } from './PrimaryRevenueWidgetEvents';
+
+interface ErrorHandlerConfig {
+  onActionClick?: () => void;
+  onSecondaryActionClick?: () => void;
+  statusType: StatusType;
+}
+
+interface ErrorTextConfig {
+  description: string;
+  primaryAction?: string;
+  secondaryAction?: string;
+}
+
+type AllErrorTextConfigs = Record<MintErrorTypes, ErrorTextConfig>;
 
 export interface PrimaryRevenueWidgetProps {
   config: StrongCheckoutWidgetsConfig;
@@ -39,7 +60,15 @@ export function PrimaryRevenueWidget(props: PrimaryRevenueWidgetProps) {
     config, amount, items, fromContractAddress, env, environmentId,
   } = props;
 
-  console.log('@@@ PrimaryRevenueWidget', config, amount, items, fromContractAddress, env, environmentId);
+  console.log(
+    '@@@ PrimaryRevenueWidget',
+    config,
+    amount,
+    items,
+    fromContractAddress,
+    env,
+    environmentId,
+  );
 
   const { connectLoaderState } = useContext(ConnectLoaderContext);
   const { checkout, provider } = connectLoaderState;
@@ -54,6 +83,10 @@ export function PrimaryRevenueWidget(props: PrimaryRevenueWidgetProps) {
     () => ({ viewState, viewDispatch }),
     [viewState, viewDispatch],
   );
+
+  const {
+    eventTargetState: { eventTarget },
+  } = useContext(EventTargetContext);
 
   const onMount = useCallback(() => {
     if (!checkout || !provider) return;
@@ -73,6 +106,85 @@ export function PrimaryRevenueWidget(props: PrimaryRevenueWidgetProps) {
 
     onMount();
   }, [checkout, provider]);
+
+  const updateToPaymentMethods = () => {
+    viewDispatch({
+      payload: {
+        type: ViewActions.UPDATE_VIEW,
+        view: {
+          type: PrimaryRevenueWidgetViews.PAYMENT_METHODS,
+        },
+      },
+    });
+  };
+
+  const closeWidget = () => {
+    sendPrimaryRevenueWidgetCloseEvent(eventTarget);
+  };
+
+  const errorHandlersConfig: Record<MintErrorTypes, ErrorHandlerConfig> = {
+    [MintErrorTypes.TRANSACTION_FAILED]: {
+      onActionClick: updateToPaymentMethods,
+      onSecondaryActionClick: () => {
+        /* redirects to Immutascan to check the transaction */
+      },
+      statusType: StatusType.FAILURE,
+    },
+    [MintErrorTypes.SERVICE_BREAKDOWN]: {
+      onSecondaryActionClick: closeWidget,
+      statusType: StatusType.WARNING,
+    },
+    [MintErrorTypes.TRANSAK_FAILED]: {
+      onActionClick: () => {
+        /* TODO: start over the transak flow */
+      },
+      onSecondaryActionClick: closeWidget,
+      statusType: StatusType.WARNING,
+    },
+    [MintErrorTypes.PASSPORT_FAILED]: {
+      onActionClick: updateToPaymentMethods,
+      onSecondaryActionClick: closeWidget,
+      statusType: StatusType.WARNING,
+    },
+    [MintErrorTypes.PASSPORT_REJECTED_NO_FUNDS]: {
+      onActionClick: updateToPaymentMethods,
+      onSecondaryActionClick: closeWidget,
+      statusType: StatusType.WARNING,
+    },
+    [MintErrorTypes.PASSPORT_REJECTED]: {
+      onActionClick: () => {
+        /* TODO: trigger the approve and execute flow pop up flow again */
+      },
+      onSecondaryActionClick: closeWidget,
+      statusType: StatusType.WARNING,
+    },
+  };
+
+  const determineStatusViewProps = (): StatusViewProps => {
+    console.log(
+      'text[PrimaryRevenueWidgetViews.FAIL]',
+      text.views[PrimaryRevenueWidgetViews.FAIL],
+    );
+    console.log(
+      '@@@@@@@@@@@@@@@@@@@@@viewState.view.data.error',
+      viewState.view.data.error,
+    );
+    const errorTextConfig: AllErrorTextConfigs = text.views[PrimaryRevenueWidgetViews.FAIL].errors;
+    const errorType = viewState.view.data.error as MintErrorTypes;
+
+    const handlers = errorHandlersConfig[errorType] || {};
+
+    return {
+      statusText: errorTextConfig[errorType].description,
+      actionText: errorTextConfig[errorType]?.primaryAction,
+      onActionClick: handlers?.onActionClick,
+      secondaryActionText: errorTextConfig[errorType].secondaryAction,
+      onSecondaryActionClick: handlers?.onSecondaryActionClick,
+      onCloseClick: closeWidget,
+      statusType: handlers.statusType,
+      testId: 'fail-view',
+    };
+  };
 
   return (
     <BiomeCombinedProviders theme={{ base: biomeTheme }}>
@@ -96,14 +208,26 @@ export function PrimaryRevenueWidget(props: PrimaryRevenueWidgetProps) {
             <div>{viewState.view.error.message}</div>
           )}
           {viewState.view.type
-            === PrimaryRevenueWidgetViews.PAYMENT_METHODS && (
-            <PaymentMethods />
-          )}
+            === PrimaryRevenueWidgetViews.PAYMENT_METHODS && <PaymentMethods />}
           {viewState.view.type === PrimaryRevenueWidgetViews.PAY_WITH_CARD && (
             <PayWithCard />
           )}
           {viewState.view.type === PrimaryRevenueWidgetViews.PAY_WITH_COINS && (
             <PayWithCoins />
+          )}
+          {viewState.view.type === PrimaryRevenueWidgetViews.SUCCESS && (
+            <StatusView
+              statusText="Connection secure"
+              actionText="Continue"
+              statusType={StatusType.SUCCESS}
+              testId="success-view"
+              onActionClick={() => {}}
+              secondaryActionText="Dismiss"
+              onSecondaryActionClick={() => {}}
+            />
+          )}
+          {viewState.view.type === PrimaryRevenueWidgetViews.FAIL && (
+            <StatusView {...determineStatusViewProps()} />
           )}
         </SharedContextProvider>
       </ViewContext.Provider>
