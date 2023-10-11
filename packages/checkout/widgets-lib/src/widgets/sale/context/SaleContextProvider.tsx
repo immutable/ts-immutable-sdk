71ddcd67927d5883db194bf0bae28a289f53a4d4
Jhon Gonzalez <jhon.gonzalez@immutable.com>
2023-10-11 14:00:04 +1100

    add fn to help route errors

--- packages/checkout/widgets-lib/src/widgets/sale/context/SaleContextProvider.tsx
+++ packages/checkout/widgets-lib/src/widgets/sale/context/SaleContextProvider.tsx
@@ -6,15 +6,23 @@ import {
   useEffect,
   useState,
   useCallback,
+  useRef,
 } from 'react';
 import { Passport } from '@imtbl/passport';
 
 import { SaleSuccess } from '@imtbl/checkout-widgets';
 
-import { Item, PaymentTypes, SignResponse } from '../types';
+import {
+  Item, PaymentTypes, SignResponse, MintErrorTypes,
+} from '../types';
 import { useSignOrder } from '../hooks/useSignOrder';
 import { ConnectLoaderState } from '../../../context/connect-loader-context/ConnectLoaderContext';
 import { StrongCheckoutWidgetsConfig } from '../../../lib/withDefaultWidgetConfig';
+import {
+  ViewActions,
+  ViewContext,
+} from '../../../context/view-context/ViewContext';
+import { SaleWidgetViews } from '../../../context/view-context/SaleViewContextTypes';
 
 type SaleContextProps = {
   config: StrongCheckoutWidgetsConfig;
@@ -37,6 +45,8 @@ type SaleContextValues = SaleContextProps & {
   isPassportWallet: boolean;
   paymentMethod: PaymentTypes | undefined;
   setPaymentMethod: (paymentMethod: PaymentTypes) => void;
+  goBackToPaymentMethods: (paymentMethod?: PaymentTypes | undefined) => void;
+  goToErrorView: (type: MintErrorTypes, data?: Record<string, unknown>) => void;
 };
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -57,10 +67,15 @@ const SaleContext = createContext<SaleContextValues>({
   isPassportWallet: false,
   paymentMethod: undefined,
   setPaymentMethod: () => {},
+  goBackToPaymentMethods: () => {},
+  goToErrorView: () => {},
   config: {} as StrongCheckoutWidgetsConfig,
 });
 
 SaleContext.displayName = 'SaleSaleContext';
+
+/** Max attemps to retry with same payment method */
+const MAX_ERROR_RETRIES = 1;
 
 export function SaleContextProvider(props: {
   children: ReactNode;
@@ -81,6 +96,8 @@ export function SaleContextProvider(props: {
     },
   } = props;
 
+  const errorRetries = useRef(0);
+  const { viewDispatch } = useContext(ViewContext);
   const [{ recipientEmail, recipientAddress }, setUserInfo] = useState<{
     recipientEmail: string;
     recipientAddress: string;
@@ -91,11 +108,41 @@ export function SaleContextProvider(props: {
 
   const [paymentMethod, setPaymentMethod] = useState<PaymentTypes | undefined>(undefined);
 
-  // Get user info
+  const goBackToPaymentMethods = useCallback((type?: PaymentTypes | undefined) => {
+    setPaymentMethod(type);
+    viewDispatch({
+      payload: {
+        type: ViewActions.UPDATE_VIEW,
+        view: { type: SaleWidgetViews.PAYMENT_METHODS },
+      },
+    });
+  }, []);
+
+  const goToErrorView = useCallback(
+    (errorType: MintErrorTypes, data: Record<string, unknown> = {}) => {
+      errorRetries.current += 1;
+      if (errorRetries.current > MAX_ERROR_RETRIES) {
+        errorRetries.current = 0;
+        setPaymentMethod(undefined);
+      }
+
+      viewDispatch({
+        payload: {
+          type: ViewActions.UPDATE_VIEW,
+          view: {
+            type: SaleWidgetViews.MINT_FAIL,
+            data: { errorType, ...data },
+          },
+        },
+      });
+    },
+    [],
+  );
+
   useEffect(() => {
     const getUserInfo = async () => {
       const signer = provider?.getSigner();
-      const address = await signer?.getAddress() || '';
+      const address = (await signer?.getAddress()) || '';
       const email = (await passport?.getUserInfo())?.email || '';
 
       setUserInfo({ recipientEmail: email, recipientAddress: address });
@@ -113,11 +160,19 @@ export function SaleContextProvider(props: {
     env,
   });
 
-  const sign = useCallback(async (type: PaymentTypes, callback?: (r?: SignResponse) => void) => {
-    const response = await signOrder(type);
-    callback?.(response);
-    return response;
-  }, [signOrder]);
+  const sign = useCallback(
+    async (
+      type: PaymentTypes,
+      callback?: (r?: SignResponse) => void,
+    ): Promise<SignResponse | undefined> => {
+      const response = await signOrder(type);
+      if (!response) return undefined;
+
+      callback?.(response);
+      return response;
+    },
+    [signOrder],
+  );
 
   const values = useMemo(
     () => ({
@@ -136,6 +191,8 @@ export function SaleContextProvider(props: {
       recipientEmail,
       paymentMethod,
       setPaymentMethod,
+      goBackToPaymentMethods,
+      goToErrorView,
       isPassportWallet: !!(provider?.provider as any)?.isPassport,
     }),
     [
@@ -152,6 +209,8 @@ export function SaleContextProvider(props: {
       signResponse,
       paymentMethod,
       signResponse,
+      goBackToPaymentMethods,
+      goToErrorView,
     ],
   );
 
