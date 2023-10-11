3fd4d9f81c9cdd39d66142639cd2a35bb6b097eb
Allan Almeida <allan.almeida@immutable.com>
2023-10-11 15:08:22 +1100

    add example

--- packages/blockchain-data/sdk/src/index.ts
+++ packages/blockchain-data/sdk/src/index.ts
@@ -1,4 +1,5 @@
 import { mr } from '@imtbl/generated-clients';
+import * as types from '@imtbl/generated-clients/dist/multi-rollup/types';
 import { APIError } from './types/errors';
 import { BlockchainData } from './blockchain-data';
 import { BlockchainDataModuleConfiguration } from './config';
@@ -6,6 +7,7 @@ import { BlockchainDataModuleConfiguration } from './config';
 type ActivityType = mr.ActivityType;
 
 export {
+  types,
   APIError,
   BlockchainData,
   BlockchainDataModuleConfiguration,
