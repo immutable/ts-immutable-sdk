6b2a4abcfd27749e2ee981f1c180407464bfa318
Keith Broughton <keith.broughton@immutable.com>
2023-10-18 12:34:48 +1100

    TokenInfo -> ERC20

--- packages/internal/dex/sdk/src/constants/tokens/immutable-testnet.ts
+++ packages/internal/dex/sdk/src/constants/tokens/immutable-testnet.ts
@@ -1,14 +1,15 @@
 import { IMMUTABLE_TESTNET_CHAIN_ID } from 'constants/chains';
-import { TokenInfo } from 'types';
+import { ERC20 } from 'types';
 
-export const TIMX_IMMUTABLE_TESTNET: TokenInfo = {
+export const TIMX_IMMUTABLE_TESTNET: ERC20 = {
   chainId: IMMUTABLE_TESTNET_CHAIN_ID,
   address: '0x0000000000000000000000000000000000001010',
   decimals: 18,
   symbol: 'tIMX',
   name: 'Immutable Testnet Token',
+  type: 'erc20',
 };
 
-export const IMMUTABLE_TESTNET_COMMON_ROUTING_TOKENS: TokenInfo[] = [
+export const IMMUTABLE_TESTNET_COMMON_ROUTING_TOKENS: ERC20[] = [
   TIMX_IMMUTABLE_TESTNET,
 ];
