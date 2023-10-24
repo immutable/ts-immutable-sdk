f30ed1df61d74f2081b8d7308d019242ddd28bdd
Zach Couchman <zachary.couchman@immutable.com>
2023-10-24 14:10:59 +1100

    update sample app code

--- packages/checkout/widgets-sample-app/src/components/ui/connect/connect.tsx
+++ packages/checkout/widgets-sample-app/src/components/ui/connect/connect.tsx
@@ -5,55 +5,31 @@ import {
   ConnectTargetLayer,
   WidgetType,
   WidgetTheme,
-  Widget
+  Widget,
+  ConnectEventType
 } from '@imtbl/checkout-sdk';
 import { WidgetsFactory } from '@imtbl/checkout-widgets';
 
+const CONNECT_TARGET_ID = "connect-widget-target";
 function ConnectUI() {
-  const [provider, setProvider] = useState();
   const checkout = useMemo(() => new Checkout(), []);
-  
-  const [connectWidget, setConnectWidget] = useState<Widget | undefined>();
-  const [factory, setFactory] = useState<any>();
-  const [theme, setTheme] = useState(WidgetTheme.DARK);
-  const firstRender = useRef(true);
+  const factory = useMemo(() => new WidgetsFactory(checkout, {theme: WidgetTheme.DARK}), []);
+  const connect = useMemo(() => factory.create(WidgetType.CONNECT, {targetLayer: ConnectTargetLayer.LAYER2}), [factory]);
+  const [provider, setProvider] = useState();
   
   useEffect(() => {
-    if(firstRender.current){
-      firstRender.current = false;
-      (async () => {
-        const factory = new WidgetsFactory(checkout, {theme: WidgetTheme.DARK})
-        const connect = factory.create(WidgetType.CONNECT, {targetLayer: ConnectTargetLayer.LAYER2})
-        const bridge = factory.create(WidgetType.BRIDGE, {fromContractAddress: "0x2Fa06C6672dDCc066Ab04631192738799231dE4a"})
-
-        setFactory(factory);
-        setConnectWidget(connect);
-        connect.mount("connect", {})
-        bridge.mount("bridge", {})
-        
-      })()
-    }
-    
-  }, [checkout, firstRender])
-
-  const updateTheme = useCallback((widgetTheme: WidgetTheme) => {
-    let newTheme = widgetTheme === WidgetTheme.DARK ? WidgetTheme.LIGHT : WidgetTheme.DARK;
-    connectWidget?.updateConfig({theme: newTheme});
-    setTheme(newTheme);
-  }, [connectWidget])
-
-  const updateThemeForAll = useCallback((widgetTheme: WidgetTheme) => {
-    let newTheme = widgetTheme === WidgetTheme.DARK ? WidgetTheme.LIGHT : WidgetTheme.DARK;
-    factory?.update({theme: newTheme});
-    setTheme(newTheme);
-  }, [factory])
+    connect.mount(CONNECT_TARGET_ID, {})
+    connect.on(ConnectEventType.SUCCESS, (data) => {
+      setProvider(data.provider);
+    })
+  }, [connect])
 
   return (
     <div>
       <h1 className="sample-heading">Checkout Connect</h1>
-      <div id="connect"></div>
-      <div id="bridge"></div>
-      <button onClick={() => updateThemeForAll(theme)}>Toggle theme</button>
+      <div id={CONNECT_TARGET_ID}></div>
+      <button onClick={() => connect.mount(CONNECT_TARGET_ID, {})}>Mount</button>
+      <button onClick={() => connect.unmount()}>Unmount</button>
     </div>
   );
 }
