246f382502d6645020eea86a2d95b56d8fe24477
Andrea Rampin <andrea.rampin@gmail.com>
2023-10-25 11:49:17 +1100

    patched isDevMode

--- packages/checkout/sdk/src/lib/env.test.ts
+++ packages/checkout/sdk/src/lib/env.test.ts
@@ -1,4 +1,4 @@
-import { isDevMode, globalPackageVersion, SDK_VERSION_MARKER } from './env';
+import { useLocalBundle, globalPackageVersion, SDK_VERSION_MARKER } from './env';
 
 describe('env', () => {
   const OLD_ENV = process.env;
@@ -12,15 +12,15 @@ describe('env', () => {
     process.env = OLD_ENV;
   });
 
-  it('isDevMode', () => {
+  it('useLocalBundle', () => {
     process.env = {
       ...OLD_ENV,
       // eslint-disable-next-line @typescript-eslint/naming-convention
       CHECKOUT_LOCAL_MODE: 'true',
     };
-    expect(isDevMode()).toBeTruthy();
+    expect(useLocalBundle()).toBeTruthy();
     process.env = { ...OLD_ENV };
-    expect(isDevMode()).toBeFalsy();
+    expect(useLocalBundle()).toBeFalsy();
   });
 
   it('globalPackageVersion', () => {
