e0d067d601b5d47c7950073194b2124f39cc5cf3
James Hardwick <james.hardwick@immutable.com>
2023-11-03 12:00:02 +1100

    Refactors useSmartCheckout for testability. Adds test for fractional balance check.

--- packages/checkout/widgets-lib/src/widgets/sale/hooks/useSmartCheckout.ts
+++ packages/checkout/widgets-lib/src/widgets/sale/hooks/useSmartCheckout.ts
@@ -1,18 +1,10 @@
 import { Web3Provider } from '@ethersproject/providers';
 import {
   Checkout,
-  ERC20ItemRequirement,
-  GasAmount,
-  GasTokenType,
-  ItemType,
   SmartCheckoutResult,
-  TransactionOrGasType,
 } from '@imtbl/checkout-sdk';
-import { BigNumber } from 'ethers';
-import { parseUnits } from 'ethers/lib/utils';
 import { useCallback, useState } from 'react';
-import { IMX_ADDRESS_ZKEVM } from '../../../lib/constants';
-import { getL2ChainId } from '../../../lib/networkUtils';
+import { getGasEstimate, getItemRequirements, isUserFractionalBalanceBlocked } from '../functions/smartCheckoutUtils';
 import {
   Item, SaleErrorTypes, SmartCheckoutError, SmartCheckoutErrorTypes,
 } from '../types';
@@ -25,26 +17,6 @@ type UseSmartCheckoutInput = {
   contractAddress: string,
 };
 
-const MAX_GAS_LIMIT = '30000000';
-
-const getItemRequirements = (amount: string, spenderAddress: string, contractAddress: string)
-: ERC20ItemRequirement[] => [
-  {
-    type: ItemType.ERC20,
-    contractAddress,
-    spenderAddress,
-    amount,
-  },
-];
-
-const getGasEstimate = (): GasAmount => ({
-  type: TransactionOrGasType.GAS,
-  gasToken: {
-    type: GasTokenType.NATIVE,
-    limit: BigNumber.from(MAX_GAS_LIMIT),
-  },
-});
-
 export const useSmartCheckout = ({
   checkout, provider, items, amount, contractAddress,
 }: UseSmartCheckoutInput) => {
@@ -55,39 +27,18 @@ export const useSmartCheckout = ({
     undefined,
   );
 
-  const isUserFractionalBalanceBlocked = async (walletAddress: string) => {
-    const chainId = getL2ChainId(checkout!.config);
-    const balanceResponse = await checkout!.getAllBalances({ provider: provider!, walletAddress, chainId });
-    const zero = BigNumber.from('0');
-
-    const purchaseBalance = balanceResponse.balances.find((balance) => balance.token.address === contractAddress);
-    if (!purchaseBalance) {
-      return false;
-    }
-    const formattedAmount = parseUnits(amount, purchaseBalance.token.decimals);
-
-    if (purchaseBalance.balance.gt(zero) && purchaseBalance.balance.lt(formattedAmount)) {
-      return true;
-    }
-
-    const isPassport = !!(provider?.provider as any)?.isPassport;
-    if (isPassport) {
-      return false;
-    }
-    const imxBalance = balanceResponse.balances.find((balance) => balance.token.address === IMX_ADDRESS_ZKEVM);
-    const imxBalanceAmount = imxBalance ? imxBalance.balance : BigNumber.from('0');
-    if (imxBalanceAmount.gte(zero) && imxBalanceAmount.lt(BigNumber.from(MAX_GAS_LIMIT))) {
-      return true;
-    }
-    return false;
-  };
-
   const smartCheckout = useCallback(async () => {
     try {
       const signer = provider?.getSigner();
       const spenderAddress = await signer?.getAddress() || '';
 
-      const userFractionalBalanceBlocked = await isUserFractionalBalanceBlocked(spenderAddress);
+      const userFractionalBalanceBlocked = await isUserFractionalBalanceBlocked(
+        spenderAddress,
+        contractAddress,
+        amount,
+        checkout,
+        provider,
+      );
       if (userFractionalBalanceBlocked) {
         throw new Error(SmartCheckoutErrorTypes.FRACTIONAL_BALANCE_BLOCKED);
       }
