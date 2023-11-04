4779d2c58e3e9362cc781d83357eb44408a906dd
Andrea Rampin <andrea.rampin@gmail.com>
2023-11-04 15:28:14 +1100

    WT-1891 - comments

--- packages/checkout/widgets-lib/src/widgets/connect/views/ReadyToConnect.tsx
+++ packages/checkout/widgets-lib/src/widgets/connect/views/ReadyToConnect.tsx
@@ -94,7 +94,8 @@ export function ReadyToConnect({ targetChainId, allowedChains }: ReadyToConnectP
   const handleConnectViewUpdate = async (provider: Web3Provider) => {
     // Skip checks for Passport. Passport will be shipped with the
     // zkEVM network pre-configured and changes of networks are handled
-    // by the specific widgets.
+    // by the ConnectLoader.
+    // TODO: Remove this check when Passport has support for L1.
     if (!isPassport) {
       const chainId = await provider.getSigner().getChainId();
       if (chainId !== targetChainId && !allowedChains?.includes(chainId)) {
