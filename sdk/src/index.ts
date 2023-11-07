0435de9f8b5e8d4f925510d0be247ed3f2f63272
Nik Ho <studying.mole@gmail.com>
2023-11-07 19:09:49 +1300

    temp remove orderbook and seaport dep

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -2,5 +2,4 @@ export * as config from './config';
 export * as blockchainData from './blockchain_data';
 export * as immutablexClient from './immutablex_client';
 export * as passport from './passport';
-export * as orderbook from './orderbook';
 export * as provider from './provider';
