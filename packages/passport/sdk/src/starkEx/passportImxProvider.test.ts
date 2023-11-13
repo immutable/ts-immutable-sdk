c2bf5fa7c7107dbb745866b0cabd1af415aa7b7f
Carmen Liu <carmen.liujia@gmail.com>
2023-11-13 13:32:23 +1000

    correct the registerOffchain test failed when merged main2

--- packages/passport/sdk/src/starkEx/passportImxProvider.test.ts
+++ packages/passport/sdk/src/starkEx/passportImxProvider.test.ts
@@ -56,6 +56,7 @@ describe('PassportImxProvider', () => {
   const mockAuthManager = {
     login: jest.fn(),
     getUser: jest.fn(),
+    forceUserRefresh: jest.fn(),
   };
 
   const mockStarkSigner = {
@@ -340,7 +341,7 @@ describe('PassportImxProvider', () => {
 
       mockAuthManager.login.mockResolvedValue(mockUser);
       magicAdapterMock.login.mockResolvedValue(magicProviderMock);
-      mockAuthManager.loginSilent.mockResolvedValue({ ...mockUser, imx: { ethAddress: '', starkAddress: '', userAdminAddress: '' } });
+      mockAuthManager.forceUserRefresh.mockResolvedValue({ ...mockUser, imx: { ethAddress: '', starkAddress: '', userAdminAddress: '' } });
 
       await passportImxProvider.registerOffchain();
 
@@ -349,8 +350,7 @@ describe('PassportImxProvider', () => {
         starkSigner: mockStarkSigner,
         usersApi: immutableXClient.usersApi,
       }, mockUserImx.accessToken);
-      expect(mockAuthManager.loginSilent).toHaveBeenCalledTimes(1);
-      expect(mockAuthManager.loginSilent).toHaveBeenCalledWith({ forceRefresh: true });
+      expect(mockAuthManager.forceUserRefresh).toHaveBeenCalledTimes(1);
     });
   });
 
