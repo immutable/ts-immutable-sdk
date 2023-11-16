2c3eec45963f3d0e50b77d83e9d41f3368e1e5cf
Zach Couchman <zachary.couchman@immutable.com>
2023-11-16 13:56:05 +1100

    Fix wallet balance loading issues

--- packages/checkout/widgets-lib/src/widgets/wallet/functions/tokenBalances.ts
+++ packages/checkout/widgets-lib/src/widgets/wallet/functions/tokenBalances.ts
@@ -1,10 +1,10 @@
 import { Web3Provider } from '@ethersproject/providers';
-import { Checkout, ChainId } from '@imtbl/checkout-sdk';
+import { Checkout, ChainId, GetBalanceResult } from '@imtbl/checkout-sdk';
 import { calculateCryptoToFiat, sortTokensByAmount } from '../../../lib/utils';
 import { DEFAULT_BALANCE_RETRY_POLICY } from '../../../lib';
 import { retry } from '../../../lib/retry';
 
-export interface BalanceInfo {
+export type BalanceInfo = {
   id: string;
   symbol: string;
   address?: string;
@@ -12,19 +12,13 @@ export interface BalanceInfo {
   balance: string;
   fiatAmount: string;
   iconLogo?: string;
-}
-
-const formatTokenId = (chainId: ChainId, symbol: string, address?: string) => {
-  if (!address) return `${chainId.toString()}-${symbol.toLowerCase()}`;
-  return `${chainId.toString()}-${symbol.toLowerCase()}-${address.toLowerCase()}`;
 };
 
 export const getTokenBalances = async (
   checkout: Checkout,
   provider: Web3Provider,
   chainId: ChainId,
-  conversions: Map<string, number>,
-): Promise<BalanceInfo[]> => {
+): Promise<GetBalanceResult[]> => {
   if (!checkout || !provider || !chainId) return [];
 
   const walletAddress = await provider.getSigner().getAddress();
@@ -46,21 +40,27 @@ export const getTokenBalances = async (
     chainId,
   );
 
-  const tokenBalances: BalanceInfo[] = [];
-  sortedTokens.forEach((balance) => {
-    tokenBalances.push({
-      id: formatTokenId(chainId, balance.token.symbol, balance.token.address),
-      balance: balance.formattedBalance,
-      fiatAmount: calculateCryptoToFiat(
-        balance.formattedBalance,
-        balance.token.symbol,
-        conversions,
-      ),
-      symbol: balance.token.symbol,
-      address: balance.token.address,
-      description: balance.token.name,
-    });
-  });
-
-  return tokenBalances;
+  return sortedTokens;
 };
+
+const formatTokenId = (chainId: ChainId, symbol: string, address?: string) => {
+  if (!address) return `${chainId.toString()}-${symbol.toLowerCase()}`;
+  return `${chainId.toString()}-${symbol.toLowerCase()}-${address.toLowerCase()}`;
+};
+
+export const mapTokenBalancesWithConversions = (
+  chainId: ChainId,
+  balances: GetBalanceResult[],
+  conversions: Map<string, number>,
+): BalanceInfo[] => balances.map((balance) => ({
+  id: formatTokenId(chainId, balance.token.symbol, balance.token.address),
+  balance: balance.formattedBalance,
+  fiatAmount: calculateCryptoToFiat(
+    balance.formattedBalance,
+    balance.token.symbol,
+    conversions,
+  ),
+  symbol: balance.token.symbol,
+  address: balance.token.address,
+  description: balance.token.name,
+}));
