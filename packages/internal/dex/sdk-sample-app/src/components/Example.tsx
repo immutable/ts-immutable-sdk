bd36664e8210a9b400fdbd32acbf64b6ad899c62
Keith Broughton <keith.broughton@immutable.com>
2023-11-16 21:04:36 +1100

    Improve UI

--- packages/internal/dex/sdk-sample-app/src/components/Example.tsx
+++ packages/internal/dex/sdk-sample-app/src/components/Example.tsx
@@ -1,86 +1,95 @@
-import { ethers } from "ethers";
-import { useEffect, useState } from "react";
-import {
-  Exchange,
-  TransactionDetails,
-  TransactionResponse,
-} from "@imtbl/dex-sdk";
-import { configuration } from "../config";
-import { ConnectAccount } from "./ConnectAccount";
-import { getTokenSymbol } from "../utils/getTokenSymbol";
-import { AmountInput } from "./AmountInput";
-import { SecondaryFeeInput } from "./SecondaryFeeInput";
-import { FeeBreakdown } from "./FeeBreakdown";
+import { ethers } from 'ethers';
+import { useState } from 'react';
+import { Exchange, TransactionDetails, TransactionResponse } from '@imtbl/dex-sdk';
+import { configuration } from '../config';
+import { ConnectAccount } from './ConnectAccount';
+import { AmountInput, AmountOutput } from './AmountInput';
+import { SecondaryFeeInput } from './SecondaryFeeInput';
+import { FeeBreakdown } from './FeeBreakdown';
+
+type Token = {
+  symbol: string;
+  address: string;
+};
+
+type TradeType = 'exactInput' | 'exactOutput';
 
 type mapping = {
   [address: string]: string;
 };
 
-export function Example() {
-  // Instead of hard-coding these tokens, you can optionally retrieve available tokens from the user's wallet
-  const TEST_IMX_TOKEN = "0x1CcCa691501174B4A623CeDA58cC8f1a76dc3439";
-  const ZKCORE_TOKEN = "0x43566cAB87CC147C95e2895E7b972E19993520e4";
+const allTokens = [
+  { symbol: 'IMX', address: 'native' },
+  { symbol: 'WIMX', address: '0x1CcCa691501174B4A623CeDA58cC8f1a76dc3439' },
+  { symbol: 'zkTDR', address: '0x6531F7B9158d78Ca78b46799c4Fd65C2Af8Ae506' },
+  { symbol: 'zkPSP', address: '0x88B35dF96CbEDF2946586147557F7D5D0CCE7e5c' },
+  { symbol: 'zkWLT', address: '0x8A5b0470ee48248bEb7D1E745c1EbA0DCA77215e' },
+  { symbol: 'zkSRE', address: '0x43566cAB87CC147C95e2895E7b972E19993520e4' },
+  { symbol: 'zkCORE', address: '0x4B96E7b7eA673A996F140d5De411a97b7eab934E' },
+];
 
+const buildExchange = (secondaryFeeRecipient: string, secondaryFeePercentage: number) => {
+  if (secondaryFeeRecipient && secondaryFeePercentage) {
+    return new Exchange({
+      ...configuration,
+      secondaryFees: [
+        {
+          recipient: secondaryFeeRecipient,
+          basisPoints: secondaryFeePercentage * 100,
+        },
+      ],
+    });
+  }
+
+  return new Exchange(configuration);
+};
+
+export function Example() {
   const [ethereumAccount, setEthereumAccount] = useState<string | null>(null);
   const [isFetching, setIsFetching] = useState(false);
-  const [inputAmount, setInputAmount] = useState<string>("0");
-  const [swapTransaction, setSwapTransaction] =
-    useState<ethers.providers.TransactionReceipt | null>(null);
+  const [inputAmount, setInputAmount] = useState<string>('0');
+  const [outputAmount, setOutputAmount] = useState<string>('0');
+  const [swapTransaction, setSwapTransaction] = useState<ethers.providers.TransactionReceipt | null>(null);
   const [approved, setApproved] = useState<boolean>(false);
   const [result, setResult] = useState<TransactionResponse | null>();
   const [error, setError] = useState<string | null>(null);
-  const [secondaryFeeRecipient, setSecondaryFeeRecipient] =
-    useState<string>("");
+  const [secondaryFeeRecipient, setSecondaryFeeRecipient] = useState<string>('');
   const [secondaryFeePercentage, setFeePercentage] = useState<number>(0);
-  const [addressToSymbolMapping, setAddressToSymbolMapping] = useState<mapping>(
-    {}
-  );
 
-  const inputToken = TEST_IMX_TOKEN;
-  const outputToken = ZKCORE_TOKEN;
+  const [tradeType, setTradeType] = useState<TradeType>('exactInput');
+  const [inputToken, setInputToken] = useState<Token>(allTokens[0]);
+  const [outputToken, setOutputToken] = useState<Token>(allTokens[1]);
 
-  useEffect(() => {
-    // Get the symbols for the tokens that we want to swap so we can display this to the user
-    Promise.all([getTokenSymbol(inputToken), getTokenSymbol(outputToken)]).then(
-      ([inputTokenSymbol, outputTokenSymbol]) => {
-        setAddressToSymbolMapping({
-          [inputToken]: inputTokenSymbol,
-          [outputToken]: outputTokenSymbol,
-        });
-      }
-    );
-  }, [inputToken, outputToken]);
+  const addressToSymbolMapping = allTokens.reduce((acc, token) => {
+    acc[token.address] = token.symbol!;
+    return acc;
+  }, {} as mapping);
 
   if (ethereumAccount === null) {
     return <ConnectAccount setAccount={setEthereumAccount} />;
   }
 
-  const getQuote = async () => {
+  const getQuote = async (tokenInAddress: string, tokenOutAddress: string) => {
     setIsFetching(true);
     setError(null);
 
     try {
-      let exchange: Exchange;
-      if (secondaryFeeRecipient && secondaryFeePercentage) {
-        exchange = new Exchange({
-          ...configuration,
-          secondaryFees: [
-            {
-              recipient: secondaryFeeRecipient,
-              basisPoints: secondaryFeePercentage * 100,
-            },
-          ],
-        });
-      } else {
-        exchange = new Exchange(configuration);
-      }
+      const exchange = buildExchange(secondaryFeeRecipient, secondaryFeePercentage);
 
-      const txn = await exchange.getUnsignedSwapTxFromAmountIn(
-        ethereumAccount,
-        inputToken,
-        outputToken,
-        ethers.utils.parseEther(`${inputAmount}`)
-      );
+      const txn =
+        tradeType === 'exactInput'
+          ? await exchange.getUnsignedSwapTxFromAmountIn(
+              ethereumAccount,
+              tokenInAddress,
+              tokenOutAddress,
+              ethers.utils.parseEther(`${inputAmount}`),
+            )
+          : await exchange.getUnsignedSwapTxFromAmountOut(
+              ethereumAccount,
+              tokenInAddress,
+              tokenOutAddress,
+              ethers.utils.parseEther(`${outputAmount}`),
+            );
 
       setResult(txn);
 
@@ -88,7 +97,7 @@ export function Example() {
         setApproved(true);
       }
     } catch (e) {
-      const message = e instanceof Error ? e.message : "Unknown Error";
+      const message = e instanceof Error ? e.message : 'Unknown Error';
       setError(`Error fetching quote: ${message}`);
       setResult(null);
     }
@@ -99,24 +108,20 @@ export function Example() {
   const performSwap = async (result: TransactionResponse) => {
     setSwapTransaction(null);
     setIsFetching(true);
-    const provider = new ethers.providers.JsonRpcProvider(
-      "https://rpc-geth.testnet.immutable.com/"
-    );
+    const provider = new ethers.providers.Web3Provider((window as any).ethereum)
+    const signer = provider.getSigner();
 
     // Approve the ERC20 spend
     if (!approved) {
       try {
         // Send the Approve transaction
-        const approveReceipt = await (window as any).ethereum.send(
-          "eth_sendTransaction",
-          [result.approval?.transaction]
-        );
+        const approveReceipt = await signer.sendTransaction(result.approval!.transaction);
 
         // Wait for the Approve transaction to complete
-        await provider.waitForTransaction(approveReceipt.result, 1);
+        await provider.waitForTransaction(approveReceipt.hash, 1);
         setApproved(true);
       } catch (e) {
-        const message = e instanceof Error ? e.message : "Unknown Error";
+        const message = e instanceof Error ? e.message : 'Unknown Error';
         alert(message);
         setIsFetching(false);
         return;
@@ -125,17 +130,15 @@ export function Example() {
 
     try {
       // Send the Swap transaction
-      const receipt = await (window as any).ethereum.send(
-        "eth_sendTransaction",
-        [result.swap.transaction]
-      );
+      const receipt = await signer.sendTransaction(result.swap.transaction);
+      console.log({ receipt });
 
       // Wait for the Swap transaction to complete
-      const tx = await provider.waitForTransaction(receipt.result, 1);
+      const tx = await provider.waitForTransaction(receipt.hash, 1);
       setIsFetching(false);
       setSwapTransaction(tx);
     } catch (e) {
-      const message = e instanceof Error ? e.message : "Unknown Error";
+      const message = e instanceof Error ? e.message : 'Unknown Error';
       alert(message);
       setIsFetching(false);
       setSwapTransaction(null);
@@ -144,67 +147,117 @@ export function Example() {
   };
 
   return (
-    <div>
-      <h3 style={{ marginBottom: "12px" }}>
-        Your wallet address: {ethereumAccount}
-      </h3>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <h3 style={{ marginBottom: '12px' }}>Your wallet address: {ethereumAccount}</h3>
 
-      <h3>
-        Input Token: {inputToken} ({addressToSymbolMapping[inputToken]})
-      </h3>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ width: 150 }}>
+          <h3>Swap Type</h3>
+        </div>
+        <div>
+          <select
+            value={tradeType}
+            onChange={(e) => {
+              setTradeType(e.target.value as TradeType);
+              setResult(null);
+              setSwapTransaction(null)
+            }}
+          >
+            <option>exactInput</option>
+            <option>exactOutput</option>
+          </select>
+        </div>
+      </div>
 
-      <h3>
-        Output Token: {outputToken} ({addressToSymbolMapping[outputToken]})
-      </h3>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ width: 150 }}>
+          <h3>Input Token:</h3>
+        </div>
+        <div>
+          <select
+            value={inputToken.address}
+            onChange={(e) => {
+              setInputToken({
+                address: e.target.value,
+                symbol: addressToSymbolMapping[e.target.value],
+              });
+              setResult(null);
+              setSwapTransaction(null)
+            }}
+          >
+            {allTokens.map((token) => (
+              <option key={token.address} value={token.address}>
+                {token.symbol}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ width: 150 }}>
+          <h3>Output Token:</h3>
+        </div>
+        <div>
+          <select
+            value={outputToken.address}
+            onChange={(e) => {
+              setOutputToken({
+                address: e.target.value,
+                symbol: addressToSymbolMapping[e.target.value],
+              });
+              setResult(null);
+              setSwapTransaction(null)
+            }}
+          >
+            {allTokens.map((token) => (
+              <option key={token.address} value={token.address}>
+                {token.symbol}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
 
       <hr className="my-4" />
 
-      <SecondaryFeeInput
-        setSecondaryFeeRecipient={setSecondaryFeeRecipient}
-        setFeePercentage={setFeePercentage}
-      />
-      <AmountInput
-        inputTokenSymbol={addressToSymbolMapping[inputToken]}
-        setInputAmount={setInputAmount}
-      />
+      <SecondaryFeeInput setSecondaryFeeRecipient={setSecondaryFeeRecipient} setFeePercentage={setFeePercentage} />
+      {tradeType === 'exactInput' && inputToken && (
+        <AmountInput tokenSymbol={inputToken.symbol} setAmount={setInputAmount} />
+      )}
+      {tradeType === 'exactOutput' && outputToken && (
+        <AmountOutput tokenSymbol={outputToken.symbol} setAmount={setOutputAmount} />
+      )}
 
-      <button
-        className="disabled:opacity-50 mt-2 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-        onClick={async () => await getQuote()}
-        disabled={isFetching}
-      >
-        Get Quote
-      </button>
+      {inputToken && outputToken && (
+        <button
+          className="disabled:opacity-50 mt-2 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          onClick={() => getQuote(inputToken.address, outputToken.address)}
+          disabled={isFetching}
+        >
+          Get Quote
+        </button>
+      )}
 
       <hr className="my-4" />
       {error && <ErrorMessage message={error} />}
       {result && (
         <>
           <h3>
-            Expected amount:{" "}
-            {ethers.utils.formatEther(result.quote.amount.value)}{" "}
+            Expected amount: {ethers.utils.formatEther(result.quote.amount.value)}{' '}
             {`${addressToSymbolMapping[result.quote.amount.token.address]}`}
           </h3>
           <h3>
-            Minimum amount:{" "}
-            {ethers.utils.formatEther(result.quote.amountWithMaxSlippage.value)}{" "}
-            {`${
-              addressToSymbolMapping[
-                result.quote.amountWithMaxSlippage.token.address
-              ]
-            }`}
+            {tradeType === 'exactInput' ? 'Minimum' : 'Maximum'} amount:{' '}
+            {ethers.utils.formatEther(result.quote.amountWithMaxSlippage.value)}{' '}
+            {`${addressToSymbolMapping[result.quote.amountWithMaxSlippage.token.address]}`}
           </h3>
 
           <h3>Slippage: {result.quote.slippage}%</h3>
-          {result.approval && (
-            <h3>Approval Gas Estimate: {showGasEstimate(result.approval)}</h3>
-          )}
+          {result.approval && <h3>Approval Gas Estimate: {showGasEstimate(result.approval)}</h3>}
           <h3>Swap Gas estimate: {showGasEstimate(result.swap)}</h3>
 
-          <FeeBreakdown
-            fees={result.quote.fees}
-            addressMap={addressToSymbolMapping}
-          />
+          <FeeBreakdown fees={result.quote.fees} addressMap={addressToSymbolMapping} />
 
           <>
             <button
@@ -212,14 +265,13 @@ export function Example() {
               onClick={() => performSwap(result)}
               disabled={isFetching}
             >
-              {approved ? "Swap" : "Approve"}
+              {approved ? 'Swap' : 'Approve'}
             </button>
             {isFetching && <h3>loading...</h3>}
             {swapTransaction && (
               <>
-                <h3 style={{ marginTop: "12px" }}>
-                  Swap successful! Check your metamask to see updated token
-                  balances
+                <h3 style={{ marginTop: '12px' }}>
+                  Swap successful! Check your metamask to see updated token balances
                 </h3>
                 <a
                   className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
@@ -238,9 +290,7 @@ export function Example() {
 }
 
 const showGasEstimate = (txn: TransactionDetails) =>
-  txn.gasFeeEstimate
-    ? `${ethers.utils.formatEther(txn.gasFeeEstimate.value)} IMX`
-    : "No gas estimate available";
+  txn.gasFeeEstimate ? `${ethers.utils.formatEther(txn.gasFeeEstimate.value)} IMX` : 'No gas estimate available';
 
 const ErrorMessage = ({ message }: { message: string }) => {
   return (
--- packages/internal/dex/sdk-sample-app/src/config/index.ts
+++ packages/internal/dex/sdk-sample-app/src/config/index.ts
@@ -2,11 +2,11 @@ import { Environment, ImmutableConfiguration } from '@imtbl/config';
 
 const chainId = 13473; // You can optionally retrieve the chain ID from the users wallet, or prompt the user to change networks
 
-const immutableConfig = new ImmutableConfiguration({
+const baseConfig = new ImmutableConfiguration({
   environment: Environment.SANDBOX,
 });
 
 export const configuration = {
   chainId,
-  baseConfig: immutableConfig,
+  baseConfig,
 };
