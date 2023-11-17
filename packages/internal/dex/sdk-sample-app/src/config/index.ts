2a1626bb32499d4a9cbd2c2e0928e8cc0a961877
Keith Broughton <keith.broughton@immutable.com>
2023-11-17 12:06:30 +1100

    Revert

--- packages/internal/dex/sdk-sample-app/src/config/index.ts
+++ packages/internal/dex/sdk-sample-app/src/config/index.ts
@@ -1,6 +1,6 @@
 import { Environment, ImmutableConfiguration } from '@imtbl/config';
 
-const chainId = 13473; // You can optionally retrieve the chain ID from the users wallet, or prompt the user to change networks
+const chainId = 13472; // You can optionally retrieve the chain ID from the users wallet, or prompt the user to change networks
 
 const baseConfig = new ImmutableConfiguration({
   environment: Environment.SANDBOX,
