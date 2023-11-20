a5802a211640c3bf906fbe4233416ad06c01c7ad
Nik Ho <studying.mole@gmail.com>
2023-11-20 17:50:39 +1300

    Add react-router-dom

--- samples/apps/ts-immutable-sample/src/components/App.tsx
+++ samples/apps/ts-immutable-sample/src/components/App.tsx
@@ -1,27 +1,5 @@
-import { Environment, ImmutableConfiguration } from '@imtbl/sdk/config';
-import { Passport } from '@imtbl/sdk/passport';
-import {
-  clientId,
-  redirectUri,
-  logoutRedirectUri,
-  audience,
-  scope,
-} from '@/lib/env';
-
 const App = () => {
-  const passportInstance = new Passport({
-    baseConfig: new ImmutableConfiguration({
-      environment: Environment.SANDBOX,
-    }),
-    clientId,
-    redirectUri,
-    logoutRedirectUri,
-    audience,
-    scope,
-  });
-  console.log(passportInstance);
-
-  return <h1>Hello world</h1>;
+  return <h1>Immutable Sample App</h1>;
 };
 
 export default App;
--- samples/apps/ts-immutable-sample/src/lib/router.tsx
+++ samples/apps/ts-immutable-sample/src/lib/router.tsx
@@ -4,9 +4,15 @@ import {
   Route,
 } from 'react-router-dom';
 import App from '@/components/App';
+import PassportPage from '@/pages/PassportPage';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/' element={<App />} />),
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<App />}></Route>
+      <Route path='/passport' element={<PassportPage />} />
+    </Route>
+  ),
 );
 
 export default router;
