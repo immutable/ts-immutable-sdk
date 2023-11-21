72b602b646468904fc1f64af657b584c84fad986
Zach Couchman <zachary.couchman@immutable.com>
2023-11-22 09:45:37 +1100

    copying bridge to x bridge allow for local dev

---
+++ packages/checkout/widgets-lib/src/widgets/x-bridge/BridgeStyles.ts
@@ -0,0 +1,5 @@
+export const bridgeWidgetStyle = {
+  maxWidth: '400px',
+  backgroundColor: '#333333',
+  padding: '20px',
+};
---
+++ packages/checkout/widgets-lib/src/widgets/x-bridge/XBridgeWidget.tsx
@@ -0,0 +1,238 @@
+import {
+  BiomeCombinedProviders, Body,
+} from '@biom3/react';
+import {
+  BridgeWidgetParams,
+  NetworkFilterTypes, TokenFilterTypes,
+} from '@imtbl/checkout-sdk';
+import {
+  useCallback,
+  useContext,
+  useEffect, useMemo, useReducer,
+} from 'react';
+import { ImmutableConfiguration } from '@imtbl/config';
+import { ethers } from 'ethers';
+import {
+  BridgeConfiguration,
+  ETH_MAINNET_TO_ZKEVM_MAINNET,
+  ETH_SEPOLIA_TO_ZKEVM_DEVNET,
+  ETH_SEPOLIA_TO_ZKEVM_TESTNET,
+  TokenBridge,
+} from '@imtbl/bridge-sdk';
+import { StrongCheckoutWidgetsConfig } from 'lib/withDefaultWidgetConfig';
+import { SimpleLayout } from 'components/SimpleLayout/SimpleLayout';
+import { HeaderNavigation } from 'components/Header/HeaderNavigation';
+import { FooterLogo } from 'components/Footer/FooterLogo';
+import { BridgeWidgetViews } from 'context/view-context/BridgeViewContextTypes';
+import { CryptoFiatProvider } from 'context/crypto-fiat-context/CryptoFiatProvider';
+import {
+  DEFAULT_BALANCE_RETRY_POLICY,
+  getL1ChainId,
+  getL2ChainId,
+} from '../../lib';
+import {
+  SharedViews,
+  ViewActions,
+  ViewContext,
+  initialViewState,
+  viewReducer,
+} from '../../context/view-context/ViewContext';
+import {
+  BridgeActions, BridgeContext, bridgeReducer, initialBridgeState,
+} from './context/BridgeContext';
+import { ConnectLoaderContext } from '../../context/connect-loader-context/ConnectLoaderContext';
+import { EventTargetContext } from '../../context/event-target-context/EventTargetContext';
+import { GetAllowedBalancesResultType, getAllowedBalances } from '../../lib/balance';
+import { widgetTheme } from '../../lib/theme';
+import { isPassportProvider } from '../../lib/providerUtils';
+import { BridgeComingSoon } from './views/BridgeComingSoon';
+import { sendBridgeWidgetCloseEvent } from './BridgeWidgetEvents';
+
+export type BridgeWidgetInputs = BridgeWidgetParams & {
+  config: StrongCheckoutWidgetsConfig,
+};
+
+export function XBridgeWidget({
+  amount,
+  fromContractAddress,
+  config,
+}: BridgeWidgetInputs) {
+  const { environment, theme } = config;
+
+  const { connectLoaderState: { checkout, provider } } = useContext(ConnectLoaderContext);
+  const { eventTargetState: { eventTarget } } = useContext(EventTargetContext);
+
+  const [viewState, viewDispatch] = useReducer(viewReducer, initialViewState);
+  const [bridgeState, bridgeDispatch] = useReducer(bridgeReducer, initialBridgeState);
+
+  const viewReducerValues = useMemo(() => ({ viewState, viewDispatch }), [viewState, viewDispatch]);
+  const bridgeReducerValues = useMemo(() => ({ bridgeState, bridgeDispatch }), [bridgeState, bridgeDispatch]);
+  const themeReducerValue = useMemo(() => widgetTheme(theme), [theme]);
+
+  // Passport currently does not have an L1 representation and therefore there
+  // is not need to show the bridge widget for Passport connected users.
+  if (isPassportProvider(provider)) {
+    return <BridgeComingSoon onCloseEvent={() => sendBridgeWidgetCloseEvent(eventTarget)} />;
+  }
+
+  const showErrorView = useCallback((error: any, tryAgain?: () => Promise<boolean>) => {
+    viewDispatch({
+      payload: {
+        type: ViewActions.UPDATE_VIEW,
+        view: {
+          type: SharedViews.ERROR_VIEW,
+          tryAgain,
+          error,
+        },
+      },
+    });
+  }, [viewDispatch]);
+
+  const showBridgeView = useCallback(() => {
+    viewDispatch({
+      payload: {
+        type: ViewActions.UPDATE_VIEW,
+        view: { type: BridgeWidgetViews.BRIDGE },
+      },
+    });
+  }, [viewDispatch]);
+
+  const loadBalances = async (): Promise<boolean> => {
+    if (!checkout) throw new Error('loadBalances: missing checkout');
+    if (!provider) throw new Error('loadBalances: missing provider');
+
+    let tokensAndBalances: GetAllowedBalancesResultType = {
+      allowList: { tokens: [] },
+      allowedBalances: [],
+    };
+    try {
+      tokensAndBalances = await getAllowedBalances({
+        checkout,
+        provider,
+        allowTokenListType: TokenFilterTypes.BRIDGE,
+      });
+    } catch (err: any) {
+      if (DEFAULT_BALANCE_RETRY_POLICY.nonRetryable!(err)) {
+        showErrorView(err, loadBalances);
+        return false;
+      }
+    }
+
+    bridgeDispatch({
+      payload: {
+        type: BridgeActions.SET_ALLOWED_TOKENS,
+        allowedTokens: tokensAndBalances.allowList.tokens,
+      },
+    });
+
+    bridgeDispatch({
+      payload: {
+        type: BridgeActions.SET_TOKEN_BALANCES,
+        tokenBalances: tokensAndBalances.allowedBalances,
+      },
+    });
+
+    return true;
+  };
+
+  useEffect(() => {
+    const bridgetWidgetSetup = async () => {
+      if (!checkout || !provider) return;
+
+      const getNetworkResult = await checkout.getNetworkInfo({ provider });
+
+      /* If the provider's network is not supported, return out of this and let the
+      connect loader handle the switch network functionality */
+      if (!getNetworkResult.isSupported) {
+        return;
+      }
+
+      bridgeDispatch({
+        payload: {
+          type: BridgeActions.SET_NETWORK,
+          network: getNetworkResult,
+        },
+      });
+
+      const rootProvider = new ethers.providers.JsonRpcProvider(
+        checkout.config.networkMap.get(getL1ChainId(checkout.config))?.rpcUrls[0],
+      );
+
+      const toChainId = getL2ChainId(checkout.config);
+
+      const childProvider = new ethers.providers.JsonRpcProvider(
+        checkout.config.networkMap.get(toChainId)?.rpcUrls[0],
+      );
+
+      let bridgeInstance = ETH_SEPOLIA_TO_ZKEVM_TESTNET;
+      if (checkout.config.isDevelopment) bridgeInstance = ETH_SEPOLIA_TO_ZKEVM_DEVNET;
+      if (checkout.config.isProduction) bridgeInstance = ETH_MAINNET_TO_ZKEVM_MAINNET;
+
+      bridgeDispatch({
+        payload: {
+          type: BridgeActions.SET_TOKEN_BRIDGE,
+          tokenBridge: new TokenBridge(new BridgeConfiguration({
+            baseConfig: new ImmutableConfiguration(config),
+            bridgeInstance,
+            rootProvider,
+            childProvider,
+          })),
+        },
+      });
+
+      const allowedBridgingNetworks = await checkout.getNetworkAllowList({
+        type: NetworkFilterTypes.ALL,
+      });
+
+      const toNetwork = allowedBridgingNetworks.networks.find(
+        (network) => network.chainId === toChainId,
+      );
+      if (toNetwork) {
+        bridgeDispatch({
+          payload: {
+            type: BridgeActions.SET_TO_NETWORK,
+            toNetwork,
+          },
+        });
+      }
+
+      if (!await loadBalances()) return;
+
+      showBridgeView();
+    };
+
+    bridgetWidgetSetup();
+  }, [checkout, provider]);
+
+  return (
+    <BiomeCombinedProviders theme={{ base: themeReducerValue }}>
+      <ViewContext.Provider value={viewReducerValues}>
+        <BridgeContext.Provider value={bridgeReducerValues}>
+          <CryptoFiatProvider environment={environment}>
+            <SimpleLayout
+              testId="bridge-view"
+              header={(
+                <HeaderNavigation
+                  title="Move"
+                  onCloseButtonClick={() => sendBridgeWidgetCloseEvent(eventTarget)}
+                />
+                )}
+              footer={<FooterLogo />}
+            >
+              <Body>Hello from the X Wallet Bridge</Body>
+              <Body>
+                Params for amount are
+                {' '}
+                {amount}
+                and fromContractAddress
+                {' '}
+                {fromContractAddress}
+                {' '}
+              </Body>
+            </SimpleLayout>
+          </CryptoFiatProvider>
+        </BridgeContext.Provider>
+      </ViewContext.Provider>
+    </BiomeCombinedProviders>
+  );
+}
