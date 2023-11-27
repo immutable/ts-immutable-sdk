24bd11eb3acba9e2415cc7188f239da11ce51c05
Zach Couchman <zachary.couchman@immutable.com>
2023-11-27 15:46:18 +1100

    fixing jsbi for vite prod

--- packages/checkout/widgets-sample-app/vite.config.ts
+++ packages/checkout/widgets-sample-app/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +9,23 @@ export default defineConfig({
     'process.env': {},
     'global': {}
   },
-  plugins: [react()],
+  plugins: [
+    nodePolyfills({
+      include: ['assert', 'events', 'buffer', 'crypto', 'https', 'os', 'stream'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+    react()
+  ],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      'jsbi': path.resolve(__dirname, '../../../node_modules/jsbi'),
+    }
   }
 })
