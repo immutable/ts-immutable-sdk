d8d7b9c5cdd33a0323c9bc86ecb9c722979a1071
Sharon Sheah <sharonsheah@gmail.com>
2023-11-29 14:36:19 +0800

    refactor and create BridgeReviewSummary component

---
+++ packages/checkout/widgets-lib/src/widgets/x-bridge/components/BridgeReviewSummary.tsx
@@ -0,0 +1,174 @@
+import { text } from 'resources/text/textConfig';
+import { XBridgeWidgetViews } from 'context/view-context/XBridgeViewContextTypes';
+import {
+  Body,
+  Box, Heading, Icon, MenuItem,
+} from '@biom3/react';
+import { ChainId, WalletProviderName } from '@imtbl/checkout-sdk';
+import { networkIconStyles } from './WalletNetworkButtonStyles';
+import {
+  arrowIconStyles,
+  arrowIconWrapperStyles,
+  bottomMenuItemStyles,
+  bridgeReviewHeadingStyles,
+  bridgeReviewWrapperStyles,
+  gasAmountHeadingStyles,
+  topMenuItemStyles,
+  walletLogoStyles,
+} from './BridgeReviewSummaryStyles';
+
+const networkIcon = {
+  [ChainId.IMTBL_ZKEVM_DEVNET]: 'Immutable',
+  [ChainId.IMTBL_ZKEVM_MAINNET]: 'Immutable',
+  [ChainId.IMTBL_ZKEVM_TESTNET]: 'Immutable',
+  [ChainId.ETHEREUM]: 'EthToken',
+  [ChainId.SEPOLIA]: 'EthToken',
+};
+
+const logo = {
+  [WalletProviderName.PASSPORT]: 'PassportSymbolOutlined',
+  [WalletProviderName.METAMASK]: 'MetaMaskSymbol',
+};
+
+const currencyImageUrl = {
+  usdc: 'https://design-system.immutable.com/hosted-for-ds/currency-icons/currency--usdc.svg',
+  eth: 'https://design-system.immutable.com/hosted-for-ds/currency-icons/currency--eth.svg',
+  imx: 'https://design-system.immutable.com/hosted-for-ds/currency-icons/currency--imx.svg',
+};
+
+interface BridgeReviewSummaryProps {
+  testId?: string;
+  fromAmount?: string;
+  fromFiatAmount?: string;
+  fromAddress?: string;
+  fromWalletProviderName?: WalletProviderName;
+  fromChainId?: ChainId;
+  toAddress?: string;
+  toWalletProviderName?: WalletProviderName;
+  toChainId?: ChainId;
+  gasEstimate?: string;
+  gasFiatEstimate?: string;
+}
+
+export function BridgeReviewSummary({
+  testId,
+  fromAmount,
+  fromFiatAmount,
+  fromAddress,
+  fromWalletProviderName,
+  fromChainId,
+  toAddress,
+  toWalletProviderName,
+  toChainId,
+  gasEstimate,
+  gasFiatEstimate,
+}: BridgeReviewSummaryProps) {
+  const {
+    heading, from, to, fees,
+  } = text.views[XBridgeWidgetViews.BRIDGE_REVIEW];
+
+  return (
+    <Box testId={testId} sx={bridgeReviewWrapperStyles}>
+      <Heading
+        testId={`${testId}-heading`}
+        size="small"
+        weight="regular"
+        sx={bridgeReviewHeadingStyles}
+      >
+        {heading}
+      </Heading>
+
+      {/* From review */}
+      <MenuItem
+        testId={`${testId}-from-amount`}
+        size="small"
+        emphasized
+        sx={topMenuItemStyles}
+      >
+        <MenuItem.Label size="small" sx={{ marginBottom: 'base.spacing.x4', fontWeight: 'bold' }}>
+          {from.amountHeading}
+        </MenuItem.Label>
+        <MenuItem.Caption />
+        <MenuItem.PriceDisplay
+          use={<Heading size="xSmall" weight="light" />}
+          price={fromAmount ?? '-'}
+          fiatAmount={fromFiatAmount}
+          currencyImageUrl={currencyImageUrl.usdc}
+        />
+      </MenuItem>
+      <MenuItem
+        testId={`${testId}-from-address`}
+        size="xSmall"
+        emphasized
+        sx={bottomMenuItemStyles}
+      >
+        {fromWalletProviderName && (
+          <MenuItem.FramedLogo
+            logo={logo[fromWalletProviderName] as any}
+            sx={walletLogoStyles(fromWalletProviderName)}
+          />
+        )}
+        <MenuItem.Label>
+          <strong>{from.heading}</strong>
+          {' '}
+          {fromAddress}
+        </MenuItem.Label>
+        {fromChainId && (
+          <MenuItem.IntentIcon
+            icon={networkIcon[fromChainId] as any}
+            sx={networkIconStyles(fromChainId)}
+          />
+        )}
+      </MenuItem>
+
+      <Box sx={arrowIconWrapperStyles}>
+        <Icon icon="ArrowBackward" sx={arrowIconStyles} />
+      </Box>
+
+      {/* To review */}
+      <MenuItem
+        testId={`${testId}-to-address`}
+        size="xSmall"
+        emphasized
+        sx={topMenuItemStyles}
+      >
+        {toWalletProviderName && (
+          <MenuItem.FramedLogo
+            logo={logo[toWalletProviderName] as any}
+            sx={walletLogoStyles(toWalletProviderName)}
+          />
+        )}
+        <MenuItem.Label>
+          <strong>{to.heading}</strong>
+          {' '}
+          {toAddress}
+        </MenuItem.Label>
+        {toChainId && (
+          <MenuItem.IntentIcon
+            icon={networkIcon[toChainId] as any}
+            sx={networkIconStyles(toChainId)}
+          />
+        )}
+      </MenuItem>
+      <MenuItem
+        testId={`${testId}-gas-amount`}
+        size="small"
+        emphasized
+        sx={bottomMenuItemStyles}
+      >
+        <MenuItem.Label
+          size="small"
+          sx={gasAmountHeadingStyles}
+        >
+          {fees.heading}
+        </MenuItem.Label>
+        <MenuItem.PriceDisplay
+          use={<Body size="xSmall" />}
+          price={gasEstimate ?? '-'}
+          fiatAmount={`~ ${gasFiatEstimate}`}
+          currencyImageUrl={currencyImageUrl.eth}
+        />
+      </MenuItem>
+    </Box>
+  );
+}
