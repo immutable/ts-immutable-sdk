7df5e9716b4964e4fad23cd14df986f2ac7cc5e0
Charlie McKenzie <charlie@dreamoftrees.com>
2023-11-30 12:10:45 +1100

    Updates to shimmer design

--- packages/checkout/widgets-lib/src/widgets/x-bridge/components/TokenSelectShimmer.tsx
+++ packages/checkout/widgets-lib/src/widgets/x-bridge/components/TokenSelectShimmer.tsx
@@ -32,7 +32,7 @@ export function TokenSelectShimmer({ sx }: TokenSelectShimmerProps) {
             // eslint-disable-next-line @typescript-eslint/naming-convention
             WebkitMaskSize: 'contain',
             // eslint-disable-next-line @typescript-eslint/naming-convention,max-len
-            WebkitMaskImage: 'url(\'data:image/svg+xml;utf8, <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"><path d="M111.5 34q0-1.65-1.15-2.8-1.2-1.2-2.85-1.2H18.9q-2.3 0-3.85 1.2-1.55 1.15-1.55 2.8v4q0 1.7 1.55 2.85Q16.6 42 18.9 42h88.6q1.65 0 2.85-1.15 1.15-1.15 1.15-2.85v-4m29-14q-6.65 0-11.3 4.7-4.7 4.7-4.7 11.3 0 6.65 4.7 11.3 4.65 4.7 11.3 4.7 6.6 0 11.3-4.7 3.75-3.7 4.5-8.65.2-1.3.2-2.65 0-1.35-.2-2.65-.75-4.9-4.5-8.65-4.7-4.7-11.3-4.7Z" id="a"/></svg>\')',
+            WebkitMaskImage: 'url(\'data:image/svg+xml;utf8, <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"><path d="M154.75 39q1.65 0 2.85-1.15 1.15-1.15 1.15-2.85v-4q0-1.65-1.15-2.8-1.2-1.2-2.85-1.2h-93.6q-2.3 0-3.85 1.2-1.55 1.15-1.55 2.8v4q0 1.7 1.55 2.85Q58.85 39 61.15 39h93.6m-111.7-3.35q.2-1.3.2-2.65 0-1.35-.2-2.65-.75-4.9-4.5-8.65-4.7-4.7-11.3-4.7-6.65 0-11.3 4.7-4.7 4.7-4.7 11.3 0 6.65 4.7 11.3Q20.6 49 27.25 49q6.6 0 11.3-4.7 3.75-3.7 4.5-8.65Z" id="a"/></svg>\')',
           }}
           rc={<span />}
         >
