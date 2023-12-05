82990cae065a48e732a5f8aeff7305e8391d1a1f
Keith Broughton <keith.broughton@immutable.com>
2023-12-05 18:38:03 +1100

    optimize

--- packages/internal/dex/sdk/src/exchange.ts
+++ packages/internal/dex/sdk/src/exchange.ts
@@ -47,7 +47,9 @@ const toPublicQuote = (
 });
 
 export class Exchange {
-  private provider: ethers.providers.JsonRpcBatchProvider;
+  private provider: ethers.providers.StaticJsonRpcProvider;
+
+  private batchProvider: ethers.providers.JsonRpcBatchProvider;
 
   private router: Router;
 
@@ -76,11 +78,20 @@ export class Exchange {
     this.routerContractAddress = config.chain.contracts.peripheryRouter;
     this.secondaryFeeContractAddress = config.chain.contracts.secondaryFee;
 
-    this.provider = new ethers.providers.JsonRpcBatchProvider(config.chain.rpcUrl);
+    this.provider = new ethers.providers.StaticJsonRpcProvider({
+      url: config.chain.rpcUrl,
+      skipFetchSetup: true,
+    }, config.chain.chainId);
+
+    this.batchProvider = new ethers.providers.JsonRpcBatchProvider({
+      url: config.chain.rpcUrl,
+      skipFetchSetup: true,
+    }, config.chain.chainId);
+
     const multicallContract = Multicall__factory.connect(config.chain.contracts.multicall, this.provider);
 
     this.router = new Router(
-      this.provider,
+      this.batchProvider,
       multicallContract,
       config.chain.commonRoutingTokens,
       config.chain.contracts,
@@ -104,12 +115,12 @@ export class Exchange {
     assert(slippagePercent >= 0, new InvalidSlippageError('slippage percent must be greater than or equal to 0'));
   }
 
-  private async getSecondaryFees() {
+  private async getSecondaryFees(provider: ethers.providers.JsonRpcBatchProvider) {
     if (this.secondaryFees.length === 0) {
       return [];
     }
 
-    const secondaryFeeContract = SecondaryFee__factory.connect(this.secondaryFeeContractAddress, this.provider);
+    const secondaryFeeContract = SecondaryFee__factory.connect(this.secondaryFeeContractAddress, provider);
 
     if (await secondaryFeeContract.paused()) {
       // Do not use secondary fees if the contract is paused
@@ -145,11 +156,14 @@ export class Exchange {
     Exchange.validate(tokenInLiteral, tokenOutLiteral, maxHops, slippagePercent, fromAddress);
 
     // get the decimals of the tokens that will be swapped
-    const [tokenInDecimals, tokenOutDecimals, secondaryFees] = await Promise.all([
-      getTokenDecimals(tokenInLiteral, this.provider, this.nativeToken),
-      getTokenDecimals(tokenOutLiteral, this.provider, this.nativeToken),
-      this.getSecondaryFees(),
-    ]);
+    const promises = [
+      getTokenDecimals(tokenInLiteral, this.batchProvider, this.nativeToken),
+      getTokenDecimals(tokenOutLiteral, this.batchProvider, this.nativeToken),
+      this.getSecondaryFees(this.batchProvider),
+      fetchGasPrice(this.batchProvider, this.nativeToken),
+    ] as const;
+
+    const [tokenInDecimals, tokenOutDecimals, secondaryFees, gasPrice] = await Promise.all(promises);
 
     const tokenIn = this.parseTokenLiteral(tokenInLiteral, tokenInDecimals);
     const tokenOut = this.parseTokenLiteral(tokenOutLiteral, tokenOutDecimals);
@@ -165,15 +179,12 @@ export class Exchange {
     const ourQuoteReqAmount = getOurQuoteReqAmount(amountSpecified, fees, tradeType, this.nativeTokenService);
 
     // Quotes will always use ERC20s. If the user-specified token is Native, we use the Wrapped Native Token pool
-    const [ourQuote, gasPrice] = await Promise.all([
-      this.router.findOptimalRoute(
-        ourQuoteReqAmount,
-        this.nativeTokenService.maybeWrapToken(otherToken),
-        tradeType,
-        maxHops,
-      ),
-      fetchGasPrice(this.provider, this.nativeToken),
-    ]);
+    const ourQuote = await this.router.findOptimalRoute(
+      ourQuoteReqAmount,
+      this.nativeTokenService.maybeWrapToken(otherToken),
+      tradeType,
+      maxHops,
+    );
 
     const adjustedQuote = adjustQuoteWithFees(ourQuote, amountSpecified, fees, this.nativeTokenService);
 
