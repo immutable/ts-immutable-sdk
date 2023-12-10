da40b08f02b4bdd2c4af42a70e9c3131fd79a958
Craig M <craig.b.macgregor@gmail.com>
2023-12-11 12:57:39 +1300

    port axelar SDK functions for transaction status

--- packages/internal/bridge/sdk/src/index.ts
+++ packages/internal/bridge/sdk/src/index.ts
@@ -3,6 +3,7 @@ export * from './constants/bridges';
 export * from './errors/index';
 export * from './config/index';
 export * from './types/index';
+export * from './types/axelar';
 export * from './contracts/ABIs/ERC20';
 export * from './contracts/ABIs/RootERC20BridgeFlowRate';
 export * from './contracts/ABIs/ChildERC20Bridge';
