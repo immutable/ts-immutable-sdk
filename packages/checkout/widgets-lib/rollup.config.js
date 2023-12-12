033eae740ce2b38ea076a2ff6fdaa1f420744d9a
Charlie McKenzie <charlie@dreamoftrees.com>
2023-12-13 03:24:01 +1100

    Connect widget i18next refactor

--- packages/checkout/widgets-lib/rollup.config.js
+++ packages/checkout/widgets-lib/rollup.config.js
@@ -7,6 +7,7 @@ import replace from '@rollup/plugin-replace';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 const defaultPlugin = [
+  json(),
   replace({
     preventAssignment: true,
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
@@ -41,7 +42,6 @@ export default [
       }),
       nodePolyfills(),
       commonjs(),
-      json(),
       ...defaultPlugin,
       terser(),
     ]
