5822ac4eea590d6fd12a00b68ed0607d43228d9e
Carl Menezes <carl.menezes@immutable.com>
2023-12-18 16:45:51 +1300

    Make chains feature test pass

--- tests/func-tests/zkevm/config/env.ts
+++ tests/func-tests/zkevm/config/env.ts
@@ -2,19 +2,20 @@ export function getEnv(name: string, defaultValue?: string): string {
   const value = process.env[name];
   if (value !== undefined) {
     return value;
-  } else if (defaultValue !== undefined) {
+  }
+  if (defaultValue !== undefined) {
     return defaultValue;
   }
   throw new Error(`Environment variable '${name}' not set`);
 }
 
 export const env = {
-  mnemonic: getEnv('MNEMONIC'),
+  mnemonic: getEnv('TEST_ZKEVM_WALLET_MNEMONIC'),
   bankerPrivateKey: getEnv('BANKER_PRIVATE_KEY'),
-  apiRateLimitBypassKey: getEnv('API_RATE_LIMIT_BYPASS_KEY'),
-  privateKey: process.env['PRIVATE_KEY'],
+  apiRateLimitBypassKey: getEnv('TEST_ZKEVM_API_RATE_LIMIT_BYPASS_KEY'),
+  privateKey: getEnv('BANKER_PRIVATE_KEY'),
   chainName: getEnv('CHAIN_NAME'),
   apiUrl: getEnv('API_URL'),
   rpcUrl: getEnv('RPC_URL'),
-  imxAPIKey: getEnv('HUB_GENERATED_API_KEY'),
+  imxAPIKey: getEnv('TEST_ZKEVM_PROJECT_HUB_GENERATED_API_KEY'),
 };
