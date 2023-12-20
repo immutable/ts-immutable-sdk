0f136fcb41287a7d1ceae05b94091a5739c031e9
Charlie McKenzie <charlie@dreamoftrees.com>
2023-12-20 11:00:49 +1100

    SmartCheckoutForm tokenAddress trefactor

--- packages/checkout/sdk-sample-app/src/components/SmartCheckoutForm.tsx
+++ packages/checkout/sdk-sample-app/src/components/SmartCheckoutForm.tsx
@@ -52,7 +52,7 @@ export const SmartCheckoutForm = ({ checkout, provider }: SmartCheckoutProps) =>
   const [amountError, setAmountError] = useState<string>('');
   const [id, setId] = useState<string>('');
   const [idError, setIdError] = useState<string>('');
-  const [tokenAddress, setContractAddress] = useState<string>('');
+  const [tokenAddress, setTokenAddress] = useState<string>('');
   const [tokenAddressError, setTokenAddressError] = useState<string>('');
   const [spenderAddress, setSpenderAddress] = useState<string>('');
   const [spenderAddressError, setSpenderAddressError] = useState<string>('');
@@ -268,7 +268,7 @@ export const SmartCheckoutForm = ({ checkout, provider }: SmartCheckoutProps) =>
   const selectItemType = (value: OptionKey) => {
     setAmountError('');
     setIdError('');
-    setContractAddressError('');
+    setTokenAddressError('');
     setSpenderAddressError('');
 
     switch (value) {
@@ -279,7 +279,7 @@ export const SmartCheckoutForm = ({ checkout, provider }: SmartCheckoutProps) =>
         setDisabledContractAddress(true);
         setDisabledSpenderAddress(true);
         setId('');
-        setContractAddress('');
+        setTokenAddress('');
         setSpenderAddress('');
         break;
       case 'erc20':
@@ -373,17 +373,17 @@ export const SmartCheckoutForm = ({ checkout, provider }: SmartCheckoutProps) =>
               </FormControl>
             </td>
             <td>
-              <FormControl validationStatus={contractAddressError ? 'error' : 'success'}>
+              <FormControl validationStatus={tokenAddressError ? 'error' : 'success'}>
                 <TextInput
                   value={tokenAddress}
                   disabled={disableContractAddress}
                   onChange={(event: any) => {
-                    setContractAddress(event.target.value);
-                    setContractAddressError('');
+                    setTokenAddress(event.target.value);
+                    setTokenAddressError('');
                   }}
                 />
-              {contractAddressError && (
-                  <FormControl.Validation>{contractAddressError}</FormControl.Validation>
+              {tokenAddressError && (
+                  <FormControl.Validation>{tokenAddressError}</FormControl.Validation>
                 )}
               </FormControl>
             </td>
