86f2ca9dd089f073ebfa68e1f94412b751973199
Andrea Rampin <andrearampin@users.noreply.github.com>
2024-01-10 10:01:28 +1100

    WT-2019 - migrate to bridge configs per chain (#1330)

--- packages/checkout/sdk/jest.config.ts
+++ packages/checkout/sdk/jest.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
     '@imtbl/blockchain-data': '<rootDir>../../blockchain-data/sdk/src',
     '@imtbl/generated-clients': '<rootDir>../../internal/generated-clients/src'
   },
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
--- packages/checkout/sdk/src/smartCheckout/allowList/types.ts
+++ packages/checkout/sdk/src/smartCheckout/allowList/types.ts
@@ -1,9 +1,7 @@
 import { TokenInfo } from '../../types';
 
-export type OnRampTokensAllowList = { [key: string]: TokenInfo[] };
-
 export type RoutingTokensAllowList = {
   'bridge'?: TokenInfo[],
   'swap'?: TokenInfo[],
-  'onRamp'?: OnRampTokensAllowList,
+  'onRamp'?: TokenInfo[],
 };
