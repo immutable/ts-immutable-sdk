68309cec4cf72364e6578a73b9935dc717350437
Zach Couchman <zachary.couchman@immutable.com>
2024-01-09 17:04:28 +1100

    add jsdom env to widgets load.test.ts

--- packages/checkout/sdk/test/__mocks__/window.ts
+++ packages/checkout/sdk/test/__mocks__/window.ts
@@ -1,10 +1,10 @@
 /* eslint-disable */
-global.TextEncoder = require('util').TextEncoder;
-global.TextDecoder = require('util').TextDecoder;
+// global.TextEncoder = require('util').TextEncoder;
+// global.TextDecoder = require('util').TextDecoder;
 
-const { JSDOM } = require('jsdom');
-const dom = new JSDOM();
+// const { JSDOM } = require('jsdom');
+// const dom = new JSDOM();
 
-global.window = dom.window;
-global.document = dom.window.document;
-global.navigator = dom.window.navigator;
+// global.window = dom.window;
+// global.document = dom.window.document;
+// global.navigator = dom.window.navigator;
