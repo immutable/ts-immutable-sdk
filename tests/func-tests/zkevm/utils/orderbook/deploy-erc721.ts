2b880dcdcd17c7974bb004bfa2e74dbdde1bf11e
Sam Jeston <sam.jeston@gmail.com>
2024-01-15 16:47:53 +1000

    cleanup comments

--- tests/func-tests/zkevm/utils/orderbook/deploy-erc721.ts
+++ tests/func-tests/zkevm/utils/orderbook/deploy-erc721.ts
@@ -4,9 +4,6 @@ import { deployTestToken } from './erc721';
 
 config();
 
-// For dev create a banker who has power to mint from this contract
-// For sandbox create a banker who has power to mint for this contract
-
 const seaportAddress = process.env.SEAPORT_CONTRACT_ADDRESS;
 const deployerKey = process.env.ORDERBOOK_BANKER;
 const rpcUrl = process.env.RPC_ENDPOINT;
