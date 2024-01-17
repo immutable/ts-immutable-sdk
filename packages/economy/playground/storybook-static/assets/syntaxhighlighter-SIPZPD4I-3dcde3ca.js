703c78af896472f943338c18164590ab325b45d1
Sam Jeston <sam.jeston@gmail.com>
2024-01-17 16:41:25 +1000

    Remove static files

--- packages/economy/playground/storybook-static/assets/syntaxhighlighter-SIPZPD4I-3dcde3ca.js
+++
@@ -1,2 +0,0 @@
-import{S as s,c,s as l}from"./index-0e014c14.js";import"./iframe-6aa095a6.js";import"../sb-preview/runtime.js";import"./index-41fe3f86.js";import"./index-d37d4223.js";import"./index-6f63b3d8.js";import"./index-356e4a49.js";export{s as SyntaxHighlighter,c as createCopyToClipboardFunction,l as default};
-//# sourceMappingURL=syntaxhighlighter-SIPZPD4I-3dcde3ca.js.map
