07d54c6a89797936a6bcb3792cee413e1fa2e6a5
Nik Ho <studying.mole@gmail.com>
2024-01-22 12:57:01 +1300

    update imx functional tests

--- tests/func-tests/imx/jest.config.ts
+++ tests/func-tests/imx/jest.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   testTimeout: 60000,
   moduleDirectories: ["node_modules", "<rootDir>"],
   moduleNameMapper: {
-    "@imtbl/sdk/provider": "<rootDir>/../../../node_modules/@imtbl/sdk/dist/provider",
+    "@imtbl/sdk/x_provider": "<rootDir>/../../../node_modules/@imtbl/sdk/dist/x_provider",
     "@imtbl/sdk/config": "<rootDir>/../../../node_modules/@imtbl/sdk/dist/config",
     "@imtbl/sdk/x_client": "<rootDir>/../../../node_modules/@imtbl/sdk/dist/x_client",
   },
