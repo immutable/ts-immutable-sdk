66658139d5c5e70ba1f93c61fa3e70e3f2a786c2
Sharon Sheah <sharonsheah@gmail.com>
2024-01-24 14:18:23 +0800

    add rocket button & guidance badge on move in progress screen

--- packages/checkout/widgets-lib/src/widgets/bridge/views/MoveInProgress.tsx
+++ packages/checkout/widgets-lib/src/widgets/bridge/views/MoveInProgress.tsx
@@ -1,6 +1,10 @@
 import { useContext, useEffect } from 'react';
 import { UserJourney, useAnalytics } from 'context/analytics-provider/SegmentAnalyticsProvider';
 import { useTranslation } from 'react-i18next';
+import { ButtonNavigationStyles } from 'components/Header/HeaderStyles';
+import { BridgeWidgetViews } from 'context/view-context/BridgeViewContextTypes';
+import { ViewActions, ViewContext } from 'context/view-context/ViewContext';
+import { Badge, ButtCon } from '@biom3/react';
 import { SimpleTextBody } from '../../../components/Body/SimpleTextBody';
 import { HeaderNavigation } from '../../../components/Header/HeaderNavigation';
 import { RocketHero } from '../../../components/Hero/RocketHero';
@@ -19,6 +23,7 @@ export function MoveInProgress({ transactionHash }: MoveInProgressProps) {
   const { eventTargetState: { eventTarget } } = useContext(EventTargetContext);
   const { page } = useAnalytics();
 
+  const { viewDispatch } = useContext(ViewContext);
   const {
     bridgeState: { checkout },
   } = useContext(BridgeContext);
@@ -41,6 +46,32 @@ export function MoveInProgress({ transactionHash }: MoveInProgressProps) {
         <HeaderNavigation
           transparent
           onCloseButtonClick={() => sendBridgeWidgetCloseEvent(eventTarget)}
+          rightActions={(
+            <>
+              <ButtCon
+                icon="Minting"
+                sx={ButtonNavigationStyles()}
+                onClick={() => {
+                  viewDispatch({
+                    payload: {
+                      type: ViewActions.UPDATE_VIEW,
+                      view: { type: BridgeWidgetViews.TRANSACTIONS },
+                    },
+                  });
+                }}
+                testId="settings-button"
+              />
+              <Badge
+                isAnimated
+                variant="guidance"
+                sx={{
+                  position: 'absolute',
+                  right: 'base.spacing.x16',
+                  top: 'base.spacing.x2',
+                }}
+              />
+            </>
+          )}
         />
       )}
       footer={(
@@ -51,6 +82,9 @@ export function MoveInProgress({ transactionHash }: MoveInProgressProps) {
     >
       <SimpleTextBody heading={t('views.IN_PROGRESS.heading')}>
         {t('views.IN_PROGRESS.body2')}
+        <br />
+        <br />
+        {t('views.IN_PROGRESS.body3')}
       </SimpleTextBody>
     </SimpleLayout>
   );
