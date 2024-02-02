ddcd492a70a2f2770eda70ecb6b5c3c14fb8c642
Mikhala <mikhala.kurtjak@immutable.com>
2024-02-02 10:27:40 +0800

    rollup configuration

--- sdk/scripts/copyBrowserBundles.js
+++ sdk/scripts/copyBrowserBundles.js
@@ -53,6 +53,23 @@ const main = () => {
         // data = findAndReplace(data, <find>, <replace>);
 
         fs.writeFileSync(destPath, data);
+
+        // Copy over all chunks when the splitBundle flag is set
+        if (item.splitBundle) {
+          const srcDirectory = path.dirname(sourceFile);
+          const chunkFiles = fs.readdirSync(srcDirectory);
+          chunkFiles.forEach((chunkFile) => {
+            // Check if the file is a .js file
+            if (path.extname(chunkFile) === '.js') {
+              // Skip copying the original file and only copy .js chunks
+              if (chunkFile !== path.basename(sourceFile)) {
+                const chunkSrcPath = path.join(srcDirectory, chunkFile);
+                const chunkDestPath = path.join(directoryPath, chunkFile);
+                fs.copyFileSync(chunkSrcPath, chunkDestPath);
+              }
+            }
+          });
+        }
       });
     }
   });
