d01fab9ecd3cff14e54d44b87cccfd7adc57100b
Samuel Castleman <142861450+scastleman-immutable@users.noreply.github.com>
2024-02-07 12:07:34 +1100

    fixed build tool failing

--- packages/passport/sdk-sample-app/scripts/updateSdkVersion.sh
+++ packages/passport/sdk-sample-app/scripts/updateSdkVersion.sh
@@ -2,4 +2,4 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 sed -i '' "s/__SDK_VERSION__/$(git describe --tags --abbrev=0)/g" $1
 else
 sed -i "s/__SDK_VERSION__/$(git describe --tags --abbrev=0)/g" $1
-i
+fi
