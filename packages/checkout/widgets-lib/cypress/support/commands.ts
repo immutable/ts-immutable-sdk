742e407bbbd1fab4c1610051acb5528fd164d027
Charlie McKenzie <charlie@dreamoftrees.com>
2024-02-08 10:53:24 +1100

    Fixes to support cypress tests

--- packages/checkout/widgets-lib/cypress/support/commands.ts
+++ packages/checkout/widgets-lib/cypress/support/commands.ts
@@ -35,3 +35,19 @@
 //     }
 //   }
 // }
+
+import { useTranslation } from 'react-i18next';
+import * as ReactI18next from 'react-i18next';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      stubI18n: typeof useTranslation;
+    }
+  }
+}
+
+Cypress.Commands.add('stubI18n', () => {
+  const t = (key: string) => key; // Simplistic translation function
+  cy.stub(ReactI18next, 'useTranslation').returns({ t });
+});
