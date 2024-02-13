6904ff4355f10d15a2ae510de91d73f6f40cb97b
Nik Ho <studying.mole@gmail.com>
2024-02-13 13:38:12 +1300

    export IMX generated clients types

---
+++ packages/x-client/src/types/api.ts
@@ -0,0 +1,2 @@
+export * from '@imtbl/generated-clients/dist/imx/api';
+export * from '@imtbl/generated-clients/dist/imx/models';
--- packages/x-client/src/types/index.ts
+++ packages/x-client/src/types/index.ts
@@ -3,3 +3,5 @@ export * from './tokens';
 export * from './requests';
 export * from './transfers';
 export * from './errors';
+// eslint-disable-next-line import/export
+export * from './api';
