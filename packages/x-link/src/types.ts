26ad2624600895dcdd863197cd180094c3e1782a
Alex Connolly <alexconnolly@192-168-1-11.tpgi.com.au>
2024-02-16 06:09:08 +1100

    Finish IMXProvider implementation and highlight missing components

--- packages/x-link/src/types.ts
+++ packages/x-link/src/types.ts
@@ -1,13 +1,25 @@
-import { ProviderPreference } from "@imtbl/imx-sdk";
+import { ProviderPreference } from '@imtbl/imx-sdk';
 
 export interface SetupOptions {
-    providerPreference?: ProviderPreference;
+  providerPreference?: ProviderPreference;
 }
 
 export interface SetupResult {
-    address: string;
-    starkPublicKey: string;
-    ethNetwork: string;
-    providerPreference: string;
-    email?: string;
+  address: string;
+  starkPublicKey: string;
+  ethNetwork: string;
+  providerPreference: string;
+  email?: string;
+}
+
+export interface NFTCheckoutSecondaryParams {
+  provider: string;
+  orderId: string;
+  userWalletAddress: string;
+}
+
+export interface NFTCheckoutPrimaryParams {
+  contractAddress: string;
+  offerId: string;
+  provider: string;
 }
