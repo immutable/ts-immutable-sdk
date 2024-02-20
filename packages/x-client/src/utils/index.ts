9a13b1ce4b80f8498bbf6c5635344b685750943e
Nik Ho <studying.mole@gmail.com>
2024-02-20 14:50:44 +1300

    add stark functions

--- packages/x-client/src/utils/index.ts
+++ packages/x-client/src/utils/index.ts
@@ -1,2 +1,7 @@
 export * from './convertToSignableToken';
 export * from './formatError';
+export {
+  createStarkSigner,
+  generateLegacyStarkPrivateKey,
+  generateStarkPrivateKey,
+} from './starkcurve';
---
+++ packages/x-client/src/utils/starkcurve/README.md
@@ -0,0 +1,3 @@
+# Starkcurve
+
+Index files are generated and should not be edited directly.
