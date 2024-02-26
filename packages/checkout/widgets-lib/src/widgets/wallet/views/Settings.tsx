318805abaf7ce8c42f2446a042dc7d554dbdb3d5
Zach Couchman <zachary.couchman@immutable.com>
2024-02-27 09:33:07 +1100

    handle wallet disconnect

--- packages/checkout/widgets-lib/src/widgets/wallet/views/Settings.tsx
+++ packages/checkout/widgets-lib/src/widgets/wallet/views/Settings.tsx
@@ -1,6 +1,8 @@
 import { Box, Button } from '@biom3/react';
 import { useContext, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useWalletConnect } from 'lib/hooks/useWalletConnect';
+import { isMetaMaskProvider, isPassportProvider, isWalletConnectProvider } from 'lib/providerUtils';
 import { FooterLogo } from '../../../components/Footer/FooterLogo';
 import { HeaderNavigation } from '../../../components/Header/HeaderNavigation';
 import { SimpleLayout } from '../../../components/SimpleLayout/SimpleLayout';
@@ -14,8 +16,10 @@ import { UserJourney, useAnalytics } from '../../../context/analytics-provider/S
 export function Settings() {
   const { t } = useTranslation();
   const { connectLoaderState } = useContext(ConnectLoaderContext);
-  const { provider } = connectLoaderState;
+  const { checkout, provider } = connectLoaderState;
   const { eventTargetState: { eventTarget } } = useContext(EventTargetContext);
+
+  const { ethereumProvider } = useWalletConnect({ checkout: checkout! });
 
   const { page } = useAnalytics();
 
@@ -25,6 +29,36 @@ export function Settings() {
       screen: 'Settings',
     });
   }, []);
+
+  const handleWCDisconnect = async () => {
+    if (isPassportProvider(provider)) {
+      console.log('disconnecting passport provider by calling logout on Passport');
+      await checkout?.passport?.logout();
+      console.log('disconnected');
+      return;
+    }
+
+    if (isMetaMaskProvider(provider)) {
+      console.log('disconnecting MetaMask provider by revoking permissions');
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      await (provider?.provider as any).request({ method: 'wallet_revokePermissions', params: [{ eth_accounts: {} }] });
+      console.log('disconnected');
+      return;
+    }
+
+    if (isWalletConnectProvider(provider)) {
+      console.log('disconnecting WalletConnect by disconnecting all pairings and then provider disconnect');
+      if (ethereumProvider?.session) {
+        const pairings = ethereumProvider?.signer.client.core.pairing.getPairings();
+        console.log('pairings', pairings);
+        // eslint-disable-next-line max-len
+        const pairingsToDisconnect = pairings.map((pairing) => ethereumProvider?.signer.client.core.pairing.disconnect({ topic: pairing.topic }));
+        await Promise.allSettled(pairingsToDisconnect);
+        await ethereumProvider.disconnect();
+        console.log('disconnected');
+      }
+    }
+  };
 
   return (
     <SimpleLayout
@@ -46,7 +80,10 @@ export function Settings() {
           testId="disconnect-button"
           variant="secondary"
           sx={settingsDisconnectButtonStyle}
-          onClick={() => sendDisconnectWalletEvent(eventTarget)}
+          onClick={() => {
+            handleWCDisconnect();
+            sendDisconnectWalletEvent(eventTarget);
+          }}
         >
           {t('views.SETTINGS.disconnectButton.label')}
         </Button>
