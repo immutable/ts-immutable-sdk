f778f4c6fa89979230778560820d95b6983f928f
Nik Ho <studying.mole@gmail.com>
2024-03-01 12:40:29 +1300

    update x-client config exports

--- packages/x-client/src/index.ts
+++ packages/x-client/src/index.ts
@@ -1,11 +1,4 @@
-export {
-  EthConfiguration,
-  ApiConfiguration,
-  ImxModuleConfiguration,
-  ImmutableXConfiguration,
-  createImmutableXConfiguration,
-  imxClientConfig,
-} from './config';
+export * from './config';
 export { IMXClient, ImmutableX } from './IMXClient';
 export * from './exportUtils';
 export * as Contracts from './exportContracts';
