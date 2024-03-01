1bd55ef65566e70c41d0fd7b341cc6fe0d07f1a1
Nik Ho <studying.mole@gmail.com>
2024-03-01 16:54:12 +1300

    alias exports to maintain backwards compatibility

--- packages/x-client/src/index.ts
+++ packages/x-client/src/index.ts
@@ -3,3 +3,12 @@ export { IMXClient, ImmutableX } from './IMXClient';
 export * from './exportUtils';
 export * as Contracts from './exportContracts';
 export * from './types';
+/**
+ * aliased exports to maintain backwards compatibility
+ */
+export { ImxModuleConfiguration as ImxClientModuleConfiguration } from './config';
+export {
+  generateLegacyStarkPrivateKey as imxClientGenerateLegacyStarkPrivateKey,
+  createStarkSigner as imxClientCreateStarkSigner,
+} from 'exportUtils';
+export { WalletConnection as ImxClientWalletConnection } from './types';
