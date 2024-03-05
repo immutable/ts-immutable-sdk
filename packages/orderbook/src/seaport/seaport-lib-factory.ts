2086ac7fbc7cdce9e35d3d65bac7cd1e2bb1e17c
Sam Jeston <sam.jeston@gmail.com>
2024-03-05 15:41:50 +1000

    Migrate to seaport-js v3

--- packages/orderbook/src/seaport/seaport-lib-factory.ts
+++ packages/orderbook/src/seaport/seaport-lib-factory.ts
@@ -1,9 +1,17 @@
 import { Seaport as SeaportLib } from '@opensea/seaport-js';
+// import { JsonRpcProvider } from 'ethers-v6';
 import { providers } from 'ethers';
 import { SEAPORT_CONTRACT_VERSION_V1_5 } from './constants';
 
 export type SeaportVersion =
   typeof SEAPORT_CONTRACT_VERSION_V1_5;
+
+// function convertToV6Provider(
+//   provider: providers.JsonRpcProvider,
+// ): JsonRpcProvider {
+//   // TODO: Not sure if this will work for browsers, need to test
+//   return new JsonRpcProvider(provider.connection.url);
+// }
 
 export class SeaportLibFactory {
   constructor(
@@ -15,7 +23,9 @@ export class SeaportLibFactory {
     const seaportVersion = orderSeaportVersion ?? SEAPORT_CONTRACT_VERSION_V1_5;
     const seaportContractAddress = orderSeaportAddress ?? this.defaultSeaportContractAddress;
 
-    return new SeaportLib(this.provider, {
+    // TODO: Another option to explor - the SDK probably only uses the provider to do direct
+    // RPC calls, so might just be able to cast it and see.... we are not sending Transactions
+    return new SeaportLib(this.provider as any, {
       seaportVersion,
       balanceAndApprovalChecksOnOrderCreation: true,
       overrides: {
--- packages/orderbook/src/seaport/transaction.ts
+++ packages/orderbook/src/seaport/transaction.ts
@@ -1,4 +1,5 @@
-import { TransactionMethods } from '@opensea/seaport-js/lib/types';
+import { TransactionMethods } from '@opensea/seaport-js/lib/utils/usecase';
+import { PopulatedTransaction, BigNumber } from 'ethers';
 import { TransactionBuilder } from 'types';
 
 // Add 20% more gas than estimate to prevent out of gas errors
@@ -9,12 +10,30 @@ export function prepareTransaction(
   chainId: number,
 ): TransactionBuilder {
   return async () => {
-    const transaction = await transactionMethods.buildTransaction();
-    transaction.gasLimit = await transactionMethods.estimateGas();
-    transaction.gasLimit = transaction.gasLimit
-      .add(transaction.gasLimit.div(5));
-    transaction.chainId = chainId;
+    const v6ContractTransaction = await transactionMethods.buildTransaction();
 
-    return transaction;
+    const v5PopulatedTransaction: PopulatedTransaction = {
+      to: v6ContractTransaction.to,
+      from: v6ContractTransaction.from,
+      type: v6ContractTransaction.type,
+      maxFeePerGas: v6ContractTransaction.maxFeePerGas
+        ? BigNumber.from(v6ContractTransaction.maxFeePerGas)
+        : undefined,
+      maxPriorityFeePerGas: v6ContractTransaction.maxPriorityFeePerGas
+        ? BigNumber.from(v6ContractTransaction.maxPriorityFeePerGas)
+        : undefined,
+      accessList: v6ContractTransaction.accessList,
+      customData: v6ContractTransaction.customData,
+      value: v6ContractTransaction.value ? BigNumber.from(v6ContractTransaction.value) : undefined,
+      data: v6ContractTransaction.data,
+      nonce: v6ContractTransaction.nonce,
+      chainId,
+    };
+
+    v5PopulatedTransaction.gasLimit = BigNumber.from(await transactionMethods.estimateGas());
+    v5PopulatedTransaction.gasLimit = v5PopulatedTransaction.gasLimit
+      .add(v5PopulatedTransaction.gasLimit.div(5));
+
+    return v5PopulatedTransaction;
   };
 }
