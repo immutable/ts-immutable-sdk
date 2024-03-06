ad8c95947c73f9b2c169926e688fc580c7565719
Jhonatan Gonzalez <jhesgodi@gmail.com>
2024-03-06 15:22:56 +1100

    Fix: [GPR-468/468/470][Sale widget] speed up payment flow by removing unnecessary views (#1569)

--- packages/checkout/widgets-lib/src/views/loading/LoadingView.tsx
+++ packages/checkout/widgets-lib/src/views/loading/LoadingView.tsx
@@ -2,14 +2,21 @@ import { LoadingOverlay } from '@biom3/react';
 import { SimpleLayout } from '../../components/SimpleLayout/SimpleLayout';
 
 export interface LoadingViewProps {
-  loadingText: string;
+  loadingText: string | string[];
+  textDuration?: number;
 }
-export function LoadingView({ loadingText }: LoadingViewProps) {
+export function LoadingView({ loadingText, textDuration }: LoadingViewProps) {
+  const text = Array.isArray(loadingText) ? loadingText : [loadingText];
+  const duration = textDuration || 2500;
+
   return (
     <SimpleLayout>
       <LoadingOverlay visible>
         <LoadingOverlay.Content>
-          <LoadingOverlay.Content.LoopingText text={[loadingText]} />
+          <LoadingOverlay.Content.LoopingText
+            text={[...text]}
+            textDuration={duration}
+          />
         </LoadingOverlay.Content>
       </LoadingOverlay>
     </SimpleLayout>
