58dd41181ba28c4d281149246aea59aea1e11a78
Nik Ho <studying.mole@gmail.com>
2024-03-11 23:51:45 +1100

    switch to default export for elliptic package

--- packages/x-client/src/utils/stark/starkCurve.ts
+++ packages/x-client/src/utils/stark/starkCurve.ts
@@ -2,7 +2,8 @@
 import { Signer } from '@ethersproject/abstract-signer';
 import { splitSignature } from '@ethersproject/bytes';
 import hash from 'hash.js';
-import { curves, ec } from 'elliptic';
+import elliptic from 'elliptic';
+// import { curves, ec } from 'elliptic';
 import * as encUtils from 'enc-utils';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import BN from 'bn.js';
@@ -41,8 +42,8 @@ export const starkEcOrder = new BN(
 );
 
 // eslint-disable-next-line new-cap
-export const starkEc = new ec(
-  new curves.PresetCurve({
+export const starkEc = new elliptic.ec(
+  new elliptic.curves.PresetCurve({
     type: 'short',
     prime: null,
     p: '08000000 00000011 00000000 00000000 00000000 00000000 00000000 00000001',
