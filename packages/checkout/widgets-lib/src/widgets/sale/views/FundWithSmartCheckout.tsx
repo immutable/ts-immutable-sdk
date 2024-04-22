9e172ec3f66830d6c47b02de483155b70e3e50a3
Jhonatan Gonzalez <jhesgodi@gmail.com>
2024-04-22 11:23:11 +0900

    feat: [Sale Widget] Add drafted order summary component (#1678)

--- packages/checkout/widgets-lib/src/widgets/sale/views/FundWithSmartCheckout.tsx
+++ packages/checkout/widgets-lib/src/widgets/sale/views/FundWithSmartCheckout.tsx
@@ -1,23 +1,28 @@
 import { Box } from '@biom3/react';
 import { FundingRoute } from '@imtbl/checkout-sdk';
 import {
-  useContext,
-  useEffect,
-  useMemo, useState,
+  useContext, useEffect, useMemo, useState,
 } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   FundWithSmartCheckoutSubViews,
   SaleWidgetViews,
 } from '../../../context/view-context/SaleViewContextTypes';
-import { ViewActions, ViewContext } from '../../../context/view-context/ViewContext';
+import {
+  ViewActions,
+  ViewContext,
+} from '../../../context/view-context/ViewContext';
 import { LoadingView } from '../../../views/loading/LoadingView';
 import { FundingRouteExecute } from '../components/FundingRouteExecute/FundingRouteExecute';
 import { FundingRouteSelect } from '../components/FundingRouteSelect/FundingRouteSelect';
 import { useSaleContext } from '../context/SaleContextProvider';
 import { useSaleEvent } from '../hooks/useSaleEvents';
-import { CryptoFiatActions, CryptoFiatContext } from '../../../context/crypto-fiat-context/CryptoFiatContext';
+import {
+  CryptoFiatActions,
+  CryptoFiatContext,
+} from '../../../context/crypto-fiat-context/CryptoFiatContext';
 import { smartCheckoutTokensList } from '../functions/smartCheckoutUtils';
+import { SignPaymentTypes } from '../types';
 
 type FundWithSmartCheckoutProps = {
   subView: FundWithSmartCheckoutSubViews;
@@ -32,7 +37,12 @@ export function FundWithSmartCheckout({ subView }: FundWithSmartCheckoutProps) {
   >(undefined);
   const [fundingRouteStepIndex, setFundingRouteStepIndex] = useState<number>(0);
   const {
-    querySmartCheckout, fundingRoutes, smartCheckoutResult, collectionName, fromTokenAddress,
+    querySmartCheckout,
+    fundingRoutes,
+    smartCheckoutResult,
+    collectionName,
+    fromTokenAddress,
+    sign,
   } = useSaleContext();
   const { cryptoFiatDispatch } = useContext(CryptoFiatContext);
 
@@ -44,6 +54,24 @@ export function FundWithSmartCheckout({ subView }: FundWithSmartCheckoutProps) {
     if (subView !== FundWithSmartCheckoutSubViews.INIT || !fromTokenAddress) return;
     querySmartCheckout();
   }, [subView, fromTokenAddress]);
+
+  useEffect(() => {
+    if (!smartCheckoutResult) {
+      return;
+    }
+
+    if (smartCheckoutResult.sufficient) {
+      sign(SignPaymentTypes.CRYPTO);
+      viewDispatch({
+        payload: {
+          type: ViewActions.UPDATE_VIEW,
+          view: {
+            type: SaleWidgetViews.PAY_WITH_COINS,
+          },
+        },
+      });
+    }
+  }, [smartCheckoutResult]);
 
   useEffect(() => {
     if (!cryptoFiatDispatch || !smartCheckoutResult) return;
@@ -98,7 +126,11 @@ export function FundWithSmartCheckout({ subView }: FundWithSmartCheckoutProps) {
   return (
     <Box>
       {subView === FundWithSmartCheckoutSubViews.INIT && (
-        <LoadingView loadingText={t('views.FUND_WITH_SMART_CHECKOUT.loading.checkingBalances')} />
+        <LoadingView
+          loadingText={t(
+            'views.FUND_WITH_SMART_CHECKOUT.loading.checkingBalances',
+          )}
+        />
       )}
       {subView === FundWithSmartCheckoutSubViews.FUNDING_ROUTE_SELECT && (
         <FundingRouteSelect
