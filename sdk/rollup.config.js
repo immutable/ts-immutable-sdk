a7efc7309680c73b2b4d06bd689c3814a4286afe
Shine Li <shine.li@immutable.com>
2024-05-07 08:00:12 +1000

    try fix build

--- sdk/rollup.config.js
+++ sdk/rollup.config.js
@@ -69,8 +69,12 @@ const getFileBuild = (inputFilename) => [
     plugins: [
       dts({
         respectExternal: true,
+        compilerOptions: {
+          skipLibCheck: true,
+        }
       }),
     ],
+    external: ['events'] 
   },
 ];
 
