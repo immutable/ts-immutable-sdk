d18b0b39f01ee7c44a7d7320cb8815d826527e63
Sam Jeston <sam.jeston@gmail.com>
2024-05-14 10:50:48 +1000

    Allowance aggregator specs

--- packages/checkout/sdk/src/smartCheckout/aggregators/allowanceAggregator.test.ts
+++ packages/checkout/sdk/src/smartCheckout/aggregators/allowanceAggregator.test.ts
@@ -62,6 +62,65 @@ describe('allowanceAggregator', () => {
     }]);
   });
 
+  it('should return an array with the insufficient ERC1155 allowance', () => {
+    const erc20Allowances: ItemAllowance = {
+      sufficient: true,
+      allowances: [{
+        sufficient: true,
+        itemRequirement: {
+          type: ItemType.ERC20,
+          tokenAddress: '0xERC20',
+          amount: BigNumber.from(1),
+          spenderAddress: '0xSEAPORT',
+        },
+      }],
+    };
+    const erc721Allowances: ItemAllowance = {
+      sufficient: true,
+      allowances: [
+        {
+          sufficient: true,
+          itemRequirement: {
+            type: ItemType.ERC721,
+            contractAddress: '0xERC721',
+            id: '0xID',
+            spenderAddress: '0xSEAPORT',
+          },
+        },
+      ],
+    };
+    const erc1155Allowances: ItemAllowance = {
+      sufficient: false,
+      allowances: [
+        {
+          sufficient: false,
+          approvalTransaction: undefined,
+          type: ItemType.ERC1155,
+          itemRequirement: {
+            amount: '10',
+            type: ItemType.ERC1155,
+            contractAddress: '0xERC1155',
+            id: '0xID',
+            spenderAddress: '0xSEAPORT',
+          },
+        },
+      ],
+    };
+    const result = allowanceAggregator(erc20Allowances, erc721Allowances, erc1155Allowances);
+    expect(result).toEqual([{
+      sufficient: false,
+      type: ItemType.ERC1155,
+      itemRequirement: {
+        amount: '10',
+        type: ItemType.ERC1155,
+        contractAddress: '0xERC1155',
+        id: '0xID',
+        spenderAddress: '0xSEAPORT',
+      },
+      approvalTransaction: undefined,
+    }]);
+  });
+
   it('should return an array with the insufficient ERC721 allowance', () => {
     const erc20Allowances: ItemAllowance = {
       sufficient: true,
@@ -118,7 +177,7 @@ describe('allowanceAggregator', () => {
     }]);
   });
 
-  it('should return an array with both allowances if they are not sufficient', () => {
+  it('should return an array with all allowances if they are not sufficient', () => {
     const erc20Allowances: ItemAllowance = {
       sufficient: false,
       allowances: [{
@@ -149,8 +208,21 @@ describe('allowanceAggregator', () => {
       }],
     };
     const erc1155Allowances: ItemAllowance = {
-      sufficient: true,
-      allowances: [],
+      sufficient: false,
+      allowances: [
+        {
+          sufficient: false,
+          approvalTransaction: undefined,
+          type: ItemType.ERC1155,
+          itemRequirement: {
+            amount: '10',
+            type: ItemType.ERC1155,
+            contractAddress: '0xERC1155',
+            id: '0xID',
+            spenderAddress: '0xSEAPORT',
+          },
+        },
+      ],
     };
     const result = allowanceAggregator(erc20Allowances, erc721Allowances, erc1155Allowances);
     expect(result).toEqual(expect.arrayContaining([
@@ -177,10 +249,22 @@ describe('allowanceAggregator', () => {
         },
         approvalTransaction: undefined,
       },
+      {
+        sufficient: false,
+        type: ItemType.ERC1155,
+        itemRequirement: {
+          amount: '10',
+          type: ItemType.ERC1155,
+          contractAddress: '0xERC1155',
+          id: '0xID',
+          spenderAddress: '0xSEAPORT',
+        },
+        approvalTransaction: undefined,
+      },
     ]));
   });
 
-  it('should return an empty array if both allowances are sufficient', () => {
+  it('should return an empty array if all allowances are sufficient', () => {
     const erc20Allowances: ItemAllowance = {
       sufficient: true,
       allowances: [
@@ -211,7 +295,18 @@ describe('allowanceAggregator', () => {
     };
     const erc1155Allowances: ItemAllowance = {
       sufficient: true,
-      allowances: [],
+      allowances: [
+        {
+          sufficient: true,
+          itemRequirement: {
+            type: ItemType.ERC1155,
+            amount: '5',
+            contractAddress: '0xERC1155',
+            id: '0xID',
+            spenderAddress: '0xSEAPORT',
+          },
+        },
+      ],
     };
     const result = allowanceAggregator(erc20Allowances, erc721Allowances, erc1155Allowances);
     expect(result).toEqual([]);
