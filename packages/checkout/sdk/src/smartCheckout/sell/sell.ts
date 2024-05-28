3b5440cd0b7c5eb430fa39e6289d1dfb750793fc
mdymalla <markus.dymalla@immutable.com>
2024-05-28 10:58:13 +1000

    fix type issue

--- packages/checkout/sdk/src/smartCheckout/sell/sell.ts
+++ packages/checkout/sdk/src/smartCheckout/sell/sell.ts
@@ -253,10 +253,7 @@ export const sell = async (
       let tokenQuantity = BigNumber.from(1);
 
       // if type exists in sellToken then it is valid ERC721 or ERC1155 and not deprecated type
-      if ('type' in sellToken) {
-        const erc1155Token = sellToken as unknown as ERC1155Item | ERC721Item;
-        if (erc1155Token.type === ItemType.ERC1155) tokenQuantity = BigNumber.from(erc1155Token.amount);
-      }
+      if (sellTokenHasType && sellToken.type === ItemType.ERC1155) tokenQuantity = BigNumber.from(sellToken.amount);
 
       const orderBookFees = calculateFees(makerFees, buyTokenOrNative.amount, decimals, tokenQuantity);
       if (orderBookFees.length !== makerFees.length) {
