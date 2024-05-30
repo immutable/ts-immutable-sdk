b813e0198b76e6055723a921772196a886252782
markus <55011443+mdymalla@users.noreply.github.com>
2024-05-31 08:49:34 +1000

    chore: [TD-1424] update oapi spec (#1835)

--- packages/orderbook/src/openapi/sdk/models/FillStatus.ts
+++ packages/orderbook/src/openapi/sdk/models/FillStatus.ts
@@ -2,6 +2,9 @@
 /* tslint:disable */
 /* eslint-disable */
 
+/**
+ * The ratio of the order that has been filled, an order that has been fully filled will have the same numerator and denominator values.
+ */
 export type FillStatus = {
   /**
    * The numerator of the fill status
