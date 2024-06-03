8804b90ca10567f7dfbacd309b535517e057ddb4
Mimi Immutable <mimi.tran@immutable.com>
2024-06-03 13:35:37 +1000

    Add pay with coins flow placeholders

--- packages/checkout/widgets-lib/src/widgets/sale/views/PayWithCoins.tsx
+++ packages/checkout/widgets-lib/src/widgets/sale/views/PayWithCoins.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useRef } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useCallback, useEffect, useRef } from 'react';
+// import { useTranslation } from 'react-i18next';
+import { Heading } from '@biom3/react';
+import { getRemoteImage } from 'lib/utils';
+import { Environment } from '@imtbl/config';
+import { useHandover } from 'lib/hooks/useHandover';
+import { HandoverTarget } from 'context/handover-context/HandoverContext';
 import { SaleWidgetViews } from '../../../context/view-context/SaleViewContextTypes';
 import { useSaleContext } from '../context/SaleContextProvider';
-import { LoadingView } from '../../../views/loading/LoadingView';
 import { useSaleEvent } from '../hooks/useSaleEvents';
 
 export function PayWithCoins() {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
   const processing = useRef(false);
   const {
     sendPageView,
@@ -18,21 +22,58 @@ export function PayWithCoins() {
   const {
     executeAll, signResponse, executeResponse, signTokenIds,
   } = useSaleContext();
-  const executedTxns = executeResponse?.transactions.length || 0;
 
-  let loadingText = [
-    t('views.PAYMENT_METHODS.loading.ready1'),
-    t('views.PAYMENT_METHODS.loading.ready2'),
-    t('views.PAYMENT_METHODS.loading.ready3'),
-  ];
+  const { addHandover } = useHandover({ id: HandoverTarget.GLOBAL });
 
-  if (executedTxns >= 1) {
-    loadingText = [
-      t('views.PAYMENT_METHODS.loading.processing1'),
-      t('views.PAYMENT_METHODS.loading.processing2'),
-      t('views.PAYMENT_METHODS.loading.processing3'),
-    ];
-  }
+  const onBeforeApproveCallback = useCallback(() => {
+    setTimeout(() => {
+      addHandover({
+        animationUrl: getRemoteImage(
+          Environment.SANDBOX,
+          '/approve-handover.riv',
+        ),
+        children: (
+          <Heading>Waiting for you to approve access to your coins</Heading>
+        ),
+      });
+    }, 0);
+  }, [addHandover]);
+
+  const onAfterApproveCallback = useCallback(() => {
+    setTimeout(() => {
+      addHandover({
+        animationUrl: getRemoteImage(
+          Environment.SANDBOX,
+          '/approve-handover.riv',
+        ),
+        children: <Heading>Coins ready for item purchase</Heading>,
+      });
+    }, 0);
+  }, [addHandover]);
+
+  const onBeforeExecuteCallback = useCallback(() => {
+    setTimeout(() => {
+      addHandover({
+        animationUrl: getRemoteImage(
+          Environment.SANDBOX,
+          '/execute-handover.riv',
+        ),
+        children: <Heading>Finalising your order </Heading>,
+      });
+    }, 0);
+  }, [addHandover]);
+
+  const onAfterExecuteCallback = useCallback(() => {
+    setTimeout(() => {
+      addHandover({
+        animationUrl: getRemoteImage(
+          Environment.SANDBOX,
+          '/execute-handover.riv',
+        ),
+        children: <Heading>Processing purchase</Heading>,
+      });
+    }, 0);
+  }, [addHandover]);
 
   const sendTransaction = async () => {
     executeAll(
@@ -50,6 +91,14 @@ export function PayWithCoins() {
   useEffect(() => {
     if (signResponse !== undefined && processing.current === false) {
       processing.current = true;
+
+      addHandover({
+        animationUrl: getRemoteImage(Environment.SANDBOX, '/handover.riv'),
+        children: (
+          <Heading sx={{ px: 'base.spacing.x6' }}>Preparing Order</Heading>
+        ),
+      });
+
       sendTransaction();
     }
   }, [signResponse]);
@@ -57,12 +106,19 @@ export function PayWithCoins() {
   useEffect(() => {
     if (executeResponse?.done === true) {
       const details = { transactionId: signResponse?.transactionId };
-      sendSuccessEvent(SaleWidgetViews.SALE_SUCCESS, executeResponse?.transactions, signTokenIds, details); // checkoutPrimarySaleSaleSuccess_SuccessEventSucceeded
+      sendSuccessEvent(
+        SaleWidgetViews.SALE_SUCCESS,
+        executeResponse?.transactions,
+        signTokenIds,
+        details,
+      ); // checkoutPrimarySaleSaleSuccess_SuccessEventSucceeded
       sendCloseEvent(SaleWidgetViews.SALE_SUCCESS); // checkoutPrimarySaleSaleSuccess_CloseButtonPressed
+      console.log('@@@ sale success', executeResponse);
     }
   }, [executeResponse]);
 
   useEffect(() => sendPageView(SaleWidgetViews.PAY_WITH_COINS), []); // checkoutPrimarySalePayWithCoinsViewed
 
-  return <LoadingView loadingText={loadingText} />;
+  // return <LoadingView loadingText={loadingText} />;
+  return null;
 }
