919effa37c0259b9601a97e4eb7260c9179a40b7
Mimi Immutable <mimi.tran@immutable.com>
2024-06-07 15:39:38 +1000

    Add ExecuteTransactionStep type

--- packages/checkout/widgets-lib/src/widgets/sale/types.ts
+++ packages/checkout/widgets-lib/src/widgets/sale/types.ts
@@ -135,3 +135,8 @@ export type FundingBalanceResult = {
   currency: OrderQuoteCurrency;
   smartCheckoutResult: SmartCheckoutResult;
 };
+
+export enum ExecuteTransactionStep {
+  BEFORE = 'before',
+  AFTER = 'after',
+}
