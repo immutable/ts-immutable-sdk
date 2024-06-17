1e5ce6fc1843b6ee6f2051cc222fb8650256dd22
Naveen <116692862+naveen-imtb@users.noreply.github.com>
2024-06-17 15:19:26 +1000

    TD-1484: docs: add comments.

--- packages/orderbook/src/orderbook.ts
+++ packages/orderbook/src/orderbook.ts
@@ -38,16 +38,18 @@ import {
 
 function determineFillableUnits(order: Order, amountToFill?: string): string | undefined {
   if (order.sell[0].type === 'ERC1155' && !amountToFill) {
-    const totalFilled = order.fill_status.numerator;
-    const totalSize = order.fill_status.denominator;
+    // fill status is expressed as a ratio
+    const { numerator, denominator } = order.fill_status;
     const originalOfferAmt = BigInt(((order.sell[0] as unknown) as ERC1155Item).amount);
 
-    if (totalFilled === '0' || totalSize === '0') {
+    if (numerator === '0' || denominator === '0') {
       return originalOfferAmt.toString();
     }
 
-    return ((BigInt(totalSize) - (BigInt(totalFilled)) * BigInt(originalOfferAmt))
-        / BigInt(totalSize)).toString();
+    // calculate the remaining amount to fill
+    // remaining = ((denominator - numerator) * originalOfferAmt) / denominator
+    return ((BigInt(denominator) - (BigInt(numerator)) * BigInt(originalOfferAmt))
+        / BigInt(denominator)).toString();
   }
 
   return amountToFill;
