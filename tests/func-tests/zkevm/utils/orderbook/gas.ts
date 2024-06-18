d78e5beb67179e37d9a29b595535d2a5f087e67b
Sam Jeston <sam.jeston@gmail.com>
2024-06-18 11:12:24 +1000

    bump fees

--- tests/func-tests/zkevm/utils/orderbook/gas.ts
+++ tests/func-tests/zkevm/utils/orderbook/gas.ts
@@ -1,6 +1,6 @@
 import { BigNumber } from 'ethers';
 
 export const GAS_OVERRIDES = {
-  maxFeePerGas: BigNumber.from(101e9),
-  maxPriorityFeePerGas: BigNumber.from(100e9),
+  maxFeePerGas: BigNumber.from(15e8),
+  maxPriorityFeePerGas: BigNumber.from(12e8),
 };
