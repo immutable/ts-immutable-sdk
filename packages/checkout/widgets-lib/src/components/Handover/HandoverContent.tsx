6f9d75d463f6345add7e8825f10b7d5c862c60d0
Mimi Immutable <mimi.tran@immutable.com>
2024-06-18 15:28:32 +1000

    Add styles to HandoverContent

--- packages/checkout/widgets-lib/src/components/Handover/HandoverContent.tsx
+++ packages/checkout/widgets-lib/src/components/Handover/HandoverContent.tsx
@@ -1,44 +1,67 @@
-import { Button, Heading } from '@biom3/react';
+import {
+  Body, Box, Button, Heading,
+} from '@biom3/react';
 
 export function HandoverContent({
   headingText,
+  subheadingText,
   primaryButtonText,
   onPrimaryButtonClick,
   secondaryButtonText,
   onSecondaryButtonClick,
 }: {
   headingText: string;
+  subheadingText?: string;
   primaryButtonText?: string;
   onPrimaryButtonClick?: () => void;
   secondaryButtonText?: string;
   onSecondaryButtonClick?: () => void;
 }) {
   return (
-    <>
-      <Heading sx={{ paddingBottom: 'base.spacing.x42' }}>
-        {headingText}
-      </Heading>
-      {primaryButtonText && onPrimaryButtonClick && (
-        <Button
-          sx={{ width: '100%' }}
-          variant="primary"
-          size="large"
-          onClick={onPrimaryButtonClick}
-        >
-          {primaryButtonText}
-        </Button>
-      )}
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Box>
+        <Heading>{headingText}</Heading>
 
-      {secondaryButtonText && onSecondaryButtonClick && (
-        <Button
-          sx={{ width: '100%' }}
-          variant="tertiary"
-          size="large"
-          onClick={onSecondaryButtonClick}
+        <Body
+          size="small"
+          sx={{
+            textAlign: 'center',
+            color: 'base.color.text.body.secondary',
+            paddingTop: 'base.spacing.x4',
+          }}
         >
-          {secondaryButtonText}
-        </Button>
-      )}
-    </>
+          {subheadingText}
+        </Body>
+      </Box>
+
+      <Box sx={{ paddingTop: 'base.spacing.x10' }}>
+        {primaryButtonText && onPrimaryButtonClick && (
+          <Button
+            sx={{ width: '100%' }}
+            variant="primary"
+            size="large"
+            onClick={onPrimaryButtonClick}
+          >
+            {primaryButtonText}
+          </Button>
+        )}
+        {secondaryButtonText && onSecondaryButtonClick && (
+          <Button
+            sx={{ width: '100%', paddingTop: 'base.spacing.x3' }}
+            variant="tertiary"
+            size="large"
+            onClick={onSecondaryButtonClick}
+          >
+            {secondaryButtonText}
+          </Button>
+        )}
+      </Box>
+    </Box>
   );
 }
