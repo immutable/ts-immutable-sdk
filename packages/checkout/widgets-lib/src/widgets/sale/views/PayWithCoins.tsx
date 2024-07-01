39bd9200fba3fc165695adcd041d0fec65e03f6b
Mimi Tran <80493680+mimi-imtbl@users.noreply.github.com>
2024-07-01 10:09:41 +1000

    [feat][CM-679] [Sale Widget] Add Handover screens + user initiated Transaction flow (#1879)

--- packages/checkout/widgets-lib/src/widgets/sale/views/PayWithCoins.tsx
+++ packages/checkout/widgets-lib/src/widgets/sale/views/PayWithCoins.tsx
@@ -1,13 +1,59 @@
-import { useEffect, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SaleWidgetViews } from '../../../context/view-context/SaleViewContextTypes';
+import { useCallback, useEffect, useRef } from 'react';
+import { Heading } from '@biom3/react';
+
 import { useSaleContext } from '../context/SaleContextProvider';
-import { LoadingView } from '../../../views/loading/LoadingView';
+import { SaleErrorTypes } from '../types';
 import { useSaleEvent } from '../hooks/useSaleEvents';
+import {
+  StateMachineInput,
+  TransactionMethod,
+  getRiveAnimationName,
+  useHandoverSteps,
+} from '../hooks/useHandoverSteps';
+import { useHandover } from '../../../lib/hooks/useHandover';
+import { HandoverTarget } from '../../../context/handover-context/HandoverContext';
+import { HandoverContent } from '../../../components/Handover/HandoverContent';
+import { SaleWidgetViews } from '../../../context/view-context/SaleViewContextTypes';
+import { isPassportProvider } from '../../../lib/provider';
+import { getRemoteRive } from '../../../lib/utils';
+import { HandoverDuration } from '../../../context/handover-context/HandoverProvider';
+
+interface StepConfig {
+  headingTextKey: string;
+  animationUrl: string;
+  inputValue: number;
+  ctaButtonTextKey?: string;
+}
+
+const initateExecuteNextTransactionHandover: Record<
+TransactionMethod,
+StepConfig
+> = {
+  [TransactionMethod.APPROVE]: {
+    headingTextKey:
+      'views.PAYMENT_METHODS.handover.approve.beforeWithCta.heading',
+    ctaButtonTextKey:
+      'views.PAYMENT_METHODS.handover.approve.beforeWithCta.ctaButton',
+
+    animationUrl: getRiveAnimationName(TransactionMethod.APPROVE),
+    inputValue: StateMachineInput.WAITING,
+  },
+  [TransactionMethod.EXECUTE]: {
+    headingTextKey:
+      'views.PAYMENT_METHODS.handover.execute.beforeWithCta.heading',
+    ctaButtonTextKey:
+      'views.PAYMENT_METHODS.handover.execute.beforeWithCta.ctaButton',
+    animationUrl: getRiveAnimationName(TransactionMethod.EXECUTE),
+    inputValue: StateMachineInput.START,
+  },
+};
 
 export function PayWithCoins() {
-  const { t } = useTranslation();
   const processing = useRef(false);
+  const prevTransactionIndexRef = useRef<number | null>(null);
+
+  const { t } = useTranslation();
   const {
     sendPageView,
     sendTransactionSuccessEvent,
@@ -15,57 +61,138 @@ export function PayWithCoins() {
     sendCloseEvent,
     sendSuccessEvent,
   } = useSaleEvent();
+
   const {
-    execute, signResponse, executeResponse, signTokenIds,
+    executeAll,
+    executeNextTransaction,
+    signResponse,
+    filteredTransactions,
+    currentTransactionIndex,
+    executeResponse,
+    signTokenIds,
+    environment,
+    provider,
+    goToErrorView,
   } = useSaleContext();
-  const executedTxns = executeResponse?.transactions.length || 0;
 
-  let loadingText = [
-    t('views.PAYMENT_METHODS.loading.ready1'),
-    t('views.PAYMENT_METHODS.loading.ready2'),
-    t('views.PAYMENT_METHODS.loading.ready3'),
-  ];
+  const { onTxnStepExecuteNextTransaction, onTxnStepExecuteAll } = useHandoverSteps(environment);
 
-  if (executedTxns >= 1) {
-    loadingText = [
-      t('views.PAYMENT_METHODS.loading.processing1'),
-      t('views.PAYMENT_METHODS.loading.processing2'),
-      t('views.PAYMENT_METHODS.loading.processing3'),
-    ];
-  }
+  const { addHandover } = useHandover({
+    id: HandoverTarget.GLOBAL,
+  });
 
-  const sendTransaction = async () => {
-    execute(
+  const executeAllTransactions = useCallback(async () => {
+    executeAll(
       signResponse,
       (txn) => {
         sendTransactionSuccessEvent(txn); // not an analytics event
       },
       (error, txns) => {
-        const details = {
-          transactionId: signResponse?.transactionId,
-          errorType: error?.type,
-        };
-        sendFailedEvent(String(error?.data?.error), error, txns, undefined, details); // checkoutPrimarySalePaymentMethods_FailEventFailed
+        const details = { transactionId: signResponse?.transactionId };
+        sendFailedEvent(error.toString(), error, txns, undefined, details); // checkoutPrimarySalePaymentMethods_FailEventFailed
+        goToErrorView(error.type, error.data);
       },
+      onTxnStepExecuteAll,
     );
-  };
+  }, [signResponse, environment]);
 
-  useEffect(() => {
-    if (signResponse !== undefined && processing.current === false) {
-      processing.current = true;
-      sendTransaction();
-    }
-  }, [signResponse]);
+  const executeUserInitiatedTransaction = useCallback(() => {
+    const transaction = filteredTransactions[currentTransactionIndex];
 
-  useEffect(() => {
-    if (executeResponse?.done === true) {
-      const details = { transactionId: signResponse?.transactionId };
-      sendSuccessEvent(SaleWidgetViews.SALE_SUCCESS, executeResponse?.transactions, signTokenIds, details); // checkoutPrimarySaleSaleSuccess_SuccessEventSucceeded
-      sendCloseEvent(SaleWidgetViews.SALE_SUCCESS); // checkoutPrimarySaleSaleSuccess_CloseButtonPressed
-    }
-  }, [executeResponse]);
+    const config = initateExecuteNextTransactionHandover[transaction.methodCall];
+
+    const headingTextBefore = t(config.headingTextKey) || '';
+    const ctaButtonTextBefore = t(config.ctaButtonTextKey) || '';
+
+    const handleTransaction = () => {
+      try {
+        executeNextTransaction(
+          (txn) => {
+            sendTransactionSuccessEvent(txn);
+          },
+          (err, txns) => {
+            const details = {
+              transactionId: signResponse?.transactionId,
+            };
+            sendFailedEvent(err.toString(), err, txns, undefined, details); // checkoutPrimarySalePaymentMethods_FailEventFailed
+            goToErrorView(err.type, err.data);
+          },
+          onTxnStepExecuteNextTransaction,
+        );
+      } catch (error) {
+        goToErrorView(SaleErrorTypes.SERVICE_BREAKDOWN, { error });
+      }
+    };
+
+    addHandover({
+      animationUrl: getRemoteRive(environment, config.animationUrl),
+      inputValue: config.inputValue,
+      children: (
+        <HandoverContent
+          headingText={headingTextBefore}
+          primaryButtonText={ctaButtonTextBefore}
+          onPrimaryButtonClick={handleTransaction}
+        />
+      ),
+    });
+  }, [
+    filteredTransactions,
+    currentTransactionIndex,
+    signResponse,
+    environment,
+  ]);
 
   useEffect(() => sendPageView(SaleWidgetViews.PAY_WITH_COINS), []); // checkoutPrimarySalePayWithCoinsViewed
 
-  return <LoadingView loadingText={loadingText} />;
+  useEffect(() => {
+    if (!provider || filteredTransactions.length === 0) return;
+
+    const hadPendingTransactions = currentTransactionIndex < filteredTransactions.length
+      && prevTransactionIndexRef.current !== currentTransactionIndex;
+
+    if (isPassportProvider(provider) && hadPendingTransactions) {
+      prevTransactionIndexRef.current = currentTransactionIndex;
+      executeUserInitiatedTransaction();
+    }
+  }, [filteredTransactions, currentTransactionIndex, provider]);
+
+  useEffect(() => {
+    if (!signResponse || !provider || processing.current) return;
+
+    if (!isPassportProvider(provider)) {
+      processing.current = true;
+      executeAllTransactions();
+    }
+  }, [signResponse, provider]);
+
+  useEffect(() => {
+    if (executeResponse?.done) {
+      const details = { transactionId: signResponse?.transactionId };
+
+      addHandover({
+        duration: HandoverDuration.MEDIUM,
+        animationUrl: getRemoteRive(
+          environment,
+          getRiveAnimationName(TransactionMethod.EXECUTE),
+        ),
+        inputValue: StateMachineInput.COMPLETED,
+        children: (
+          <Heading sx={{ px: 'base.spacing.x6' }}>
+            {t('views.PAYMENT_METHODS.handover.success')}
+          </Heading>
+        ),
+        onClose: () => {
+          sendSuccessEvent(
+            SaleWidgetViews.SALE_SUCCESS,
+            executeResponse?.transactions,
+            signTokenIds,
+            details,
+          ); // checkoutPrimarySaleSaleSuccess_SuccessEventSucceeded
+          sendCloseEvent(SaleWidgetViews.SALE_SUCCESS); // checkoutPrimarySaleSaleSuccess_CloseButtonPressed
+        },
+      });
+    }
+  }, [executeResponse]);
+
+  return null;
 }
--- packages/checkout/widgets-lib/src/widgets/sale/views/SaleErrorView.tsx
+++ packages/checkout/widgets-lib/src/widgets/sale/views/SaleErrorView.tsx
@@ -1,16 +1,28 @@
+import { useState, useEffect, useContext } from 'react';
 import { BaseTokens } from '@biom3/design-tokens';
-import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import { SalePaymentTypes } from '@imtbl/checkout-sdk';
 import { StatusType } from '../../../components/Status/StatusType';
-import {
-  StatusView,
-  StatusViewProps,
-} from '../../../components/Status/StatusView';
 import { SaleErrorTypes } from '../types';
 import { useSaleContext } from '../context/SaleContextProvider';
 import { sendSaleWidgetCloseEvent } from '../SaleWidgetEvents';
 import { EventTargetContext } from '../../../context/event-target-context/EventTargetContext';
+import { useSaleEvent } from '../hooks/useSaleEvents';
+import {
+  StateMachineInput,
+  TransactionMethod,
+  getRiveAnimationName,
+  useHandoverSteps,
+} from '../hooks/useHandoverSteps';
+import {
+  ViewActions,
+  ViewContext,
+} from '../../../context/view-context/ViewContext';
+import { getRemoteRive } from '../../../lib/utils';
+import { HandoverContent } from '../../../components/Handover/HandoverContent';
+import { useHandover } from '../../../lib/hooks/useHandover';
+import { HandoverTarget } from '../../../context/handover-context/HandoverContext';
+import { isPassportProvider } from '../../../lib/provider';
+import { SaleWidgetViews } from '../../../context/view-context/SaleViewContextTypes';
 
 interface ErrorHandlerConfig {
   onActionClick?: () => void;
@@ -30,21 +42,68 @@ export function SaleErrorView({
   biomeTheme,
   transactionHash,
   blockExplorerLink,
-  errorType = SaleErrorTypes.DEFAULT,
+  errorType,
 }: SaleErrorViewProps) {
   const { t } = useTranslation();
-  const { goBackToPaymentMethods } = useSaleContext();
+  const {
+    goBackToPaymentMethods,
+    executeNextTransaction,
+    signResponse,
+    environment,
+    provider,
+  } = useSaleContext();
+  const { sendTransactionSuccessEvent, sendFailedEvent } = useSaleEvent();
+  const { viewDispatch } = useContext(ViewContext);
   const {
     eventTargetState: { eventTarget },
   } = useContext(EventTargetContext);
+  const { addHandover, closeHandover } = useHandover({
+    id: HandoverTarget.GLOBAL,
+  });
+
+  const { onTxnStepExecuteNextTransaction } = useHandoverSteps(environment);
+  const [currentErrorType, setCurrentErrorType] = useState<
+  SaleErrorTypes | undefined
+  >(errorType);
 
   const closeWidget = () => {
     sendSaleWidgetCloseEvent(eventTarget);
   };
 
+  const retryLastTransaction = () => {
+    try {
+      executeNextTransaction(
+        (txn) => {
+          sendTransactionSuccessEvent(txn);
+          viewDispatch({
+            payload: {
+              type: ViewActions.UPDATE_VIEW,
+              view: {
+                type: SaleWidgetViews.PAY_WITH_COINS,
+              },
+            },
+          });
+        },
+        (err, txns) => {
+          const details = {
+            transactionId: signResponse?.transactionId,
+          };
+          sendFailedEvent(err.toString(), err, txns, undefined, details);
+          setCurrentErrorType(SaleErrorTypes.TRANSACTION_FAILED);
+        },
+        onTxnStepExecuteNextTransaction,
+      );
+    } catch (error) {
+      setCurrentErrorType(SaleErrorTypes.SERVICE_BREAKDOWN);
+    }
+  };
+
   const errorHandlersConfig: Record<SaleErrorTypes, ErrorHandlerConfig> = {
     [SaleErrorTypes.TRANSACTION_FAILED]: {
-      onActionClick: goBackToPaymentMethods,
+      onActionClick: () => {
+        closeHandover();
+        goBackToPaymentMethods();
+      },
       onSecondaryActionClick: transactionHash
         ? () => {
           window.open(blockExplorerLink);
@@ -77,12 +136,22 @@ export function SaleErrorView({
       },
     },
     [SaleErrorTypes.TRANSAK_FAILED]: {
-      onActionClick: goBackToPaymentMethods,
+      onActionClick: () => {
+        closeHandover();
+        goBackToPaymentMethods();
+      },
       onSecondaryActionClick: closeWidget,
       statusType: StatusType.INFORMATION,
     },
     [SaleErrorTypes.WALLET_FAILED]: {
-      onActionClick: goBackToPaymentMethods,
+      onActionClick: () => {
+        if (isPassportProvider(provider)) {
+          retryLastTransaction();
+        } else {
+          closeHandover();
+          goBackToPaymentMethods();
+        }
+      },
       onSecondaryActionClick: closeWidget,
       statusType: StatusType.INFORMATION,
       statusIconStyles: {
@@ -90,26 +159,42 @@ export function SaleErrorView({
       },
     },
     [SaleErrorTypes.WALLET_REJECTED_NO_FUNDS]: {
-      onActionClick: goBackToPaymentMethods,
+      onActionClick: () => {
+        closeHandover();
+        goBackToPaymentMethods();
+      },
       onSecondaryActionClick: closeWidget,
       statusType: StatusType.INFORMATION,
     },
     [SaleErrorTypes.WALLET_REJECTED]: {
       onActionClick: () => {
-        goBackToPaymentMethods(SalePaymentTypes.CRYPTO);
+        if (isPassportProvider(provider)) {
+          retryLastTransaction();
+        } else {
+          closeHandover();
+          goBackToPaymentMethods();
+        }
       },
       onSecondaryActionClick: closeWidget,
       statusType: StatusType.INFORMATION,
     },
     [SaleErrorTypes.WALLET_POPUP_BLOCKED]: {
       onActionClick: () => {
-        goBackToPaymentMethods(SalePaymentTypes.CRYPTO);
+        if (isPassportProvider(provider)) {
+          retryLastTransaction();
+        } else {
+          closeHandover();
+          goBackToPaymentMethods();
+        }
       },
       onSecondaryActionClick: closeWidget,
       statusType: StatusType.INFORMATION,
     },
     [SaleErrorTypes.FUNDING_ROUTE_EXECUTE_ERROR]: {
-      onActionClick: goBackToPaymentMethods,
+      onActionClick: () => {
+        closeHandover();
+        goBackToPaymentMethods();
+      },
       onSecondaryActionClick: closeWidget,
       statusType: StatusType.INFORMATION,
     },
@@ -122,33 +207,47 @@ export function SaleErrorView({
       },
     },
     [SaleErrorTypes.DEFAULT]: {
-      onActionClick: goBackToPaymentMethods,
+      onActionClick: () => {
+        closeHandover();
+        goBackToPaymentMethods();
+      },
       onSecondaryActionClick: closeWidget,
       statusType: StatusType.INFORMATION,
     },
   };
 
-  const getErrorViewProps = (): StatusViewProps => {
-    const handlers = errorHandlersConfig[errorType] || {};
-    const secondaryActionText = errorType === SaleErrorTypes.TRANSACTION_FAILED && transactionHash
-      ? t(`views.SALE_FAIL.errors.${errorType}.secondaryAction`)
+  const getErrorViewProps = () => {
+    const handlers = errorHandlersConfig[currentErrorType || SaleErrorTypes.DEFAULT] || {};
+    const secondaryButtonText = currentErrorType === SaleErrorTypes.TRANSACTION_FAILED && transactionHash
+      ? t(`views.SALE_FAIL.errors.${currentErrorType}.secondaryAction`)
       : t(`views.SALE_FAIL.errors.${SaleErrorTypes.DEFAULT}.secondaryAction`);
 
     return {
-      testId: 'fail-view',
-      statusText: t(`views.SALE_FAIL.errors.${errorType}.description`),
-      actionText: t(`views.SALE_FAIL.errors.${errorType}.primaryAction`),
-      onActionClick: handlers?.onActionClick,
-      secondaryActionText,
-      onSecondaryActionClick: handlers?.onSecondaryActionClick,
-      onCloseClick: closeWidget,
-      statusType: handlers.statusType,
-      statusIconStyles: {
-        transform: 'rotate(180deg)',
-        fill: biomeTheme.color.status.guidance.dim,
-        ...handlers.statusIconStyles,
-      },
+      headingText: t('views.PAYMENT_METHODS.handover.error.heading'),
+      subheadingText: t(
+        `views.SALE_FAIL.errors.${currentErrorType}.description`,
+      ),
+      primaryButtonText: t(
+        `views.SALE_FAIL.errors.${currentErrorType}.primaryAction`,
+      ),
+      onPrimaryButtonClick: handlers?.onActionClick,
+      secondaryButtonText,
+      onSecondaryButtonClick: handlers?.onSecondaryActionClick,
     };
   };
-  return <StatusView {...getErrorViewProps()} />;
+
+  useEffect(() => {
+    if (!environment || !currentErrorType) return;
+
+    addHandover({
+      animationUrl: getRemoteRive(
+        environment,
+        getRiveAnimationName(TransactionMethod.APPROVE),
+      ),
+      inputValue: StateMachineInput.ERROR,
+      children: <HandoverContent {...getErrorViewProps()} />,
+    });
+  }, [currentErrorType, environment]);
+
+  return null;
 }
