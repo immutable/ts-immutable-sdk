807a118f6bbbd2042396b115a2e80cdeb2a33f08
Mikhala <mikhala.kurtjak@immutable.com>
2024-07-01 11:45:37 +0800

    test scenario without using buffer

--- packages/passport/sdk/src/utils/string.test.ts
+++ packages/passport/sdk/src/utils/string.test.ts
@@ -2,36 +2,36 @@ import { hexToString } from './string';
 
 describe('string', () => {
   describe('hexToString', () => {
-    it('should return hex if it is not a valid hex', async () => {
+    it('should return hex if it is not a valid hex', () => {
       const hex = '0x123';
       const hex2 = 'test';
 
-      expect(await hexToString(hex)).toEqual(hex);
-      expect(await hexToString(hex2)).toEqual(hex2);
+      expect(hexToString(hex)).toEqual(hex);
+      expect(hexToString(hex2)).toEqual(hex2);
     });
 
-    it('should return utf8 string if it is a valid utf8', async () => {
+    it('should return utf8 string if it is a valid utf8', () => {
       const hex = '0x68656c6c6f20776f726c64';
 
-      expect(await hexToString(hex)).toEqual('hello world');
+      expect(hexToString(hex)).toEqual('hello world');
     });
 
-    it('should return utf8 string if it is a valid utf8 with leading zeros', async () => {
+    it('should return utf8 string if it is a valid utf8 with leading zeros', () => {
       const hex = '0x0068656c6c6f20776f726c64'; // 'hello world' with leading zero
 
-      expect(await hexToString(hex)).toEqual('hello world');
+      expect(hexToString(hex)).toEqual('hello world');
     });
 
-    it('should return empty string if input is an empty string', async () => {
+    it('should return empty string if input is an empty string', () => {
       const hex = '';
 
-      expect(await hexToString(hex)).toEqual('');
+      expect(hexToString(hex)).toEqual('');
     });
 
-    it('should return hex if it is a valid hex but not a valid utf8', async () => {
+    it('should return hex if it is a valid hex but not a valid utf8', () => {
       const hex = '0x1234567890abcdef'; // valid hex but not a valid utf8
 
-      expect(await hexToString(hex)).toEqual(hex);
+      expect(hexToString(hex)).toEqual(hex);
     });
   });
 });
--- packages/passport/sdk/src/utils/string.ts
+++ packages/passport/sdk/src/utils/string.ts
@@ -1,18 +1,11 @@
 import { utils } from 'ethers';
 
-export const hexToString = async (hex: string) => {
+export const hexToString = (hex: string) => {
   if (!hex) return hex;
 
   try {
-    // Ensures Buffer is available when using the browser bundle of the SDK
-    if (typeof window !== 'undefined' && !window.Buffer) {
-      const bufferModule = await import('buffer');
-      window.Buffer = bufferModule.Buffer;
-    }
-
     const stripped = utils.stripZeros(utils.arrayify(hex));
-    const buff = Buffer.from(stripped);
-    return buff.length === 32 ? hex : utils.toUtf8String(stripped);
+    return utils.toUtf8String(stripped);
   } catch (e) {
     return hex;
   }
