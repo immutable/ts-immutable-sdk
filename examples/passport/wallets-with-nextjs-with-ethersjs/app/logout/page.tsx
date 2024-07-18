0a4631d0c66618623ab661c576890c68ca192e46
Craig M. <craig.b.macgregor@gmail.com>
2024-07-18 18:19:31 +1200

    WIP

---
+++ examples/passport/wallets-with-nextjs-with-ethersjs/app/logout/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Web3Provider } from '@ethersproject/providers';
+import { config, passport } from '@imtbl/sdk';
+import { useEffect } from 'react';
+
+
+export default function Logout() {
+
+  return (<h1>Redirect</h1>);
+
+}
