80138f3e6ca11b5f37e804f8e4489e9fa428749e
Craig M. <craig.b.macgregor@gmail.com>
2024-07-18 18:19:31 +1200

    WIP

---
+++ examples/passport/wallets-with-nextjs-with-ethersjs/app/page.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Web3Provider } from '@ethersproject/providers';
+import { config, passport } from '@imtbl/sdk';
+import { useEffect } from 'react';
+
+
+export default function Home() {
+
+  const passportInstance = new passport.Passport({
+    baseConfig: {
+      environment: config.Environment.SANDBOX,
+      publishableKey: 'XXX', // replace with your publishable API key from Hub
+    },
+    clientId: 'XXX', // replace with your client ID from Hub
+    redirectUri: 'http://localhost:3000/redirect', // replace with one of your redirect URIs from Hub
+    logoutRedirectUri: 'http://localhost:3000/logout', // replace with one of your logout URIs from Hub
+    audience: 'platform_api',
+    scope: 'openid offline_access email transact',
+    popupOverlayOptions: {
+      disableGenericPopupOverlay: false, // Set to true to disable the generic pop-up overlay
+      disableBlockedPopupOverlay: false, // Set to true to disable the blocked pop-up overlay
+    }
+  });
+
+  passportInstance.loginCallback();
+
+  const passportProvider = passportInstance.connectEvm();
+
+  console.log('passportInstance', passportInstance)
+
+  const web3Provider = new Web3Provider(passportProvider);
+
+  (async () => {
+    if (!web3Provider.provider.request) return;
+      const accounts = await web3Provider.provider.request({ method: "eth_requestAccounts" });
+      const signer = web3Provider.getSigner();
+
+      console.log('accounts', accounts);
+      console.log('signer', signer);
+  })()
+
+
+  return (<></>);
+}
---
+++ examples/passport/wallets-with-nextjs-with-ethersjs/app/redirect/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Web3Provider } from '@ethersproject/providers';
+import { config, passport } from '@imtbl/sdk';
+import { useEffect } from 'react';
+
+
+export default function Redirect() {
+
+  return (<h1>Redirect</h1>);
+}
