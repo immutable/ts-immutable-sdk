c9573497f616b2d580add62738b200187403083a
Craig M. <craig.b.macgregor@gmail.com>
2024-07-19 17:35:07 +1200

    login & logout working

--- examples/passport/wallets-with-nextjs-with-ethersjs/app/redirect/page.tsx
+++ examples/passport/wallets-with-nextjs-with-ethersjs/app/redirect/page.tsx
@@ -3,9 +3,13 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { config, passport } from '@imtbl/sdk';
 import { useEffect } from 'react';
+import { passportInstance } from '../page';
+
 
 
 export default function Redirect() {
+
+  passportInstance.loginCallback();
 
   return (<h1>Redirect</h1>);
 }
