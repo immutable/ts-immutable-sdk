8d68e03fa322219e35672edbeb60078d5de7fcc9
Craig M. <craig.b.macgregor@gmail.com>
2024-07-22 17:51:07 +1200

    WIP

--- examples/passport/wallets-with-nextjs/app/connect-with-wagmi/account.tsx
+++ examples/passport/wallets-with-nextjs/app/connect-with-wagmi/account.tsx
@@ -1,4 +1,6 @@
 import { useAccount, useDisconnect, useEnsAvatar, useEnsName } from 'wagmi'
+import { passportInstance } from '../page';
+import { useState } from 'react';
 
 export function Account() {
   const { address } = useAccount()
@@ -6,11 +8,26 @@ export function Account() {
   const { data: ensName } = useEnsName({ address })
   const { data: ensAvatar } = useEnsAvatar({ name: ensName! })
 
+  // setup the loading state to enable/disable buttons when loading
+  const [loading, setLoadingState] = useState<boolean>(false);
+
+  const passportLogout = async () => {
+    // disable button while loading
+    setLoadingState(true)
+    // disconnect Passport from Wagmi
+    disconnect()
+    // logout from Passport
+    await passportInstance.logout()
+  }
+
   return (
-    <div>
+    <div>   
+      <button onClick={() => passportLogout()} disabled={loading}>Passport Logout</button>
       {ensAvatar && <img alt="ENS Avatar" src={ensAvatar} />}
-      {address && <div>{ensName ? `${ensName} (${address})` : address}</div>}
-      <button onClick={() => disconnect()}>Disconnect</button>
+      {loading 
+        ? <p>Loading...</p> 
+        : <p>Connected Account: {address && <span>{ensName ? `${ensName} (${address})` : address}</span>}</p>
+      }
     </div>
   )
 }
--- examples/passport/wallets-with-nextjs/app/connect-with-wagmi/wallet-options.tsx
+++ examples/passport/wallets-with-nextjs/app/connect-with-wagmi/wallet-options.tsx
@@ -3,17 +3,26 @@ import { Connector, useConnect } from 'wagmi'
 
 export function WalletOptions() {
   const { connectors, connect } = useConnect()
-
+  
+  // setup the loading state to enable/disable buttons when loading
+  const [loading, setLoadingState] = useState<boolean>(true);
+  
   const [filteredConnectors, setFilteredConnectors] = useState<Connector[]>([])
 
+  //filter 
   useEffect(() => {
     if (!connectors) return
-    setFilteredConnectors(connectors.filter((connector) => !connector.name.includes('Injected')))
-  }, [connectors])
+    setFilteredConnectors(connectors.filter((connector) => connector.name.includes('Immutable Passport')))
+    setLoadingState(false)
+}, [connectors])
 
-  return filteredConnectors.map((connector) => (
+  return (<>
+  {!loading && filteredConnectors.map((connector) => (
     <button key={connector.uid} onClick={() => connect({ connector })}>
       {connector.name}
     </button>
-  ))
+  ))}
+  {loading && <p>Loading...</p>}
+  </>)
+  
 }
