2b105271eab5d779bfca09151fe38f31383f1e82
Craig M. <craig.b.macgregor@gmail.com>
2024-07-22 12:06:31 +1200

    update

--- examples/passport/wallets-with-nextjs/app/redirect/page.tsx
+++ examples/passport/wallets-with-nextjs/app/redirect/page.tsx
@@ -3,10 +3,13 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { config, passport } from '@imtbl/sdk';
 import { useEffect } from 'react';
+import { passportInstance } from '../page';
 
 
-export default function Logout() {
 
-  return (<h1>Logout</h1>);
+export default function Redirect() {
 
+  passportInstance.loginCallback();
+
+  return (<h1>Logged in</h1>);
 }
