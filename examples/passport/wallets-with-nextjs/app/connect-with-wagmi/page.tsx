476ee95aba8fcab5d077cd145137c845093fe33b
Craig M. <craig.b.macgregor@gmail.com>
2024-07-22 16:49:33 +1200

    Ethers.js and EIP1193 working, Wagmi WIP

--- examples/passport/wallets-with-nextjs/app/connect-with-wagmi/page.tsx
+++ examples/passport/wallets-with-nextjs/app/connect-with-wagmi/page.tsx
@@ -1,11 +1,34 @@
 'use client';
  
 import { useState } from 'react';
-import { ethers } from 'ethers';
+import { http, createConfig, WagmiProvider, useConnect, useAccount } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { mainnet, immutableZkEvm } from 'wagmi/chains';
+import { injected } from 'wagmi/connectors';
+
 import { ProviderEvent } from '@imtbl/sdk/passport';
 import { passportInstance } from '../page';
+import { WalletOptions } from './wallet-options';
+import { Account } from './account';
 
-export default function ConnectWithEtherJS() {
+const queryClient = new QueryClient()
+
+export const config = createConfig({
+  chains: [mainnet, immutableZkEvm],
+  connectors: [injected()],
+  transports: {
+    [mainnet.id]: http(),
+    [immutableZkEvm.id]: http(),
+  },
+});
+
+function ConnectWallet() {
+  const { isConnected } = useAccount()
+  if (isConnected) return <Account />
+  return <WalletOptions />
+}
+
+export default function ConnectWithWagmi() {
   
   // setup the accounts state
   const [accountsState, setAccountsState] = useState<any>([]);
@@ -16,42 +39,29 @@ export default function ConnectWithEtherJS() {
   // fetch the Passport provider from the Passport instance
   const passportProvider = passportInstance.connectEvm()
 
-  // create the Web3Provider using the Passport provider
-  const web3Provider = new ethers.providers.Web3Provider(passportProvider);
-
-  const passportLogin = async () => {
-    if (web3Provider.provider.request) {
-      // disable button while loading
-      setLoadingState(true)
-      // calling eth_requestAccounts triggers the Passport login flow
-      const accounts = await web3Provider.provider.request({ method: "eth_requestAccounts" });
-      // once logged in Passport is connected to the wallet and ready to transact
-      setAccountsState(accounts)
-      // enable button when loading has finished
-      setLoadingState(false)
-    }
-  }
-
   // listen to the ACCOUNTS_CHANGED event and update the accounts state when it changes
   passportProvider.on(ProviderEvent.ACCOUNTS_CHANGED, (accounts: string[]) => {
+    console.log('ACCOUNTS_CHANGED', accounts)
     setAccountsState(accounts);
   });
 
+  // reset the accounts state when logout is called
   const passportLogout = async () => {
-     // disable button while loading
-     setLoadingState(true)
-     // reset the account state
-     setAccountsState([])
-     // logout from passport
-     await passportInstance.logout()
+    setAccountsState([])
+    await passportInstance.logout()
   }
 
   // render the view to login and show the connected accounts
   return (<>
-    <h1>Passport Wallet - Connect with EtherJS</h1>
-    {accountsState.length == 0 && 
-      <button onClick={passportLogin} disabled={loading}>Passport Login</button>
-    }
+    <h1>Passport Wallet - Connect with Wagmi</h1>
+    <div>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <ConnectWallet />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </div>
+
     {accountsState.length >= 1 && 
       <button onClick={passportLogout} disabled={loading}>Passport Logout</button>
     }
