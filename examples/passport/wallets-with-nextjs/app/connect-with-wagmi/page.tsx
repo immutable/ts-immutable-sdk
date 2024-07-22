cfebc3a6435380bf70f6695c1ae74d8c76820a60
Craig M. <craig.b.macgregor@gmail.com>
2024-07-22 17:51:07 +1200

    WIP

--- examples/passport/wallets-with-nextjs/app/connect-with-wagmi/page.tsx
+++ examples/passport/wallets-with-nextjs/app/connect-with-wagmi/page.tsx
@@ -1,23 +1,21 @@
 'use client';
  
-import { useState } from 'react';
-import { http, createConfig, WagmiProvider, useConnect, useAccount } from 'wagmi';
+import { http, createConfig, WagmiProvider, useAccount } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { mainnet, immutableZkEvm } from 'wagmi/chains';
-import { injected } from 'wagmi/connectors';
-
+import { immutableZkEvm } from 'wagmi/chains';
+// immutableZkEvmTestnet for testnet
 import { ProviderEvent } from '@imtbl/sdk/passport';
 import { passportInstance } from '../page';
 import { WalletOptions } from './wallet-options';
 import { Account } from './account';
+import { injected } from 'wagmi/connectors';
 
 const queryClient = new QueryClient()
 
 export const config = createConfig({
-  chains: [mainnet, immutableZkEvm],
+  chains: [immutableZkEvm], // connects to Immutable zkEVM mainnet
   connectors: [injected()],
   transports: {
-    [mainnet.id]: http(),
     [immutableZkEvm.id]: http(),
   },
 });
@@ -29,12 +27,6 @@ function ConnectWallet() {
 }
 
 export default function ConnectWithWagmi() {
-  
-  // setup the accounts state
-  const [accountsState, setAccountsState] = useState<any>([]);
-
-  // setup the loading state to enable/disable buttons when loading
-  const [loading, setLoadingState] = useState<boolean>(false);
 
   // fetch the Passport provider from the Passport instance
   const passportProvider = passportInstance.connectEvm()
@@ -42,33 +34,16 @@ export default function ConnectWithWagmi() {
   // listen to the ACCOUNTS_CHANGED event and update the accounts state when it changes
   passportProvider.on(ProviderEvent.ACCOUNTS_CHANGED, (accounts: string[]) => {
     console.log('ACCOUNTS_CHANGED', accounts)
-    setAccountsState(accounts);
   });
-
-  // reset the accounts state when logout is called
-  const passportLogout = async () => {
-    setAccountsState([])
-    await passportInstance.logout()
-  }
 
   // render the view to login and show the connected accounts
   return (<>
     <h1>Passport Wallet - Connect with Wagmi</h1>
-    <div>
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <ConnectWallet />
-        </QueryClientProvider>
-      </WagmiProvider>
-    </div>
-
-    {accountsState.length >= 1 && 
-      <button onClick={passportLogout} disabled={loading}>Passport Logout</button>
-    }
-    {loading 
-      ? <p>Loading...</p> 
-      : <p>Connected Account: {accountsState.length >= 1 ? accountsState : '(not connected)'}</p>
-    }
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectWallet />
+      </QueryClientProvider>
+    </WagmiProvider>
     <p>
       <a href="/">Return to Examples</a>
     </p>
