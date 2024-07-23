0f035b3542006e838eb7876137ebfb90f832bab2
zaid.arain <zaid.arain@immutable.com>
2024-07-23 15:40:16 +1000

    update tests

--- examples/passport/wallets-connect-with-nextjs/tests/base.spec.ts
+++ examples/passport/wallets-connect-with-nextjs/tests/base.spec.ts
@@ -11,7 +11,7 @@ test.describe("home page", () => {
     await expect(page.getByRole("heading", { name: "Passport Wallet Examples" })).toBeVisible();
 
     await expect(page.getByRole("link", { name: "Connect Wallet with EtherJS" })).toBeVisible();
-    await expect(page.getByRole("link", { name: "Connect Wallet ith EIP1193" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Connect Wallet with EIP1193" })).toBeVisible();
     await expect(page.getByRole("link", { name: "Connect Wallet with Wagmi" })).toBeVisible();
   });
 });
@@ -23,6 +23,30 @@ test.describe("connect wallet with etherjs", () => {
     await expect(page.getByRole("heading", { name: "Passport Wallet - Connect with EtherJS" })).toBeVisible();
 
     await expect(page.getByRole("button", { name: "Passport Login" })).toBeVisible();
+    await expect(page.getByText("Connected Account:")).toBeVisible();
+    await expect(page.getByRole("link", { name: "Return to Examples" })).toBeVisible();
+  });
+});
+
+test.describe("connect wallet with eip1193", () => {
+  test("has heading, login button and initial account status set correctly", async ({ page }) => {
+    await page.click("text=Connect Wallet with EIP1193");
+
+    await expect(page.getByRole("heading", { name: "Passport Wallet - Connect with EIP1193" })).toBeVisible();
+
+    await expect(page.getByRole("button", { name: "Passport Login" })).toBeVisible();
+    await expect(page.getByText("Connected Account:")).toBeVisible();
+    await expect(page.getByRole("link", { name: "Return to Examples" })).toBeVisible();
+  });
+});
+
+test.describe("connect wallet with wagmi", () => {
+  test("has heading and login button set correctly", async ({ page }) => {
+    await page.click("text=Connect Wallet with Wagmi");
+
+    await expect(page.getByRole("heading", { name: "Passport Wallet - Connect with Wagmi" })).toBeVisible();
+
+    await expect(page.getByRole("button", { name: "Immutable Passport" })).toBeVisible();
     await expect(page.getByRole("link", { name: "Return to Examples" })).toBeVisible();
   });
 });
