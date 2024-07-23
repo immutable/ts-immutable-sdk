fc4e335e4b7d6abbef3b301f2fd071328f59ae05
Ji Young Lee <ji.younglee@immutable.com>
2024-07-23 16:25:32 +1000

    Update sample app checkout to mount connect and wallet

--- packages/checkout/widgets-sample-app/src/components/ui/checkout/checkout.tsx
+++ packages/checkout/widgets-sample-app/src/components/ui/checkout/checkout.tsx
@@ -1,27 +1,64 @@
-import { Checkout, WidgetLanguage, WidgetTheme, WidgetType } from '@imtbl/checkout-sdk';
-import { WidgetsFactory } from '@imtbl/checkout-widgets';
-import { useEffect, useMemo } from 'react';
-import { Environment } from '@imtbl/config';
+import {
+  Checkout,
+  CheckoutFlowType,
+  WidgetLanguage,
+  WidgetTheme,
+  WidgetType,
+  WalletProviderName
+} from "@imtbl/checkout-sdk";
+import { WidgetsFactory } from "@imtbl/checkout-widgets";
+import { useMemo } from "react";
+import { Environment } from "@imtbl/config";
 
 function CheckoutUI() {
-  const checkout = useMemo(() => new Checkout({ baseConfig: { environment: Environment.SANDBOX } }), []);
-  const factory = useMemo(() => new WidgetsFactory(checkout, { theme: WidgetTheme.DARK }), [checkout]);
-  const checkoutWidget = useMemo(() => factory.create(WidgetType.CHECKOUT), [checkout])
+  const checkout = useMemo(
+    () => new Checkout({ baseConfig: { environment: Environment.SANDBOX } }),
+    []
+  );
+  const factory = useMemo(
+    () => new WidgetsFactory(checkout, { theme: WidgetTheme.DARK }),
+    [checkout]
+  );
+  const checkoutWidget = useMemo(
+    () =>
+      factory.create(WidgetType.CHECKOUT, {
+        config: { wallet: { showNetworkMenu: false } },
+      }),
+    [checkout]
+  );
 
-  const unmount = () => { checkoutWidget.unmount() }
-  const mount = () => { checkoutWidget.mount('checkout') }
-  const update = (theme: WidgetTheme) => { checkoutWidget.update({ config: { theme } }) }
-
-  useEffect(() => {
-    mount();
-  }, []);
+  const unmount = () => {
+    checkoutWidget.unmount();
+  };
+  const update = (theme: WidgetTheme) => {
+    checkoutWidget.update({ config: { theme } });
+  };
 
   return (
     <div>
       <h1 className="sample-heading">Checkout Widget</h1>
       <div id="checkout"></div>
       <button onClick={unmount}>Unmount</button>
-      <button onClick={mount}>Mount</button>
+      <button
+        onClick={() => {
+          checkoutWidget.mount("checkout", {
+            flow: CheckoutFlowType.CONNECT,
+            blocklistWalletRdns: ["com.metamask.io"],
+          });
+        }}
+      >
+        Mount Connect
+      </button>
+      <button
+        onClick={() => {
+          checkoutWidget.mount("checkout", {
+            flow: CheckoutFlowType.WALLET,
+            walletProviderName: WalletProviderName.METAMASK,
+          });
+        }}
+      >
+        Mount Wallet
+      </button>
       <button onClick={() => update(WidgetTheme.LIGHT)}>Light theme</button>
       <button onClick={() => update(WidgetTheme.DARK)}>Dark theme</button>
       <select
