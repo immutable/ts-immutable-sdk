221865998f6161d25e25e7320bc7f2fa2eab47ff
Ji Young Lee <ji.younglee@immutable.com>
2024-07-23 16:22:24 +1000

    Add connect and wallet configuration and params for checkout widget
    Co-authored-by: Jhonatan Gonzalez <jhesgodi@users.noreply.github.com>

--- packages/checkout/sdk/src/widgets/definitions/configurations/checkout.ts
+++ packages/checkout/sdk/src/widgets/definitions/configurations/checkout.ts
@@ -1,6 +1,9 @@
 /* eslint-disable max-len */
+import { ConnectWidgetConfiguration } from './connect';
+import { WalletWidgetConfiguration } from './wallet';
 import { WidgetConfiguration } from './widget';
 
 export type CheckoutWidgetConfiguration = {
-
+  connect?: Omit<ConnectWidgetConfiguration, 'WidgetConfiguration'>;
+  wallet?: Omit<WalletWidgetConfiguration, 'WidgetConfiguration'>;
 } & WidgetConfiguration;
--- packages/checkout/sdk/src/widgets/definitions/parameters/checkout.ts
+++ packages/checkout/sdk/src/widgets/definitions/parameters/checkout.ts
@@ -1,22 +1,20 @@
 /* eslint-disable max-len */
 import { WidgetLanguage } from '../configurations';
+import { ConnectWidgetParams } from './connect';
+import { WalletWidgetParams } from './wallet';
 
 export enum CheckoutFlowType {
   CONNECT = 'connect',
   WALLET = 'wallet',
 }
 
-export type CheckoutWidgetConnectFlow = {
-  flow: CheckoutFlowType.CONNECT
-  walletRdns: string,
-};
+export type CheckoutWidgetConnectFlow = { flow: CheckoutFlowType.CONNECT } & ConnectWidgetParams;
 
-export type CheckoutWidgetWalletFlow = {
-  flow: CheckoutFlowType.WALLET
-  showDisconnectButton: boolean,
-};
+export type CheckoutWidgetWalletFlow = { flow: CheckoutFlowType.WALLET } & WalletWidgetParams;
+
+export type CheckoutWidgetFlow = CheckoutWidgetConnectFlow | CheckoutWidgetWalletFlow;
 
 export type CheckoutWidgetParams = {
   /** The language to use for the checkout widget */
   language?: WidgetLanguage;
-} & (CheckoutWidgetConnectFlow | CheckoutWidgetWalletFlow);
+} & CheckoutWidgetFlow;
