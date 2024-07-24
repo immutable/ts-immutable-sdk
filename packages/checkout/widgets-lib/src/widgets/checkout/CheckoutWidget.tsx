c2b6ca0f77ae1c0ee2d3f1630d9aa945660f03d3
Jhonatan Gonzalez <jhesgodi@gmail.com>
2024-07-24 13:29:43 +1000

    feat: [NO-CHANGELOG][Checkout] Load checkout app from iframe with params (#1997)
    Co-authored-by: Ji Young Lee <jiyounglee@users.noreply.github.com>
    Co-authored-by: Ji Young Lee <ji.younglee@immutable.com>
    Co-authored-by: Jhonatan Gonzalez <jhesgodi@users.noreply.github.com>

--- packages/checkout/widgets-lib/src/widgets/checkout/CheckoutWidget.tsx
+++ packages/checkout/widgets-lib/src/widgets/checkout/CheckoutWidget.tsx
@@ -1,26 +1,48 @@
-import { CheckoutWidgetParams } from '@imtbl/checkout-sdk';
-import {
-  useContext,
-} from 'react';
-import { ConnectLoaderContext } from '../../context/connect-loader-context/ConnectLoaderContext';
-import { StrongCheckoutWidgetsConfig } from '../../lib/withDefaultWidgetConfig';
+import { useEffect, useState } from 'react';
+import { Box } from '@biom3/react';
+import { CheckoutWidgetParams, Checkout } from '@imtbl/checkout-sdk';
 
-export type CheckoutWidgetInputs = CheckoutWidgetParams & {
-  config: StrongCheckoutWidgetsConfig,
+import { StrongCheckoutWidgetsConfig } from '../../lib/withDefaultWidgetConfig';
+import { getIframeURL } from './functions/iframeParams';
+
+export type CheckoutWidgetInputs = {
+  checkout: Checkout;
+  config: StrongCheckoutWidgetsConfig;
+  params: CheckoutWidgetParams;
 };
 
 export default function CheckoutWidget(props: CheckoutWidgetInputs) {
-  const {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    config,
-  } = props;
+  const { config, checkout, params } = props;
+  const { environment } = config;
+  const { publishableKey } = checkout.config;
 
-  const {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    connectLoaderState: { checkout, provider },
-  } = useContext(ConnectLoaderContext);
+  const [iframeURL, setIframeURL] = useState<string>();
+
+  useEffect(() => {
+    if (!publishableKey || !params.language) return;
+
+    const url = getIframeURL(params, environment, publishableKey);
+
+    setIframeURL(url);
+  }, [publishableKey, params]);
+
+  // TODO:
+  // on iframe load error, go to error view 500
+  // on iframe loading, show loading view, requires iframe to trigger an initialised event
+
+  if (!iframeURL) {
+    return null;
+  }
 
   return (
-    <div />
+    <Box
+      rc={<iframe src={iframeURL} title="checkout" />}
+      sx={{
+        w: '100%',
+        h: '100%',
+        border: 'none',
+        boxShadow: 'none',
+      }}
+    />
   );
 }
