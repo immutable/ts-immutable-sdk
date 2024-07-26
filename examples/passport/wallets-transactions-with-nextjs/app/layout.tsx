7afd3ce30d276fad784df7952275489e76f51b85
Andrew Yang <ddvkid@gmail.com>
2024-07-26 15:42:52 +1000

    send transaction examples

---
+++ examples/passport/wallets-transactions-with-nextjs/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ReactNode } from 'react';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Passport Identity example',
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
