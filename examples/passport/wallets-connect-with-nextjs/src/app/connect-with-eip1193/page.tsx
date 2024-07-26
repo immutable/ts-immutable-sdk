64659cd2a04859ca80d60c9e5c1943ed0ef22959
Andrew Yang <ddvkid@gmail.com>
2024-07-26 14:14:27 +1000

    lint

--- examples/passport/wallets-connect-with-nextjs/src/app/connect-with-eip1193/page.tsx
+++ examples/passport/wallets-connect-with-nextjs/src/app/connect-with-eip1193/page.tsx
@@ -1,34 +1,33 @@
 'use client';
- 
+
 import { useState } from 'react';
 import { ProviderEvent } from '@imtbl/sdk/passport';
 import { passportInstance } from '../utils';
 
 export default function ConnectWithEtherJS() {
-  
   // setup the accounts state
   const [accountsState, setAccountsState] = useState<any>([]);
-  
+
   // setup the loading state to enable/disable buttons when loading
   const [loading, setLoadingState] = useState<boolean>(false);
 
   // #doc passport-wallets-nextjs-connect-eip1193-create
   // fetch the Passport provider from the Passport instance
-  const passportProvider = passportInstance.connectEvm()
+  const passportProvider = passportInstance.connectEvm();
   // #enddoc passport-wallets-nextjs-connect-eip1193-create
-  
+
   const passportLogin = async () => {
-      // disable button while loading
-      setLoadingState(true)
-      // #doc passport-wallets-nextjs-connect-eip1193-request
-      // calling eth_requestAccounts triggers the Passport login flow
-      const accounts = await passportProvider.request({ method: "eth_requestAccounts" });
-      // #enddoc passport-wallets-nextjs-connect-eip1193-request
-      // once logged in Passport is connected to the wallet and ready to transact
-      setAccountsState(accounts)
-      // enable button once loading finished
-      setLoadingState(false)
-  }
+    // disable button while loading
+    setLoadingState(true);
+    // #doc passport-wallets-nextjs-connect-eip1193-request
+    // calling eth_requestAccounts triggers the Passport login flow
+    const accounts = await passportProvider.request({ method: 'eth_requestAccounts' });
+    // #enddoc passport-wallets-nextjs-connect-eip1193-request
+    // once logged in Passport is connected to the wallet and ready to transact
+    setAccountsState(accounts);
+    // enable button once loading finished
+    setLoadingState(false);
+  };
 
   // #doc passport-wallets-nextjs-connect-eip1193-event
   // listen to the ACCOUNTS_CHANGED event and update the accounts state when it changes
@@ -40,28 +39,32 @@ export default function ConnectWithEtherJS() {
   // reset the accounts state when logout is called
   const passportLogout = async () => {
     // disable button while loading
-    setLoadingState(true)
+    setLoadingState(true);
     // reset the account state
-    setAccountsState([])
+    setAccountsState([]);
     // logout from passport
-    await passportInstance.logout()
-  }
+    await passportInstance.logout();
+  };
 
   // render the view to login/logout and show the connected accounts
-  return (<>
-    <h1>Passport Wallet - Connect with EtherJS</h1>
-    {accountsState.length == 0 && 
-      <button onClick={passportLogin} disabled={loading}>Passport Login</button>
-    }
-    {accountsState.length >= 1 && 
-      <button onClick={passportLogout} disabled={loading}>Passport Logout</button>
-    }
-    {loading 
-      ? <p>Loading...</p> 
-      : <p>Connected Account: {accountsState.length >= 1 ? accountsState : '(not connected)'}</p>
-    }
-    <p>
-      <a href="/">Return to Examples</a>
-    </p>
-  </>);
+  return (
+    <>
+      <h1>Passport Wallet - Connect with EtherJS</h1>
+      {accountsState.length == 0
+      && <button onClick={passportLogin} disabled={loading}>Passport Login</button>}
+      {accountsState.length >= 1
+      && <button onClick={passportLogout} disabled={loading}>Passport Logout</button>}
+      {loading
+        ? <p>Loading...</p>
+        : (
+          <p>
+            Connected Account:
+            {accountsState.length >= 1 ? accountsState : '(not connected)'}
+          </p>
+        )}
+      <p>
+        <a href="/">Return to Examples</a>
+      </p>
+    </>
+  );
 }
--- examples/passport/wallets-connect-with-nextjs/src/app/layout.tsx
+++ examples/passport/wallets-connect-with-nextjs/src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Immutable Passport Example",
-  description: "Wallets",
+  title: 'Immutable Passport Example',
+  description: 'Wallets',
 };
 
 export default function RootLayout({
