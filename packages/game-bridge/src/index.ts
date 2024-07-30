da7b23541c8b9bdde3b1e30b904186ebcfd95ce7
Nik Ho <studying.mole@gmail.com>
2024-07-30 14:00:49 +1200

    send addition data for track duration in transfer and transaction methods

--- packages/game-bridge/src/index.ts
+++ packages/game-bridge/src/index.ts
@@ -110,7 +110,6 @@ type CallbackData = CommonCallbackData | InitDeviceFlowCallbackData | RequestAcc
 const callbackToGame = (data: CallbackData) => {
   const message = JSON.stringify(data);
   console.log(`callbackToGame: ${message}`);
-  console.log(message);
   if (typeof window.ue !== 'undefined') {
     if (typeof window.ue.jsconnector === 'undefined') {
       const unrealError = 'Unreal JSConnector not defined';
@@ -549,8 +548,24 @@ window.callFunction = async (jsonData: string) => {
       }
       case PASSPORT_FUNCTIONS.imx.transfer: {
         const unsignedTransferRequest = JSON.parse(data);
-        const response = await getProvider().transfer(unsignedTransferRequest);
-        trackDuration(moduleName, 'performedImxTransfer', mt(markStart));
+
+        // throw new Error('Transfer is not supported in this version');
+
+        // const response = await getProvider().transfer(unsignedTransferRequest);
+        let response;
+        for (let i = 0; i < 50; i++) {
+          // eslint-disable-next-line no-await-in-loop
+          response = await getProvider().transfer(unsignedTransferRequest);
+          console.log(`transfer ${i}::response`, response);
+        }
+
+        console.log('PASSPORT_FUNCTIONS.imx.transfer::response', response);
+
+        trackDuration(moduleName, 'performedImxTransfer', mt(markStart), {
+          requestId,
+          transferRequest: JSON.stringify(unsignedTransferRequest),
+          transferResponse: JSON.stringify(response),
+        });
         callbackToGame({
           ...{
             responseFor: fxName,
@@ -567,7 +582,11 @@ window.callFunction = async (jsonData: string) => {
         const response = await getProvider().batchNftTransfer(
           nftTransferDetails,
         );
-        trackDuration(moduleName, 'performedImxBatchNftTransfer', mt(markStart));
+        trackDuration(moduleName, 'performedImxBatchNftTransfer', mt(markStart), {
+          requestId,
+          transferRequest: JSON.stringify(nftTransferDetails),
+          transferResponse: JSON.stringify(response),
+        });
         callbackToGame({
           ...{
             responseFor: fxName,
@@ -610,7 +629,11 @@ window.callFunction = async (jsonData: string) => {
           throw new Error('Failed to send transaction');
         }
 
-        trackDuration(moduleName, 'performedZkevmSendTransaction', mt(markStart));
+        trackDuration(moduleName, 'performedZkevmSendTransaction', mt(markStart), {
+          requestId,
+          transactionRequest: JSON.stringify(transaction),
+          transactionResponse: transactionHash,
+        });
         callbackToGame({
           responseFor: fxName,
           requestId,
@@ -628,7 +651,11 @@ window.callFunction = async (jsonData: string) => {
 
         const tx = await signer.sendTransaction(transaction);
         const response = await tx.wait();
-        trackDuration(moduleName, 'performedZkevmSendTransactionWithConfirmation', mt(markStart));
+        trackDuration(moduleName, 'performedZkevmSendTransactionWithConfirmation', mt(markStart), {
+          requestId,
+          transactionRequest: JSON.stringify(transaction),
+          transactionResponse: JSON.stringify(response),
+        });
         callbackToGame({
           ...{
             responseFor: fxName,
@@ -722,6 +749,7 @@ window.callFunction = async (jsonData: string) => {
         break;
     }
   } catch (error: any) {
+    console.log('!!!!callFunction error!!!!', error);
     let wrappedError;
 
     if (!(error instanceof Error)) {
@@ -755,6 +783,14 @@ window.callFunction = async (jsonData: string) => {
     });
   }
 };
+
+window.addEventListener('offline', () => {
+  console.log('gameBridge offline');
+});
+
+window.addEventListener('online', () => {
+  console.log('gameBridge online');
+});
 
 function onLoadHandler() {
   // File loaded
