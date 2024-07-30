b75ee551abeb352a660953db005816fdb63b0255
Shine Li <shine.li@immutable.com>
2024-07-30 16:16:04 +1000

    chore: Apply nx to passport sample deployment (#2036)

--- examples/ts-immutable-sample/src/pages/PassportPage/PassportPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import PassportPage from './PassportPage';
-
-test('renders correctly', () => {
-  render(<PassportPage />);
-  const pasportHeading = screen.getByText('Passport');
-  expect(pasportHeading).toBeInTheDocument();
-});
