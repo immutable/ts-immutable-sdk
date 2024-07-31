94a9dd195cb5302c9854f88c30ba250a73eea216
Craig M. <craig.b.macgregor@gmail.com>
2024-08-01 11:58:52 +1200

    base tests for signing done

--- examples/README.md
+++ examples/README.md
@@ -195,4 +195,55 @@ All examples should be heavily commented and the comments should make sense in t
 
 All examples should be covered by e2e tests to ensure they successfully do the action the code sample is showing in the docs site.
 
-More info on e2e tests coming soon.
+Install `@playwright/test` as a dev dependency for the e2e tests.
+
+```
+yarn add -D @playwright/test
+```
+
+Create a `playwright.config.ts` file in the root of the example app and add this configuration;
+
+```
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+
+  use: {
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    { name: "webkit", use: { ...devices["Desktop Safari"] } },
+
+    { name: "Mobile Chrome", use: { ...devices["Pixel 5"] } },
+    { name: "Mobile Safari", use: { ...devices["iPhone 12"] } },
+  ],
+
+  webServer: {
+    command: "yarn dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+  },
+});
+```
+
+Create a `tests` directory in the root of the example app and start adding tests.
+
+Example tests can be found in `/examples/passport/wallets-signing-with-nextjs/tests/`
+
+Add the test runner to the scripts in your package.json
+
+```
+"test": "playwright install --with-deps && playwright test"
+```
+
+Run your tests with `yarn test`
---
+++ examples/passport/wallets-signing-with-nextjs/tests/base.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test.describe("home page", () => {
+  test("has title, heading and link", async ({ page }) => {
+    await expect(page).toHaveTitle("Passport Wallets Signing");
+
+    await expect(page.getByRole("heading", { name: "Passport Wallets Signing Examples" })).toBeVisible();
+
+    await expect(page.getByRole("link", { name: "Sign message with EIP-712" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Sign message with ERC-191" })).toBeVisible();
+  });
+});
+
+test.describe("sign message with EIP-712", () => {
+  test("has heading, login button and initial account status set correctly", async ({ page }) => {
+    await page.click("text=Sign message with EIP-712");
+
+    await expect(page.getByRole("heading", { name: "Passport Wallets - Sign EIP-712 message" })).toBeVisible();
+
+    await expect(page.getByRole("button", { name: "Passport Login" })).toBeVisible();
+    await expect(page.getByText("Connected Account:")).toBeVisible();
+    await expect(page.getByRole("link", { name: "Return to Examples" })).toBeVisible();
+  });
+});
+
+test.describe("sign message with ERC-191", () => {
+  test("has heading, login button and initial account status set correctly", async ({ page }) => {
+    await page.click("text=Sign message with ERC-191");
+
+    await expect(page.getByRole("heading", { name: "Passport Wallets - Sign ERC-191 message" })).toBeVisible();
+
+    await expect(page.getByRole("button", { name: "Passport Login" })).toBeVisible();
+    await expect(page.getByText("Connected Account:")).toBeVisible();
+    await expect(page.getByRole("link", { name: "Return to Examples" })).toBeVisible();
+  });
+});
+
