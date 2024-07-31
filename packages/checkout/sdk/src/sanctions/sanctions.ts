32fab5bead0d954115f5ac081d141eeb4e11e263
Mimi Immutable <mimi.tran@immutable.com>
2024-07-31 15:19:36 +1000

    Move sanction function to sdk

---
+++ packages/checkout/sdk/src/sanctions/sanctions.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { Environment } from '@imtbl/config';
+import { CHECKOUT_CDN_BASE_URL } from '../env';
+
+export const isAddressSanctioned = async (
+  address: string,
+  environment: Environment,
+): Promise<boolean> => {
+  let isSanctioned = false;
+  try {
+    const response = await axios.get(
+      `${CHECKOUT_CDN_BASE_URL[environment]}/v1/address/check/${address}`,
+    );
+
+    if (response.data.identifications.length > 0) {
+      isSanctioned = true;
+    }
+  } catch (error) {
+    // Keep isAddressSanctioned as false
+  }
+
+  return isSanctioned;
+};
