0b7fc6d1158de15fda7af8b1e152528ed70b625d
Shine Li <shine.li@immutable.com>
2024-08-06 17:52:07 +1000

    change hooks

--- packages/passport/sdk/src/react/PassportContext.tsx
+++ packages/passport/sdk/src/react/PassportContext.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import React, {
-  DependencyList,
-  createContext, useContext, useEffect, useMemo,
+  createContext, useContext, useMemo,
   useState,
 } from 'react';
 import { Web3Provider } from '@ethersproject/providers';
@@ -36,70 +35,108 @@ export function usePassport() {
 
 // eslint-disable-next-line @typescript-eslint/comma-dangle
 const useEffectAsync = <T,>(
-  effect: Promise<T>,
-  deps: DependencyList,
+  effect: () => Promise<T>,
 ) => {
   const [data, setData] = useState<T | null>();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
-  useEffect(() => {
+  const action = async () => {
     setLoading(true);
-    effect.then(setData).catch(setError).finally(() => setLoading(false));
-  }, deps);
+    try {
+      const d = await effect();
+      setData(d);
+    } catch (e: any) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    data, loading, error, action,
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/comma-dangle
+const useValueAsync = <T,>(
+  effect: Promise<T>,
+) => {
+  const {
+    data, loading, error, action,
+  } = useEffectAsync(() => effect);
+
+  action();
 
   return { data, loading, error };
 };
 
 export function useLogin() {
   const passport = usePassport();
-  const provider = passport.connectEvm();
-  const { data, loading, error } = useEffectAsync<string[]>(
-    provider.request({ method: 'eth_requestAccounts' }),
-    [provider],
-  );
 
-  return { accounts: data, loading, error };
+  const {
+    data, loading, error, action,
+  } = useEffectAsync(async () => {
+    const provider = passport.connectEvm();
+    return await provider.request({ method: 'eth_requestAccounts' });
+  });
+
+  return {
+    accounts: data, loading, error, login: action,
+  };
 }
 
 export function useLogout() {
   const passport = usePassport();
-  const { loading, error } = useEffectAsync(
-    passport.logout(),
-    [passport],
+  const {
+    data,
+    loading,
+    error,
+    action,
+  } = useEffectAsync(
+    () => passport.logout(),
   );
 
-  return { loading, error };
+  return {
+    profile: data, loading, error, logout: action,
+  };
 }
 
 export function useLoginWithoutWallet() {
   const passport = usePassport();
-  const { data, loading, error } = useEffectAsync(
-    passport.login(),
-    [passport],
+
+  const {
+    data, loading, error, action,
+  } = useEffectAsync(
+    () => passport.login(),
   );
 
-  return { profile: data, loading, error };
+  return {
+    profile: data, loading, error, login: action,
+  };
 }
 
 type Constructor<T> = new (...args: any[]) => T;
 export function useLoginWithEthersjs(ctor: Constructor<Web3Provider>) {
   const passport = usePassport();
-  const provider = passport.connectEvm();
-  const { data, loading, error } = useEffectAsync(
-    // eslint-disable-next-line new-cap
-    new ctor(provider).send('eth_requestAccounts', []),
-    [provider],
-  );
 
-  return { accounts: data, loading, error };
+  const {
+    data, loading, error, action,
+  } = useEffectAsync(async () => {
+    const provider = passport.connectEvm();
+    // eslint-disable-next-line new-cap
+    return await new ctor(provider).send('eth_requestAccounts', []);
+  });
+
+  return {
+    accounts: data, loading, error, login: action,
+  };
 }
 
 export function useIdToken() {
   const passport = usePassport();
-  const { data, loading, error } = useEffectAsync(
+  const { data, loading, error } = useValueAsync(
     passport.getIdToken(),
-    [passport],
   );
 
   return { idToken: data, loading, error };
@@ -107,9 +144,8 @@ export function useIdToken() {
 
 export function useAccessToken() {
   const passport = usePassport();
-  const { data, loading, error } = useEffectAsync(
+  const { data, loading, error } = useValueAsync(
     passport.getAccessToken(),
-    [passport],
   );
 
   return { accessToken: data, loading, error };
@@ -117,9 +153,8 @@ export function useAccessToken() {
 
 export function useLinkedAddresses() {
   const passport = usePassport();
-  const { data, loading, error } = useEffectAsync(
+  const { data, loading, error } = useValueAsync(
     passport.getLinkedAddresses(),
-    [passport],
   );
 
   return { linkedAddresses: data, loading, error };
@@ -127,9 +162,8 @@ export function useLinkedAddresses() {
 
 export function useUserInfo() {
   const passport = usePassport();
-  const { data, loading, error } = useEffectAsync(
+  const { data, loading, error } = useValueAsync(
     passport.getUserInfo(),
-    [passport],
   );
 
   return { userInfo: data, loading, error };
