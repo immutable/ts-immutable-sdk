84cfbc183781cc4e49767972de20bcc7f9f090a8
Shine Li <shine.li@immutable.com>
2024-08-07 17:36:47 +1000

    lint

--- examples/passport/identity-with-nextjs/src/context/passport2.tsx
+++ examples/passport/identity-with-nextjs/src/context/passport2.tsx
@@ -8,9 +8,9 @@ export function PassportProvider({ children }: { children: ReactNode }) {
       config={{
         baseConfig: {
           environment: config.Environment.SANDBOX, // or config.Environment.SANDBOX
-          publishableKey: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY || "<YOUR_PUBLISHABLE_KEY>", // replace with your publishable API key from Hub
+          publishableKey: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY || '<YOUR_PUBLISHABLE_KEY>', // replace with your publishable API key from Hub
         },
-        clientId: process.env.NEXT_PUBLIC_CLIENT_ID || "<YOUR_CLIENT_ID>", // replace with your client ID from Hub
+        clientId: process.env.NEXT_PUBLIC_CLIENT_ID || '<YOUR_CLIENT_ID>', // replace with your client ID from Hub
         redirectUri: 'http://localhost:3000/redirect', // replace with one of your redirect URIs from Hub
         logoutRedirectUri: 'http://localhost:3000/logout', // replace with one of your logout URIs from Hub
         audience: 'platform_api',
