2d97c606067d28fb076552e2dff2392190afcd7e
Shine Li <shine.li@immutable.com>
2024-08-07 16:09:08 +1000

    add typeAssertions back

--- sdk/scripts/copyBrowserBundles.js
+++ sdk/scripts/copyBrowserBundles.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import * as glob from 'glob';
 import path from 'path';
-import pkg from '../package.json';
+import pkg from '../package.json' assert { type: 'json' };
 
 const dirname = path.dirname(new URL(import.meta.url).pathname);
 
