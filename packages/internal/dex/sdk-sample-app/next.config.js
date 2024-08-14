740f062868b8752846f583c2d1949014bfa15825
zaid.arain <zaid.arain@immutable.com>
2024-08-14 13:33:17 +1000

    fix syncpack

--- packages/internal/dex/sdk-sample-app/next.config.js
+++ packages/internal/dex/sdk-sample-app/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    const name = "default";
+
+    class ConditionalExportsPlugin {
+      apply(compiler) {
+        compiler.hooks.afterEnvironment.tap("ConditionalExportsPlugin", () => {
+          compiler.options.resolve.conditionNames = [...compiler.options.resolve.conditionNames, name]
+        });
+      }
+    }
+  
+    config.plugins.push(new ConditionalExportsPlugin());
+
+    return config;
+  }
 };
 
 module.exports = nextConfig;
