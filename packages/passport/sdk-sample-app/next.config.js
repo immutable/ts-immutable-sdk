1bac114e73318714c3c88eaca638b73328a59add
zaid.arain <zaid.arain@immutable.com>
2024-08-14 13:29:04 +1000

    fix builds and add fallback main field

--- packages/passport/sdk-sample-app/next.config.js
+++ packages/passport/sdk-sample-app/next.config.js
@@ -12,7 +12,25 @@ if (basePath) {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   ...pathConfig,
+  typescript: {
+    tsconfigPath: './tsconfig.build.json',
+  },
   reactStrictMode: true,
+  webpack: (config) => {
+    const name = "default";
+
+    class ConditionalExportsPlugin {
+      apply(compiler) {
+        compiler.hooks.afterEnvironment.tap("ConditionalExportsPlugin", () => {
+          compiler.options.resolve.conditionNames = [...compiler.options.resolve.conditionNames, name]
+        });
+      }
+    }
+  
+    config.plugins.push(new ConditionalExportsPlugin());
+
+    return config;
+  }
 };
 
 module.exports = nextConfig
