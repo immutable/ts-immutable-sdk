f6b6524f7ecd02c1726d8ac952a69660a97b605d
Jhonatan Gonzalez <jhesgodi@gmail.com>
2024-09-05 12:39:41 +1000

    notify iframe if initialised with a provider

--- packages/checkout/widgets-lib/src/widgets/checkout/CheckoutWidget.tsx
+++ packages/checkout/widgets-lib/src/widgets/checkout/CheckoutWidget.tsx
@@ -3,6 +3,7 @@ import {
   Checkout,
   CheckoutWidgetConfiguration,
   CheckoutWidgetParams,
+  PostMessageHandlerEventType,
 } from '@imtbl/checkout-sdk';
 import { Web3Provider } from '@ethersproject/providers';
 import {
@@ -13,12 +14,13 @@ import {
 import { CheckoutContextProvider } from './context/CheckoutContextProvider';
 import { CheckoutAppIframe } from './views/CheckoutAppIframe';
 import { getIframeURL } from './functions/iframeParams';
+import { useMount } from './hooks/useMount';
 
 export type CheckoutWidgetInputs = {
   checkout: Checkout;
   params: CheckoutWidgetParams;
   config: CheckoutWidgetConfiguration;
-  provider?: Web3Provider
+  provider?: Web3Provider;
 };
 
 export default function CheckoutWidget(props: CheckoutWidgetInputs) {
@@ -37,20 +39,32 @@ export default function CheckoutWidget(props: CheckoutWidgetInputs) {
   );
   const checkoutReducerValues = useMemo(
     () => ({
-      checkoutState: {
-        ...checkoutState, iframeURL, checkout,
-      },
+      checkoutState: { ...checkoutState, iframeURL, checkout },
       checkoutDispatch,
     }),
     [checkoutState, checkoutDispatch, iframeURL, checkout],
   );
 
+  // If the widget was initialized with a provider,
+  // notify iframe via postMessage
+  const { postMessageHandler } = checkoutState;
+  useMount(
+    () => {
+      if (!provider) return;
+
+      postMessageHandler?.send(PostMessageHandlerEventType.PROVIDER_UPDATED, {
+        isMetamask: provider.provider.isMetaMask,
+        isPassport: (provider.provider as any)?.isPassport,
+      });
+    },
+    [postMessageHandler, provider] as const,
+    ([_postMessageHandler]) => _postMessageHandler !== undefined,
+  );
+
+  // keep the provider updated in the state
   useEffect(() => {
     checkoutDispatch({
-      payload: {
-        type: CheckoutActions.SET_PROVIDER,
-        provider,
-      },
+      payload: { type: CheckoutActions.SET_PROVIDER, provider },
     });
   }, [provider]);
 
