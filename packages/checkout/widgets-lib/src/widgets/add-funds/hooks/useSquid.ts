ca8cce64a0179d96fcea8f415463703c380e7b11
Mimi Immutable <mimi.tran@immutable.com>
2024-09-09 15:58:12 +1000

    Create config file for squid base url

--- packages/checkout/widgets-lib/src/widgets/add-funds/hooks/useSquid.ts
+++ packages/checkout/widgets-lib/src/widgets/add-funds/hooks/useSquid.ts
@@ -1,5 +1,6 @@
 import { Squid } from '@0xsquid/sdk';
 import { useEffect, useState } from 'react';
+import { SQUID_SDK_BASE_URL } from '../utils/config';
 
 export const useSquid = () => {
   const [squid, setSquid] = useState<Squid | null>(null);
@@ -11,7 +12,7 @@ export const useSquid = () => {
 
     const initialiseSquid = async () => {
       const squidSDK = new Squid({
-        baseUrl: 'https://apiplus.squidrouter.com',
+        baseUrl: SQUID_SDK_BASE_URL,
         integratorId: 'immutable-bridge-f126614b-1683-471c-9e16-5df422c515cf',
       });
 
