595a43e68f893c523b956e6f56388a2df2c4db37
Naveen <116692862+naveen-imtb@users.noreply.github.com>
2024-09-09 14:35:28 +1000

    chore: TD-1653: use biome components

--- examples/orderbook/create-listing-with-nextjs/src/app/redirect/page.tsx
+++ examples/orderbook/create-listing-with-nextjs/src/app/redirect/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from 'react';
 import { passportInstance } from '../utils/setupPassport';
+import {Box, Heading} from "@biom3/react";
 
 export default function Redirect() {
   useEffect(() => {
@@ -11,8 +12,8 @@ export default function Redirect() {
 
   // render the view for the login popup after the login is complete
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8">
-      <h1 className="text-3xl font-bold mb-8">Logged in</h1>
-    </div>
+    <Box>
+      <Heading size="medium">Logged in</Heading>
+    </Box>
   );
 }
