e606213628eff5f0ea17b62782b7cb6c0c6cbb2c
Naveen <116692862+naveen-imtb@users.noreply.github.com>
2024-09-09 14:20:54 +1000

    chore: TD-1653: add listing creation example

---
+++ examples/orderbook/create-listing-with-nextjs/src/app/utils/setupOrderbook.ts
@@ -0,0 +1,8 @@
+import {orderbook} from "@imtbl/sdk";
+import {Environment} from "@imtbl/config";
+
+export const orderbookSDK = new orderbook.Orderbook({
+    baseConfig: {
+        environment: Environment.SANDBOX,
+    },
+});
