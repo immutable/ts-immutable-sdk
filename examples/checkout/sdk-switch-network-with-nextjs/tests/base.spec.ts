e8d844e378a958e29afe91fd6bcbe1ac2c7a793c
ImmutableAustinG <austin.guo@immutable.com>
2024-09-10 11:03:00 +1000

    Added playwright tests

--- examples/checkout/sdk-switch-network-with-nextjs/tests/base.spec.ts
+++ examples/checkout/sdk-switch-network-with-nextjs/tests/base.spec.ts
@@ -6,8 +6,8 @@ test.beforeEach(async ({ page }) => {
 
 test.describe("home page", () => {
   test("has title, heading and link", async ({ page }) => {
-    await expect(page).toHaveTitle("Checkout SDK - Connect with NextJS");
-    await expect(page.getByRole("heading", { name: "Checkout SDK Connect" })).toBeVisible();
-    await expect(page.getByRole("link", { name: "Connect with Metamask" })).toBeVisible();
+    await expect(page).toHaveTitle("Checkout SDK - Switching Networks with NextJS");
+    await expect(page.getByRole("heading", { name: "Checkout SDK Connect and Switch Networks" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "Switch Network with MetaMask" })).toBeVisible();
   });
 });
