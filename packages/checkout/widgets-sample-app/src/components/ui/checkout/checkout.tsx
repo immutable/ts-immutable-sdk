29b0caa39a7c8c9d9068441b2164eba20b521fc3
Jhonatan Gonzalez <jhesgodi@gmail.com>
2024-09-12 13:17:36 +1000

    [NO CHANGELOG][Checkout Widget] Feat: Checkout widget refactor iframe params (#2168)

--- packages/checkout/widgets-sample-app/src/components/ui/checkout/checkout.tsx
+++ packages/checkout/widgets-sample-app/src/components/ui/checkout/checkout.tsx
@@ -1,10 +1,15 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import {
   AppHeaderBar,
+  Body,
   Box,
   Button,
+  Checkbox,
   FormControl,
+  Heading,
+  Link,
   MenuItem,
+  Select,
   Stack,
   Sticker,
   Toggle,
@@ -22,28 +27,40 @@ import {
   WidgetType,
   CheckoutFlowType,
   WalletProviderName,
+  Widget,
 } from "@imtbl/checkout-sdk";
 import { Passport } from "@imtbl/passport";
 import { WidgetsFactory } from "@imtbl/checkout-widgets";
 import { Environment, ImmutableConfiguration } from "@imtbl/config";
 
 import { useAsyncMemo, usePrevState } from "../../../hooks";
+import { Message } from "./components/messages";
+import { Legend } from "./components/legend";
+import { itemsMock } from "./items.mock";
+
+//
+const ENVIRONMENT_DEV = "development" as Environment;
 
 const publishableKey = "pk_imapik-test-Xdera@";
 
 // create a base config
-const getBaseConfig = () =>
-  new ImmutableConfiguration({
-    environment: Environment.SANDBOX,
+const getBaseConfig = (_environment: Environment) => {
+  // skip DEV as its not technically supported by config
+  const environment =
+    _environment === ENVIRONMENT_DEV ? Environment.SANDBOX : _environment;
+
+  return new ImmutableConfiguration({
+    environment,
     publishableKey,
     // apiKey
     // rateLimitingKey
   });
+};
 
 // create a passport client
-const getPassportClient = () =>
+const getPassportClient = (environment: Environment) =>
   new Passport({
-    baseConfig: getBaseConfig(),
+    baseConfig: getBaseConfig(environment),
     audience: "platform_api",
     scope: "openid offline_access email transact",
     clientId: "ViaYO6JWck4TZOiiojEak8mz6WvQh3wK",
@@ -52,11 +69,11 @@ const getPassportClient = () =>
   });
 
 // create Checkout SDK
-const getCheckoutSdk = (passportClient: Passport) =>
+const getCheckoutSdk = (passportClient: Passport, environment: Environment) =>
   new Checkout({
     publishableKey,
     passport: passportClient,
-    baseConfig: getBaseConfig(),
+    baseConfig: getBaseConfig(environment),
     overrides: {
       // checkoutAppUrl: "http://localhost:3001",
       // environment: "development" as Environment,
@@ -105,25 +122,58 @@ const createWeb3Provider = async (
   }
 };
 
+// checkout widget flows
+const flows: Array<CheckoutFlowType> = [
+  CheckoutFlowType.CONNECT,
+  CheckoutFlowType.WALLET,
+  CheckoutFlowType.ONRAMP,
+  CheckoutFlowType.SWAP,
+  CheckoutFlowType.BRIDGE,
+  CheckoutFlowType.SALE,
+];
+
 function CheckoutUI() {
   // avoid re mounting the widget
   const mounted = useRef<boolean>(false);
 
+  //
+  const [environment, prevEnvironment, setEnvironment] = usePrevState(
+    Environment.SANDBOX
+  );
+
+  const [checkoutAppURL, setCheckoutAppURL] = useState("");
+
   // setup passport client
-  const passportClient = useMemo(() => getPassportClient(), []);
+  const passportClient = useMemo(
+    () => getPassportClient(environment),
+    [environment]
+  );
   // handle passport login
   usePassportLoginCallback(passportClient);
 
   // setup checkout sdk
   const checkoutSdk = useMemo(
-    () => getCheckoutSdk(passportClient),
-    [passportClient]
+    () => getCheckoutSdk(passportClient, environment),
+    [passportClient, environment]
   );
 
   // set a state to keep widget params and configs
   const [params, setParams] = useState<CheckoutWidgetParams | undefined>(
     undefined
   );
+
+  const [flowParams, setFlowParams] = useState<
+    Partial<Record<CheckoutFlowType, CheckoutWidgetParams>>
+  >({
+    sale: {
+      flow: CheckoutFlowType.SALE,
+      items: itemsMock,
+      environmentId: "249d9b0b-ee16-4dd5-91ee-96bece3b0473",
+      collectionName: "Pixel Aussie Farm",
+      // excludePaymentTypes: [checkout.SalePaymentTypes.CREDIT],
+      // preferredCurrency: 'USDC',
+    },
+  });
 
   // set a state to keep widget event results
   const [eventResults, setEventResults] = useState<unknown[]>([]);
@@ -178,6 +228,9 @@ function CheckoutUI() {
 
   // create the widget once factory is available
   // ignore language or theme changes
+  const prevWidget = useRef<Widget<typeof WidgetType.CHECKOUT> | undefined>(
+    undefined
+  );
   const widget = useAsyncMemo(async () => {
     if (widgetsFactory === undefined) return undefined;
     if (renderAfterConnect && !web3Provider) return undefined;
@@ -210,7 +263,18 @@ function CheckoutUI() {
     // add event listeners
     widget.addListener(CheckoutEventType.INITIALISED, () => {
       setEventResults((prev) => [...prev, { initialised: true }]);
+
+      if (typeof window === "undefined") return;
+
+      const checkoutAppIframe = document.getElementById(
+        "checkout-app"
+      ) as HTMLIFrameElement;
+
+      if (checkoutAppIframe?.src) {
+        setCheckoutAppURL(checkoutAppIframe.src);
+      }
     });
+
     widget.addListener(CheckoutEventType.DISCONNECTED, () => {
       setEventResults((prev) => [...prev, { disconnected: true }]);
     });
@@ -250,9 +314,8 @@ function CheckoutUI() {
     // });
   }, [widget]);
 
-  // mount & re-rende widget everytime params change
+  // mount & re-render widget everytime params change
   useEffect(() => {
-    if (mounted.current) return;
     if (params == undefined) return;
     if (renderAfterConnect && !web3Provider) return;
 
@@ -289,9 +352,24 @@ function CheckoutUI() {
     }
   }, [renderAfterConnect, prevRenderAfterConnect, unmount]);
 
+  // unmount when environment changes
+  useEffect(() => {
+    if (environment !== prevEnvironment) {
+      console.log("ENV", environment, prevEnvironment);
+      unmount();
+    }
+  }, [environment, prevEnvironment]);
+
+  // unmount when web3Provider is undefined
+  useEffect(() => {
+    if (web3Provider === undefined && widget && mounted.current) {
+      unmount();
+    }
+  }, [web3Provider, widget]);
+
   return (
-    <>
-      <Box>
+    <Box sx={{ p: "base.spacing.x4" }}>
+      <Box sx={{ mb: "base.spacing.x4" }}>
         <AppHeaderBar>
           <AppHeaderBar.OverflowPopoverMenu variant="secondary">
             <MenuItem
@@ -352,7 +430,7 @@ function CheckoutUI() {
                       name: "Kangaroo",
                       image:
                         "https://iguanas.mystagingwebsite.com/wp-content/uploads/2024/05/character-image-10-1.png",
-                      description: "Pixel Art Kangaroo",
+                      description: "Pixel Art Kangaroo"
                     },
                   ],
                   environmentId: "249d9b0b-ee16-4dd5-91ee-96bece3b0473",
@@ -410,83 +488,194 @@ function CheckoutUI() {
               <option value="ko">🇰🇷</option>
               <option value="zh">🇨🇳</option>
             </Box>
-            <Sticker>
-              <Button
-                variant="secondary"
-                onClick={async () => {
-                  if (web3Provider) {
-                    setWeb3Provider(undefined);
-                    return;
-                  }
-
-                  setWeb3Provider(
-                    await createWeb3Provider(checkoutSdk, {
-                      walletProviderName: WalletProviderName.PASSPORT,
-                    })
-                  );
-                }}
-              >
-                <Button.Logo logo="PassportSymbol" />
-              </Button>
-              {!isPassport && (
-                <Sticker.Badge variant="fatal" badgeContent="off" />
-              )}
-              {isPassport && (
-                <Sticker.Badge variant="success" isAnimated badgeContent="on" />
-              )}
-            </Sticker>
-            <Sticker>
-              <Button
-                variant="secondary"
-                onClick={async () => {
-                  if (web3Provider) {
-                    setWeb3Provider(undefined);
-                    return;
-                  }
-
-                  setWeb3Provider(
-                    await createWeb3Provider(checkoutSdk, {
-                      walletProviderName: WalletProviderName.METAMASK,
-                    })
-                  );
-                }}
-              >
-                <Button.Logo logo="MetaMaskSymbol" />
-              </Button>
-              {!isMetamask && (
-                <Sticker.Badge variant="fatal" badgeContent="off" />
-              )}
-              {isMetamask && (
-                <Sticker.Badge variant="success" isAnimated badgeContent="on" />
-              )}
-            </Sticker>
           </AppHeaderBar.RightSlot>
           <AppHeaderBar.LeftSlot gap="base.spacing.x4">
             <AppHeaderBar.Title>{params?.flow || ""}</AppHeaderBar.Title>
           </AppHeaderBar.LeftSlot>
         </AppHeaderBar>
       </Box>
-      <Box>
-        <Stack sx={{ flexWrap: "wrap", py: "base.spacing.x2" }}>
-          <FormControl sx={{ alignItems: "center" }}>
-            <Toggle
-              onChange={toggleRenderAfterConnect}
-              checked={renderAfterConnect}
-            />
-            <FormControl.Label>Render after connect</FormControl.Label>
-          </FormControl>
-        </Stack>
-      </Box>
       <Box sx={{ display: "flex", flexDirection: "row" }}>
-        <Box id="widget-root" />
-        <Box>
-          <Box>Events Log</Box>
-          {eventResults.map((result) => (
-            <Box children={JSON.stringify(result, null, 2)} />
-          ))}
+        <Box sx={{ w: "75%", mr: "base.spacing.x4" }}>
+          <Stack sx={{ flexWrap: "wrap", py: "base.spacing.x2" }}>
+            {/* --- --- --- */}
+            <Legend>Environment: {environment.toUpperCase()}</Legend>
+            {checkoutAppURL && new URL(checkoutAppURL).origin}
+            <Box
+              sx={{
+                display: "flex",
+                gap: "base.spacing.x1",
+              }}
+            >
+              <Checkbox
+                checked={environment === ENVIRONMENT_DEV}
+                onChange={() => setEnvironment(ENVIRONMENT_DEV)}
+              />
+              <Body>{ENVIRONMENT_DEV.toUpperCase()}</Body>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "base.spacing.x1",
+              }}
+            >
+              <Checkbox
+                checked={environment === Environment.SANDBOX}
+                onChange={() => setEnvironment(Environment.SANDBOX)}
+              />
+              <Body>{Environment.SANDBOX.toUpperCase()}</Body>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "base.spacing.x1",
+              }}
+            >
+              <Checkbox
+                checked={environment === Environment.PRODUCTION}
+                onChange={() => setEnvironment(Environment.PRODUCTION)}
+              />
+              <Body>{Environment.PRODUCTION.toUpperCase()}</Body>
+            </Box>
+
+            {/* --- --- --- */}
+            <Legend>Flow: {params?.flow.toLocaleUpperCase()}</Legend>
+            <FormControl sx={{ alignItems: "center", mb: "base.spacing.x4" }}>
+              <FormControl.Label>Connect a provider first</FormControl.Label>
+              <Toggle
+                onChange={toggleRenderAfterConnect}
+                checked={renderAfterConnect}
+              />
+            </FormControl>
+
+            {(renderAfterConnect || web3Provider) && (
+              <>
+                <Heading size="xSmall">Connect a provider</Heading>
+                <Stack direction="row" gap="base.spacing.x6">
+                  <Sticker>
+                    <Button
+                      size="small"
+                      variant="secondary"
+                      onClick={async () => {
+                        if (web3Provider) {
+                          setWeb3Provider(undefined);
+                          return;
+                        }
+
+                        setWeb3Provider(
+                          await createWeb3Provider(checkoutSdk, {
+                            walletProviderName: WalletProviderName.PASSPORT,
+                          })
+                        );
+                      }}
+                    >
+                      Passport <Button.Logo logo="PassportSymbol" />
+                    </Button>
+                    {!isPassport && (
+                      <Sticker.Badge variant="fatal" badgeContent="off" />
+                    )}
+                    {isPassport && (
+                      <Sticker.Badge
+                        variant="success"
+                        isAnimated
+                        badgeContent="on"
+                      />
+                    )}
+                  </Sticker>
+                  <Sticker>
+                    <Button
+                      size="small"
+                      variant="secondary"
+                      onClick={async () => {
+                        if (web3Provider) {
+                          setWeb3Provider(undefined);
+                          return;
+                        }
+
+                        setWeb3Provider(
+                          await createWeb3Provider(checkoutSdk, {
+                            walletProviderName: WalletProviderName.METAMASK,
+                          })
+                        );
+                      }}
+                    >
+                      MetaMask
+                      <Button.Logo logo="MetaMaskSymbol" />
+                    </Button>
+                    {!isMetamask && (
+                      <Sticker.Badge variant="fatal" badgeContent="off" />
+                    )}
+                    {isMetamask && (
+                      <Sticker.Badge
+                        variant="success"
+                        isAnimated
+                        badgeContent="on"
+                      />
+                    )}
+                  </Sticker>
+                </Stack>
+              </>
+            )}
+
+            {((renderAfterConnect && web3Provider) || !renderAfterConnect) && (
+              <>
+                <Heading size="xSmall">Select a flow</Heading>
+                <Select
+                  defaultLabel="Select a Flow"
+                  onSelectChange={(value) => {
+                    const flow = value as CheckoutFlowType;
+                    setParams({
+                      ...(flowParams[flow as keyof typeof flowParams] || {}),
+                      flow,
+                      // spread rest of params for given flow
+                    });
+                  }}
+                >
+                  {flows.map((flow) => (
+                    <Select.Option key={flow} optionKey={flow}>
+                      <Select.Option.Label>{flow}</Select.Option.Label>
+                    </Select.Option>
+                  ))}
+                </Select>
+              </>
+            )}
+
+            {/* --- --- --- */}
+            <Legend>Params & Config:</Legend>
+          </Stack>
         </Box>
+        <Box
+          id="widget-root"
+          sx={{
+            minw: "430px",
+            minh: "650px",
+            bg: "base.color.translucent.standard.300",
+            brad: "base.borderRadius.x5",
+          }}
+        />
       </Box>
-    </>
+      <Box>
+        {checkoutAppURL && (
+          <Message type="success" title="Checkout App URL">
+            <Link
+              variant="secondary"
+              weight="bold"
+              onClick={() => {
+                window.open(checkoutAppURL, "_blank", "noopener,noreferrer");
+              }}
+            >
+              {checkoutAppURL}
+              <Link.Icon icon="JumpTo" />
+            </Link>
+          </Message>
+        )}
+      </Box>
+      <Box>
+        <Heading>Events</Heading>
+        {eventResults.map((result) => (
+          <Box children={JSON.stringify(result, null, 2)} />
+        ))}
+      </Box>
+    </Box>
   );
 }
 
