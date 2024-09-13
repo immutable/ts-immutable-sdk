c82ce2e6f8edd6b4a88bb3c6cf45312a13bdb984
Ji Young Lee <ji.younglee@immutable.com>
2024-09-13 15:34:07 +1000

    Rename props
    Co-authored-by: Mimi Tran <mimi-imtbl@users.noreply.github.com>

--- packages/checkout/widgets-sample-app/src/components/ui/add-funds-integration/addFunds.tsx
+++ packages/checkout/widgets-sample-app/src/components/ui/add-funds-integration/addFunds.tsx
@@ -18,8 +18,8 @@ function AddFundsIntegration() {
     if (!addFunds) return;
     addFunds.mount(ADD_FUNDS_TARGET_ID, {
       showSwapOption:false,
-      amount: "10",
-      tokenAddress: "0x1CcCa691501174B4A623CeDA58cC8f1a76dc3439",
+      toAmount: "10",
+      toTokenAddress: "0x1CcCa691501174B4A623CeDA58cC8f1a76dc3439",
     });
     addFunds.addListener(AddFundsEventType.GO_BACK, (data: any) => {
       console.log("GO_BACK", data);
