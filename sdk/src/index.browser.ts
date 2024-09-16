79dc1a5ea814cf34fe119d415b9be3af6d31d86e
zaid.arain <zaid.arain@immutable.com>
2024-09-16 17:37:30 +1000

    pnpm, tsup migration and fix builds/errors

--- sdk/src/index.browser.ts
+++
@@ -1,6 +0,0 @@
-export * as config from './config';
-export * as blockchainData from './blockchain_data';
-export * as passport from './passport';
-export * as orderbook from './orderbook';
-export * as checkout from './checkout';
-export * as x from './x';
