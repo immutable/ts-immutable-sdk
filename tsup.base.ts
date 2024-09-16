834206b703c2541db8196135fb16633d1e8d9cb7
zaid.arain <zaid.arain@immutable.com>
2024-09-16 17:37:30 +1000

    pnpm, tsup migration and fix builds/errors

---
+++ tsup.base.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: ['esm', 'cjs'],
+  target: 'es2022',
+  bundle: true,
+  clean: true,
+  skipNodeModulesBundle: true,
+});
