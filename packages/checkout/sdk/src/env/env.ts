0c607563228f678413543bf6e6d83bf7742f96f4
zaid.arain <zaid.arain@immutable.com>
2024-09-16 17:37:30 +1000

    pnpm, tsup migration and fix builds/errors

--- packages/checkout/sdk/src/env/env.ts
+++ packages/checkout/sdk/src/env/env.ts
@@ -2,5 +2,5 @@ export const SDK_VERSION_MARKER = '__SDK_VERSION__';
 
 export const useLocalBundle = () => process.env.CHECKOUT_LOCAL_MODE === 'true';
 
-// This SDK version is replaced by the `yarn build` command ran on the root level
+// This SDK version is replaced by the `pnpm build` command ran on the root level
 export const globalPackageVersion = () => SDK_VERSION_MARKER;
