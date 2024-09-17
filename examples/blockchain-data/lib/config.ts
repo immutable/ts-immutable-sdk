c35f098c08ae6d17fa8b816abcccca295df56a43
Allan Almeida <allan.almeida@immutable.com>
2024-09-17 14:34:15 +1000

    clean up

--- examples/blockchain-data/lib/config.ts
+++ examples/blockchain-data/lib/config.ts
@@ -1,10 +1,8 @@
 import { config as immutableConfig, blockchainData } from '@imtbl/sdk';
 
-const PUBLISHABLE_KEY = 'YOUR_PUBLISHABLE_KEY'; // Replace with your Publishable Key from the Immutable Hub
-
 export const config: blockchainData.BlockchainDataModuleConfiguration = {
   baseConfig: {
     environment: immutableConfig.Environment.SANDBOX,
-    publishableKey: PUBLISHABLE_KEY,
+    publishableKey: process.env.PUBLISHABLE_KEY,
   },
 };
