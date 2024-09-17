53a429a20ca69c6e7be7e9f229afcd2e54a6d8a5
Hiep Doan <hiep.doan@immutable.com>
2024-09-17 15:31:41 +1000

    Add comments to function

--- examples/blockchain-data/api-examples-with-node/verify-successful-mints.ts
+++ examples/blockchain-data/api-examples-with-node/verify-successful-mints.ts
@@ -5,9 +5,11 @@ import { client } from '../lib';
 export async function verifySuccessfulMints(
   contractAddress: string,
 ): Promise<blockchainData.Types.ListActivitiesResult> {
+  // #doc blockchain-data-api-verify-success-mints
   return await client.listActivities({
     chainName: 'imtbl-zkevm-testnet',
     contractAddress,
     activityType: blockchainData.Types.ActivityType.Mint,
   });
+  // #enddoc blockchain-data-api-verify-success-mints
 }
