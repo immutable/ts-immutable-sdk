e1d108a09eea53e408b80f652d6b796aefdc9916
Mimi Immutable <mimi.tran@immutable.com>
2024-09-19 14:46:13 +1000

    Add types

--- packages/checkout/widgets-lib/src/widgets/add-funds/types.ts
+++ packages/checkout/widgets-lib/src/widgets/add-funds/types.ts
@@ -1,6 +1,22 @@
+import { RouteResponse, Token } from '@0xsquid/squid-types';
+import { TokenBalance } from '@imtbl/checkout-sdk';
+
 export type Chain = {
   id: string;
   type: string;
   name: string;
   iconUrl: string;
+};
+
+export type AmountData = {
+  fromToken: Token;
+  fromAmount: string;
+  toToken: Token;
+  toAmount: string;
+  balance: TokenBalance;
+};
+
+export type RouteData = {
+  amountData: AmountData;
+  route: RouteResponse;
 };
