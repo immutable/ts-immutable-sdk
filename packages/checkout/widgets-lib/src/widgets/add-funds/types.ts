a154af5fecf14a8945b1326ac3070dfe9affb3ab
Mimi Tran <80493680+mimi-imtbl@users.noreply.github.com>
2024-09-19 15:09:51 +1000

    [NO CHANGELOG][Add Funds Widget] Add funds update useRoutes to handle rate limiting (#2189)

--- packages/checkout/widgets-lib/src/widgets/add-funds/types.ts
+++ packages/checkout/widgets-lib/src/widgets/add-funds/types.ts
@@ -1,6 +1,22 @@
+import { TokenBalance } from '@0xsquid/sdk/dist/types';
+import { RouteResponse, Token } from '@0xsquid/squid-types';
+
 export type Chain = {
   id: string;
   type: string;
   name: string;
   iconUrl: string;
+};
+
+export type AmountData = {
+  fromToken: Token;
+  fromAmount: string;
+  toToken: Token;
+  toAmount: string;
+  balance: TokenBalance;
+};
+
+export type RouteData = {
+  amountData: AmountData;
+  route: RouteResponse;
 };
