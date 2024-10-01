c4940f206dc583285f26579dd15198feaab0455c
tim <tim@glo.id.au>
2024-10-01 16:52:33 +1000

    creating new AddFunds main view layout

--- packages/checkout/widgets-lib/src/widgets/add-funds/views/AddFunds.tsx
+++ packages/checkout/widgets-lib/src/widgets/add-funds/views/AddFunds.tsx
@@ -1,332 +1,382 @@
 import {
-  Checkout,
-  IMTBLWidgetEvents,
-  TokenFilterTypes,
-  TokenInfo,
-} from '@imtbl/checkout-sdk';
+	Body,
+	Box,
+	ButtCon,
+	Button,
+	FramedIcon,
+	HeroFormControl,
+	HeroTextInput,
+	MenuItem,
+	OverflowDrawerMenu,
+	Stack,
+	VerticalMenu,
+} from "@biom3/react";
 import {
-  Body, Box, MenuItem, OverflowPopoverMenu, HeroTextInput,
-} from '@biom3/react';
+	type Checkout,
+	IMTBLWidgetEvents,
+	TokenFilterTypes,
+	type TokenInfo,
+} from "@imtbl/checkout-sdk";
 import {
-  useCallback, useContext, useEffect, useRef, useState,
-} from 'react';
-import { SimpleLayout } from '../../../components/SimpleLayout/SimpleLayout';
-import { HeaderNavigation } from '../../../components/Header/HeaderNavigation';
-import { OptionsDrawer } from '../components/OptionsDrawer';
-import { EventTargetContext } from '../../../context/event-target-context/EventTargetContext';
-import { orchestrationEvents } from '../../../lib/orchestrationEvents';
-import { OptionTypes } from '../components/Option';
-import { AddFundsActions, AddFundsContext } from '../context/AddFundsContext';
-import { getL2ChainId } from '../../../lib';
+	useCallback,
+	useContext,
+	useEffect,
+	useMemo,
+	useRef,
+	useState,
+} from "react";
+import { HeaderNavigation } from "../../../components/Header/HeaderNavigation";
+import { SimpleLayout } from "../../../components/SimpleLayout/SimpleLayout";
+import { EventTargetContext } from "../../../context/event-target-context/EventTargetContext";
 import {
-  SharedViews,
-  ViewActions,
-  ViewContext,
-} from '../../../context/view-context/ViewContext';
+	SharedViews,
+	ViewActions,
+	ViewContext,
+} from "../../../context/view-context/ViewContext";
+import { getL2ChainId } from "../../../lib";
+import { orchestrationEvents } from "../../../lib/orchestrationEvents";
+import { OptionTypes } from "../components/Option";
+import { OptionsDrawer } from "../components/OptionsDrawer";
+import { AddFundsActions, AddFundsContext } from "../context/AddFundsContext";
 
 interface AddFundsProps {
-  checkout?: Checkout;
-  showBackButton?: boolean;
-  showOnrampOption?: boolean;
-  showSwapOption?: boolean;
-  showBridgeOption?: boolean;
-  toTokenAddress?: string;
-  toAmount?: string;
-  onCloseButtonClick?: () => void;
-  onBackButtonClick?: () => void;
+	checkout?: Checkout;
+	showBackButton?: boolean;
+	showOnrampOption?: boolean;
+	showSwapOption?: boolean;
+	showBridgeOption?: boolean;
+	toTokenAddress?: string;
+	toAmount?: string;
+	onCloseButtonClick?: () => void;
+	onBackButtonClick?: () => void;
 }
 
 export function AddFunds({
-  checkout,
-  toAmount,
-  toTokenAddress,
-  showBackButton = false,
-  showOnrampOption = true,
-  showSwapOption = true,
-  showBridgeOption = true,
-  onBackButtonClick,
-  onCloseButtonClick,
+	checkout,
+	toAmount,
+	toTokenAddress,
+	showBackButton = false,
+	showOnrampOption = true,
+	showSwapOption = true,
+	showBridgeOption = true,
+	onBackButtonClick,
+	onCloseButtonClick,
 }: AddFundsProps) {
-  const showBack = showBackButton || !!onBackButtonClick;
+	const showBack = showBackButton || !!onBackButtonClick;
 
-  const { addFundsDispatch } = useContext(AddFundsContext);
+	const { addFundsDispatch } = useContext(AddFundsContext);
 
-  const { viewDispatch } = useContext(ViewContext);
+	const { viewDispatch } = useContext(ViewContext);
 
-  const {
-    eventTargetState: { eventTarget },
-  } = useContext(EventTargetContext);
+	const {
+		eventTargetState: { eventTarget },
+	} = useContext(EventTargetContext);
 
-  const [showOptionsDrawer, setShowOptionsDrawer] = useState(false);
-  const [onRampAllowedTokens, setOnRampAllowedTokens] = useState<TokenInfo[]>(
-    [],
-  );
-  const [allowedTokens, setAllowedTokens] = useState<TokenInfo[]>([]);
-  const [inputValue, setInputValue] = useState<string>(toAmount || '0');
-  const [currentToAmount, setCurrentToAmount] = useState<string>(
-    toAmount || '0',
-  );
-  const [currentToTokenAddress, setCurrentToTokenAddress] = useState<
-  TokenInfo | undefined
-  >();
+	const [showOptionsDrawer, setShowOptionsDrawer] = useState(false);
+	const [onRampAllowedTokens, setOnRampAllowedTokens] = useState<TokenInfo[]>(
+		[],
+	);
+	const [allowedTokens, setAllowedTokens] = useState<TokenInfo[]>([]);
+	const [inputValue, setInputValue] = useState<string>(toAmount || "0");
+	const [currentToAmount, setCurrentToAmount] = useState<string>(
+		toAmount || "0",
+	);
+	const [currentToTokenAddress, setCurrentToTokenAddress] = useState<
+		TokenInfo | undefined
+	>();
 
-  const debounceTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+	const debounceTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  const handleAmountChange = (value: string) => {
-    if (debounceTimeoutRef.current) {
-      clearTimeout(debounceTimeoutRef.current);
-    }
+	const handleAmountChange = (value: string) => {
+		if (debounceTimeoutRef.current) {
+			clearTimeout(debounceTimeoutRef.current);
+		}
 
-    debounceTimeoutRef.current = setTimeout(() => {
-      setCurrentToAmount(value);
-    }, 1500);
-  };
+		debounceTimeoutRef.current = setTimeout(() => {
+			setCurrentToAmount(value);
+		}, 1500);
+	};
 
-  const updateAmount = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    setInputValue(value);
-    handleAmountChange(value);
-  };
+	const updateAmount = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { value } = event.target;
+		setInputValue(value);
+		handleAmountChange(value);
+	};
 
-  const showErrorView = useCallback(
-    (error: Error) => {
-      viewDispatch({
-        payload: {
-          type: ViewActions.UPDATE_VIEW,
-          view: {
-            type: SharedViews.ERROR_VIEW,
-            error,
-          },
-        },
-      });
-    },
-    [viewDispatch],
-  );
+	const showErrorView = useCallback(
+		(error: Error) => {
+			viewDispatch({
+				payload: {
+					type: ViewActions.UPDATE_VIEW,
+					view: {
+						type: SharedViews.ERROR_VIEW,
+						error,
+					},
+				},
+			});
+		},
+		[viewDispatch],
+	);
 
-  useEffect(() => {
-    if (!checkout) {
-      showErrorView(new Error('Checkout object is missing'));
-      return;
-    }
+	useEffect(() => {
+		if (!checkout) {
+			showErrorView(new Error("Checkout object is missing"));
+			return;
+		}
 
-    const fetchTokens = async () => {
-      try {
-        const tokenResponse = await checkout.getTokenAllowList({
-          type: TokenFilterTypes.SWAP,
-          chainId: getL2ChainId(checkout.config),
-        });
+		const fetchTokens = async () => {
+			try {
+				const tokenResponse = await checkout.getTokenAllowList({
+					type: TokenFilterTypes.SWAP,
+					chainId: getL2ChainId(checkout.config),
+				});
 
-        if (tokenResponse?.tokens.length > 0) {
-          setAllowedTokens(tokenResponse.tokens);
+				if (tokenResponse?.tokens.length > 0) {
+					setAllowedTokens(tokenResponse.tokens);
 
-          const token = tokenResponse.tokens.find((t) => t.address === toTokenAddress)
-            || tokenResponse.tokens[0];
-          setCurrentToTokenAddress(token);
+					// @TODO: i think this can be removed...as we are starting off in the
+					// default state, where no pay with and deliver to addresses are set ... ?
+					// --------------------------------------------------------------------------------
+					// const token = tokenResponse.tokens.find((t) => t.address === toTokenAddress)
+					//   || tokenResponse.tokens[0];
+					// setCurrentToTokenAddress(token);
 
-          addFundsDispatch({
-            payload: {
-              type: AddFundsActions.SET_ALLOWED_TOKENS,
-              allowedTokens: tokenResponse.tokens,
-            },
-          });
-        }
-      } catch (error) {
-        showErrorView(new Error('Failed to fetch tokens'));
-      }
-    };
+					addFundsDispatch({
+						payload: {
+							type: AddFundsActions.SET_ALLOWED_TOKENS,
+							allowedTokens: tokenResponse.tokens,
+						},
+					});
+				}
+			} catch (error) {
+				showErrorView(new Error("Failed to fetch tokens"));
+			}
+		};
 
-    fetchTokens();
-  }, [checkout, toTokenAddress]);
+		fetchTokens();
+	}, [checkout, toTokenAddress]);
 
-  useEffect(() => {
-    if (!checkout) {
-      showErrorView(new Error('Checkout object is missing'));
-      return;
-    }
+	useEffect(() => {
+		if (!checkout) {
+			showErrorView(new Error("Checkout object is missing"));
+			return;
+		}
 
-    const fetchOnRampTokens = async () => {
-      try {
-        const tokenResponse = await checkout.getTokenAllowList({
-          type: TokenFilterTypes.ONRAMP,
-          chainId: getL2ChainId(checkout.config),
-        });
+		const fetchOnRampTokens = async () => {
+			try {
+				const tokenResponse = await checkout.getTokenAllowList({
+					type: TokenFilterTypes.ONRAMP,
+					chainId: getL2ChainId(checkout.config),
+				});
 
-        if (tokenResponse?.tokens.length > 0) {
-          setOnRampAllowedTokens(tokenResponse.tokens);
-        }
-      } catch (error) {
-        showErrorView(new Error('Failed to fetch onramp tokens'));
-      }
-    };
-    fetchOnRampTokens();
-  }, [checkout]);
+				if (tokenResponse?.tokens.length > 0) {
+					setOnRampAllowedTokens(tokenResponse.tokens);
+				}
+			} catch (error) {
+				showErrorView(new Error("Failed to fetch onramp tokens"));
+			}
+		};
+		fetchOnRampTokens();
+	}, [checkout]);
 
-  const openDrawer = () => {
-    setShowOptionsDrawer(true);
-  };
+	const openDrawer = () => {
+		setShowOptionsDrawer(true);
+	};
 
-  const isSelected = (token: TokenInfo) => token.address === currentToTokenAddress;
+	const isSelected = (token: TokenInfo) =>
+		token.address === currentToTokenAddress;
+	// const isDisabled = !currentToTokenAddress || !currentToAmount || Number.parseFloat(currentToAmount) <= 0;
 
-  const isDisabled = !currentToTokenAddress || !currentToAmount || parseFloat(currentToAmount) <= 0;
+	const handleTokenChange = (token: TokenInfo) => {
+		setCurrentToTokenAddress(token);
+	};
 
-  const handleTokenChange = (token: TokenInfo) => {
-    setCurrentToTokenAddress(token);
-  };
+	const handleReviewClick = useCallback(() => {
+		console.log("handle review click");
+	}, []);
 
-  // const handleReviewClick = () => {
-  //   console.log('handle review click');
-  // };
+	const onPayWithCard = (paymentType: OptionTypes) => {
+		if (paymentType === OptionTypes.SWAP) {
+			orchestrationEvents.sendRequestSwapEvent(
+				eventTarget,
+				IMTBLWidgetEvents.IMTBL_ADD_FUNDS_WIDGET_EVENT,
+				{
+					toTokenAddress: currentToTokenAddress?.address ?? "",
+					amount: toAmount ?? "",
+					fromTokenAddress: "",
+				},
+			);
+		} else {
+			const data = {
+				tokenAddress: currentToTokenAddress?.address ?? "",
+				amount: toAmount ?? "",
+			};
+			orchestrationEvents.sendRequestOnrampEvent(
+				eventTarget,
+				IMTBLWidgetEvents.IMTBL_ADD_FUNDS_WIDGET_EVENT,
+				data,
+			);
+		}
+	};
 
-  const onPayWithCard = (paymentType: OptionTypes) => {
-    if (paymentType === OptionTypes.SWAP) {
-      orchestrationEvents.sendRequestSwapEvent(
-        eventTarget,
-        IMTBLWidgetEvents.IMTBL_ADD_FUNDS_WIDGET_EVENT,
-        {
-          toTokenAddress: currentToTokenAddress?.address ?? '',
-          amount: toAmount ?? '',
-          fromTokenAddress: '',
-        },
-      );
-    } else {
-      const data = {
-        tokenAddress: currentToTokenAddress?.address ?? '',
-        amount: toAmount ?? '',
-      };
-      orchestrationEvents.sendRequestOnrampEvent(
-        eventTarget,
-        IMTBLWidgetEvents.IMTBL_ADD_FUNDS_WIDGET_EVENT,
-        data,
-      );
-    }
-  };
+	const shouldShowOnRampOption = useMemo(() => {
+		if (showOnrampOption && currentToTokenAddress) {
+			const token = onRampAllowedTokens.find(
+				(t) =>
+					t.address?.toLowerCase() ===
+					currentToTokenAddress.address?.toLowerCase(),
+			);
+			return !!token;
+		}
+		return false;
+	}, [currentToTokenAddress, onRampAllowedTokens, showOnrampOption]);
 
-  const checkShowOnRampOption = () => {
-    if (showOnrampOption && currentToTokenAddress) {
-      const token = onRampAllowedTokens.find(
-        (t) => t.address?.toLowerCase()
-          === currentToTokenAddress.address?.toLowerCase(),
-      );
-      return !!token;
-    }
-    return false;
-  };
+	const showInitialEmptyState = !currentToTokenAddress;
 
-  return (
-    <SimpleLayout
-      header={(
-        <HeaderNavigation
-          title="Add"
-          onCloseButtonClick={onCloseButtonClick}
-          showBack={showBack}
-          onBackButtonClick={() => {
-            orchestrationEvents.sendRequestGoBackEvent(
-              eventTarget,
-              IMTBLWidgetEvents.IMTBL_ADD_FUNDS_WIDGET_EVENT,
-              {},
-            );
-            onBackButtonClick?.();
-          }}
-        />
-      )}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-          height: '100%',
-        }}
-      >
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: 'base.spacing.x10',
-          }}
-        >
-          <Box sx={{ width: 'base.spacing.x40' }}>
-            <Box sx={{ marginBottom: 'base.spacing.x3' }}>
-              <HeroTextInput
-                testId="add-funds-amount-input"
-                type="number"
-                value={inputValue}
-                onChange={(value) => updateAmount(value)}
-                placeholder="0"
-                weight="bold"
-              />
-            </Box>
+	return (
+		<SimpleLayout
+			header={
+				<HeaderNavigation
+					onCloseButtonClick={onCloseButtonClick}
+					showBack={showBack}
+					onBackButtonClick={() => {
+						orchestrationEvents.sendRequestGoBackEvent(
+							eventTarget,
+							IMTBLWidgetEvents.IMTBL_ADD_FUNDS_WIDGET_EVENT,
+							{},
+						);
+						onBackButtonClick?.();
+					}}
+				/>
+			}
+		>
+			{/*
+        <Body size="large" weight="bold">
+          {currentToTokenAddress?.name ?? ''}
+        </Body>
 
-            <Box
-              sx={{
-                display: 'flex',
-                borderRadius: 'base.borderRadius.x20',
-                alignItems: 'center',
-                gap: 'base.spacing.x5',
-                justifyContent: 'center',
-                border: '1px solid grey',
-              }}
+        {allowedTokens.map((token: any) => (
+            <MenuItem
+              key={token.address}
+              onClick={() => handleTokenChange(token)}
+              selected={isSelected(token)}
             >
-              <Body size="large" weight="bold">
-                {currentToTokenAddress?.name ?? ''}
-              </Body>
-              <OverflowPopoverMenu testId="add-funds-tokens-menu">
-                {allowedTokens.map((token: any) => (
-                  <MenuItem
-                    key={token.address}
-                    onClick={() => handleTokenChange(token)}
-                    selected={isSelected(token)}
-                  >
-                    <MenuItem.Label>{token.name}</MenuItem.Label>
-                  </MenuItem>
-                ))}
-              </OverflowPopoverMenu>
-            </Box>
-          </Box>
-        </Box>
+              <MenuItem.Label>{token.name}</MenuItem.Label>
+            </MenuItem>
+          ))}
 
-        <MenuItem
-          size="small"
-          emphasized
-          disabled={isDisabled}
-          sx={{
-            opacity: isDisabled ? 0.5 : 1,
-            cursor: isDisabled ? 'not-allowed' : 'pointer',
-          }}
-          onClick={() => {
-            openDrawer();
-          }}
-        >
-          <MenuItem.IntentIcon icon="ChevronExpand" />
-          <MenuItem.Label size="medium">Choose payment option</MenuItem.Label>
-        </MenuItem>
-        <Box
-          sx={{
-            marginBottom: 'base.spacing.x10',
-          }}
-        >
-          <OptionsDrawer
-            showOnrampOption={checkShowOnRampOption()}
-            showSwapOption={showSwapOption}
-            showBridgeOption={showBridgeOption}
-            visible={showOptionsDrawer}
-            onClose={() => setShowOptionsDrawer(false)}
-            onPayWithCard={onPayWithCard}
-          />
-        </Box>
-        {/* <Button
-          testId="add-funds-button"
-          variant="primary"
-          onClick={handleReviewClick}
-          size="large"
-          sx={{
-            marginBottom: 'base.spacing.x10',
-            mx: 'base.spacing.x3',
-          }}
-        >
-          Review
-        </Button> */}
-      </Box>
-    </SimpleLayout>
-  );
+        <OptionsDrawer
+          showOnrampOption={shouldShowOnRampOption}
+          showSwapOption={showSwapOption}
+          showBridgeOption={showBridgeOption}
+          visible={showOptionsDrawer}
+          onClose={() => setShowOptionsDrawer(false)}
+          onPayWithCard={onPayWithCard}
+        />
+
+        */}
+
+			<Stack alignItems="center" sx={{ flex: 1 }}>
+				<Stack testId="top-section" sx={{ flex: 1 }} justifyContent="center">
+					{showInitialEmptyState ? (
+						<Stack alignItems="center">
+							<OverflowDrawerMenu
+								icon="Add"
+								size="large"
+								drawerSize="full"
+								headerBarTitle="Add Token"
+							>
+								{allowedTokens.map((token) => (
+									<MenuItem
+										size="medium"
+										key={token.name}
+										// onClick={() => handleTokenChange(token)}
+										// selected={isSelected(token)}
+										emphasized
+									>
+										<MenuItem.FramedImage
+											circularFrame
+											use={<img src={token.icon} />}
+											emphasized={false}
+										/>
+										<MenuItem.Label>{token.name}</MenuItem.Label>
+									</MenuItem>
+								))}
+							</OverflowDrawerMenu>
+							<Body>Add Token</Body>
+						</Stack>
+					) : (
+						<HeroFormControl>
+							<HeroFormControl.Label>
+								Add {currentToTokenAddress.symbol}
+							</HeroFormControl.Label>
+							<HeroTextInput
+								testId="add-funds-amount-input"
+								type="number"
+								value={inputValue}
+								onChange={(value) => updateAmount(value)}
+								placeholder="0"
+							/>
+							<HeroFormControl.Caption>USD $0.00</HeroFormControl.Caption>
+						</HeroFormControl>
+					)}
+				</Stack>
+				<Stack
+					testId="bottom-ui"
+					sx={{
+						alignSelf: "stretch",
+						p: "base.spacing.x3",
+						pb: "base.spacing.x10",
+						bg: "base.color.neutral.800",
+					}}
+					gap="base.spacing.x6"
+				>
+					<Stack gap="0px">
+						<MenuItem size="small" emphasized onClick={() => openDrawer()}>
+							<MenuItem.FramedIcon
+								icon="Dollar"
+								variant="bold"
+								emphasized={false}
+							/>
+							<MenuItem.Label>Pay with</MenuItem.Label>
+						</MenuItem>
+						<Stack
+							sx={{ pos: "relative", h: "base.spacing.x3" }}
+							alignItems="center"
+						>
+							<FramedIcon
+								icon="ArrowDown"
+								sx={{
+									top: "0",
+									pos: "absolute",
+									translate: ({ base }) => `0 -${base.spacing.x3}`,
+								}}
+							/>
+						</Stack>
+						<MenuItem
+							size="small"
+							emphasized
+							onClick={() => console.log("@TODO - need to hook this up!")}
+						>
+							<MenuItem.FramedIcon
+								icon="Wallet"
+								variant="bold"
+								emphasized={false}
+							/>
+							<MenuItem.Label>Deliver to</MenuItem.Label>
+						</MenuItem>
+					</Stack>
+
+					<Button
+						testId="add-funds-button"
+						variant="secondary"
+						onClick={handleReviewClick}
+						size="large"
+					>
+						Review
+					</Button>
+				</Stack>
+			</Stack>
+		</SimpleLayout>
+	);
 }
