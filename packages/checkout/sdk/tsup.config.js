9f854914628dec808e372472985caaab07ccef03
zaid.arain <zaid.arain@immutable.com>
2024-10-02 15:45:23 +1000

    update checkout build process, update publish process to handle sdk versioning replacement in dist output correctly, remove typechecking from sdk pr workflow

---
+++ packages/checkout/sdk/tsup.config.js
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from 'tsup'
+import { replace } from 'esbuild-plugin-replace';
+import pkg from './package.json' assert { type: 'json' };
+
+export default defineConfig((options) => ({
+  entry: ['src/index.ts'],
+  outDir: 'dist',
+  format: !options.watch ? ['esm', 'cjs'] : 'esm',
+  target: 'es2022',
+  bundle: true,
+  clean: !options.watch,
+  splitting: false,
+  minify: !options.watch,
+  skipNodeModulesBundle: true,
+  esbuildPlugins: [
+    replace({ 
+      '__SDK_VERSION__': pkg.version, 
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+      'process.env.CHECKOUT_DEV_MODE': JSON.stringify(process.env.CHECKOUT_DEV_MODE || 'false'),
+      'process.env.CHECKOUT_LOCAL_MODE': JSON.stringify(process.env.CHECKOUT_LOCAL_MODE || 'false'),
+      'process.versions': JSON.stringify(process.versions || {}),
+    })
+  ]
+}));
