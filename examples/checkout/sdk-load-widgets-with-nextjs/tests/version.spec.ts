4d762f46617a768daf555a3f030488748fe1096f
James Hardwick <james.hardwick@immutable.com>
2024-10-02 15:02:00 +1000

    Adds example next app for checkout widgets.

---
+++ examples/checkout/sdk-load-widgets-with-nextjs/tests/version.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from "@playwright/test";
+import { interceptWidgets } from "./utils/intercept-widgets";
+
+const useLocalBundle = process.env.USE_LOCAL_BUNDLE === 'true';
+
+test.beforeEach(async ({ page }) => {
+  if (useLocalBundle) {
+    await interceptWidgets(page);
+  }
+
+  await page.goto("/version");
+});
+
+// test.beforeEach(async ({ page }) => {
+//   await page.goto("/");
+// });
+
+test.describe("widget loading", () => {
+  test("basic check", async ({ page }) => {
+    const versionText = page.getByText('Version');
+    await expect(versionText).toBeVisible();
+
+  });
+
+  test("loads widgets into root", async ({ page }) => {
+
+    // Wait for the widget-root element to be visible
+    await page.waitForSelector('#widget-root');
+
+    // Check if the widget-root element contains child elements
+    const widgetRoot = page.locator('#widget-root');
+    await expect(widgetRoot).not.toBeEmpty();
+
+    const connectWidget = page.getByTestId('connect-wallet');
+    await expect(connectWidget).toBeVisible();
+  });
+});
+
+
+
+
+
+// https://cdn.jsdelivr.net/npm/@imtbl/sdk@1.54.0-alpha.1/dist/browser/checkout/widgets-esm.js
