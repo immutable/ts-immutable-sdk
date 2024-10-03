76dcf92ea4ea9c734450a5ad33ec29604a16385d
zaid.arain <zaid.arain@immutable.com>
2024-10-03 18:40:28 +1000

    update dev mode to instant refresh for all packages, support jest listening for workspace dependencies

--- packages/x-client/jest.config.cjs
+++ packages/x-client/jest.config.cjs
@@ -1,6 +1,16 @@
+import { execSync } from 'child_process';
+import { name } from './package.json';
+
+const rootDirs = execSync(`pnpm --filter ${name}... exec pwd`)
+  .toString()
+  .split('\n')
+  .filter(Boolean)
+  .map((dir) => `${dir}/dist`);
+  
 module.exports = {
+  roots: ['<rootDir>/src', ...rootDirs],
   testEnvironment: 'node',
-  moduleDirectories: ['node_modules', '<rootDir>/src', '<rootDir>/.yalc'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
   modulePathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/backup/'],
   moduleNameMapper: { '^@imtbl/(.*)$': '<rootDir>/../../node_modules/@imtbl/$1/src' },
   testRegex: '^.+\\.test\\.(js|ts|jsx|tsx)$',
@@ -13,5 +23,4 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules\//(?!node-fetch)/',
   ],
-  modulePathIgnorePatterns: ['<rootDir>/.yalc'],
 };
