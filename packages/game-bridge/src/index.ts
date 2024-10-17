03741e29854b9c9c7f56c39079d965fb2a2b95f2
Nik Ho <studying.mole@gmail.com>
2024-10-17 14:11:00 +1300

    add callback to game with invalid function error from the default case

--- packages/game-bridge/src/index.ts
+++ packages/game-bridge/src/index.ts
@@ -818,8 +818,19 @@ window.callFunction = async (jsonData: string) => {
         });
         break;
       }
-      default:
+      default: {
+        const request = JSON.parse(data);
+        const properties = request.properties ? JSON.parse(request.properties) : {};
+        properties.fxName = fxName;
+        track(moduleName, 'callFunctionDefaultCaseCalled', properties);
+        callbackToGame({
+          responseFor: fxName,
+          requestId,
+          success: false,
+          error: `Invalid game bridge function: ${fxName}`,
+        });
         break;
+      }
     }
   } catch (error: any) {
     console.log(`Error in callFunction: ${error}`);
