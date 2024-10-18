aa6a8b2f9d8a1a2fa3bb0e14b74e9d9946a60fc5
zaidarain1 <zaid.arain@immutable.com>
2024-10-18 12:08:08 +1100

    Revert "chore: [DX-3334] Add the Subresource Integrity attribute to Checkout …"
    This reverts commit aa442266463a6974b61a7d05541bb00cf73dfc2d.

--- packages/checkout/sdk/src/sdk.ts
+++ packages/checkout/sdk/src/sdk.ts
@@ -247,7 +247,7 @@ export class Checkout {
   ) {
     const checkout = this;
     try {
-      const cdnUrl = await getWidgetsEsmUrl(validVersion);
+      const cdnUrl = getWidgetsEsmUrl(validVersion);
 
       // WebpackIgnore comment required to prevent webpack modifying the import statement and
       // breaking the dynamic import in certain applications integrating checkout
