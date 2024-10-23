ea6feaf0d4ae6bbc782c46c5ec41f54afd97c020
Mimi Immutable <mimi.tran@immutable.com>
2024-10-23 14:07:23 +1100

    test

--- packages/checkout/widgets-lib/src/widgets/add-funds/AddFundsWidget.tsx
+++ packages/checkout/widgets-lib/src/widgets/add-funds/AddFundsWidget.tsx
@@ -126,19 +126,19 @@ export default function AddFundsWidget({
     })();
   }, []);
 
-  useEffect(() => {
-    if (!checkout) return;
-    (async () => {
-      if (!(await checkout.isSwapAvailable())) {
-        viewDispatch({
-          payload: {
-            type: ViewActions.UPDATE_VIEW,
-            view: { type: AddFundsWidgetViews.GEO_BLOCK_ERROR },
-          },
-        });
-      }
-    })();
-  }, [checkout]);
+  // useEffect(() => {
+  // if (!checkout) return;
+  // (async () => {
+  //   if (!(await checkout.isSwapAvailable())) {
+  //     viewDispatch({
+  //       payload: {
+  //         type: ViewActions.UPDATE_VIEW,
+  //         view: { type: AddFundsWidgetViews.GEO_BLOCK_ERROR },
+  //       },
+  //     });
+  //   }
+  // })();
+  // }, [checkout]);
 
   useEffect(() => {
     if (!squid || !chains || !fromProvider || fetchingBalances.current) return;
