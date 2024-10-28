b6f5bda9424f3784f0c90fdfa95d5abac878403a
zaid.arain <zaid.arain@immutable.com>
2024-10-28 11:30:23 +1100

    fix: failing commerce func test

--- tests/func-tests/checkout/widgets-nextjs/src/app/widgets/page.tsx
+++ tests/func-tests/checkout/widgets-nextjs/src/app/widgets/page.tsx
@@ -1,21 +1,21 @@
 "use client";
 import { Box } from '@biom3/react';
 import { checkout } from '@imtbl/sdk';
-import { CheckoutFlowType, ConnectionSuccess, Widget, WidgetType } from '@imtbl/sdk/checkout';
+import { CommerceFlowType, ConnectionSuccess, Widget, WidgetType } from '@imtbl/sdk/checkout';
 import { useEffect, useState } from 'react';
 
 const checkoutSDK = new checkout.Checkout();
 
 function Widgets() {
 
-  const [widget, setWidget] = useState<Widget<WidgetType.CHECKOUT>>();
+  const [widget, setWidget] = useState<Widget<WidgetType.IMMUTABLE_COMMERCE>>();
 
   useEffect(() => {
 
     const loadWidgets = async () => {
       const widgetsFactory = await checkoutSDK.widgets({ config: {} });
 
-      const widget = widgetsFactory.create(WidgetType.CHECKOUT, {})
+      const widget = widgetsFactory.create(WidgetType.IMMUTABLE_COMMERCE, {})
       setWidget(widget);
     }
 
@@ -26,16 +26,16 @@ function Widgets() {
   useEffect(() => {
     if (!widget) return;
     widget.mount("widget-root", {
-      flow: CheckoutFlowType.WALLET,
+      flow: CommerceFlowType.WALLET,
     });
 
     widget.addListener(
-      checkout.CheckoutEventType.SUCCESS,
-      (payload: checkout.CheckoutSuccessEvent) => {
+      checkout.CommerceEventType.SUCCESS,
+      (payload: checkout.CommerceSuccessEvent) => {
         const { type, data } = payload;
 
         // capture provider after user connects their wallet
-        if (type === checkout.CheckoutSuccessEventType.CONNECT_SUCCESS) {
+        if (type === checkout.CommerceSuccessEventType.CONNECT_SUCCESS) {
           const { walletProviderName } = data as ConnectionSuccess;
           console.log('connected to ', walletProviderName);
           // setProvider(data.provider);
@@ -48,26 +48,26 @@ function Widgets() {
 
     // detect when user fails to connect
     widget.addListener(
-      checkout.CheckoutEventType.FAILURE,
-      (payload: checkout.CheckoutFailureEvent) => {
+      checkout.CommerceEventType.FAILURE,
+      (payload: checkout.CommerceFailureEvent) => {
         const { type, data } = payload;
 
-        if (type === checkout.CheckoutFailureEventType.CONNECT_FAILED) {
+        if (type === checkout.CommerceFailureEventType.CONNECT_FAILED) {
           console.log('failed to connect', data.reason);
         }
       }
     );
 
     // remove widget from view when closed
-    widget.addListener(checkout.CheckoutEventType.CLOSE, () => {
+    widget.addListener(checkout.CommerceEventType.CLOSE, () => {
       widget.unmount();
     });
 
     // clean up event listeners
     return () => {
-      widget.removeListener(checkout.CheckoutEventType.SUCCESS);
-      widget.removeListener(checkout.CheckoutEventType.DISCONNECTED);
-      widget.removeListener(checkout.CheckoutEventType.CLOSE);
+      widget.removeListener(checkout.CommerceEventType.SUCCESS);
+      widget.removeListener(checkout.CommerceEventType.DISCONNECTED);
+      widget.removeListener(checkout.CommerceEventType.CLOSE);
     };
 
 
