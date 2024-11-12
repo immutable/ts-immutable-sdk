6a516cebab3d040af3930ca7068dec890ac3f1ee
Mimi Immutable <mimi.tran@immutable.com>
2024-11-12 15:43:12 +1100

    Fetch chains from squid sdk

--- packages/checkout/widgets-lib/src/widgets/add-tokens/utils/config.ts
+++ packages/checkout/widgets-lib/src/widgets/add-tokens/utils/config.ts
@@ -1,7 +1,5 @@
 export const SQUID_SDK_BASE_URL = 'https://apiplus.squidrouter.com';
 
-export const SQUID_API_BASE_URL = 'https://api.squidrouter.com/v1';
-
 export const SQUID_NATIVE_TOKEN = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
 
 export const FIXED_HANDOVER_DURATION = 2000;
