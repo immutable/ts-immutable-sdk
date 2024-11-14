2b143688b43c6e651a60df8e4f632a798facb9c3
Mimi Immutable <mimi.tran@immutable.com>
2024-11-14 13:05:27 +1100

    Update regex

--- packages/checkout/widgets-lib/src/widgets/add-tokens/functions/amountValidation.ts
+++ packages/checkout/widgets-lib/src/widgets/add-tokens/functions/amountValidation.ts
@@ -1,4 +1,4 @@
-const VALID_NUMBER_REGEX = /^[0-9]+(\.[0-9]+)?$/;
+const VALID_NUMBER_REGEX = /^(0|[1-9]\d*)(\.\d*)?$/;
 
 /**
  * Validate the amount input
@@ -6,7 +6,7 @@ const VALID_NUMBER_REGEX = /^[0-9]+(\.[0-9]+)?$/;
  * @returns An object containing the sanitized value, the float amount, and a boolean indicating if the amount is valid
  */
 export const validateToAmount = (amount: string) => {
-  const value = amount || '0';
+  const value = amount || '';
   const sanitizedValue = value.replace(/^0+(?=\d)/, '');
   const floatAmount = parseFloat(sanitizedValue);
 
