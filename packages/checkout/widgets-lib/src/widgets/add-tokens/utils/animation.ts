e2302318820d9901be9bcb47449a64a299caceeb
Mimi Tran <80493680+mimi-imtbl@users.noreply.github.com>
2024-11-18 15:13:32 +1100

    [NO CHANGELOG] [Add Tokens Widget] Update animation (#2403)

--- packages/checkout/widgets-lib/src/widgets/add-tokens/utils/animation.ts
+++ packages/checkout/widgets-lib/src/widgets/add-tokens/utils/animation.ts
@@ -5,7 +5,7 @@ export const PULSE_SHADOW = keyframes`
         box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.3);
     }
     50% {
-        box-shadow: 0 0 10px 3px rgba(54, 210, 227, 0.3);
+        box-shadow: 0 0 10px 2px rgba(54, 210, 227, 0.2);
     }
     100% {
         box-shadow: 0 0 0 0 rgba(255, 255, 255, 0);
