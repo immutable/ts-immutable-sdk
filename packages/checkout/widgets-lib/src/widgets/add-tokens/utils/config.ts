590b732dc4d897f4a951c0a9acfa3624bf7b2a95
Ji Young Lee <ji.younglee@immutable.com>
2024-11-22 14:06:44 +1100

    Add new riv animation

--- packages/checkout/widgets-lib/src/widgets/add-tokens/utils/config.ts
+++ packages/checkout/widgets-lib/src/widgets/add-tokens/utils/config.ts
@@ -8,4 +8,8 @@ export const APPROVE_TXN_ANIMATION = '/access_coins.riv';
 
 export const EXECUTE_TXN_ANIMATION = '/swapping_coins.riv';
 
+export const BLOCK_TXN_ANIMATION = '/blocked.riv';
+
+export const ERROR_TXN_ANIMATION = '/error.riv';
+
 export const TOKEN_PRIORITY_ORDER = ['IMX', 'USDC', 'ETH'];
