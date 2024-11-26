6055ca734e260e317fd59295c9042b3878c4c39f
Luã de Souza <eumesmo.ht@gmail.com>
2024-11-26 15:42:35 +1100

    Ensure anonymousId is kept across multiple identify calls

--- packages/checkout/widgets-lib/src/components/WalletDrawer/ConnectWalletDrawer.tsx
+++ packages/checkout/widgets-lib/src/components/WalletDrawer/ConnectWalletDrawer.tsx
@@ -67,7 +67,7 @@ export function ConnectWalletDrawer({
     providersDispatch,
   } = useProvidersContext();
 
-  const { identify, track } = useAnalytics();
+  const { identify, track, user } = useAnalytics();
 
   const prevWalletChangeEvent = useRef<WalletChangeEvent | undefined>();
 
@@ -147,9 +147,16 @@ export function ConnectWalletDrawer({
         shouldRequestWalletPermissions,
       );
 
+      // Identify connected wallet, retaining current anonymousId
       if (shouldIdentifyUser) {
-        // Identify connected wallet
+        const userData = user ? await user() : undefined;
+        const anonymousId = userData?.anonymousId();
+
         await identifyUser(identify, provider);
+
+        if (anonymousId) {
+          (await user()).anonymousId(anonymousId);
+        }
       }
 
       // Store selected provider as fromProvider in context
