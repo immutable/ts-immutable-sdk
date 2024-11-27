a887d05caf046c176123befd02cea06042ac5b22
Alejo L <alejo.loaiza@gmail.com>
2024-11-28 09:36:32 +1100

    Move useQuoteOder and useSignOrder to common lib

---
+++ packages/checkout/widgets-lib/src/lib/hooks/useSignOrder.ts
@@ -0,0 +1,418 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { useCallback, useState } from 'react';
+import { SaleItem } from '@imtbl/checkout-sdk';
+
+import {
+  SignResponse,
+  SignOrderInput,
+  SignedOrderProduct,
+  SignOrderError,
+  ExecuteOrderResponse,
+  ExecutedTransaction,
+  SaleErrorTypes,
+  SignPaymentTypes,
+  SignedTransaction,
+  ExecuteTransactionStep,
+  PRIMARY_SALES_API_BASE_URL,
+  SignApiProduct,
+  SignApiResponse,
+  SignApiRequest,
+  SignApiError,
+  SignCurrencyFilter,
+} from '../primary-sales';
+import { filterAllowedTransactions, hexToText } from '../utils';
+
+const toSignedProduct = (
+  product: SignApiProduct,
+  currency: string,
+  item?: SaleItem,
+): SignedOrderProduct => ({
+  productId: product.product_id,
+  image: item?.image || '',
+  qty: item?.qty || 1,
+  name: item?.name || '',
+  description: item?.description || '',
+  currency,
+  contractType: product.contract_type,
+  collectionAddress: product.collection_address,
+  amount: product.detail.map(({ amount }) => amount),
+  tokenId: product.detail.map(({ token_id: tokenId }) => tokenId),
+});
+
+const toSignResponse = (
+  signApiResponse: SignApiResponse,
+  items: SaleItem[],
+): SignResponse => {
+  const { order, transactions } = signApiResponse;
+
+  return {
+    order: {
+      currency: {
+        name: order.currency.name,
+        erc20Address: order.currency.erc20_address,
+      },
+      products: order.products
+        .map((product) => toSignedProduct(
+          product,
+          order.currency.name,
+          items.find((item) => item.productId === product.product_id),
+        ))
+        .reduce((acc, product) => {
+          const index = acc.findIndex((n) => n.name === product.name);
+
+          if (index === -1) {
+            acc.push({ ...product });
+          }
+
+          if (index > -1) {
+            acc[index].amount = [...acc[index].amount, ...product.amount];
+            acc[index].tokenId = [...acc[index].tokenId, ...product.tokenId];
+          }
+
+          return acc;
+        }, [] as SignedOrderProduct[]),
+      totalAmount: Number(order.total_amount),
+    },
+    transactions: transactions.map((transaction) => ({
+      tokenAddress: transaction.contract_address,
+      gasEstimate: transaction.gas_estimate,
+      methodCall: transaction.method_call,
+      params: {
+        reference: transaction.params.reference || '',
+        amount: transaction.params.amount || 0,
+        spender: transaction.params.spender || '',
+      },
+      rawData: transaction.raw_data,
+    })),
+    transactionId: hexToText(
+      transactions.find((txn) => txn.method_call.startsWith('execute'))?.params
+        .reference || '',
+    ),
+  };
+};
+
+export const useSignOrder = (input: SignOrderInput) => {
+  const {
+    provider,
+    items,
+    environment,
+    environmentId,
+    waitFulfillmentSettlements,
+    customOrderData,
+  } = input;
+  const [signError, setSignError] = useState<SignOrderError | undefined>(
+    undefined,
+  );
+  const [signResponse, setSignResponse] = useState<SignResponse | undefined>(
+    undefined,
+  );
+  const [executeResponse, setExecuteResponse] = useState<ExecuteOrderResponse>({
+    done: false,
+    transactions: [],
+  });
+  const [tokenIds, setTokenIds] = useState<string[]>([]);
+  const [currentTransactionIndex, setCurrentTransactionIndex] = useState<number>(0);
+  const [filteredTransactions, setFilteredTransactions] = useState<
+  SignedTransaction[]
+  >([]);
+
+  const setExecuteTransactions = (transaction: ExecutedTransaction) => {
+    setExecuteResponse((prev) => ({
+      ...prev,
+      transactions: [...prev.transactions, transaction],
+    }));
+  };
+
+  const setExecuteDone = () => setExecuteResponse((prev) => ({ ...prev, done: true }));
+
+  const setTransactionIndex = () => setCurrentTransactionIndex((prev) => prev + 1);
+
+  const setExecuteFailed = () => setExecuteResponse({
+    done: false,
+    transactions: [],
+  });
+
+  const sendTransaction = useCallback(
+    async (
+      to: string,
+      data: string,
+      gasLimit: number,
+    ): Promise<[hash: string | undefined, error?: SignOrderError]> => {
+      try {
+        const signer = provider?.getSigner();
+        const gasPrice = await provider?.getGasPrice();
+        const txnResponse = await signer?.sendTransaction({
+          to,
+          data,
+          gasPrice,
+          gasLimit,
+        });
+
+        if (waitFulfillmentSettlements) {
+          await txnResponse?.wait();
+        }
+
+        const transactionHash = txnResponse?.hash;
+        if (!transactionHash) {
+          throw new Error('Transaction hash is undefined');
+        }
+        return [transactionHash, undefined];
+      } catch (err) {
+        const reason = `${
+          (err as any)?.reason || (err as any)?.message || ''
+        }`.toLowerCase();
+
+        let errorType = SaleErrorTypes.WALLET_FAILED;
+
+        if (reason.includes('failed') && reason.includes('open confirmation')) {
+          errorType = SaleErrorTypes.WALLET_POPUP_BLOCKED;
+        }
+
+        if (reason.includes('rejected') && reason.includes('user')) {
+          errorType = SaleErrorTypes.WALLET_REJECTED;
+        }
+
+        if (
+          reason.includes('failed to submit')
+          && reason.includes('highest gas limit')
+        ) {
+          errorType = SaleErrorTypes.WALLET_REJECTED_NO_FUNDS;
+        }
+
+        if (
+          reason.includes('status failed')
+          || reason.includes('transaction failed')
+        ) {
+          errorType = SaleErrorTypes.TRANSACTION_FAILED;
+        }
+        const error: SignOrderError = {
+          type: errorType,
+          data: { error: err },
+        };
+        setSignError(error);
+        return [undefined, error];
+      }
+    },
+    [provider, waitFulfillmentSettlements],
+  );
+
+  const sign = useCallback(
+    async (
+      paymentType: SignPaymentTypes,
+      fromTokenAddress: string,
+    ): Promise<SignResponse | undefined> => {
+      try {
+        const signer = provider?.getSigner();
+        const address = (await signer?.getAddress()) || '';
+
+        const data: SignApiRequest = {
+          recipient_address: address,
+          payment_type: paymentType,
+          currency_filter: SignCurrencyFilter.CONTRACT_ADDRESS,
+          currency_value: fromTokenAddress,
+          products: items.map((item) => ({
+            product_id: item.productId,
+            quantity: item.qty,
+          })),
+          custom_data: customOrderData,
+        };
+
+        const baseUrl = `${PRIMARY_SALES_API_BASE_URL[environment]}/${environmentId}/order/sign`;
+        const response = await fetch(baseUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+
+        const { ok, status } = response;
+        if (!ok) {
+          const { code } = (await response.json()) as SignApiError;
+          let errorType: SaleErrorTypes;
+          switch (status) {
+            case 400:
+              errorType = SaleErrorTypes.SERVICE_BREAKDOWN;
+              break;
+            case 404:
+              if (code === 'insufficient_stock') {
+                errorType = SaleErrorTypes.INSUFFICIENT_STOCK;
+              } else {
+                errorType = SaleErrorTypes.PRODUCT_NOT_FOUND;
+              }
+              break;
+            case 429:
+            case 500:
+              errorType = SaleErrorTypes.DEFAULT;
+              break;
+            default:
+              throw new Error('Unknown error');
+          }
+
+          setSignError({ type: errorType });
+          return undefined;
+        }
+
+        const apiResponse: SignApiResponse = await response.json();
+        const apiTokenIds = apiResponse.order.products
+          .map((product) => product.detail.map(({ token_id }) => token_id))
+          .flat();
+
+        const responseData = toSignResponse(apiResponse, items);
+
+        setTokenIds(apiTokenIds);
+        setSignResponse(responseData);
+
+        if (provider) {
+          const filterTransactions = await filterAllowedTransactions(
+            responseData.transactions,
+            provider,
+          );
+          setFilteredTransactions(filterTransactions);
+        }
+
+        return responseData;
+      } catch (e: any) {
+        setSignError({ type: SaleErrorTypes.DEFAULT, data: { error: e } });
+      }
+      return undefined;
+    },
+    [items, environmentId, environment, provider],
+  );
+
+  const executeTransaction = async (
+    transaction: SignedTransaction,
+    onTxnSuccess: (txn: ExecutedTransaction) => void,
+    onTxnError: (error: any, txns: ExecutedTransaction[]) => void,
+  ) => {
+    if (!transaction) {
+      return false;
+    }
+
+    const {
+      tokenAddress: to,
+      rawData: data,
+      methodCall: method,
+      gasEstimate,
+    } = transaction;
+
+    const [hash, txnError] = await sendTransaction(to, data, gasEstimate);
+
+    if (txnError || !hash) {
+      onTxnError(txnError, executeResponse.transactions);
+      return false;
+    }
+
+    const execTransaction = { method, hash };
+    setExecuteTransactions(execTransaction);
+    onTxnSuccess(execTransaction);
+
+    return true;
+  };
+
+  const executeAll = useCallback(
+    async (
+      signData: SignResponse | undefined,
+      onTxnSuccess: (txn: ExecutedTransaction) => void,
+      onTxnError: (error: any, txns: ExecutedTransaction[]) => void,
+      onTxnStep?: (method: string, step: ExecuteTransactionStep) => void,
+    ): Promise<ExecutedTransaction[]> => {
+      if (!signData || !provider) {
+        setSignError({
+          type: SaleErrorTypes.DEFAULT,
+          data: { reason: 'No sign data' },
+        });
+
+        return [];
+      }
+
+      const transactions = await filterAllowedTransactions(
+        signData.transactions,
+        provider,
+      );
+
+      let successful = true;
+      for (const transaction of transactions) {
+        if (onTxnStep) {
+          onTxnStep(transaction.methodCall, ExecuteTransactionStep.BEFORE);
+        }
+
+        // eslint-disable-next-line no-await-in-loop
+        const success = await executeTransaction(
+          transaction,
+          onTxnSuccess,
+          onTxnError,
+        );
+
+        if (!success) {
+          successful = false;
+          break;
+        }
+
+        if (onTxnStep) {
+          onTxnStep(transaction.methodCall, ExecuteTransactionStep.AFTER);
+        }
+      }
+      (successful ? setExecuteDone : setExecuteFailed)();
+
+      return executeResponse.transactions;
+    },
+    [
+      provider,
+      executeTransaction,
+      setExecuteDone,
+      setExecuteFailed,
+      filterAllowedTransactions,
+      sendTransaction,
+    ],
+  );
+
+  const executeNextTransaction = useCallback(
+    async (
+      onTxnSuccess: (txn: ExecutedTransaction) => void,
+      onTxnError: (error: any, txns: ExecutedTransaction[]) => void,
+      onTxnStep?: (method: string, step: ExecuteTransactionStep) => void,
+    ): Promise<boolean> => {
+      if (!filteredTransactions || executeResponse.done || !provider) return false;
+
+      const transaction = filteredTransactions[currentTransactionIndex];
+
+      if (onTxnStep) {
+        onTxnStep(transaction.methodCall, ExecuteTransactionStep.BEFORE);
+      }
+
+      const success = await executeTransaction(
+        transaction,
+        onTxnSuccess,
+        onTxnError,
+      );
+
+      if (success) {
+        setTransactionIndex();
+
+        if (currentTransactionIndex === filteredTransactions.length - 1) {
+          setExecuteDone();
+        }
+
+        if (onTxnStep) {
+          onTxnStep(transaction.methodCall, ExecuteTransactionStep.AFTER);
+        }
+      }
+
+      return success;
+    },
+    [currentTransactionIndex, provider, filteredTransactions],
+  );
+
+  return {
+    sign,
+    signResponse,
+    signError,
+    filteredTransactions,
+    currentTransactionIndex,
+    executeAll,
+    executeResponse,
+    tokenIds,
+    executeNextTransaction,
+  };
+};
---
+++ packages/checkout/widgets-lib/src/lib/primary-sales.ts
@@ -0,0 +1,200 @@
+import { Web3Provider } from '@ethersproject/providers';
+import { Environment } from '@imtbl/config';
+import {
+  SaleItem,
+  FundingStep,
+  FundingItem,
+  SmartCheckoutResult,
+} from '@imtbl/checkout-sdk';
+
+export type SignedOrderProduct = {
+  productId: string;
+  qty: number;
+  image: string;
+  name: string;
+  description: string;
+  amount: number[];
+  tokenId: string[];
+  currency: string;
+  contractType: string;
+  collectionAddress: string;
+};
+
+export type SignedOrder = {
+  currency: {
+    name: string;
+    erc20Address: string;
+  };
+  totalAmount: number;
+  products: SignedOrderProduct[];
+};
+
+export type SignedTransaction = {
+  tokenAddress: string;
+  gasEstimate: number;
+  methodCall: string;
+  params: {
+    reference: string;
+    amount: number;
+    spender: string;
+  };
+  rawData: string;
+};
+
+export type SignResponse = {
+  order: SignedOrder;
+  transactions: SignedTransaction[];
+  transactionId: string;
+};
+
+export type SignOrderInput = {
+  provider: Web3Provider | undefined;
+  items: SaleItem[];
+  fromTokenAddress: string;
+  recipientAddress: string;
+  environment: string;
+  environmentId: string;
+  waitFulfillmentSettlements: boolean;
+  customOrderData?: Record<string, unknown>;
+};
+
+export type SignOrderError = {
+  type: SaleErrorTypes;
+  data?: Record<string, unknown>;
+};
+
+export type ExecutedTransaction = {
+  method: string;
+  hash: string | undefined;
+};
+
+export type ExecuteOrderResponse = {
+  done: boolean;
+  transactions: ExecutedTransaction[];
+};
+
+export enum SaleErrorTypes {
+  DEFAULT = 'DEFAULT_ERROR',
+  INVALID_PARAMETERS = 'INVALID_PARAMETERS',
+  TRANSACTION_FAILED = 'TRANSACTION_FAILED',
+  SERVICE_BREAKDOWN = 'SERVICE_BREAKDOWN',
+  PRODUCT_NOT_FOUND = 'PRODUCT_NOT_FOUND',
+  INSUFFICIENT_STOCK = 'INSUFFICIENT_STOCK',
+  TRANSAK_FAILED = 'TRANSAK_FAILED',
+  WALLET_FAILED = 'WALLET_FAILED',
+  WALLET_REJECTED = 'WALLET_REJECTED',
+  WALLET_REJECTED_NO_FUNDS = 'WALLET_REJECTED_NO_FUNDS',
+  WALLET_POPUP_BLOCKED = 'WALLET_POPUP_BLOCKED',
+  FUNDING_ROUTE_EXECUTE_ERROR = 'FUNDING_ROUTE_EXECUTE_ERROR',
+}
+
+export type OrderQuoteCurrency = {
+  base: boolean;
+  decimals: number;
+  address: string;
+  exchangeId: string;
+  name: string;
+};
+
+export type OrderQuotePricing = {
+  amount: number;
+  currency: string;
+  type: string;
+};
+
+export type OrderQuoteProduct = {
+  productId: string;
+  quantity: number;
+  pricing: Record<string, OrderQuotePricing>;
+};
+
+export type OrderQuote = {
+  config: {
+    contractId: string;
+  },
+  currencies: Array<OrderQuoteCurrency>;
+  products: Record<string, OrderQuoteProduct>;
+  totalAmount: Record<string, OrderQuotePricing>;
+};
+
+export enum SignPaymentTypes {
+  CRYPTO = 'crypto',
+  FIAT = 'fiat',
+}
+
+export enum FundingBalanceType {
+  SUFFICIENT = 'SUFFICIENT',
+}
+
+export type SufficientFundingStep = {
+  type: FundingBalanceType.SUFFICIENT;
+  fundingItem: FundingItem;
+};
+
+export type FundingBalance = FundingStep | SufficientFundingStep;
+
+export type FundingBalanceResult = {
+  currency: OrderQuoteCurrency;
+  smartCheckoutResult: SmartCheckoutResult;
+};
+
+export enum ExecuteTransactionStep {
+  BEFORE = 'before',
+  PENDING = 'pending',
+  AFTER = 'after',
+}
+
+/* eslint-disable @typescript-eslint/naming-convention */
+export type OrderQuoteApiResponse = {
+  config: {
+    contract_id: string;
+  };
+  currencies: Array<{
+    base: boolean;
+    decimals: number;
+    erc20_address: string;
+    exchange_id: string;
+    name: string;
+  }>;
+  products: Record<
+  string,
+  {
+    pricing: Record<
+    string,
+    {
+      amount: number;
+      currency: string;
+      type: string;
+    }
+    >;
+    product_id: string;
+    quantity: number;
+  }
+  >;
+  total_amount: Record<
+  string,
+  {
+    amount: number;
+    currency: string;
+    type: string;
+  }
+  >;
+};
+
+export const PRIMARY_SALES_API_BASE_URL = {
+  [Environment.SANDBOX]: 'https://api.sandbox.immutable.com/v1/primary-sales',
+  [Environment.PRODUCTION]: 'https://api.immutable.com/v1/primary-sales',
+};
+
+export type UseQuoteOrderParams = {
+  items: SaleItem[];
+  environmentId: string;
+  environment: Environment;
+  provider: Web3Provider | undefined;
+  preferredCurrency?: string;
+};
+
+export type ConfigError = {
+  type: SaleErrorTypes;
+  data?: Record<string, unknown>;
+};
