9b540bca6e2bee0d55a67e680e74ebc253839796
zaid.arain <zaid.arain@immutable.com>
2024-12-03 20:11:47 +1100

    refine sdk bundling and improve treeshaking

--- tsup.config.js
+++ tsup.config.js
@@ -2,7 +2,7 @@
 import { defineConfig } from 'tsup'
 import { nodeModulesPolyfillPlugin } from 'esbuild-plugins-node-modules-polyfill';
 import { replace } from 'esbuild-plugin-replace';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './sdk/package.json' assert { type: 'json' };
 
 export default defineConfig((options) => {
   if (options.watch) {
@@ -36,10 +36,6 @@ export default defineConfig((options) => {
         }),
         replace({ 
           '__SDK_VERSION__': pkg.version, 
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
-          'process.env.CHECKOUT_DEV_MODE': JSON.stringify(process.env.CHECKOUT_DEV_MODE || 'false'),
-          'process.env.CHECKOUT_LOCAL_MODE': JSON.stringify(process.env.CHECKOUT_LOCAL_MODE || 'false'),
-          'process.versions': JSON.stringify(process.versions || {}),
         })
       ]
     },
@@ -56,10 +52,6 @@ export default defineConfig((options) => {
       esbuildPlugins: [
         replace({ 
           '__SDK_VERSION__': pkg.version, 
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
-          'process.env.CHECKOUT_DEV_MODE': JSON.stringify(process.env.CHECKOUT_DEV_MODE || 'false'),
-          'process.env.CHECKOUT_LOCAL_MODE': JSON.stringify(process.env.CHECKOUT_LOCAL_MODE || 'false'),
-          'process.versions': JSON.stringify(process.versions || {}),
         })
       ]
     },
