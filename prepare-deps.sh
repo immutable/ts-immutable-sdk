8fff0eb8eb518102ae9af837818d8fc7b86427a3
zaid.arain <zaid.arain@immutable.com>
2024-12-12 12:20:15 +1100

    revert sdk prep changes

--- prepare-deps.sh
+++ prepare-deps.sh
@@ -53,8 +53,8 @@ prepare_deps() {
     fi
     
     # Get the packed dependency filename. For example, @imtbl/sdk -> imtbl-sdk-0.0.0.tgz
-    version=$(pnpm exec jq -r ".version" package.json)
-    packedDepFilename=$(echo "$dep" | sed "s/@imtbl\///" | sed "s/\//-/" | sed "s/^/imtbl-/" | sed "s/$/-$version.tgz/")
+    # version=$(pnpm exec jq -r ".version" package.json)
+    packedDepFilename=$(echo "$dep" | sed "s/@imtbl\///" | sed "s/\//-/" | sed "s/^/imtbl-/" | sed "s/$/-0.0.0.tgz/")
     # Check if the dependency is a dependency or a devDependency and replace the version with a local tarball version
     if echo "$deps" | grep -q "$dep"; then
       pnpm exec jq ".dependencies[\"$dep\"] = \"file:$(dirname $(pnpm root -w))/$packedDepFilename\"" package.json > package.tmp.json
