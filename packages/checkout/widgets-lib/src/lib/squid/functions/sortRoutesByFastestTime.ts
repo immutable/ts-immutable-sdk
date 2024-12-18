96f10cb49349c024c260280ebe12eb339598fce6
Mimi Immutable <mimi.tran@immutable.com>
2024-12-18 13:48:26 +1100

    Sort routes by estimatedDuration, then insufficient gas, then insufficient balance

--- packages/checkout/widgets-lib/src/lib/squid/functions/sortRoutesByFastestTime.ts
+++ packages/checkout/widgets-lib/src/lib/squid/functions/sortRoutesByFastestTime.ts
@@ -4,15 +4,25 @@ export const sortRoutesByFastestTime = (routes: RouteData[]): RouteData[] => {
   if (!routes) return [];
 
   return routes.slice().sort((a, b) => {
-    // Prioritize isInsufficientGas = false
-    if (a.isInsufficientGas !== b.isInsufficientGas) {
-      return a.isInsufficientGas ? 1 : -1;
+    // 1.: Prioritise routes where both isInsufficientGas and isInsufficientBalance are false
+    if (a.isInsufficientGas !== b.isInsufficientGas || a.isInsufficientBalance !== b.isInsufficientBalance) {
+      if (!a.isInsufficientGas && !a.isInsufficientBalance) return -1;
+      if (!b.isInsufficientGas && !b.isInsufficientBalance) return 1;
+      if (a.isInsufficientGas && !a.isInsufficientBalance) return -1; // isInsufficientGas = true has higher priority
+      if (b.isInsufficientGas && !b.isInsufficientBalance) return 1;
     }
 
-    // Sort by estimatedRouteDuration if isInsufficientGas is the same
+    // 2.: Sort by estimatedRouteDuration
     const timeA = a.route.route.estimate.estimatedRouteDuration;
     const timeB = b.route.route.estimate.estimatedRouteDuration;
 
-    return timeA - timeB;
+    if (timeA !== timeB) return timeA - timeB;
+
+    // 3.: Place isInsufficientGas before isInsufficientBalance
+    if (a.isInsufficientGas !== b.isInsufficientGas) {
+      return a.isInsufficientGas ? -1 : 1;
+    }
+
+    return 0;
   });
 };
