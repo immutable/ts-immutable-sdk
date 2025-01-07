4015bf766697c603cf763a58d50132717b21aa71
Mimi Immutable <mimi.tran@immutable.com>
2025-01-07 14:15:57 +1100

    Move add tokens config into common widgets lib

--- packages/checkout/widgets-lib/src/lib/squid/config.ts
+++ packages/checkout/widgets-lib/src/lib/squid/config.ts
@@ -1,3 +1,17 @@
 export const SQUID_SDK_BASE_URL = 'https://apiplus.squidrouter.com';
 
 export const SQUID_NATIVE_TOKEN = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+
+export const APPROVE_TXN_ANIMATION = '/access_coins.riv';
+
+export const EXECUTE_TXN_ANIMATION = '/swapping_coins.riv';
+
+export const BLOCK_TXN_ANIMATION = '/blocked.riv';
+
+export const ERROR_TXN_ANIMATION = '/error.riv';
+
+export const TOOLKIT_SQUID_URL = 'https://toolkit.immutable.com/squid-bridge/';
+
+export const FIXED_HANDOVER_DURATION = 2000;
+
+export const TOKEN_PRIORITY_ORDER = ['IMX', 'USDC', 'ETH'];
