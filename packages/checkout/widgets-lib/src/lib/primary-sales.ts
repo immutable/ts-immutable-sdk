0840ffc40f493f32d90998aa6b11e0e69358fb9e
Luã de Souza <eumesmo.ht@gmail.com>
2025-01-13 11:54:55 +1100

    WIP end to end NFT purchase
    Show success message after purchase is done
    
    Adjust code to cater for hooks refactor
    
    Random fixes
    
    Adjust import
    
    Add handover screens to all steps

--- packages/checkout/widgets-lib/src/lib/primary-sales.ts
+++ packages/checkout/widgets-lib/src/lib/primary-sales.ts
@@ -249,6 +249,7 @@ export enum SignCurrencyFilter {
 }
 
 export type SignApiRequest = {
+  spender_address?: string;
   recipient_address: string;
   spender_address?: string;
   currency_filter: SignCurrencyFilter;
