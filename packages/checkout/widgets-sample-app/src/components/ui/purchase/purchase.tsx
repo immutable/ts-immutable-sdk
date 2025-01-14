ffefc70790fbe83387f30ec50da66d5232039c5d
Mimi Immutable <mimi.tran@immutable.com>
2025-01-14 15:19:34 +1100

    Move mock items to sample app

--- packages/checkout/widgets-sample-app/src/components/ui/purchase/purchase.tsx
+++ packages/checkout/widgets-sample-app/src/components/ui/purchase/purchase.tsx
@@ -1,8 +1,51 @@
-import { Checkout, PurchaseEventType, WidgetLanguage, WidgetTheme, WidgetType } from '@imtbl/checkout-sdk';
+import { Checkout, PurchaseEventType, PurchaseItem, WidgetLanguage, WidgetTheme, WidgetType } from '@imtbl/checkout-sdk';
 import { useEffect, useMemo } from 'react';
 import { WidgetsFactory } from '@imtbl/checkout-widgets';
 
 const PURCHASE_WIDGET_ID = 'purchase';
+
+const defaultItems: PurchaseItem[] = [
+  {
+    productId: "kangaroo",
+    qty: 1,
+    name: "Kangaroo",
+    image:
+      "https://iguanas.mystagingwebsite.com/wp-content/uploads/2024/05/character-image-10-1.png",
+    description: "Kangaroo",
+  },
+  {
+    productId: "kookaburra",
+    qty: 3,
+    name: "Kookaburra",
+    image:
+      "https://iguanas.mystagingwebsite.com/wp-content/uploads/2024/05/character-image-4-1.png",
+    description: "Kookaburra",
+  },
+  {
+    productId: "quokka",
+    qty: 2,
+    name: "Quokka",
+    image:
+      "https://iguanas.mystagingwebsite.com/wp-content/uploads/2024/05/character-image-8-1.png",
+    description: "Quokka",
+  },
+  {
+    productId: "ibis",
+    qty: 1,
+    name: "Ibis",
+    image:
+      "https://iguanas.mystagingwebsite.com/wp-content/uploads/2024/05/character-image-1-1.png",
+    description: "Ibis",
+  },
+  {
+    productId: "emu",
+    qty: 5,
+    name: "Emu",
+    image:
+      "https://iguanas.mystagingwebsite.com/wp-content/uploads/2024/05/character-image-5-1.png",
+    description: "Emu",
+  },
+];
 
 export default function PurchaseUI() {
   const checkout = useMemo(() => new Checkout(), []);
@@ -10,7 +53,10 @@ export default function PurchaseUI() {
   const purchase = useMemo(() => factory.create(WidgetType.PURCHASE), [factory]);
 
   useEffect(() => {
-    purchase.mount(PURCHASE_WIDGET_ID, {});
+    purchase.mount(PURCHASE_WIDGET_ID, {
+      items: defaultItems,
+      },
+    );
 
     purchase.addListener(PurchaseEventType.CLOSE_WIDGET, (data: any) => {
       console.log("CLOSE_WIDGET", data);
