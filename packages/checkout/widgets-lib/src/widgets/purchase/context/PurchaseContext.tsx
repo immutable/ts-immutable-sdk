670e8ce017c2af18e5996010f51ddb751ed6245b
Mimi Immutable <mimi.tran@immutable.com>
2025-01-20 12:08:36 +1100

    Add selectedToken to context

--- packages/checkout/widgets-lib/src/widgets/purchase/context/PurchaseContext.tsx
+++ packages/checkout/widgets-lib/src/widgets/purchase/context/PurchaseContext.tsx
@@ -1,15 +1,17 @@
 import { Squid } from '@0xsquid/sdk';
-import { PurchaseItem } from '@imtbl/checkout-sdk';
+import { PurchaseItem, TokenInfo } from '@imtbl/checkout-sdk';
 import { createContext } from 'react';
 
 export interface PurchaseState {
   squid: Squid | null;
   items: PurchaseItem[];
+  selectedToken: TokenInfo | undefined;
 }
 
 export const initialPurchaseState: PurchaseState = {
   squid: null,
   items: [],
+  selectedToken: undefined,
 };
 
 export interface PurchaseContextState {
@@ -23,11 +25,13 @@ export interface PurchaseAction {
 
 type ActionPayload =
   | SetSquid
-  | SetItems;
+  | SetItems
+  | SetSelectedToken;
 
 export enum PurchaseActions {
   SET_SQUID = 'SET_SQUID',
   SET_ITEMS = 'SET_ITEMS',
+  SET_SELECTED_TOKEN = 'SET_SELECTED_TOKEN',
 }
 
 export interface SetSquid {
@@ -38,6 +42,11 @@ export interface SetSquid {
 export interface SetItems {
   type: PurchaseActions.SET_ITEMS;
   items: PurchaseItem[];
+}
+
+export interface SetSelectedToken {
+  type: PurchaseActions.SET_SELECTED_TOKEN;
+  selectedToken: TokenInfo;
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -64,6 +73,11 @@ export const purchaseReducer: Reducer<PurchaseState, PurchaseAction> = (
       return {
         ...state,
         items: action.payload.items,
+      };
+    case PurchaseActions.SET_SELECTED_TOKEN:
+      return {
+        ...state,
+        selectedToken: action.payload.selectedToken,
       };
     default:
       return state;
