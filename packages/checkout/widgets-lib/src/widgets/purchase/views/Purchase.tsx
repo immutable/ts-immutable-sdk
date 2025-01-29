9438a6c5cae24db70fe6d19691b2eed79ddb26a2
Alejo L <alejo.loaiza@gmail.com>
2025-01-29 11:09:50 +0800

    Add hasSufficientGas function for direct crypto payments

--- packages/checkout/widgets-lib/src/widgets/purchase/views/Purchase.tsx
+++ packages/checkout/widgets-lib/src/widgets/purchase/views/Purchase.tsx
@@ -100,7 +100,7 @@ export function Purchase({
   } = useProvidersContext();
 
   const {
-    fetchRoutes, getRoute, getFromAmountData, hasSufficientBalance,
+    fetchRoutes, getRoute, getFromAmountData, hasSufficientBalance, hasSufficientGas,
   } = useRoutes();
   const { providers } = useInjectedProviders({ checkout });
 
@@ -188,7 +188,7 @@ export function Purchase({
 
         if (token && balance) {
           const directCryptoRoute = {
-            isInsufficientGas: true,
+            isInsufficientGas: !hasSufficientGas(balances, ChainId.IMTBL_ZKEVM_MAINNET.toString(), fromProvider),
             amountData: {
               fromToken: token,
               fromAmount: tokenAmount,
