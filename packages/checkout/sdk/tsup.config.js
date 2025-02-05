51f92baa399cb5be1e6ef90b1f3ad63a822214b5
zaid.arain <zaid.arain@immutable.com>
2025-02-05 13:05:16 +1100

    fix production node env

--- packages/checkout/sdk/tsup.config.js
+++ packages/checkout/sdk/tsup.config.js
@@ -37,7 +37,6 @@ export default defineConfig((options) => {
         }),
         replace({ 
           '__SDK_VERSION__': pkg.version, 
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
           'process.env.CHECKOUT_DEV_MODE': JSON.stringify(process.env.CHECKOUT_DEV_MODE || 'false'),
           'process.env.CHECKOUT_LOCAL_MODE': JSON.stringify(process.env.CHECKOUT_LOCAL_MODE || 'false'),
           'process.versions': JSON.stringify(process.versions || {}),
@@ -58,7 +57,6 @@ export default defineConfig((options) => {
       esbuildPlugins: [
         replace({ 
           '__SDK_VERSION__': pkg.version, 
-          'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
           'process.env.CHECKOUT_DEV_MODE': JSON.stringify(process.env.CHECKOUT_DEV_MODE || 'false'),
           'process.env.CHECKOUT_LOCAL_MODE': JSON.stringify(process.env.CHECKOUT_LOCAL_MODE || 'false'),
           'process.versions': JSON.stringify(process.versions || {}),
