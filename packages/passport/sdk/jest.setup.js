ca55095fc69d1e5ccbf8eecd16ffe3493acf320f
Keith Broughton <101608096+keithbro-imx@users.noreply.github.com>
2025-02-27 13:11:31 +1100

    fix(passport): ID-3274: Fix zero nonce issue (#2564)

--- packages/passport/sdk/jest.setup.js
+++ packages/passport/sdk/jest.setup.js
@@ -1,3 +1,16 @@
 import { TextEncoder } from 'util';
 
 global.TextEncoder = TextEncoder;
+
+/**
+ * Required for ethers v6
+ * @see https://github.com/ethers-io/ethers.js/issues/4365
+ */
+Object.defineProperty(Uint8Array, Symbol.hasInstance, {
+    value(potentialInstance) {
+      return this === Uint8Array
+        ? Object.prototype.toString.call(potentialInstance) ===
+            '[object Uint8Array]'
+        : Uint8Array[Symbol.hasInstance].call(this, potentialInstance);
+    },
+  });
