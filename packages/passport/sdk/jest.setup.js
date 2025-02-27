7e318b7de36b40e6925e08766495a0f656adbdf7
Keith Broughton <keith.broughton@immutable.com>
2025-02-27 11:22:21 +1100

    Add comment

--- packages/passport/sdk/jest.setup.js
+++ packages/passport/sdk/jest.setup.js
@@ -2,6 +2,9 @@ import { TextEncoder } from 'util';
 
 global.TextEncoder = TextEncoder;
 
+/**
+ * @see https://github.com/ethers-io/ethers.js/issues/4365
+ */
 Object.defineProperty(Uint8Array, Symbol.hasInstance, {
     value(potentialInstance) {
       return this === Uint8Array
