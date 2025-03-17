0c680bbfcd9d6f5bae3fb7bdf2558f3cc8473d84
Keith Broughton <keith.broughton@immutable.com>
2025-03-17 14:55:21 +1100

    Fix TransferWidgetParams

--- packages/checkout/sdk/src/widgets/definitions/parameters/transfer.ts
+++ packages/checkout/sdk/src/widgets/definitions/parameters/transfer.ts
@@ -1,5 +1,15 @@
+import { WidgetLanguage } from '../configurations/widget';
+
 export type TransferWidgetParams = {
+  /** The language to use for the Purchase widget */
+  language?: WidgetLanguage;
+
+  /** The amount to transfer */
   amount?: string;
+
+  /** The token address to transfer */
   tokenAddress?: `0x${string}` | 'native';
+
+  /** The to address to transfer to */
   toAddress?: `0x${string}`;
 };
