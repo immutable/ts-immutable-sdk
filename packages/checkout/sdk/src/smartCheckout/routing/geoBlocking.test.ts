f96e1f43488cb680f6beddd28191dba2e934d0a6
Keith Broughton <keith.broughton@immutable.com>
2025-03-31 22:35:21 +1100

    More

--- packages/checkout/sdk/src/smartCheckout/routing/geoBlocking.test.ts
+++ packages/checkout/sdk/src/smartCheckout/routing/geoBlocking.test.ts
@@ -1,45 +1,35 @@
-import { Environment } from '@imtbl/config';
-import { CheckoutConfiguration } from '../../config';
 import { isSwapAvailable } from './geoBlocking';
-import { availabilityService } from '../../availability';
-import { HttpClient } from '../../api/http';
+import { AvailabilityService } from '../../availability';
 
 jest.mock('../../availability');
 
 describe('geoBlocking', () => {
-  let config: CheckoutConfiguration;
-
-  beforeEach(() => {
-    const mockedHttpClient = new HttpClient() as jest.Mocked<HttpClient>;
-    config = new CheckoutConfiguration({
-      baseConfig: { environment: Environment.PRODUCTION },
-    }, mockedHttpClient);
-  });
+  let availabilityService: AvailabilityService;
 
   describe('isSwapAvailable', () => {
     it('should return true if checkDexAvailability returns true', async () => {
-      (availabilityService as jest.Mock).mockReturnValue({
+      availabilityService = {
         checkDexAvailability: jest.fn().mockResolvedValue(true),
-      });
-      const response = await isSwapAvailable(config);
+      } as unknown as AvailabilityService;
+      const response = await isSwapAvailable(availabilityService);
 
       expect(response).toEqual(true);
     });
 
     it('should return false if checkDexAvailability returns false', async () => {
-      (availabilityService as jest.Mock).mockReturnValue({
+      availabilityService = {
         checkDexAvailability: jest.fn().mockResolvedValue(false),
-      });
-      const response = await isSwapAvailable(config);
+      } as unknown as AvailabilityService;
+      const response = await isSwapAvailable(availabilityService);
 
       expect(response).toEqual(false);
     });
 
     it('should return false by default', async () => {
-      (availabilityService as jest.Mock).mockReturnValue({
+      availabilityService = {
         checkDexAvailability: jest.fn().mockRejectedValue(new Error()),
-      });
-      const response = await isSwapAvailable(config);
+      } as unknown as AvailabilityService;
+      const response = await isSwapAvailable(availabilityService);
 
       expect(response).toEqual(false);
     });
