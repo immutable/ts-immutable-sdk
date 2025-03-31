65e4f1dc4a36b2f82ace7d0a518eaba98e0e2017
darrenmelvison1 <darren.melvison@immutable.com>
2025-03-31 14:45:21 +1100

    removed generated example apps

--- examples/passport/login-with-passport-provider-announce-with-nextjs/tests/global.d.ts
+++
@@ -1,7 +0,0 @@
-interface Window {
-  __coverage__: any;
-  passportInstance: any;
-  mockPassportLogin: () => Promise<void>;
-  mockConnectEvm: () => Promise<any>;
-  providerCallbacks: Record<string, any>;
-} 
