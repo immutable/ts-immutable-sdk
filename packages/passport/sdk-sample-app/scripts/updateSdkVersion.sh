7847ac1399900c960da04ddbe684a215af11a684
zaidarain1 <zaid.arain@immutable.com>
2025-04-29 17:14:23 +1000

    chore(passport): Fix sample app deployment (#2624)

--- packages/passport/sdk-sample-app/scripts/updateSdkVersion.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-find $1 -type f -name "*.js" -exec sed -i '' "s/__SDK_VERSION__/$(git describe --tags --abbrev=0)/g" {} +;
-else
-find $1 -type f -name "*.js" -exec sed -i "s/__SDK_VERSION__/$(git describe --tags --abbrev=0)/g" {} +;
-fi
