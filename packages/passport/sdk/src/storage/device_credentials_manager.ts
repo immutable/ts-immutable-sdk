e68daba7702343a296b13b0cab3bbf7f78272732
Natalie Bunduwongse <nf1993@gmail.com>
2025-06-19 15:31:33 +1200

    feat: remove device code auth

--- packages/passport/sdk/src/storage/device_credentials_manager.ts
+++ packages/passport/sdk/src/storage/device_credentials_manager.ts
@@ -1,21 +1,12 @@
 /* eslint-disable class-methods-use-this */
 import jwt_decode from 'jwt-decode';
-import { DeviceTokenResponse, TokenPayload, PKCEData } from '../types';
+import { TokenPayload, PKCEData } from '../types';
 
 const KEY_PKCE_STATE = 'pkce_state';
 const KEY_PKCE_VERIFIER = 'pkce_verifier';
 const validCredentialsMinTtlSec = 3600; // 1 hour
 
 export default class DeviceCredentialsManager {
-  public areValid(tokenResponse: DeviceTokenResponse): boolean {
-    if (tokenResponse) {
-      const accessTokenValid = this.isTokenValid(tokenResponse.access_token);
-      const idTokenValid = this.isTokenValid(tokenResponse.id_token);
-      return accessTokenValid && idTokenValid;
-    }
-    return false;
-  }
-
   private isTokenValid(jwt: string): boolean {
     try {
       const tokenPayload: TokenPayload = jwt_decode(jwt);
