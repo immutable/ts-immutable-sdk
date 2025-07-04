b2be3a8e9e7d1bce3279db0efea555ec924a8285
Nik Ho <studying.mole@gmail.com>
2025-07-04 19:54:48 +1200

    update token expiry check with 30 sec leeway

--- packages/passport/sdk/src/utils/token.ts
+++ packages/passport/sdk/src/utils/token.ts
@@ -14,10 +14,21 @@ export function isIdTokenExpired(idToken: string | undefined): boolean {
   return decodedToken.exp < now;
 }
 
-export function isTokenExpired(oidcUser: OidcUser): boolean {
-  const { id_token: idToken, expired } = oidcUser;
+export function isAccessTokenExpiredOrExpiring(oidcUser: OidcUser): boolean {
+  const { id_token: idToken, expired, expires_in } = oidcUser;
   if (expired) {
     return true;
   }
+
+  // if token will expire in 30 seconds or less, return true
+  if (expires_in && expires_in <= 30) {
+    return true;
+  }
+
+  // Handle missing idToken - assume they need to login again
+  if (!idToken) {
+    return true;
+  }
+
   return isIdTokenExpired(idToken);
 }
