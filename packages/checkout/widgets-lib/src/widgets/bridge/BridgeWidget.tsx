228f21862c2e3606852549ced3323ccdc15a933d
Keith Broughton <keith.broughton@immutable.com>
2025-08-15 13:30:33 +1000

    rm

--- packages/checkout/widgets-lib/src/widgets/bridge/BridgeWidget.tsx
+++ packages/checkout/widgets-lib/src/widgets/bridge/BridgeWidget.tsx
@@ -61,7 +61,6 @@ import {
   BridgeClaimWithdrawalFailure,
   BridgeWidgetViews,
 } from '../../context/view-context/BridgeViewContextTypes';
-import { ClaimWithdrawal } from './views/ClaimWithdrawal';
 import { ServiceUnavailableErrorView } from '../../views/error/ServiceUnavailableErrorView';
 
 export type BridgeWidgetInputs = BridgeWidgetParams & {
@@ -258,9 +257,6 @@ export default function BridgeWidget({
           )}
           {viewState.view.type === BridgeWidgetViews.TRANSACTIONS && (
             <Transactions onBackButtonClick={goBackToWalletNetworkSelector} />
-          )}
-          {viewState.view.type === BridgeWidgetViews.CLAIM_WITHDRAWAL && (
-            <ClaimWithdrawal transaction={viewState.view.transaction} />
           )}
           {viewState.view.type === SharedViews.ERROR_VIEW && (
             <ErrorView
