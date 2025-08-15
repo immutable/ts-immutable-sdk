0337336fb93cf722c52c6eb0db4e945bb8374929
Keith Broughton <keith.broughton@immutable.com>
2025-08-15 13:13:37 +1000

    GFI-995: Point users to AxelarScan

--- packages/checkout/widgets-lib/src/components/Transactions/SupportMessage.tsx
+++ packages/checkout/widgets-lib/src/components/Transactions/SupportMessage.tsx
@@ -1,26 +1,91 @@
 import { Body, Box, Link } from '@biom3/react';
 import { Checkout } from '@imtbl/checkout-sdk';
-import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { PASSPORT_URL } from '../../lib';
 import { supportMessageBoxStyle, bodyStyle } from './SupportMessageStyles';
+import { useAnalytics, UserJourney } from '../../context/analytics-provider/SegmentAnalyticsProvider';
 
 type SupportMessageProps = {
   checkout: Checkout,
   isPassport: boolean,
 };
 
+function NeedHelpContactSupport() {
+  const { t } = useTranslation();
+  const { track } = useAnalytics();
+
+  return (
+    <Box>
+      <Body
+        size="small"
+      >
+        {t('views.TRANSACTIONS.support.body1')}
+      </Body>
+      <Body
+        size="small"
+        sx={bodyStyle}
+      >
+        {t('views.TRANSACTIONS.support.body2')}
+        <Link
+          size="small"
+          rc={(
+            <a
+              target="_blank"
+              onClick={() => track({
+                userJourney: UserJourney.BRIDGE,
+                screen: 'Transactions',
+                control: 'Support',
+                controlType: 'Link',
+              })}
+              href={t('views.TRANSACTIONS.support.supportLink')}
+              rel="noreferrer"
+            />
+          )}
+        >
+          {t('views.TRANSACTIONS.support.body3')}
+        </Link>
+      </Body>
+    </Box>
+  );
+}
+
+function LookingForPastTransactions() {
+  const { track } = useAnalytics();
+
+  return (
+    <Box sx={{ mb: '16px' }}>
+      <Body>
+        In progress and historical bridge transactions can be found on
+        {' '}
+        <Link rc={(
+          <a
+            target="_blank"
+            onClick={() => track({
+              userJourney: UserJourney.BRIDGE,
+              screen: 'Transactions',
+              control: 'Axelarscan',
+              controlType: 'Link',
+            })}
+            href="https://axelarscan.io/"
+            rel="noreferrer"
+          />
+        )}
+        >
+          Axelarscan
+        </Link>
+        . Search there using a non-Passport wallet address.
+      </Body>
+    </Box>
+  );
+}
+
 export function SupportMessage({
   checkout,
   isPassport,
 }: SupportMessageProps) {
+  const { track } = useAnalytics();
   const { t } = useTranslation();
-  const [passportLink, setPassportLink] = useState('');
-
-  useEffect(() => {
-    if (!checkout) return;
-    setPassportLink(PASSPORT_URL[checkout.config.environment]);
-  }, [checkout]);
+  const passportLink = PASSPORT_URL[checkout.config.environment];
 
   return (
     <Box
@@ -32,25 +97,8 @@ export function SupportMessage({
           flexDirection: 'column',
         }}
       >
-        <Box>
-          <Body
-            size="small"
-          >
-            {t('views.TRANSACTIONS.support.body1')}
-          </Body>
-          <Body
-            size="small"
-            sx={bodyStyle}
-          >
-            {t('views.TRANSACTIONS.support.body2')}
-            <Link
-              size="small"
-              rc={<a target="_blank" href={t('views.TRANSACTIONS.support.supportLink')} rel="noreferrer" />}
-            >
-              {t('views.TRANSACTIONS.support.body3')}
-            </Link>
-          </Body>
-        </Box>
+        <LookingForPastTransactions />
+        <NeedHelpContactSupport />
         {isPassport && (
           <Body
             size="small"
@@ -60,7 +108,19 @@ export function SupportMessage({
             {' '}
             <Link
               size="small"
-              rc={<a target="_blank" href={passportLink} rel="noreferrer" />}
+              rc={(
+                <a
+                  target="_blank"
+                  onClick={() => track({
+                    userJourney: UserJourney.BRIDGE,
+                    screen: 'Transactions',
+                    control: 'Passport',
+                    controlType: 'Link',
+                  })}
+                  href={passportLink}
+                  rel="noreferrer"
+                />
+              )}
             >
               {t('views.TRANSACTIONS.support.passport.body2')}
             </Link>
