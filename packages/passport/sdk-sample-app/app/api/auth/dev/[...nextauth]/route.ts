66f1e18111e3a2583a6c54eeb711c6944dc598e5
Shine Li <shine.li@immutable.com>
2026-01-06 10:09:49 +1100

    add env routes in passport sample app

--- packages/passport/sdk-sample-app/app/api/auth/dev/[...nextauth]/route.ts
+++ packages/passport/sdk-sample-app/app/api/auth/dev/[...nextauth]/route.ts
@@ -2,3 +2,4 @@ import { devAuth } from "@/lib/auth-nextjs";
 
 export const { GET, POST } = devAuth.handlers;
 
+
--- packages/passport/sdk-sample-app/app/api/auth/prod/[...nextauth]/route.ts
+++ packages/passport/sdk-sample-app/app/api/auth/prod/[...nextauth]/route.ts
@@ -2,3 +2,4 @@ import { prodAuth } from "@/lib/auth-nextjs";
 
 export const { GET, POST } = prodAuth.handlers;
 
+
