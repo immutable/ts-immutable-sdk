5c69edaf3d90f6e21692fd09def75c45034bdf2b
John Carlo San Pedro <johncarlo.sanpedro@immutable.com>
2026-03-19 12:24:29 +1300

    feat(sale): Surface vendor errors from custom webooks (#2802)

--- packages/checkout/widgets-lib/src/widgets/sale/hooks/useSignOrder.ts
+++ packages/checkout/widgets-lib/src/widgets/sale/hooks/useSignOrder.ts
@@ -291,11 +291,17 @@ export const useSignOrder = (input: SignOrderInput) => {
 
         const { ok, status } = response;
         if (!ok) {
-          const { code } = (await response.json()) as SignApiError;
+          const { code, message } = (await response.json()) as SignApiError;
           let errorType: SaleErrorTypes;
+          let errorData: { code: string; message: string } | undefined;
+
           switch (status) {
             case 400:
-              errorType = SaleErrorTypes.SERVICE_BREAKDOWN;
+              errorType = SaleErrorTypes.SALE_AUTHORIZATION_REJECTED;
+              errorData = {
+                code,
+                message,
+              };
               break;
             case 404:
               if (code === 'insufficient_stock') {
@@ -312,7 +318,7 @@ export const useSignOrder = (input: SignOrderInput) => {
               throw new Error('Unknown error');
           }
 
-          setSignError({ type: errorType });
+          setSignError({ type: errorType, data: errorData });
           return undefined;
         }
 
